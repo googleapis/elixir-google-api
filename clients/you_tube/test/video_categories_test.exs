9aa3fdead4f2daee3b2abba9b12acb82fbd4ba62
Jeff Ching <ching.jeff@gmail.com>
2018-01-26 14:04:01 -0800

    Build Youtube Data API client (#42)
    * Update youtube client
    
    * Add simple external test for using a read-only api
    
    * Rebuild YouTube client
    
    * Put test_helper and test dependencies in the templates
    
    * Fix test helper module name
    
    * Add goth config for dev/test to the templates
    
    * Skip goth in mix.exs template

---
+++ clients/you_tube/test/video_categories_test.exs
@@ -0,0 +1,12 @@
+defmodule GoogleApi.YouTube.VideoCategoriesTest do
+  use GoogleApi.YouTube.V3.TestHelper
+
+  test "video categories" do
+    conn = GoogleApi.YouTube.V3.Connection.new(&for_scope/1)
+    part = "snippet"
+    region_code = "US"
+    {:ok, resp} = GoogleApi.YouTube.V3.Api.VideoCategories.youtube_video_categories_list(conn, part, regionCode: region_code)
+
+    assert %GoogleApi.YouTube.V3.Model.VideoCategoryListResponse{} = resp
+  end
+end
--- template/config.exs.mustache
+++ template/config.exs.mustache
@@ -28,3 +28,4 @@ use Mix.Config
 # here (which is why it is important to import them last).
 #
 #     import_config "#{Mix.env}.exs"
+config :goth, json: {:system, "GCP_CREDENTIALS"}
