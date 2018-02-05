7f530f8a848c032e450e528a22cdbb7c48b9ed98
Jeff Ching <chingor@google.com>
2018-02-05 14:20:17 -0800

    Parameterize the bucket id for testing

--- clients/storage/test/storage_test.exs
+++ clients/storage/test/storage_test.exs
@@ -2,19 +2,19 @@ defmodule GoogleApi.Storage.StorageTest do
   use GoogleApi.Storage.V1.TestHelper
   alias GoogleApi.Storage.V1.Model.Object
 
-  @bucket_id "chingor-php-gcs"
-
   test "storage" do
+    bucket_id = System.get_env("BUCKET")
+    assert bucket_id, "Please set the BUCKET environment variable"
+
     conn = GoogleApi.Storage.V1.Connection.new(&for_scope/1)
 
     # insert a file
-    obj = %Object{name: "README.md", bucket: @bucket_id}
+    obj = %Object{name: "README.md", bucket: bucket_id}
     path = "README.md"
-    {:ok, object} = GoogleApi.Storage.V1.Api.Objects.storage_objects_insert_simple(conn, @bucket_id, "multipart", obj, path)
-    |> IO.inspect
+    {:ok, object} = GoogleApi.Storage.V1.Api.Objects.storage_objects_insert_simple(conn, bucket_id, "multipart", obj, path)
     assert %Object{} = object
 
     # delete the file
-    {:ok, _} = GoogleApi.Storage.V1.Api.Objects.storage_objects_delete(conn, @bucket_id, "README.md")
+    {:ok, _} = GoogleApi.Storage.V1.Api.Objects.storage_objects_delete(conn, bucket_id, "README.md")
   end
 end
