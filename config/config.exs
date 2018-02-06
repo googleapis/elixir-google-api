0573688a977422805ae05fb7e76619a6d28287f4
Jeff Ching <chingor@google.com>
2018-02-05 17:22:49 -0800

    Add failing test for handling nil dates

--- config/config.exs
+++ config/config.exs
@@ -32,4 +32,4 @@ use Mix.Config
 config :google_apis,
   spec_converter: GoogleApis.Converter.ApiSpecConverter,
   client_generator: GoogleApis.Generator.SwaggerCli,
-  swagger_cli_image: "swaggerapi/swagger-codegen-cli"
+  swagger_cli_image: "swaggerapi/swagger-codegen-cli:v2.3.1"
