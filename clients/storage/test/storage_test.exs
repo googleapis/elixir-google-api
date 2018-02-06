0d096b9e1628b6130afd36912e3afa73fd6d467b
Jeff Ching <ching.jeff@gmail.com>
2018-02-06 09:20:44 -0800

    Accept 2xx responses as success (#50)
    * Regen storage client and add simple test for deleting a file
    
    * Interpret all 2xx response statuses as success
    
    * Parameterize the bucket id for testing
    
    * Add decode tests
    
    * Add failing test for when the response body is nil
    
    * Refactor RequestBuilder.decode to be a single entrypoint to simplify cases
    
    * Lock to older version of swagger-cli
    
    * Use a function definition header rather than adding a private do_decode

---
+++ clients/storage/test/storage_test.exs
@@ -0,0 +1,20 @@
+defmodule GoogleApi.Storage.StorageTest do
+  use GoogleApi.Storage.V1.TestHelper
+  alias GoogleApi.Storage.V1.Model.Object
+
+  test "storage" do
+    bucket_id = System.get_env("BUCKET")
+    assert bucket_id, "Please set the BUCKET environment variable"
+
+    conn = GoogleApi.Storage.V1.Connection.new(&for_scope/1)
+
+    # insert a file
+    obj = %Object{name: "README.md", bucket: bucket_id}
+    path = "README.md"
+    {:ok, object} = GoogleApi.Storage.V1.Api.Objects.storage_objects_insert_simple(conn, bucket_id, "multipart", obj, path)
+    assert %Object{} = object
+
+    # delete the file
+    {:ok, _} = GoogleApi.Storage.V1.Api.Objects.storage_objects_delete(conn, bucket_id, "README.md")
+  end
+end
--- clients/storage/test/test_helper.exs
+++ clients/storage/test/test_helper.exs
@@ -1,1 +1,18 @@
 ExUnit.start()
+
+defmodule GoogleApi.Storage.V1.TestHelper do
+
+  defmacro __using__(opts) do
+    quote do
+      use ExUnit.Case, unquote(opts)
+      import GoogleApi.Storage.V1.TestHelper
+    end
+  end
+
+  def for_scope(scopes) when is_list(scopes), do: for_scope(Enum.join(scopes, " "))
+  def for_scope(scope) do
+    {:ok, token} = Goth.Token.for_scope(scope)
+    token.token
+  end
+
+end
