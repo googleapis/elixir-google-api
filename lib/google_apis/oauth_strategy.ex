51c841225f84b9c4a23b2031fb3995eadaabc6cf
Jeff Ching <chingor@google.com>
2018-04-16 15:36:23 -0700

    Fix license header

--- lib/google_apis/oauth_strategy.ex
+++ lib/google_apis/oauth_strategy.ex
@@ -1,3 +1,17 @@
+# Copyright 2018 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 defmodule GoogleApis.OauthStrategy do
   use OAuth2.Strategy
 
@@ -19,8 +33,8 @@ defmodule GoogleApis.OauthStrategy do
   end
 
   # you can pass options to the underlying http library via `opts` parameter
-  def get_token!(client, params \\ [], headers \\ [], opts \\ []) do
-    OAuth2.Client.get_token!(client, params, headers, opts)
+  def get_token(client, params \\ [], headers \\ [], opts \\ []) do
+    OAuth2.Client.get_token(client, params, headers, opts)
   end
 
   # Strategy Callbacks
