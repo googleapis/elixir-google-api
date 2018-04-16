1c28da570dac66871434f142504b3b008913686a
Shawn Campbell <jaegerpicker@gmail.com>
2018-04-16 13:06:15 -0400

    Add firestore (#60)
    * Created initial firestore project
    
    * wip
    
    * Built firestore api
    
    * updated README and remove cruft
    
    * add_firestore: rebuild Firestore client to get document deletion; update mix file and README to conform to other clients
    
    * add_firestore: fix typos; test locally
    
    * add_firestore: run formatter on Firestore client
    
    * add_firestore: fix copyright for 2018 in Firestore client

---
+++ clients/firestore/README.md
@@ -0,0 +1,21 @@
+# GoogleCloudFirestore
+
+**Cloud Firestore is a NoSQL document database built for automatic scaling, high performance, and ease of application development.**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed
+by adding `google_cloud_firestore` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:google_cloud_firestore, "~> 0.0.1"}
+  ]
+end
+```
+
+Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
+and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
+be found at [https://hexdocs.pm/google_cloud_firestore](https://hexdocs.pm/google_cloud_firestore).
+
---
+++ clients/firestore/mix.exs
@@ -0,0 +1,56 @@
+defmodule GoogleApiFirestore.MixProject do
+  use Mix.Project
+
+  @version "0.0.1"
+
+  def project do
+    [
+      app: :google_api_firestore,
+      version: @version,
+      elixir: "~> 1.6",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
+      deps: deps(),
+      source_url:
+        "https://github.com/GoogleCloudPlatform/elixir-google-api/tree/master/clients/firestore"
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:tesla, ">= 0.8.0 and <= 0.10.0"},
+      {:poison, ">= 1.0.0"},
+      {:ex_doc, ">= 0.16.0", only: :dev},
+      {:goth, "~> 0.8.0", only: :test}
+    ]
+  end
+
+  defp description() do
+    """
+    API for managing the Firestore cloud database.
+    """
+  end
+
+  defp package() do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE"],
+      maintainers: ["Jeff Ching", "Shawn Campbell", "Carlo DiCelico"],
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" =>
+          "https://github.com/GoogleCloudPlatform/elixir-google-api/tree/master/clients/firestore",
+        "Homepage" => "https://cloud.google.com/firestore"
+      }
+    ]
+  end
+end
