8300518460defcfe951176dba481ff31f8bd541b
Jeff Ching <ching.jeff@gmail.com>
2018-05-11 12:57:00 -0700

    Add a mix task to fetch an OAuth access token (#68)
    * Add a mix task that lets you fetch an OAuth 2.0 access token
    
    * Fix license header
    
    * Fix get_token!
    
    * Output the refresh token as well as the access token

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule GoogleApis.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [applications: [:oauth2], extra_applications: [:logger]]
   end
 
   # Dependencies can be Hex packages:
@@ -43,7 +43,8 @@ defmodule GoogleApis.Mixfile do
   defp deps do
     [
       {:tesla, "~> 0.5"},
-      {:poison, ">= 1.0.0"}
+      {:poison, ">= 1.0.0"},
+      {:oauth2, "~> 0.9"}
     ]
   end
 end
