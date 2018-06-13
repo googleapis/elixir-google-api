ffc39c1c4791b477ebb028d654081511c35e9c24
Jeff Ching <chingor@google.com>
2018-06-13 14:56:16 -0700

    Run test_client with the gax template

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -13,7 +13,7 @@ mix dialyzer --halt-exit-status
 popd
 
 # create the test client
-mix do google_apis.convert TestClient, google_apis.build TestClient
+TEMPLATE=gax mix do google_apis.convert TestClient, google_apis.build TestClient
 pushd clients/test_client
 mix deps.get
 mix test
