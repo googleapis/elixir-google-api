8791bb9f33d5580ae91e8fa14f794cdb97099d32
Jeff Ching <chingor@google.com>
2018-06-29 15:59:37 -0700

    StorageTransfer 0.1.0

--- clients/storage_transfer/config/config.exs
+++ clients/storage_transfer/config/config.exs
@@ -10,11 +10,11 @@ use Mix.Config
 
 # You can configure for your application as:
 #
-#     config :google_storage_transfer_api, key: :value
+#     config :storage_transfer_api, key: :value
 #
 # And access this configuration in your application as:
 #
-#     Application.get_env(:google_storage_transfer_api, :key)
+#     Application.get_env(:storage_transfer_api, :key)
 #
 # Or configure a 3rd-party app:
 #
