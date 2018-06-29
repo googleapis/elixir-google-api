50973967485a4926a4e5ff590748bc21201634d3
Jeff Ching <chingor@google.com>
2018-06-29 14:17:15 -0700

    CloudResourceManager 0.1.0

--- clients/cloud_resource_manager/config/config.exs
+++ clients/cloud_resource_manager/config/config.exs
@@ -10,11 +10,11 @@ use Mix.Config
 
 # You can configure for your application as:
 #
-#     config :google_cloud_resource_manager_api, key: :value
+#     config :cloud_resource_manager_api, key: :value
 #
 # And access this configuration in your application as:
 #
-#     Application.get_env(:google_cloud_resource_manager_api, :key)
+#     Application.get_env(:cloud_resource_manager_api, :key)
 #
 # Or configure a 3rd-party app:
 #
