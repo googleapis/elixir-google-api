81da62f3619c9276e13cc86a7f11b0051520f567
Jeff Ching <chingor@google.com>
2018-06-29 15:37:55 -0700

    People 0.1.0

--- clients/people/config/config.exs
+++ clients/people/config/config.exs
@@ -10,11 +10,11 @@ use Mix.Config
 
 # You can configure for your application as:
 #
-#     config :google_people_api, key: :value
+#     config :people_api, key: :value
 #
 # And access this configuration in your application as:
 #
-#     Application.get_env(:google_people_api, :key)
+#     Application.get_env(:people_api, :key)
 #
 # Or configure a 3rd-party app:
 #
