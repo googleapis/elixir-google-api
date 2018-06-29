4154184b10adecfa8a10989a4b8a6826107467da
Jeff Ching <chingor@google.com>
2018-06-29 15:42:41 -0700

    RuntimeConfig 0.1.0

--- clients/runtime_config/config/config.exs
+++ clients/runtime_config/config/config.exs
@@ -10,11 +10,11 @@ use Mix.Config
 
 # You can configure for your application as:
 #
-#     config :google_cloud_runtime_configuration_api, key: :value
+#     config :cloud_runtime_configuration_api, key: :value
 #
 # And access this configuration in your application as:
 #
-#     Application.get_env(:google_cloud_runtime_configuration_api, :key)
+#     Application.get_env(:cloud_runtime_configuration_api, :key)
 #
 # Or configure a 3rd-party app:
 #
