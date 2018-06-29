0888cf76232430bf90b6d44e5adb8495c8e3b6c9
Jeff Ching <chingor@google.com>
2018-06-29 13:47:23 -0700

    BigQueryDataTransfer 0.1.0

--- clients/big_query_data_transfer/config/config.exs
+++ clients/big_query_data_transfer/config/config.exs
@@ -10,11 +10,11 @@ use Mix.Config
 
 # You can configure for your application as:
 #
-#     config :big_query_data_transfer_service_api, key: :value
+#     config :big_query_data_transfer_api, key: :value
 #
 # And access this configuration in your application as:
 #
-#     Application.get_env(:big_query_data_transfer_service_api, :key)
+#     Application.get_env(:big_query_data_transfer_api, :key)
 #
 # Or configure a 3rd-party app:
 #
