6662fd84fc4b258661b2bc6ee03ac6aca2b4e6ca
Jeff Ching <chingor@google.com>
2018-06-29 14:10:19 -0700

    CloudBuild 0.1.0

--- clients/cloud_build/lib/google_api/cloud_build/v1/model/artifact_result.ex
+++ clients/cloud_build/lib/google_api/cloud_build/v1/model/artifact_result.ex
@@ -16,35 +16,34 @@
 # https://github.com/swagger-api/swagger-codegen.git
 # Do not edit the class manually.
 
-defmodule GoogleApi.CloudBuild.V1.Model.Hash do
+defmodule GoogleApi.CloudBuild.V1.Model.ArtifactResult do
   @moduledoc """
-  Container message for hash values.
+  An artifact that was uploaded during a build. This is a single record in the artifact manifest JSON file.
 
   ## Attributes
 
-  - type (String.t): The type of hash that was performed. Defaults to: `null`.
-    - Enum - one of [NONE, SHA256, MD5]
-  - value (binary()): The hash value. Defaults to: `null`.
+  - fileHash ([FileHashes]): The file hash of the artifact. Defaults to: `null`.
+  - location (String.t): The path of an artifact in a Google Cloud Storage bucket, with the generation number. For example, &#x60;gs://mybucket/path/to/output.jar#generation&#x60;. Defaults to: `null`.
   """
 
   use GoogleApi.Gax.ModelBase
 
   @type t :: %__MODULE__{
-          :type => any(),
-          :value => any()
+          :fileHash => list(GoogleApi.CloudBuild.V1.Model.FileHashes.t()),
+          :location => any()
         }
 
-  field(:type)
-  field(:value)
+  field(:fileHash, as: GoogleApi.CloudBuild.V1.Model.FileHashes, type: :list)
+  field(:location)
 end
 
-defimpl Poison.Decoder, for: GoogleApi.CloudBuild.V1.Model.Hash do
+defimpl Poison.Decoder, for: GoogleApi.CloudBuild.V1.Model.ArtifactResult do
   def decode(value, options) do
-    GoogleApi.CloudBuild.V1.Model.Hash.decode(value, options)
+    GoogleApi.CloudBuild.V1.Model.ArtifactResult.decode(value, options)
   end
 end
 
-defimpl Poison.Encoder, for: GoogleApi.CloudBuild.V1.Model.Hash do
+defimpl Poison.Encoder, for: GoogleApi.CloudBuild.V1.Model.ArtifactResult do
   def encode(value, options) do
     GoogleApi.Gax.ModelBase.encode(value, options)
   end
