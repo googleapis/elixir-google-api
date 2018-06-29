fdfa4f8b8c3737ea7a49cbdb274cf540f8803254
Jeff Ching <chingor@google.com>
2018-06-29 14:47:17 -0700

    Dataflow 0.1.0

--- clients/dataflow/config/config.exs
+++ clients/dataflow/config/config.exs
@@ -10,11 +10,11 @@ use Mix.Config
 
 # You can configure for your application as:
 #
-#     config :google_dataflow_api, key: :value
+#     config :dataflow_api, key: :value
 #
 # And access this configuration in your application as:
 #
-#     Application.get_env(:google_dataflow_api, :key)
+#     Application.get_env(:dataflow_api, :key)
 #
 # Or configure a 3rd-party app:
 #
