748d6e545a33bdd8bcd8536419a5846bc3259624
Jeff Ching <chingor@google.com>
2018-11-07 16:58:57 -0800

    Fix tempdir path

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -34,7 +34,7 @@ popd
 # create the test client
 if [ "${TEST_GENERATOR}" == "true" ]; then
     mkdir .cache
-    TEMPDIR=.cache TEMPLATE=gax mix do google_apis.convert TestClient, google_apis.build TestClient
+    TEMPDIR=$(pwd)/.cache TEMPLATE=gax mix do google_apis.convert TestClient, google_apis.build TestClient
     pushd clients/test_client
     mix deps.get
     mix test
