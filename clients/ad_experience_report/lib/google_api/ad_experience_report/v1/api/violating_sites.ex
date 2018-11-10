cab169e6929c61eb9cee53c491004963f764c299
yoshi-automation <yoshi-automation@google.com>
2018-11-10 01:01:41 -0800

    Regenerate AdExperienceReport client

--- clients/ad_experience_report/lib/google_api/ad_experience_report/v1/api/violating_sites.ex
+++ clients/ad_experience_report/lib/google_api/ad_experience_report/v1/api/violating_sites.ex
@@ -31,13 +31,13 @@ defmodule GoogleApi.AdExperienceReport.V1.Api.ViolatingSites do
 
   - connection (GoogleApi.AdExperienceReport.V1.Connection): Connection to server
   - optional_params (KeywordList): [optional] Optional parameters
-    - :key (String.t): API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
     - :access_token (String.t): OAuth access token.
+    - :key (String.t): API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
     - :upload_protocol (String.t): Upload protocol for media (e.g. \&quot;raw\&quot;, \&quot;multipart\&quot;).
     - :quotaUser (String.t): Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
     - :prettyPrint (boolean()): Returns response with indentations and line breaks.
-    - :uploadType (String.t): Legacy upload protocol for media (e.g. \&quot;media\&quot;, \&quot;multipart\&quot;).
     - :fields (String.t): Selector specifying which fields to include in a partial response.
+    - :uploadType (String.t): Legacy upload protocol for media (e.g. \&quot;media\&quot;, \&quot;multipart\&quot;).
     - :callback (String.t): JSONP
     - :oauth_token (String.t): OAuth 2.0 token for the current user.
     - :$.xgafv (String.t): V1 error format.
@@ -53,13 +53,13 @@ defmodule GoogleApi.AdExperienceReport.V1.Api.ViolatingSites do
           | {:error, Tesla.Env.t()}
   def adexperiencereport_violating_sites_list(connection, optional_params \\ [], opts \\ []) do
     optional_params_config = %{
-      :key => :query,
       :access_token => :query,
+      :key => :query,
       :upload_protocol => :query,
       :quotaUser => :query,
       :prettyPrint => :query,
-      :uploadType => :query,
       :fields => :query,
+      :uploadType => :query,
       :callback => :query,
       :oauth_token => :query,
       :"$.xgafv" => :query,
