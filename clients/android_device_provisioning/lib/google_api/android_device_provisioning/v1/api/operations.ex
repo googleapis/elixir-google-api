a17862d2e8f9b1abfa75cdab1448938e1accc955
yoshi-automation <yoshi-automation@google.com>
2018-11-10 01:41:43 -0800

    Regenerate SearchConsole client

--- clients/android_device_provisioning/lib/google_api/android_device_provisioning/v1/api/operations.ex
+++ clients/android_device_provisioning/lib/google_api/android_device_provisioning/v1/api/operations.ex
@@ -16,72 +16,74 @@
 # https://github.com/swagger-api/swagger-codegen.git
 # Do not edit the class manually.
 
-defmodule GoogleApi.SearchConsole.V1.Api.UrlTestingTools do
+defmodule GoogleApi.AndroidDeviceProvisioning.V1.Api.Operations do
   @moduledoc """
-  API calls for all endpoints tagged `UrlTestingTools`.
+  API calls for all endpoints tagged `Operations`.
   """
 
-  alias GoogleApi.SearchConsole.V1.Connection
+  alias GoogleApi.AndroidDeviceProvisioning.V1.Connection
   alias GoogleApi.Gax.{Request, Response}
 
   @doc """
-  Runs Mobile-Friendly Test for a given URL.
+  Gets the latest state of a long-running operation.  Clients can use this method to poll the operation result at intervals as recommended by the API service.
 
   ## Parameters
 
-  - connection (GoogleApi.SearchConsole.V1.Connection): Connection to server
+  - connection (GoogleApi.AndroidDeviceProvisioning.V1.Connection): Connection to server
+  - name (String.t): The name of the operation resource.
   - optional_params (KeywordList): [optional] Optional parameters
-    - :$.xgafv (String.t): V1 error format.
-    - :oauth_token (String.t): OAuth 2.0 token for the current user.
     - :callback (String.t): JSONP
+    - :oauth_token (String.t): OAuth 2.0 token for the current user.
+    - :$.xgafv (String.t): V1 error format.
     - :alt (String.t): Data format for response.
     - :key (String.t): API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
     - :access_token (String.t): OAuth access token.
     - :upload_protocol (String.t): Upload protocol for media (e.g. \&quot;raw\&quot;, \&quot;multipart\&quot;).
-    - :prettyPrint (boolean()): Returns response with indentations and line breaks.
     - :quotaUser (String.t): Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
+    - :prettyPrint (boolean()): Returns response with indentations and line breaks.
     - :uploadType (String.t): Legacy upload protocol for media (e.g. \&quot;media\&quot;, \&quot;multipart\&quot;).
     - :fields (String.t): Selector specifying which fields to include in a partial response.
-    - :body (RunMobileFriendlyTestRequest): 
 
   ## Returns
 
-  {:ok, %GoogleApi.SearchConsole.V1.Model.RunMobileFriendlyTestResponse{}} on success
+  {:ok, %GoogleApi.AndroidDeviceProvisioning.V1.Model.Operation{}} on success
   {:error, info} on failure
   """
-  @spec searchconsole_url_testing_tools_mobile_friendly_test_run(Tesla.Env.client(), keyword()) ::
-          {:ok, GoogleApi.SearchConsole.V1.Model.RunMobileFriendlyTestResponse.t()}
+  @spec androiddeviceprovisioning_operations_get(Tesla.Env.client(), String.t(), keyword()) ::
+          {:ok, GoogleApi.AndroidDeviceProvisioning.V1.Model.Operation.t()}
           | {:error, Tesla.Env.t()}
-  def searchconsole_url_testing_tools_mobile_friendly_test_run(
+  def androiddeviceprovisioning_operations_get(
         connection,
+        name,
         optional_params \\ [],
         opts \\ []
       ) do
     optional_params_config = %{
-      :"$.xgafv" => :query,
-      :oauth_token => :query,
       :callback => :query,
+      :oauth_token => :query,
+      :"$.xgafv" => :query,
       :alt => :query,
       :key => :query,
       :access_token => :query,
       :upload_protocol => :query,
-      :prettyPrint => :query,
       :quotaUser => :query,
+      :prettyPrint => :query,
       :uploadType => :query,
-      :fields => :query,
-      :body => :body
+      :fields => :query
     }
 
     request =
       Request.new()
-      |> Request.method(:post)
-      |> Request.url("/v1/urlTestingTools/mobileFriendlyTest:run")
+      |> Request.method(:get)
+      |> Request.url("/v1/{+name}", %{
+        "name" => URI.encode_www_form(name)
+      })
       |> Request.add_optional_params(optional_params_config, optional_params)
 
     connection
     |> Connection.execute(request)
     |> Response.decode(
-      opts ++ [struct: %GoogleApi.SearchConsole.V1.Model.RunMobileFriendlyTestResponse{}]
+      opts ++ [struct: %GoogleApi.AndroidDeviceProvisioning.V1.Model.Operation{}]
     )
   end
 end
