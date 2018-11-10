e3fe4aca3eff4a7e7327f838e1d800026f5172fb
yoshi-automation <yoshi-automation@google.com>
2018-11-10 01:00:25 -0800

    Regenerate AcceleratedMobilePageUrl client

--- clients/accelerated_mobile_page_url/lib/google_api/accelerated_mobile_page_url/v1/api/amp_urls.ex
+++ clients/accelerated_mobile_page_url/lib/google_api/accelerated_mobile_page_url/v1/api/amp_urls.ex
@@ -34,14 +34,14 @@ defmodule GoogleApi.AcceleratedMobilePageUrl.V1.Api.AmpUrls do
     - :upload_protocol (String.t): Upload protocol for media (e.g. \&quot;raw\&quot;, \&quot;multipart\&quot;).
     - :quotaUser (String.t): Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
     - :prettyPrint (boolean()): Returns response with indentations and line breaks.
-    - :fields (String.t): Selector specifying which fields to include in a partial response.
     - :uploadType (String.t): Legacy upload protocol for media (e.g. \&quot;media\&quot;, \&quot;multipart\&quot;).
+    - :fields (String.t): Selector specifying which fields to include in a partial response.
     - :callback (String.t): JSONP
     - :oauth_token (String.t): OAuth 2.0 token for the current user.
     - :$.xgafv (String.t): V1 error format.
     - :alt (String.t): Data format for response.
-    - :access_token (String.t): OAuth access token.
     - :key (String.t): API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+    - :access_token (String.t): OAuth access token.
     - :body (BatchGetAmpUrlsRequest): 
 
   ## Returns
@@ -57,14 +57,14 @@ defmodule GoogleApi.AcceleratedMobilePageUrl.V1.Api.AmpUrls do
       :upload_protocol => :query,
       :quotaUser => :query,
       :prettyPrint => :query,
-      :fields => :query,
       :uploadType => :query,
+      :fields => :query,
       :callback => :query,
       :oauth_token => :query,
       :"$.xgafv" => :query,
       :alt => :query,
-      :access_token => :query,
       :key => :query,
+      :access_token => :query,
       :body => :body
     }
 
