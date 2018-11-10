b1790dac6df06a6ea673be24bd8febd9ecc5484d
yoshi-automation <yoshi-automation@google.com>
2018-11-10 01:20:25 -0800

    Regenerate DigitalAssetLinks client

--- clients/digital_asset_links/lib/google_api/digital_asset_links/v1/api/assetlinks.ex
+++ clients/digital_asset_links/lib/google_api/digital_asset_links/v1/api/assetlinks.ex
@@ -31,6 +31,10 @@ defmodule GoogleApi.DigitalAssetLinks.V1.Api.Assetlinks do
 
   - connection (GoogleApi.DigitalAssetLinks.V1.Connection): Connection to server
   - optional_params (KeywordList): [optional] Optional parameters
+    - :callback (String.t): JSONP
+    - :oauth_token (String.t): OAuth 2.0 token for the current user.
+    - :$.xgafv (String.t): V1 error format.
+    - :alt (String.t): Data format for response.
     - :access_token (String.t): OAuth access token.
     - :key (String.t): API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
     - :upload_protocol (String.t): Upload protocol for media (e.g. \&quot;raw\&quot;, \&quot;multipart\&quot;).
@@ -38,17 +42,13 @@ defmodule GoogleApi.DigitalAssetLinks.V1.Api.Assetlinks do
     - :quotaUser (String.t): Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
     - :fields (String.t): Selector specifying which fields to include in a partial response.
     - :uploadType (String.t): Legacy upload protocol for media (e.g. \&quot;media\&quot;, \&quot;multipart\&quot;).
-    - :callback (String.t): JSONP
-    - :oauth_token (String.t): OAuth 2.0 token for the current user.
-    - :$.xgafv (String.t): V1 error format.
-    - :alt (String.t): Data format for response.
+    - :target.androidApp.packageName (String.t): Android App assets are naturally identified by their Java package name. For example, the Google Maps app uses the package name &#x60;com.google.android.apps.maps&#x60;. REQUIRED
+    - :source.androidApp.certificate.sha256Fingerprint (String.t): The uppercase SHA-265 fingerprint of the certificate.  From the PEM  certificate, it can be acquired like this:      $ keytool -printcert -file $CERTFILE | grep SHA256:     SHA256: 14:6D:E9:83:C5:73:06:50:D8:EE:B9:95:2F:34:FC:64:16:A0:83: \\         42:E6:1D:BE:A8:8A:04:96:B2:3F:CF:44:E5  or like this:      $ openssl x509 -in $CERTFILE -noout -fingerprint -sha256     SHA256 Fingerprint&#x3D;14:6D:E9:83:C5:73:06:50:D8:EE:B9:95:2F:34:FC:64: \\         16:A0:83:42:E6:1D:BE:A8:8A:04:96:B2:3F:CF:44:E5  In this example, the contents of this field would be &#x60;14:6D:E9:83:C5:73: 06:50:D8:EE:B9:95:2F:34:FC:64:16:A0:83:42:E6:1D:BE:A8:8A:04:96:B2:3F:CF: 44:E5&#x60;.  If these tools are not available to you, you can convert the PEM certificate into the DER format, compute the SHA-256 hash of that string and represent the result as a hexstring (that is, uppercase hexadecimal representations of each octet, separated by colons).
+    - :relation (String.t): Query string for the relation.  We identify relations with strings of the format &#x60;&lt;kind&gt;/&lt;detail&gt;&#x60;, where &#x60;&lt;kind&gt;&#x60; must be one of a set of pre-defined purpose categories, and &#x60;&lt;detail&gt;&#x60; is a free-form lowercase alphanumeric string that describes the specific use case of the statement.  Refer to [our API documentation](/digital-asset-links/v1/relation-strings) for the current list of supported relations.  For a query to match an asset link, both the query&#39;s and the asset link&#39;s relation strings must match exactly.  Example: A query with relation &#x60;delegate_permission/common.handle_all_urls&#x60; matches an asset link with relation &#x60;delegate_permission/common.handle_all_urls&#x60;.
     - :target.web.site (String.t): Web assets are identified by a URL that contains only the scheme, hostname and port parts.  The format is      http[s]://&lt;hostname&gt;[:&lt;port&gt;]  Hostnames must be fully qualified: they must end in a single period (\&quot;&#x60;.&#x60;\&quot;).  Only the schemes \&quot;http\&quot; and \&quot;https\&quot; are currently allowed.  Port numbers are given as a decimal number, and they must be omitted if the standard port numbers are used: 80 for http and 443 for https.  We call this limited URL the \&quot;site\&quot;.  All URLs that share the same scheme, hostname and port are considered to be a part of the site and thus belong to the web asset.  Example: the asset with the site &#x60;https://www.google.com&#x60; contains all these URLs:    *   &#x60;https://www.google.com/&#x60;   *   &#x60;https://www.google.com:443/&#x60;   *   &#x60;https://www.google.com/foo&#x60;   *   &#x60;https://www.google.com/foo?bar&#x60;   *   &#x60;https://www.google.com/foo#bar&#x60;   *   &#x60;https://user@password:www.google.com/&#x60;  But it does not contain these URLs:    *   &#x60;http://www.google.com/&#x60;       (wrong scheme)   *   &#x60;https://google.com/&#x60;          (hostname does not match)   *   &#x60;https://www.google.com:444/&#x60;  (port does not match) REQUIRED
     - :target.androidApp.certificate.sha256Fingerprint (String.t): The uppercase SHA-265 fingerprint of the certificate.  From the PEM  certificate, it can be acquired like this:      $ keytool -printcert -file $CERTFILE | grep SHA256:     SHA256: 14:6D:E9:83:C5:73:06:50:D8:EE:B9:95:2F:34:FC:64:16:A0:83: \\         42:E6:1D:BE:A8:8A:04:96:B2:3F:CF:44:E5  or like this:      $ openssl x509 -in $CERTFILE -noout -fingerprint -sha256     SHA256 Fingerprint&#x3D;14:6D:E9:83:C5:73:06:50:D8:EE:B9:95:2F:34:FC:64: \\         16:A0:83:42:E6:1D:BE:A8:8A:04:96:B2:3F:CF:44:E5  In this example, the contents of this field would be &#x60;14:6D:E9:83:C5:73: 06:50:D8:EE:B9:95:2F:34:FC:64:16:A0:83:42:E6:1D:BE:A8:8A:04:96:B2:3F:CF: 44:E5&#x60;.  If these tools are not available to you, you can convert the PEM certificate into the DER format, compute the SHA-256 hash of that string and represent the result as a hexstring (that is, uppercase hexadecimal representations of each octet, separated by colons).
     - :source.web.site (String.t): Web assets are identified by a URL that contains only the scheme, hostname and port parts.  The format is      http[s]://&lt;hostname&gt;[:&lt;port&gt;]  Hostnames must be fully qualified: they must end in a single period (\&quot;&#x60;.&#x60;\&quot;).  Only the schemes \&quot;http\&quot; and \&quot;https\&quot; are currently allowed.  Port numbers are given as a decimal number, and they must be omitted if the standard port numbers are used: 80 for http and 443 for https.  We call this limited URL the \&quot;site\&quot;.  All URLs that share the same scheme, hostname and port are considered to be a part of the site and thus belong to the web asset.  Example: the asset with the site &#x60;https://www.google.com&#x60; contains all these URLs:    *   &#x60;https://www.google.com/&#x60;   *   &#x60;https://www.google.com:443/&#x60;   *   &#x60;https://www.google.com/foo&#x60;   *   &#x60;https://www.google.com/foo?bar&#x60;   *   &#x60;https://www.google.com/foo#bar&#x60;   *   &#x60;https://user@password:www.google.com/&#x60;  But it does not contain these URLs:    *   &#x60;http://www.google.com/&#x60;       (wrong scheme)   *   &#x60;https://google.com/&#x60;          (hostname does not match)   *   &#x60;https://www.google.com:444/&#x60;  (port does not match) REQUIRED
     - :source.androidApp.packageName (String.t): Android App assets are naturally identified by their Java package name. For example, the Google Maps app uses the package name &#x60;com.google.android.apps.maps&#x60;. REQUIRED
-    - :target.androidApp.packageName (String.t): Android App assets are naturally identified by their Java package name. For example, the Google Maps app uses the package name &#x60;com.google.android.apps.maps&#x60;. REQUIRED
-    - :source.androidApp.certificate.sha256Fingerprint (String.t): The uppercase SHA-265 fingerprint of the certificate.  From the PEM  certificate, it can be acquired like this:      $ keytool -printcert -file $CERTFILE | grep SHA256:     SHA256: 14:6D:E9:83:C5:73:06:50:D8:EE:B9:95:2F:34:FC:64:16:A0:83: \\         42:E6:1D:BE:A8:8A:04:96:B2:3F:CF:44:E5  or like this:      $ openssl x509 -in $CERTFILE -noout -fingerprint -sha256     SHA256 Fingerprint&#x3D;14:6D:E9:83:C5:73:06:50:D8:EE:B9:95:2F:34:FC:64: \\         16:A0:83:42:E6:1D:BE:A8:8A:04:96:B2:3F:CF:44:E5  In this example, the contents of this field would be &#x60;14:6D:E9:83:C5:73: 06:50:D8:EE:B9:95:2F:34:FC:64:16:A0:83:42:E6:1D:BE:A8:8A:04:96:B2:3F:CF: 44:E5&#x60;.  If these tools are not available to you, you can convert the PEM certificate into the DER format, compute the SHA-256 hash of that string and represent the result as a hexstring (that is, uppercase hexadecimal representations of each octet, separated by colons).
-    - :relation (String.t): Query string for the relation.  We identify relations with strings of the format &#x60;&lt;kind&gt;/&lt;detail&gt;&#x60;, where &#x60;&lt;kind&gt;&#x60; must be one of a set of pre-defined purpose categories, and &#x60;&lt;detail&gt;&#x60; is a free-form lowercase alphanumeric string that describes the specific use case of the statement.  Refer to [our API documentation](/digital-asset-links/v1/relation-strings) for the current list of supported relations.  For a query to match an asset link, both the query&#39;s and the asset link&#39;s relation strings must match exactly.  Example: A query with relation &#x60;delegate_permission/common.handle_all_urls&#x60; matches an asset link with relation &#x60;delegate_permission/common.handle_all_urls&#x60;.
 
   ## Returns
 
@@ -59,6 +59,10 @@ defmodule GoogleApi.DigitalAssetLinks.V1.Api.Assetlinks do
           {:ok, GoogleApi.DigitalAssetLinks.V1.Model.CheckResponse.t()} | {:error, Tesla.Env.t()}
   def digitalassetlinks_assetlinks_check(connection, optional_params \\ [], opts \\ []) do
     optional_params_config = %{
+      :callback => :query,
+      :oauth_token => :query,
+      :"$.xgafv" => :query,
+      :alt => :query,
       :access_token => :query,
       :key => :query,
       :upload_protocol => :query,
@@ -66,17 +70,13 @@ defmodule GoogleApi.DigitalAssetLinks.V1.Api.Assetlinks do
       :quotaUser => :query,
       :fields => :query,
       :uploadType => :query,
-      :callback => :query,
-      :oauth_token => :query,
-      :"$.xgafv" => :query,
-      :alt => :query,
+      :"target.androidApp.packageName" => :query,
+      :"source.androidApp.certificate.sha256Fingerprint" => :query,
+      :relation => :query,
       :"target.web.site" => :query,
       :"target.androidApp.certificate.sha256Fingerprint" => :query,
       :"source.web.site" => :query,
-      :"source.androidApp.packageName" => :query,
-      :"target.androidApp.packageName" => :query,
-      :"source.androidApp.certificate.sha256Fingerprint" => :query,
-      :relation => :query
+      :"source.androidApp.packageName" => :query
     }
 
     request =
--- clients/digital_asset_links/lib/google_api/digital_asset_links/v1/api/statements.ex
+++ clients/digital_asset_links/lib/google_api/digital_asset_links/v1/api/statements.ex
@@ -31,6 +31,10 @@ defmodule GoogleApi.DigitalAssetLinks.V1.Api.Statements do
 
   - connection (GoogleApi.DigitalAssetLinks.V1.Connection): Connection to server
   - optional_params (KeywordList): [optional] Optional parameters
+    - :callback (String.t): JSONP
+    - :oauth_token (String.t): OAuth 2.0 token for the current user.
+    - :$.xgafv (String.t): V1 error format.
+    - :alt (String.t): Data format for response.
     - :access_token (String.t): OAuth access token.
     - :key (String.t): API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
     - :upload_protocol (String.t): Upload protocol for media (e.g. \&quot;raw\&quot;, \&quot;multipart\&quot;).
@@ -38,14 +42,10 @@ defmodule GoogleApi.DigitalAssetLinks.V1.Api.Statements do
     - :quotaUser (String.t): Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
     - :fields (String.t): Selector specifying which fields to include in a partial response.
     - :uploadType (String.t): Legacy upload protocol for media (e.g. \&quot;media\&quot;, \&quot;multipart\&quot;).
-    - :callback (String.t): JSONP
-    - :oauth_token (String.t): OAuth 2.0 token for the current user.
-    - :$.xgafv (String.t): V1 error format.
-    - :alt (String.t): Data format for response.
-    - :source.androidApp.certificate.sha256Fingerprint (String.t): The uppercase SHA-265 fingerprint of the certificate.  From the PEM  certificate, it can be acquired like this:      $ keytool -printcert -file $CERTFILE | grep SHA256:     SHA256: 14:6D:E9:83:C5:73:06:50:D8:EE:B9:95:2F:34:FC:64:16:A0:83: \\         42:E6:1D:BE:A8:8A:04:96:B2:3F:CF:44:E5  or like this:      $ openssl x509 -in $CERTFILE -noout -fingerprint -sha256     SHA256 Fingerprint&#x3D;14:6D:E9:83:C5:73:06:50:D8:EE:B9:95:2F:34:FC:64: \\         16:A0:83:42:E6:1D:BE:A8:8A:04:96:B2:3F:CF:44:E5  In this example, the contents of this field would be &#x60;14:6D:E9:83:C5:73: 06:50:D8:EE:B9:95:2F:34:FC:64:16:A0:83:42:E6:1D:BE:A8:8A:04:96:B2:3F:CF: 44:E5&#x60;.  If these tools are not available to you, you can convert the PEM certificate into the DER format, compute the SHA-256 hash of that string and represent the result as a hexstring (that is, uppercase hexadecimal representations of each octet, separated by colons).
-    - :relation (String.t): Use only associations that match the specified relation.  See the [&#x60;Statement&#x60;](#Statement) message for a detailed definition of relation strings.  For a query to match a statement, one of the following must be true:  *    both the query&#39;s and the statement&#39;s relation strings match exactly,      or *    the query&#39;s relation string is empty or missing.  Example: A query with relation &#x60;delegate_permission/common.handle_all_urls&#x60; matches an asset link with relation &#x60;delegate_permission/common.handle_all_urls&#x60;.
     - :source.web.site (String.t): Web assets are identified by a URL that contains only the scheme, hostname and port parts.  The format is      http[s]://&lt;hostname&gt;[:&lt;port&gt;]  Hostnames must be fully qualified: they must end in a single period (\&quot;&#x60;.&#x60;\&quot;).  Only the schemes \&quot;http\&quot; and \&quot;https\&quot; are currently allowed.  Port numbers are given as a decimal number, and they must be omitted if the standard port numbers are used: 80 for http and 443 for https.  We call this limited URL the \&quot;site\&quot;.  All URLs that share the same scheme, hostname and port are considered to be a part of the site and thus belong to the web asset.  Example: the asset with the site &#x60;https://www.google.com&#x60; contains all these URLs:    *   &#x60;https://www.google.com/&#x60;   *   &#x60;https://www.google.com:443/&#x60;   *   &#x60;https://www.google.com/foo&#x60;   *   &#x60;https://www.google.com/foo?bar&#x60;   *   &#x60;https://www.google.com/foo#bar&#x60;   *   &#x60;https://user@password:www.google.com/&#x60;  But it does not contain these URLs:    *   &#x60;http://www.google.com/&#x60;       (wrong scheme)   *   &#x60;https://google.com/&#x60;          (hostname does not match)   *   &#x60;https://www.google.com:444/&#x60;  (port does not match) REQUIRED
     - :source.androidApp.packageName (String.t): Android App assets are naturally identified by their Java package name. For example, the Google Maps app uses the package name &#x60;com.google.android.apps.maps&#x60;. REQUIRED
+    - :source.androidApp.certificate.sha256Fingerprint (String.t): The uppercase SHA-265 fingerprint of the certificate.  From the PEM  certificate, it can be acquired like this:      $ keytool -printcert -file $CERTFILE | grep SHA256:     SHA256: 14:6D:E9:83:C5:73:06:50:D8:EE:B9:95:2F:34:FC:64:16:A0:83: \\         42:E6:1D:BE:A8:8A:04:96:B2:3F:CF:44:E5  or like this:      $ openssl x509 -in $CERTFILE -noout -fingerprint -sha256     SHA256 Fingerprint&#x3D;14:6D:E9:83:C5:73:06:50:D8:EE:B9:95:2F:34:FC:64: \\         16:A0:83:42:E6:1D:BE:A8:8A:04:96:B2:3F:CF:44:E5  In this example, the contents of this field would be &#x60;14:6D:E9:83:C5:73: 06:50:D8:EE:B9:95:2F:34:FC:64:16:A0:83:42:E6:1D:BE:A8:8A:04:96:B2:3F:CF: 44:E5&#x60;.  If these tools are not available to you, you can convert the PEM certificate into the DER format, compute the SHA-256 hash of that string and represent the result as a hexstring (that is, uppercase hexadecimal representations of each octet, separated by colons).
+    - :relation (String.t): Use only associations that match the specified relation.  See the [&#x60;Statement&#x60;](#Statement) message for a detailed definition of relation strings.  For a query to match a statement, one of the following must be true:  *    both the query&#39;s and the statement&#39;s relation strings match exactly,      or *    the query&#39;s relation string is empty or missing.  Example: A query with relation &#x60;delegate_permission/common.handle_all_urls&#x60; matches an asset link with relation &#x60;delegate_permission/common.handle_all_urls&#x60;.
 
   ## Returns
 
@@ -56,6 +56,10 @@ defmodule GoogleApi.DigitalAssetLinks.V1.Api.Statements do
           {:ok, GoogleApi.DigitalAssetLinks.V1.Model.ListResponse.t()} | {:error, Tesla.Env.t()}
   def digitalassetlinks_statements_list(connection, optional_params \\ [], opts \\ []) do
     optional_params_config = %{
+      :callback => :query,
+      :oauth_token => :query,
+      :"$.xgafv" => :query,
+      :alt => :query,
       :access_token => :query,
       :key => :query,
       :upload_protocol => :query,
@@ -63,14 +67,10 @@ defmodule GoogleApi.DigitalAssetLinks.V1.Api.Statements do
       :quotaUser => :query,
       :fields => :query,
       :uploadType => :query,
-      :callback => :query,
-      :oauth_token => :query,
-      :"$.xgafv" => :query,
-      :alt => :query,
-      :"source.androidApp.certificate.sha256Fingerprint" => :query,
-      :relation => :query,
       :"source.web.site" => :query,
-      :"source.androidApp.packageName" => :query
+      :"source.androidApp.packageName" => :query,
+      :"source.androidApp.certificate.sha256Fingerprint" => :query,
+      :relation => :query
     }
 
     request =
