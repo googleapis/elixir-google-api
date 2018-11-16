7c590624808a1ce2857f17ccc407b0a92609e29f
yoshi-automation <yoshi-automation@google.com>
2018-11-16 15:44:35 -0800

    Regenerate Container client

--- clients/container/lib/google_api/container/v1/api/projects.ex
+++ clients/container/lib/google_api/container/v1/api/projects.ex
@@ -170,8 +170,8 @@ defmodule GoogleApi.Container.V1.Api.Projects do
     - :quotaUser (String.t): Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
     - :upload_protocol (String.t): Upload protocol for media (e.g. \&quot;raw\&quot;, \&quot;multipart\&quot;).
     - :uploadType (String.t): Legacy upload protocol for media (e.g. \&quot;media\&quot;, \&quot;multipart\&quot;).
-    - :zone (String.t): Deprecated. The name of the Google Compute Engine [zone](/compute/docs/zones#available) in which the cluster resides, or \&quot;-\&quot; for all zones. This field has been deprecated and replaced by the parent field.
     - :projectId (String.t): Deprecated. The Google Developers Console [project ID or project number](https://support.google.com/cloud/answer/6158840). This field has been deprecated and replaced by the parent field.
+    - :zone (String.t): Deprecated. The name of the Google Compute Engine [zone](/compute/docs/zones#available) in which the cluster resides, or \&quot;-\&quot; for all zones. This field has been deprecated and replaced by the parent field.
 
   ## Returns
 
@@ -198,8 +198,8 @@ defmodule GoogleApi.Container.V1.Api.Projects do
       :quotaUser => :query,
       :upload_protocol => :query,
       :uploadType => :query,
-      :zone => :query,
-      :projectId => :query
+      :projectId => :query,
+      :zone => :query
     }
 
     request =
@@ -299,10 +299,10 @@ defmodule GoogleApi.Container.V1.Api.Projects do
     - :quotaUser (String.t): Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
     - :upload_protocol (String.t): Upload protocol for media (e.g. \&quot;raw\&quot;, \&quot;multipart\&quot;).
     - :uploadType (String.t): Legacy upload protocol for media (e.g. \&quot;media\&quot;, \&quot;multipart\&quot;).
-    - :projectId (String.t): Deprecated. The Google Developers Console [project ID or project number](https://developers.google.com/console/help/new/#projectnumber). This field has been deprecated and replaced by the name field.
-    - :zone (String.t): Deprecated. The name of the Google Compute Engine [zone](/compute/docs/zones#available) in which the cluster resides. This field has been deprecated and replaced by the name field.
     - :clusterId (String.t): Deprecated. The name of the cluster. This field has been deprecated and replaced by the name field.
     - :nodePoolId (String.t): Deprecated. The name of the node pool to delete. This field has been deprecated and replaced by the name field.
+    - :projectId (String.t): Deprecated. The Google Developers Console [project ID or project number](https://developers.google.com/console/help/new/#projectnumber). This field has been deprecated and replaced by the name field.
+    - :zone (String.t): Deprecated. The name of the Google Compute Engine [zone](/compute/docs/zones#available) in which the cluster resides. This field has been deprecated and replaced by the name field.
 
   ## Returns
 
@@ -332,10 +332,10 @@ defmodule GoogleApi.Container.V1.Api.Projects do
       :quotaUser => :query,
       :upload_protocol => :query,
       :uploadType => :query,
-      :projectId => :query,
-      :zone => :query,
       :clusterId => :query,
-      :nodePoolId => :query
+      :nodePoolId => :query,
+      :projectId => :query,
+      :zone => :query
     }
 
     request =
@@ -349,6 +349,77 @@ defmodule GoogleApi.Container.V1.Api.Projects do
     connection
     |> Connection.execute(request)
     |> Response.decode(opts ++ [struct: %GoogleApi.Container.V1.Model.Operation{}])
+  end
+
+  @doc """
+  Retrieves the node pool requested.
+
+  ## Parameters
+
+  - connection (GoogleApi.Container.V1.Connection): Connection to server
+  - name (String.t): The name (project, location, cluster, node pool id) of the node pool to get. Specified in the format &#39;projects/*/locations/*/clusters/*/nodePools/*&#39;.
+  - optional_params (KeywordList): [optional] Optional parameters
+    - :$.xgafv (String.t): V1 error format.
+    - :access_token (String.t): OAuth access token.
+    - :alt (String.t): Data format for response.
+    - :callback (String.t): JSONP
+    - :fields (String.t): Selector specifying which fields to include in a partial response.
+    - :key (String.t): API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+    - :oauth_token (String.t): OAuth 2.0 token for the current user.
+    - :prettyPrint (boolean()): Returns response with indentations and line breaks.
+    - :quotaUser (String.t): Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
+    - :upload_protocol (String.t): Upload protocol for media (e.g. \&quot;raw\&quot;, \&quot;multipart\&quot;).
+    - :uploadType (String.t): Legacy upload protocol for media (e.g. \&quot;media\&quot;, \&quot;multipart\&quot;).
+    - :clusterId (String.t): Deprecated. The name of the cluster. This field has been deprecated and replaced by the name field.
+    - :nodePoolId (String.t): Deprecated. The name of the node pool. This field has been deprecated and replaced by the name field.
+    - :projectId (String.t): Deprecated. The Google Developers Console [project ID or project number](https://developers.google.com/console/help/new/#projectnumber). This field has been deprecated and replaced by the name field.
+    - :zone (String.t): Deprecated. The name of the Google Compute Engine [zone](/compute/docs/zones#available) in which the cluster resides. This field has been deprecated and replaced by the name field.
+
+  ## Returns
+
+  {:ok, %GoogleApi.Container.V1.Model.NodePool{}} on success
+  {:error, info} on failure
+  """
+  @spec container_projects_locations_clusters_node_pools_get(
+          Tesla.Env.client(),
+          String.t(),
+          keyword()
+        ) :: {:ok, GoogleApi.Container.V1.Model.NodePool.t()} | {:error, Tesla.Env.t()}
+  def container_projects_locations_clusters_node_pools_get(
+        connection,
+        name,
+        optional_params \\ [],
+        opts \\ []
+      ) do
+    optional_params_config = %{
+      :"$.xgafv" => :query,
+      :access_token => :query,
+      :alt => :query,
+      :callback => :query,
+      :fields => :query,
+      :key => :query,
+      :oauth_token => :query,
+      :prettyPrint => :query,
+      :quotaUser => :query,
+      :upload_protocol => :query,
+      :uploadType => :query,
+      :clusterId => :query,
+      :nodePoolId => :query,
+      :projectId => :query,
+      :zone => :query
+    }
+
+    request =
+      Request.new()
+      |> Request.method(:get)
+      |> Request.url("/v1/{+name}", %{
+        "name" => URI.encode_www_form(name)
+      })
+      |> Request.add_optional_params(optional_params_config, optional_params)
+
+    connection
+    |> Connection.execute(request)
+    |> Response.decode(opts ++ [struct: %GoogleApi.Container.V1.Model.NodePool{}])
   end
 
   @doc """
@@ -370,9 +441,9 @@ defmodule GoogleApi.Container.V1.Api.Projects do
     - :quotaUser (String.t): Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
     - :upload_protocol (String.t): Upload protocol for media (e.g. \&quot;raw\&quot;, \&quot;multipart\&quot;).
     - :uploadType (String.t): Legacy upload protocol for media (e.g. \&quot;media\&quot;, \&quot;multipart\&quot;).
+    - :clusterId (String.t): Deprecated. The name of the cluster. This field has been deprecated and replaced by the parent field.
     - :projectId (String.t): Deprecated. The Google Developers Console [project ID or project number](https://developers.google.com/console/help/new/#projectnumber). This field has been deprecated and replaced by the parent field.
     - :zone (String.t): Deprecated. The name of the Google Compute Engine [zone](/compute/docs/zones#available) in which the cluster resides. This field has been deprecated and replaced by the parent field.
-    - :clusterId (String.t): Deprecated. The name of the cluster. This field has been deprecated and replaced by the parent field.
 
   ## Returns
 
@@ -403,9 +474,9 @@ defmodule GoogleApi.Container.V1.Api.Projects do
       :quotaUser => :query,
       :upload_protocol => :query,
       :uploadType => :query,
+      :clusterId => :query,
       :projectId => :query,
-      :zone => :query,
-      :clusterId => :query
+      :zone => :query
     }
 
     request =
@@ -1585,74 +1656,6 @@ defmodule GoogleApi.Container.V1.Api.Projects do
     connection
     |> Connection.execute(request)
     |> Response.decode(opts ++ [struct: %GoogleApi.Container.V1.Model.Empty{}])
-  end
-
-  @doc """
-  Gets the specified operation.
-
-  ## Parameters
-
-  - connection (GoogleApi.Container.V1.Connection): Connection to server
-  - name (String.t): The name (project, location, operation id) of the operation to get. Specified in the format &#39;projects/*/locations/*/operations/*&#39;.
-  - optional_params (KeywordList): [optional] Optional parameters
-    - :$.xgafv (String.t): V1 error format.
-    - :access_token (String.t): OAuth access token.
-    - :alt (String.t): Data format for response.
-    - :callback (String.t): JSONP
-    - :fields (String.t): Selector specifying which fields to include in a partial response.
-    - :key (String.t): API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
-    - :oauth_token (String.t): OAuth 2.0 token for the current user.
-    - :prettyPrint (boolean()): Returns response with indentations and line breaks.
-    - :quotaUser (String.t): Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
-    - :upload_protocol (String.t): Upload protocol for media (e.g. \&quot;raw\&quot;, \&quot;multipart\&quot;).
-    - :uploadType (String.t): Legacy upload protocol for media (e.g. \&quot;media\&quot;, \&quot;multipart\&quot;).
-    - :zone (String.t): Deprecated. The name of the Google Compute Engine [zone](/compute/docs/zones#available) in which the cluster resides. This field has been deprecated and replaced by the name field.
-    - :operationId (String.t): Deprecated. The server-assigned &#x60;name&#x60; of the operation. This field has been deprecated and replaced by the name field.
-    - :projectId (String.t): Deprecated. The Google Developers Console [project ID or project number](https://support.google.com/cloud/answer/6158840). This field has been deprecated and replaced by the name field.
-    - :nodePoolId (String.t): Deprecated. The name of the node pool. This field has been deprecated and replaced by the name field.
-
-  ## Returns
-
-  {:ok, %GoogleApi.Container.V1.Model.Operation{}} on success
-  {:error, info} on failure
-  """
-  @spec container_projects_locations_operations_get(Tesla.Env.client(), String.t(), keyword()) ::
-          {:ok, GoogleApi.Container.V1.Model.Operation.t()} | {:error, Tesla.Env.t()}
-  def container_projects_locations_operations_get(
-        connection,
-        name,
-        optional_params \\ [],
-        opts \\ []
-      ) do
-    optional_params_config = %{
-      :"$.xgafv" => :query,
-      :access_token => :query,
-      :alt => :query,
-      :callback => :query,
-      :fields => :query,
-      :key => :query,
-      :oauth_token => :query,
-      :prettyPrint => :query,
-      :quotaUser => :query,
-      :upload_protocol => :query,
-      :uploadType => :query,
-      :zone => :query,
-      :operationId => :query,
-      :projectId => :query,
-      :nodePoolId => :query
-    }
-
-    request =
-      Request.new()
-      |> Request.method(:get)
-      |> Request.url("/v1/{+name}", %{
-        "name" => URI.encode_www_form(name)
-      })
-      |> Request.add_optional_params(optional_params_config, optional_params)
-
-    connection
-    |> Connection.execute(request)
-    |> Response.decode(opts ++ [struct: %GoogleApi.Container.V1.Model.Operation{}])
   end
 
   @doc """
