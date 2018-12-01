ddca5f3565f8aa6f5f593eb7362451ba26807e3d
yoshi-automation <yoshi-automation@google.com>
2018-12-01 01:25:41 -0800

    Regenerate Monitoring client

--- clients/monitoring/lib/google_api/monitoring/v3/model/create_time_series_request.ex
+++ clients/monitoring/lib/google_api/monitoring/v3/model/create_time_series_request.ex
@@ -22,7 +22,7 @@ defmodule GoogleApi.Monitoring.V3.Model.CreateTimeSeriesRequest do
 
   ## Attributes
 
-  - timeSeries ([TimeSeries]): The new data to be added to a list of time series. Adds at most one data point to each of several time series. The new data point must be more recent than any other point in its time series. Each TimeSeries value must fully specify a unique time series by supplying all label values for the metric and the monitored resource. Defaults to: `null`.
+  - timeSeries ([TimeSeries]): The new data to be added to a list of time series. Adds at most one data point to each of several time series. The new data point must be more recent than any other point in its time series. Each TimeSeries value must fully specify a unique time series by supplying all label values for the metric and the monitored resource.The maximum number of TimeSeries objects per Create request is 200. Defaults to: `null`.
   """
 
   use GoogleApi.Gax.ModelBase
