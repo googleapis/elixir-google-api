efb1e2f17470d02786524410d056cf12e6e0eca5
yoshi-automation <yoshi-automation@google.com>
2018-12-08 01:39:24 -0800

    Regenerate TPU client

--- clients/tpu/lib/google_api/tpu/v1/api/projects.ex
+++ clients/tpu/lib/google_api/tpu/v1/api/projects.ex
@@ -268,6 +268,61 @@ defmodule GoogleApi.TPU.V1.Api.Projects do
   end
 
   @doc """
+  Gets the details of a node.
+
+  ## Parameters
+
+  - connection (GoogleApi.TPU.V1.Connection): Connection to server
+  - name (String.t): The resource name.
+  - optional_params (KeywordList): [optional] Optional parameters
+    - :$.xgafv (String.t): V1 error format.
+    - :access_token (String.t): OAuth access token.
+    - :alt (String.t): Data format for response.
+    - :callback (String.t): JSONP
+    - :fields (String.t): Selector specifying which fields to include in a partial response.
+    - :key (String.t): API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+    - :oauth_token (String.t): OAuth 2.0 token for the current user.
+    - :prettyPrint (boolean()): Returns response with indentations and line breaks.
+    - :quotaUser (String.t): Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
+    - :upload_protocol (String.t): Upload protocol for media (e.g. \&quot;raw\&quot;, \&quot;multipart\&quot;).
+    - :uploadType (String.t): Legacy upload protocol for media (e.g. \&quot;media\&quot;, \&quot;multipart\&quot;).
+
+  ## Returns
+
+  {:ok, %GoogleApi.TPU.V1.Model.Node{}} on success
+  {:error, info} on failure
+  """
+  @spec tpu_projects_locations_nodes_get(Tesla.Env.client(), String.t(), keyword()) ::
+          {:ok, GoogleApi.TPU.V1.Model.Node.t()} | {:error, Tesla.Env.t()}
+  def tpu_projects_locations_nodes_get(connection, name, optional_params \\ [], opts \\ []) do
+    optional_params_config = %{
+      :"$.xgafv" => :query,
+      :access_token => :query,
+      :alt => :query,
+      :callback => :query,
+      :fields => :query,
+      :key => :query,
+      :oauth_token => :query,
+      :prettyPrint => :query,
+      :quotaUser => :query,
+      :upload_protocol => :query,
+      :uploadType => :query
+    }
+
+    request =
+      Request.new()
+      |> Request.method(:get)
+      |> Request.url("/v1/{+name}", %{
+        "name" => URI.encode_www_form(name)
+      })
+      |> Request.add_optional_params(optional_params_config, optional_params)
+
+    connection
+    |> Connection.execute(request)
+    |> Response.decode(opts ++ [struct: %GoogleApi.TPU.V1.Model.Node{}])
+  end
+
+  @doc """
   Lists nodes.
 
   ## Parameters
@@ -616,66 +671,6 @@ defmodule GoogleApi.TPU.V1.Api.Projects do
     connection
     |> Connection.execute(request)
     |> Response.decode(opts ++ [struct: %GoogleApi.TPU.V1.Model.ListOperationsResponse{}])
-  end
-
-  @doc """
-  Gets TensorFlow Version.
-
-  ## Parameters
-
-  - connection (GoogleApi.TPU.V1.Connection): Connection to server
-  - name (String.t): The resource name.
-  - optional_params (KeywordList): [optional] Optional parameters
-    - :$.xgafv (String.t): V1 error format.
-    - :access_token (String.t): OAuth access token.
-    - :alt (String.t): Data format for response.
-    - :callback (String.t): JSONP
-    - :fields (String.t): Selector specifying which fields to include in a partial response.
-    - :key (String.t): API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
-    - :oauth_token (String.t): OAuth 2.0 token for the current user.
-    - :prettyPrint (boolean()): Returns response with indentations and line breaks.
-    - :quotaUser (String.t): Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
-    - :upload_protocol (String.t): Upload protocol for media (e.g. \&quot;raw\&quot;, \&quot;multipart\&quot;).
-    - :uploadType (String.t): Legacy upload protocol for media (e.g. \&quot;media\&quot;, \&quot;multipart\&quot;).
-
-  ## Returns
-
-  {:ok, %GoogleApi.TPU.V1.Model.TensorFlowVersion{}} on success
-  {:error, info} on failure
-  """
-  @spec tpu_projects_locations_tensorflow_versions_get(Tesla.Env.client(), String.t(), keyword()) ::
-          {:ok, GoogleApi.TPU.V1.Model.TensorFlowVersion.t()} | {:error, Tesla.Env.t()}
-  def tpu_projects_locations_tensorflow_versions_get(
-        connection,
-        name,
-        optional_params \\ [],
-        opts \\ []
-      ) do
-    optional_params_config = %{
-      :"$.xgafv" => :query,
-      :access_token => :query,
-      :alt => :query,
-      :callback => :query,
-      :fields => :query,
-      :key => :query,
-      :oauth_token => :query,
-      :prettyPrint => :query,
-      :quotaUser => :query,
-      :upload_protocol => :query,
-      :uploadType => :query
-    }
-
-    request =
-      Request.new()
-      |> Request.method(:get)
-      |> Request.url("/v1/{+name}", %{
-        "name" => URI.encode_www_form(name)
-      })
-      |> Request.add_optional_params(optional_params_config, optional_params)
-
-    connection
-    |> Connection.execute(request)
-    |> Response.decode(opts ++ [struct: %GoogleApi.TPU.V1.Model.TensorFlowVersion{}])
   end
 
   @doc """
