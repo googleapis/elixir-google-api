4a1fc94f6941e7781669696d1f4f730b8fe8bd48
yoshi-automation <yoshi-automation@google.com>
2018-12-11 01:30:04 -0800

    Regenerate ServiceConsumerManagement client

--- clients/service_consumer_management/lib/google_api/service_consumer_management/v1/api/services.ex
+++ clients/service_consumer_management/lib/google_api/service_consumer_management/v1/api/services.ex
@@ -370,6 +370,73 @@ defmodule GoogleApi.ServiceConsumerManagement.V1.Api.Services do
   end
 
   @doc """
+  Delete a tenancy unit.  Before the tenancy unit is deleted, there should be no tenant resources in it. Operation&lt;response: Empty&gt;.
+
+  ## Parameters
+
+  - connection (GoogleApi.ServiceConsumerManagement.V1.Connection): Connection to server
+  - name (String.t): Name of the tenancy unit to be deleted.
+  - optional_params (KeywordList): [optional] Optional parameters
+    - :$.xgafv (String.t): V1 error format.
+    - :access_token (String.t): OAuth access token.
+    - :alt (String.t): Data format for response.
+    - :callback (String.t): JSONP
+    - :fields (String.t): Selector specifying which fields to include in a partial response.
+    - :key (String.t): API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+    - :oauth_token (String.t): OAuth 2.0 token for the current user.
+    - :prettyPrint (boolean()): Returns response with indentations and line breaks.
+    - :quotaUser (String.t): Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
+    - :upload_protocol (String.t): Upload protocol for media (e.g. \&quot;raw\&quot;, \&quot;multipart\&quot;).
+    - :uploadType (String.t): Legacy upload protocol for media (e.g. \&quot;media\&quot;, \&quot;multipart\&quot;).
+
+  ## Returns
+
+  {:ok, %GoogleApi.ServiceConsumerManagement.V1.Model.Operation{}} on success
+  {:error, info} on failure
+  """
+  @spec serviceconsumermanagement_services_tenancy_units_delete(
+          Tesla.Env.client(),
+          String.t(),
+          keyword()
+        ) ::
+          {:ok, GoogleApi.ServiceConsumerManagement.V1.Model.Operation.t()}
+          | {:error, Tesla.Env.t()}
+  def serviceconsumermanagement_services_tenancy_units_delete(
+        connection,
+        name,
+        optional_params \\ [],
+        opts \\ []
+      ) do
+    optional_params_config = %{
+      :"$.xgafv" => :query,
+      :access_token => :query,
+      :alt => :query,
+      :callback => :query,
+      :fields => :query,
+      :key => :query,
+      :oauth_token => :query,
+      :prettyPrint => :query,
+      :quotaUser => :query,
+      :upload_protocol => :query,
+      :uploadType => :query
+    }
+
+    request =
+      Request.new()
+      |> Request.method(:delete)
+      |> Request.url("/v1/{+name}", %{
+        "name" => URI.encode_www_form(name)
+      })
+      |> Request.add_optional_params(optional_params_config, optional_params)
+
+    connection
+    |> Connection.execute(request)
+    |> Response.decode(
+      opts ++ [struct: %GoogleApi.ServiceConsumerManagement.V1.Model.Operation{}]
+    )
+  end
+
+  @doc """
   Find the tenancy unit for a service and consumer. This method should not be used in producers&#39; runtime path, for example finding the tenant project number when creating VMs. Producers should persist the tenant project information after the project is created.
 
   ## Parameters
