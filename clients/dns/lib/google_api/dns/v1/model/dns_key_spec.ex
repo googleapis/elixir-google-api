f0b082375b2ea8c363e9f061e35e9e40fb9a5a8a
yoshi-automation <yoshi-automation@google.com>
2019-01-11 01:09:04 -0800

    Regenerate DNS client

--- clients/dns/lib/google_api/dns/v1/model/dns_key_spec.ex
+++ clients/dns/lib/google_api/dns/v1/model/dns_key_spec.ex
@@ -25,7 +25,7 @@ defmodule GoogleApi.DNS.V1.Model.DnsKeySpec do
   - algorithm (String.t): String mnemonic specifying the DNSSEC algorithm of this key. Defaults to: `null`.
     - Enum - one of [ecdsap256sha256, ecdsap384sha384, rsasha1, rsasha256, rsasha512]
   - keyLength (integer()): Length of the keys in bits. Defaults to: `null`.
-  - keyType (String.t): One of \&quot;KEY_SIGNING\&quot; or \&quot;ZONE_SIGNING\&quot;. Keys of type KEY_SIGNING have the Secure Entry Point flag set and, when active, will be used to sign only resource record sets of type DNSKEY. Otherwise, the Secure Entry Point flag will be cleared and this key will be used to sign only resource record sets of other types. Defaults to: `null`.
+  - keyType (String.t): Specifies whether this is a key signing key (KSK) or a zone signing key (ZSK). Key signing keys have the Secure Entry Point flag set and, when active, will only be used to sign resource record sets of type DNSKEY. Zone signing keys do not have the Secure Entry Point flag set and will be used to sign all other types of resource record sets. Defaults to: `null`.
     - Enum - one of [keySigning, zoneSigning]
   - kind (String.t): Identifies what kind of resource this is. Value: the fixed string \&quot;dns#dnsKeySpec\&quot;. Defaults to: `null`.
   """
