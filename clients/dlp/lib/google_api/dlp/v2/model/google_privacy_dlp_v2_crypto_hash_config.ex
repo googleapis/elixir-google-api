0ef91dc6f441c4f02b384776ed7637ee91680bf4
yoshi-automation <yoshi-automation@google.com>
2019-01-12 01:13:19 -0800

    Regenerate DLP client

--- clients/dlp/lib/google_api/dlp/v2/model/google_privacy_dlp_v2_crypto_hash_config.ex
+++ clients/dlp/lib/google_api/dlp/v2/model/google_privacy_dlp_v2_crypto_hash_config.ex
@@ -18,7 +18,7 @@
 
 defmodule GoogleApi.DLP.V2.Model.GooglePrivacyDlpV2CryptoHashConfig do
   @moduledoc """
-  Pseudonymization method that generates surrogates via cryptographic hashing. Uses SHA-256. The key size must be either 32 or 64 bytes. Outputs a base64 encoded representation of the hashed output (for example, L7k0BHmF1ha5U3NfGykjro4xWi1MPVQPjhMAZbSV9mM&#x3D;). Currently, only string and integer values can be hashed.
+  Pseudonymization method that generates surrogates via cryptographic hashing. Uses SHA-256. The key size must be either 32 or 64 bytes. Outputs a base64 encoded representation of the hashed output (for example, L7k0BHmF1ha5U3NfGykjro4xWi1MPVQPjhMAZbSV9mM&#x3D;). Currently, only string and integer values can be hashed. See https://cloud.google.com/dlp/docs/pseudonymization to learn more.
 
   ## Attributes
 
