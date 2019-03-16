a91d09307477b8d02ab3590dc93ea1c75d5c1ea5
yoshi-automation <yoshi-automation@google.com>
2019-03-16 01:46:39 -0700

    Regenerate Speech client

--- clients/speech/config/config.exs
+++ clients/speech/config/config.exs
@@ -10,11 +10,11 @@ use Mix.Config
 
 # You can configure for your application as:
 #
-#     config :cloud_speech_api, key: :value
+#     config :cloud_speech_to_text_api, key: :value
 #
 # And access this configuration in your application as:
 #
-#     Application.get_env(:cloud_speech_api, :key)
+#     Application.get_env(:cloud_speech_to_text_api, :key)
 #
 # Or configure a 3rd-party app:
 #
