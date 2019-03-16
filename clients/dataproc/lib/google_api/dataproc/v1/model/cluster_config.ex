7931cf07384dd5007f80376a218e696959dc43fe
yoshi-automation <yoshi-automation@google.com>
2019-03-16 01:19:14 -0700

    Regenerate Dataproc client

--- clients/dataproc/lib/google_api/dataproc/v1/model/cluster_config.ex
+++ clients/dataproc/lib/google_api/dataproc/v1/model/cluster_config.ex
@@ -22,9 +22,9 @@ defmodule GoogleApi.Dataproc.V1.Model.ClusterConfig do
 
   ## Attributes
 
-  - configBucket (String.t): Optional. A Cloud Storage staging bucket used for sharing generated SSH keys and config. If you do not specify a staging bucket, Cloud Dataproc will determine an appropriate Cloud Storage location (US, ASIA, or EU) for your cluster&#39;s staging bucket according to the Google Compute Engine zone where your cluster is deployed, and then it will create and manage this project-level, per-location bucket for you. Defaults to: `null`.
+  - configBucket (String.t): Optional. A Google Cloud Storage bucket used to stage job dependencies, config files, and job driver console output. If you do not specify a staging bucket, Cloud Dataproc will determine a Cloud Storage location (US, ASIA, or EU) for your cluster&#39;s staging bucket according to the Google Compute Engine zone where your cluster is deployed, and then create and manage this project-level, per-location bucket (see Cloud Dataproc staging bucket). Defaults to: `null`.
   - encryptionConfig (EncryptionConfig): Optional. Encryption settings for the cluster. Defaults to: `null`.
-  - gceClusterConfig (GceClusterConfig): Required. The shared Compute Engine config settings for all instances in a cluster. Defaults to: `null`.
+  - gceClusterConfig (GceClusterConfig): Optional. The shared Compute Engine config settings for all instances in a cluster. Defaults to: `null`.
   - initializationActions ([NodeInitializationAction]): Optional. Commands to execute on each node after config is completed. By default, executables are run on master and all worker nodes. You can test a node&#39;s role metadata to run an executable on a master or worker node, as shown below using curl (you can also use wget): ROLE&#x3D;$(curl -H Metadata-Flavor:Google http://metadata/computeMetadata/v1/instance/attributes/dataproc-role) if [[ \&quot;${ROLE}\&quot; &#x3D;&#x3D; &#39;Master&#39; ]]; then   ... master specific actions ... else   ... worker specific actions ... fi  Defaults to: `null`.
   - masterConfig (InstanceGroupConfig): Optional. The Compute Engine config settings for the master instance in a cluster. Defaults to: `null`.
   - secondaryWorkerConfig (InstanceGroupConfig): Optional. The Compute Engine config settings for additional worker instances in a cluster. Defaults to: `null`.
