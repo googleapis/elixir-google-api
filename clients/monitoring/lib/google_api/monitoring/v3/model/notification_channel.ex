c8f56686339c038309c791a6d0265c0eee7cd26d
yoshi-automation <yoshi-automation@google.com>
2019-04-02 01:34:02 -0700

    Regenerate Monitoring client

--- clients/monitoring/lib/google_api/monitoring/v3/model/notification_channel.ex
+++ clients/monitoring/lib/google_api/monitoring/v3/model/notification_channel.ex
@@ -22,7 +22,7 @@ defmodule GoogleApi.Monitoring.V3.Model.NotificationChannel do
 
   ## Attributes
 
-  - description (String.t): An optional human-readable description of this notification channel. This description may provide additional details, beyond the display name, for the channel. This may not exceeed 1024 Unicode characters. Defaults to: `null`.
+  - description (String.t): An optional human-readable description of this notification channel. This description may provide additional details, beyond the display name, for the channel. This may not exceed 1024 Unicode characters. Defaults to: `null`.
   - displayName (String.t): An optional human-readable name for this notification channel. It is recommended that you specify a non-empty and unique name in order to make it easier to identify the channels in your project, though this is not enforced. The display name is limited to 512 Unicode characters. Defaults to: `null`.
   - enabled (boolean()): Whether notifications are forwarded to the described channel. This makes it possible to disable delivery of notifications to a particular channel without removing the channel from all alerting policies that reference the channel. This is a more convenient approach when the change is temporary and you want to receive notifications from the same set of alerting policies on the channel at some point in the future. Defaults to: `null`.
   - labels (%{optional(String.t) &#x3D;&gt; String.t}): Configuration fields that define the channel and its behavior. The permissible and required labels are specified in the NotificationChannelDescriptor.labels of the NotificationChannelDescriptor corresponding to the type field. Defaults to: `null`.
