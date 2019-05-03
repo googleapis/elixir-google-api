e5ea69506073adcd00cc2a2dededf82af5e950fe
yoshi-automation <yoshi-automation@google.com>
2019-05-03 01:18:08 -0700

    Regenerate IAM client

--- clients/iam/lib/google_api/iam/v1/api/projects.ex
+++ clients/iam/lib/google_api/iam/v1/api/projects.ex
@@ -528,7 +528,7 @@ defmodule GoogleApi.IAM.V1.Api.Projects do
   end
 
   @doc """
-  Disables a ServiceAccount. The API is currently in alpha phase.
+  DisableServiceAccount is currently in the alpha launch stage.  Disables a ServiceAccount, which immediately prevents the service account from authenticating and gaining access to APIs.  Disabled service accounts can be safely restored by using EnableServiceAccount at any point. Deleted service accounts cannot be restored using this method.  Disabling a service account that is bound to VMs, Apps, Functions, or other jobs will cause those jobs to lose access to resources if they are using the disabled service account.  To improve reliability of your services and avoid unexpected outages, it is recommended to first disable a service account rather than delete it. After disabling the service account, wait at least 24 hours to verify there are no unintended consequences, and then delete the service account.
 
   ## Parameters
 
@@ -597,7 +597,7 @@ defmodule GoogleApi.IAM.V1.Api.Projects do
   end
 
   @doc """
-  Enables a ServiceAccount.  The API is currently in alpha phase.
+  EnableServiceAccount is currently in the alpha launch stage.   Restores a disabled ServiceAccount  that has been manually disabled by using DisableServiceAccount. Service  accounts that have been disabled by other means or for other reasons,  such as abuse, cannot be restored using this method.   EnableServiceAccount will have no effect on a service account that is  not disabled.  Enabling an already enabled service account will have no  effect.
 
   ## Parameters
 
@@ -1572,7 +1572,7 @@ defmodule GoogleApi.IAM.V1.Api.Projects do
   end
 
   @doc """
-  Note: This method is in the process of being deprecated. Use PatchServiceAccount instead.  Updates a ServiceAccount.  Currently, only the following fields are updatable: &#x60;display_name&#x60; . The &#x60;etag&#x60; is mandatory.
+  Note: This method is in the process of being deprecated. Use PatchServiceAccount instead.  Updates a ServiceAccount.  Currently, only the following fields are updatable: &#x60;display_name&#x60; and &#x60;description&#x60;.
 
   ## Parameters
 
--- clients/iam/mix.exs
+++ clients/iam/mix.exs
@@ -1,7 +1,7 @@
 defmodule GoogleApi.IAM.V1.Mixfile do
   use Mix.Project
 
-  @version "0.4.0"
+  @version "0.5.0"
 
   def project() do
     [
