24a1ce69a954d5decd744f7e71fa6199b54a86e8
yoshi-automation <yoshi-automation@google.com>
2019-05-04 01:04:07 -0700

    Regenerate AndroidManagement client

--- clients/android_management/lib/google_api/android_management/v1/api/enterprises.ex
+++ clients/android_management/lib/google_api/android_management/v1/api/enterprises.ex
@@ -25,72 +25,6 @@ defmodule GoogleApi.AndroidManagement.V1.Api.Enterprises do
   alias GoogleApi.Gax.{Request, Response}
 
   @doc """
-  Gets info about an application.
-
-  ## Parameters
-
-  - connection (GoogleApi.AndroidManagement.V1.Connection): Connection to server
-  - name (String.t): The name of the application in the form enterprises/{enterpriseId}/applications/{package_name}.
-  - optional_params (KeywordList): [optional] Optional parameters
-    - :$.xgafv (String.t): V1 error format.
-    - :access_token (String.t): OAuth access token.
-    - :alt (String.t): Data format for response.
-    - :callback (String.t): JSONP
-    - :fields (String.t): Selector specifying which fields to include in a partial response.
-    - :key (String.t): API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
-    - :oauth_token (String.t): OAuth 2.0 token for the current user.
-    - :prettyPrint (boolean()): Returns response with indentations and line breaks.
-    - :quotaUser (String.t): Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
-    - :upload_protocol (String.t): Upload protocol for media (e.g. \&quot;raw\&quot;, \&quot;multipart\&quot;).
-    - :uploadType (String.t): Legacy upload protocol for media (e.g. \&quot;media\&quot;, \&quot;multipart\&quot;).
-    - :languageCode (String.t): The preferred language for localized application info, as a BCP47 tag (e.g. \&quot;en-US\&quot;, \&quot;de\&quot;). If not specified the default language of the application will be used.
-    - :pageSize (integer()): The standard list page size.
-    - :pageToken (String.t): The standard list page token.
-
-  ## Returns
-
-  {:ok, %GoogleApi.AndroidManagement.V1.Model.Application{}} on success
-  {:error, info} on failure
-  """
-  @spec androidmanagement_enterprises_applications_get(Tesla.Env.client(), String.t(), keyword()) ::
-          {:ok, GoogleApi.AndroidManagement.V1.Model.Application.t()} | {:error, Tesla.Env.t()}
-  def androidmanagement_enterprises_applications_get(
-        connection,
-        name,
-        optional_params \\ [],
-        opts \\ []
-      ) do
-    optional_params_config = %{
-      :"$.xgafv" => :query,
-      :access_token => :query,
-      :alt => :query,
-      :callback => :query,
-      :fields => :query,
-      :key => :query,
-      :oauth_token => :query,
-      :prettyPrint => :query,
-      :quotaUser => :query,
-      :upload_protocol => :query,
-      :uploadType => :query,
-      :languageCode => :query,
-      :pageSize => :query,
-      :pageToken => :query
-    }
-
-    request =
-      Request.new()
-      |> Request.method(:get)
-      |> Request.url("/v1/{+name}", %{
-        "name" => URI.encode(name, &URI.char_unreserved?/1)
-      })
-      |> Request.add_optional_params(optional_params_config, optional_params)
-
-    connection
-    |> Connection.execute(request)
-    |> Response.decode(opts ++ [struct: %GoogleApi.AndroidManagement.V1.Model.Application{}])
-  end
-
-  @doc """
   Creates an enterprise. This is the last step in the enterprise signup flow.
 
   ## Parameters
@@ -598,6 +532,72 @@ defmodule GoogleApi.AndroidManagement.V1.Api.Enterprises do
       |> Request.method(:post)
       |> Request.url("/v1/{+parent}/webApps", %{
         "parent" => URI.encode(parent, &URI.char_unreserved?/1)
+      })
+      |> Request.add_optional_params(optional_params_config, optional_params)
+
+    connection
+    |> Connection.execute(request)
+    |> Response.decode(opts ++ [struct: %GoogleApi.AndroidManagement.V1.Model.WebApp{}])
+  end
+
+  @doc """
+  Gets a web app.
+
+  ## Parameters
+
+  - connection (GoogleApi.AndroidManagement.V1.Connection): Connection to server
+  - name (String.t): The name of the web app in the form enterprises/{enterpriseId}/webApp/{packageName}.
+  - optional_params (KeywordList): [optional] Optional parameters
+    - :$.xgafv (String.t): V1 error format.
+    - :access_token (String.t): OAuth access token.
+    - :alt (String.t): Data format for response.
+    - :callback (String.t): JSONP
+    - :fields (String.t): Selector specifying which fields to include in a partial response.
+    - :key (String.t): API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+    - :oauth_token (String.t): OAuth 2.0 token for the current user.
+    - :prettyPrint (boolean()): Returns response with indentations and line breaks.
+    - :quotaUser (String.t): Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
+    - :upload_protocol (String.t): Upload protocol for media (e.g. \&quot;raw\&quot;, \&quot;multipart\&quot;).
+    - :uploadType (String.t): Legacy upload protocol for media (e.g. \&quot;media\&quot;, \&quot;multipart\&quot;).
+    - :filter (String.t): The standard list filter.
+    - :pageSize (integer()): The standard list page size.
+    - :pageToken (String.t): The standard list page token.
+
+  ## Returns
+
+  {:ok, %GoogleApi.AndroidManagement.V1.Model.WebApp{}} on success
+  {:error, info} on failure
+  """
+  @spec androidmanagement_enterprises_web_apps_get(Tesla.Env.client(), String.t(), keyword()) ::
+          {:ok, GoogleApi.AndroidManagement.V1.Model.WebApp.t()} | {:error, Tesla.Env.t()}
+  def androidmanagement_enterprises_web_apps_get(
+        connection,
+        name,
+        optional_params \\ [],
+        opts \\ []
+      ) do
+    optional_params_config = %{
+      :"$.xgafv" => :query,
+      :access_token => :query,
+      :alt => :query,
+      :callback => :query,
+      :fields => :query,
+      :key => :query,
+      :oauth_token => :query,
+      :prettyPrint => :query,
+      :quotaUser => :query,
+      :upload_protocol => :query,
+      :uploadType => :query,
+      :filter => :query,
+      :pageSize => :query,
+      :pageToken => :query
+    }
+
+    request =
+      Request.new()
+      |> Request.method(:get)
+      |> Request.url("/v1/{+name}", %{
+        "name" => URI.encode(name, &URI.char_unreserved?/1)
       })
       |> Request.add_optional_params(optional_params_config, optional_params)
 
