14ce07c3d3279a6f69da5f38514f9aede7ffa529
yoshi-automation <yoshi-automation@google.com>
2019-06-13 01:39:31 -0700

    Regenerate ToolResults client

--- clients/tool_results/lib/google_api/tool_results/v1beta3/model/test_case.ex
+++ clients/tool_results/lib/google_api/tool_results/v1beta3/model/test_case.ex
@@ -27,7 +27,7 @@ defmodule GoogleApi.ToolResults.V1beta3.Model.TestCase do
   - stackTraces ([StackTrace]): The stack trace details if the test case failed or encountered an error.  The maximum size of the stack traces is 100KiB, beyond which the stack track will be truncated.  Zero if the test case passed. Defaults to: `null`.
   - startTime (Timestamp): The start time of the test case.  Optional. Defaults to: `null`.
   - status (String.t): The status of the test case.  Required. Defaults to: `null`.
-    - Enum - one of [error, failed, passed, skipped]
+    - Enum - one of [error, failed, flaky, passed, skipped]
   - testCaseId (String.t): A unique identifier within a Step for this Test Case. Defaults to: `null`.
   - testCaseReference (TestCaseReference): Test case reference, e.g. name, class name and test suite name.  Required. Defaults to: `null`.
   - toolOutputs ([ToolOutputReference]): References to opaque files of any format output by the tool execution. Defaults to: `null`.
