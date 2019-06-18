1651e034972eeef821bf06d023f4c6fff0975fb9
yoshi-automation <yoshi-automation@google.com>
2019-06-18 01:08:39 -0700

    Regenerate CloudResourceManager client

--- clients/cloud_resource_manager/lib/google_api/cloud_resource_manager/v1/model/search_organizations_request.ex
+++ clients/cloud_resource_manager/lib/google_api/cloud_resource_manager/v1/model/search_organizations_request.ex
@@ -28,12 +28,10 @@ defmodule GoogleApi.CloudResourceManager.V1.Model.SearchOrganizationsRequest do
   Organizations may be filtered by `owner.directoryCustomerId` or by
   `domain`, where the domain is a G Suite domain, for example:
 
-  clang-format off
-  | Filter                              | Description                      |
-  |-------------------------------------|----------------------------------|
-  | owner.directorycustomerid:123456789 | Organizations with `owner.directory_customer_id` equal to `123456789`.|
-  | domain:google.com                   | Organizations corresponding to the domain `google.com`.|
-  clang-format on
+  * Filter `owner.directorycustomerid:123456789` returns Organization
+  resources with `owner.directory_customer_id` equal to `123456789`.
+  * Filter `domain:google.com` returns Organization resources corresponding
+  to the domain `google.com`.
 
   This field is optional. Defaults to `nil`.
   - pageSize (integer()): The maximum number of Organizations to return in the response.
