42a465566a6a508696629ac9bd5f6f7278dbd61d
yoshi-automation <yoshi-automation@google.com>
2019-06-24 10:04:41 -0700

    Regenerate AppState client

--- clients/app_state/lib/google_api/app_state/v1/api/states.ex
+++ clients/app_state/lib/google_api/app_state/v1/api/states.ex
@@ -28,24 +28,25 @@ defmodule GoogleApi.AppState.V1.Api.States do
 
   ## Parameters
 
-  - connection (GoogleApi.AppState.V1.Connection): Connection to server
-  - state_key (integer()): The key for the data to be retrieved.
-  - optional_params (keyword()): [optional] Optional parameters
-    - :alt (String.t): Data format for the response.
-    - :fields (String.t): Selector specifying which fields to include in a partial response.
-    - :key (String.t): API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
-    - :oauth_token (String.t): OAuth 2.0 token for the current user.
-    - :prettyPrint (boolean()): Returns response with indentations and line breaks.
-    - :quotaUser (String.t): An opaque string that represents a user for quota purposes. Must not exceed 40 characters.
-    - :userIp (String.t): Deprecated. Please use quotaUser instead.
-    - :currentDataVersion (String.t): The version of the data to be cleared. Version strings are returned by the server.
+  *   `connection` (*type:* `GoogleApi.AppState.V1.Connection.t`) - Connection to server
+  *   `state_key` (*type:* `integer()`) - The key for the data to be retrieved.
+  *   `optional_params` (*type:* `keyword()`) - Optional parameters
+      *   `:alt` (*type:* `String.t`) - Data format for the response.
+      *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
+      *   `:key` (*type:* `String.t`) - API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+      *   `:oauth_token` (*type:* `String.t`) - OAuth 2.0 token for the current user.
+      *   `:prettyPrint` (*type:* `boolean()`) - Returns response with indentations and line breaks.
+      *   `:quotaUser` (*type:* `String.t`) - An opaque string that represents a user for quota purposes. Must not exceed 40 characters.
+      *   `:userIp` (*type:* `String.t`) - Deprecated. Please use quotaUser instead.
+      *   `:currentDataVersion` (*type:* `String.t`) - The version of the data to be cleared. Version strings are returned by the server.
+  *   `opts` (*type:* `keyword()`) - Call options
 
   ## Returns
 
-  {:ok, %GoogleApi.AppState.V1.Model.WriteResult{}} on success
-  {:error, info} on failure
+  *   `{:ok, %GoogleApi.AppState.V1.Model.WriteResult{}}` on success
+  *   `{:error, info}` on failure
   """
-  @spec appstate_states_clear(Tesla.Env.client(), integer(), keyword()) ::
+  @spec appstate_states_clear(Tesla.Env.client(), integer(), keyword(), keyword()) ::
           {:ok, GoogleApi.AppState.V1.Model.WriteResult.t()} | {:error, Tesla.Env.t()}
   def appstate_states_clear(connection, state_key, optional_params \\ [], opts \\ []) do
     optional_params_config = %{
@@ -77,23 +78,24 @@ defmodule GoogleApi.AppState.V1.Api.States do
 
   ## Parameters
 
-  - connection (GoogleApi.AppState.V1.Connection): Connection to server
-  - state_key (integer()): The key for the data to be retrieved.
-  - optional_params (keyword()): [optional] Optional parameters
-    - :alt (String.t): Data format for the response.
-    - :fields (String.t): Selector specifying which fields to include in a partial response.
-    - :key (String.t): API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
-    - :oauth_token (String.t): OAuth 2.0 token for the current user.
-    - :prettyPrint (boolean()): Returns response with indentations and line breaks.
-    - :quotaUser (String.t): An opaque string that represents a user for quota purposes. Must not exceed 40 characters.
-    - :userIp (String.t): Deprecated. Please use quotaUser instead.
+  *   `connection` (*type:* `GoogleApi.AppState.V1.Connection.t`) - Connection to server
+  *   `state_key` (*type:* `integer()`) - The key for the data to be retrieved.
+  *   `optional_params` (*type:* `keyword()`) - Optional parameters
+      *   `:alt` (*type:* `String.t`) - Data format for the response.
+      *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
+      *   `:key` (*type:* `String.t`) - API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+      *   `:oauth_token` (*type:* `String.t`) - OAuth 2.0 token for the current user.
+      *   `:prettyPrint` (*type:* `boolean()`) - Returns response with indentations and line breaks.
+      *   `:quotaUser` (*type:* `String.t`) - An opaque string that represents a user for quota purposes. Must not exceed 40 characters.
+      *   `:userIp` (*type:* `String.t`) - Deprecated. Please use quotaUser instead.
+  *   `opts` (*type:* `keyword()`) - Call options
 
   ## Returns
 
-  {:ok, %{}} on success
-  {:error, info} on failure
+  *   `{:ok, %{}}` on success
+  *   `{:error, info}` on failure
   """
-  @spec appstate_states_delete(Tesla.Env.client(), integer(), keyword()) ::
+  @spec appstate_states_delete(Tesla.Env.client(), integer(), keyword(), keyword()) ::
           {:ok, nil} | {:error, Tesla.Env.t()}
   def appstate_states_delete(connection, state_key, optional_params \\ [], opts \\ []) do
     optional_params_config = %{
@@ -124,23 +126,24 @@ defmodule GoogleApi.AppState.V1.Api.States do
 
   ## Parameters
 
-  - connection (GoogleApi.AppState.V1.Connection): Connection to server
-  - state_key (integer()): The key for the data to be retrieved.
-  - optional_params (keyword()): [optional] Optional parameters
-    - :alt (String.t): Data format for the response.
-    - :fields (String.t): Selector specifying which fields to include in a partial response.
-    - :key (String.t): API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
-    - :oauth_token (String.t): OAuth 2.0 token for the current user.
-    - :prettyPrint (boolean()): Returns response with indentations and line breaks.
-    - :quotaUser (String.t): An opaque string that represents a user for quota purposes. Must not exceed 40 characters.
-    - :userIp (String.t): Deprecated. Please use quotaUser instead.
+  *   `connection` (*type:* `GoogleApi.AppState.V1.Connection.t`) - Connection to server
+  *   `state_key` (*type:* `integer()`) - The key for the data to be retrieved.
+  *   `optional_params` (*type:* `keyword()`) - Optional parameters
+      *   `:alt` (*type:* `String.t`) - Data format for the response.
+      *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
+      *   `:key` (*type:* `String.t`) - API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+      *   `:oauth_token` (*type:* `String.t`) - OAuth 2.0 token for the current user.
+      *   `:prettyPrint` (*type:* `boolean()`) - Returns response with indentations and line breaks.
+      *   `:quotaUser` (*type:* `String.t`) - An opaque string that represents a user for quota purposes. Must not exceed 40 characters.
+      *   `:userIp` (*type:* `String.t`) - Deprecated. Please use quotaUser instead.
+  *   `opts` (*type:* `keyword()`) - Call options
 
   ## Returns
 
-  {:ok, %GoogleApi.AppState.V1.Model.GetResponse{}} on success
-  {:error, info} on failure
+  *   `{:ok, %GoogleApi.AppState.V1.Model.GetResponse{}}` on success
+  *   `{:error, info}` on failure
   """
-  @spec appstate_states_get(Tesla.Env.client(), integer(), keyword()) ::
+  @spec appstate_states_get(Tesla.Env.client(), integer(), keyword(), keyword()) ::
           {:ok, GoogleApi.AppState.V1.Model.GetResponse.t()} | {:error, Tesla.Env.t()}
   def appstate_states_get(connection, state_key, optional_params \\ [], opts \\ []) do
     optional_params_config = %{
@@ -171,23 +174,24 @@ defmodule GoogleApi.AppState.V1.Api.States do
 
   ## Parameters
 
-  - connection (GoogleApi.AppState.V1.Connection): Connection to server
-  - optional_params (keyword()): [optional] Optional parameters
-    - :alt (String.t): Data format for the response.
-    - :fields (String.t): Selector specifying which fields to include in a partial response.
-    - :key (String.t): API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
-    - :oauth_token (String.t): OAuth 2.0 token for the current user.
-    - :prettyPrint (boolean()): Returns response with indentations and line breaks.
-    - :quotaUser (String.t): An opaque string that represents a user for quota purposes. Must not exceed 40 characters.
-    - :userIp (String.t): Deprecated. Please use quotaUser instead.
-    - :includeData (boolean()): Whether to include the full data in addition to the version number
+  *   `connection` (*type:* `GoogleApi.AppState.V1.Connection.t`) - Connection to server
+  *   `optional_params` (*type:* `keyword()`) - Optional parameters
+      *   `:alt` (*type:* `String.t`) - Data format for the response.
+      *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
+      *   `:key` (*type:* `String.t`) - API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+      *   `:oauth_token` (*type:* `String.t`) - OAuth 2.0 token for the current user.
+      *   `:prettyPrint` (*type:* `boolean()`) - Returns response with indentations and line breaks.
+      *   `:quotaUser` (*type:* `String.t`) - An opaque string that represents a user for quota purposes. Must not exceed 40 characters.
+      *   `:userIp` (*type:* `String.t`) - Deprecated. Please use quotaUser instead.
+      *   `:includeData` (*type:* `boolean()`) - Whether to include the full data in addition to the version number
+  *   `opts` (*type:* `keyword()`) - Call options
 
   ## Returns
 
-  {:ok, %GoogleApi.AppState.V1.Model.ListResponse{}} on success
-  {:error, info} on failure
+  *   `{:ok, %GoogleApi.AppState.V1.Model.ListResponse{}}` on success
+  *   `{:error, info}` on failure
   """
-  @spec appstate_states_list(Tesla.Env.client(), keyword()) ::
+  @spec appstate_states_list(Tesla.Env.client(), keyword(), keyword()) ::
           {:ok, GoogleApi.AppState.V1.Model.ListResponse.t()} | {:error, Tesla.Env.t()}
   def appstate_states_list(connection, optional_params \\ [], opts \\ []) do
     optional_params_config = %{
@@ -217,25 +221,26 @@ defmodule GoogleApi.AppState.V1.Api.States do
 
   ## Parameters
 
-  - connection (GoogleApi.AppState.V1.Connection): Connection to server
-  - state_key (integer()): The key for the data to be retrieved.
-  - optional_params (keyword()): [optional] Optional parameters
-    - :alt (String.t): Data format for the response.
-    - :fields (String.t): Selector specifying which fields to include in a partial response.
-    - :key (String.t): API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
-    - :oauth_token (String.t): OAuth 2.0 token for the current user.
-    - :prettyPrint (boolean()): Returns response with indentations and line breaks.
-    - :quotaUser (String.t): An opaque string that represents a user for quota purposes. Must not exceed 40 characters.
-    - :userIp (String.t): Deprecated. Please use quotaUser instead.
-    - :currentStateVersion (String.t): The version of the app state your application is attempting to update. If this does not match the current version, this method will return a conflict error. If there is no data stored on the server for this key, the update will succeed irrespective of the value of this parameter.
-    - :body (GoogleApi.AppState.V1.Model.UpdateRequest.t): 
+  *   `connection` (*type:* `GoogleApi.AppState.V1.Connection.t`) - Connection to server
+  *   `state_key` (*type:* `integer()`) - The key for the data to be retrieved.
+  *   `optional_params` (*type:* `keyword()`) - Optional parameters
+      *   `:alt` (*type:* `String.t`) - Data format for the response.
+      *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
+      *   `:key` (*type:* `String.t`) - API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+      *   `:oauth_token` (*type:* `String.t`) - OAuth 2.0 token for the current user.
+      *   `:prettyPrint` (*type:* `boolean()`) - Returns response with indentations and line breaks.
+      *   `:quotaUser` (*type:* `String.t`) - An opaque string that represents a user for quota purposes. Must not exceed 40 characters.
+      *   `:userIp` (*type:* `String.t`) - Deprecated. Please use quotaUser instead.
+      *   `:currentStateVersion` (*type:* `String.t`) - The version of the app state your application is attempting to update. If this does not match the current version, this method will return a conflict error. If there is no data stored on the server for this key, the update will succeed irrespective of the value of this parameter.
+      *   `:body` (*type:* `GoogleApi.AppState.V1.Model.UpdateRequest.t`) - 
+  *   `opts` (*type:* `keyword()`) - Call options
 
   ## Returns
 
-  {:ok, %GoogleApi.AppState.V1.Model.WriteResult{}} on success
-  {:error, info} on failure
+  *   `{:ok, %GoogleApi.AppState.V1.Model.WriteResult{}}` on success
+  *   `{:error, info}` on failure
   """
-  @spec appstate_states_update(Tesla.Env.client(), integer(), keyword()) ::
+  @spec appstate_states_update(Tesla.Env.client(), integer(), keyword(), keyword()) ::
           {:ok, GoogleApi.AppState.V1.Model.WriteResult.t()} | {:error, Tesla.Env.t()}
   def appstate_states_update(connection, state_key, optional_params \\ [], opts \\ []) do
     optional_params_config = %{
