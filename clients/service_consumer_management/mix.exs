d7f4f41ec41fabb8c17fe7a706bebeb3be214a98
yoshi-automation <yoshi-automation@google.com>
2019-06-26 01:31:29 -0700

    Regenerate ServiceConsumerManagement client

--- clients/service_consumer_management/mix.exs
+++ clients/service_consumer_management/mix.exs
@@ -1,7 +1,7 @@
 defmodule GoogleApi.ServiceConsumerManagement.V1.Mixfile do
   use Mix.Project
 
-  @version "0.3.0"
+  @version "0.4.0"
 
   def project do
     [app: :google_api_service_consumer_management,
