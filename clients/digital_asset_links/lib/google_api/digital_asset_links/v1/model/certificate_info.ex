24dc88145a29a1f016d1ad392b5e8f8ed973e1ef
yoshi-automation <yoshi-automation@google.com>
2019-07-02 01:13:19 -0700

    Regenerate DigitalAssetLinks client

--- clients/digital_asset_links/lib/google_api/digital_asset_links/v1/model/certificate_info.ex
+++ clients/digital_asset_links/lib/google_api/digital_asset_links/v1/model/certificate_info.ex
@@ -25,13 +25,13 @@ defmodule GoogleApi.DigitalAssetLinks.V1.Model.CertificateInfo do
        certificate, it can be acquired like this:
 
           $ keytool -printcert -file $CERTFILE | grep SHA256:
-          SHA256: 14:6D:E9:83:C5:73:06:50:D8:EE:B9:95:2F:34:FC:64:16:A0:83: \
+          SHA256: 14:6D:E9:83:C5:73:06:50:D8:EE:B9:95:2F:34:FC:64:16:A0:83: \\
               42:E6:1D:BE:A8:8A:04:96:B2:3F:CF:44:E5
 
       or like this:
 
           $ openssl x509 -in $CERTFILE -noout -fingerprint -sha256
-          SHA256 Fingerprint=14:6D:E9:83:C5:73:06:50:D8:EE:B9:95:2F:34:FC:64: \
+          SHA256 Fingerprint=14:6D:E9:83:C5:73:06:50:D8:EE:B9:95:2F:34:FC:64: \\
               16:A0:83:42:E6:1D:BE:A8:8A:04:96:B2:3F:CF:44:E5
 
       In this example, the contents of this field would be `14:6D:E9:83:C5:73:
