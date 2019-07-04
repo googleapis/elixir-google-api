4cac6569872483b5809932a58baac1b318db2be8
yoshi-automation <yoshi-automation@google.com>
2019-07-04 01:10:46 -0700

    Regenerate Content client

--- clients/content/lib/google_api/content/v2/model/delivery_time.ex
+++ clients/content/lib/google_api/content/v2/model/delivery_time.ex
@@ -22,32 +22,38 @@ defmodule GoogleApi.Content.V2.Model.DeliveryTime do
   ## Attributes
 
   *   `cutoffTime` (*type:* `GoogleApi.Content.V2.Model.CutoffTime.t`, *default:* `nil`) - Business days cutoff time definition. If not configured the cutoff time will be defaulted to 8AM PST.
+  *   `handlingBusinessDayConfig` (*type:* `GoogleApi.Content.V2.Model.BusinessDayConfig.t`, *default:* `nil`) - The business days during which orders can be handled. If not provided, Monday to Friday business days will be assumed.
   *   `holidayCutoffs` (*type:* `list(GoogleApi.Content.V2.Model.HolidayCutoff.t)`, *default:* `nil`) - Holiday cutoff definitions. If configured, they specify order cutoff times for holiday-specific shipping.
   *   `maxHandlingTimeInDays` (*type:* `integer()`, *default:* `nil`) - Maximum number of business days spent before an order is shipped. 0 means same day shipped, 1 means next day shipped. Must be greater than or equal to minHandlingTimeInDays.
   *   `maxTransitTimeInDays` (*type:* `integer()`, *default:* `nil`) - Maximum number of business days that is spent in transit. 0 means same day delivery, 1 means next day delivery. Must be greater than or equal to minTransitTimeInDays.
   *   `minHandlingTimeInDays` (*type:* `integer()`, *default:* `nil`) - Minimum number of business days spent before an order is shipped. 0 means same day shipped, 1 means next day shipped.
-  *   `minTransitTimeInDays` (*type:* `integer()`, *default:* `nil`) - Minimum number of business days that is spent in transit. 0 means same day delivery, 1 means next day delivery. Either {min,max}transitTimeInDays or transitTimeTable must be set, but not both.
-  *   `transitTimeTable` (*type:* `GoogleApi.Content.V2.Model.TransitTable.t`, *default:* `nil`) - Transit time table, number of business days spent in transit based on row and column dimensions. Either {min,max}transitTimeInDays or transitTimeTable can be set, but not both.
+  *   `minTransitTimeInDays` (*type:* `integer()`, *default:* `nil`) - Minimum number of business days that is spent in transit. 0 means same day delivery, 1 means next day delivery. Either {min,max}TransitTimeInDays or transitTimeTable must be set, but not both.
+  *   `transitBusinessDayConfig` (*type:* `GoogleApi.Content.V2.Model.BusinessDayConfig.t`, *default:* `nil`) - The business days during which orders can be in-transit. If not provided, Monday to Friday business days will be assumed.
+  *   `transitTimeTable` (*type:* `GoogleApi.Content.V2.Model.TransitTable.t`, *default:* `nil`) - Transit time table, number of business days spent in transit based on row and column dimensions. Either {min,max}TransitTimeInDays or transitTimeTable can be set, but not both.
   """
 
   use GoogleApi.Gax.ModelBase
 
   @type t :: %__MODULE__{
           :cutoffTime => GoogleApi.Content.V2.Model.CutoffTime.t(),
+          :handlingBusinessDayConfig => GoogleApi.Content.V2.Model.BusinessDayConfig.t(),
           :holidayCutoffs => list(GoogleApi.Content.V2.Model.HolidayCutoff.t()),
           :maxHandlingTimeInDays => integer(),
           :maxTransitTimeInDays => integer(),
           :minHandlingTimeInDays => integer(),
           :minTransitTimeInDays => integer(),
+          :transitBusinessDayConfig => GoogleApi.Content.V2.Model.BusinessDayConfig.t(),
           :transitTimeTable => GoogleApi.Content.V2.Model.TransitTable.t()
         }
 
   field(:cutoffTime, as: GoogleApi.Content.V2.Model.CutoffTime)
+  field(:handlingBusinessDayConfig, as: GoogleApi.Content.V2.Model.BusinessDayConfig)
   field(:holidayCutoffs, as: GoogleApi.Content.V2.Model.HolidayCutoff, type: :list)
   field(:maxHandlingTimeInDays)
   field(:maxTransitTimeInDays)
   field(:minHandlingTimeInDays)
   field(:minTransitTimeInDays)
+  field(:transitBusinessDayConfig, as: GoogleApi.Content.V2.Model.BusinessDayConfig)
   field(:transitTimeTable, as: GoogleApi.Content.V2.Model.TransitTable)
 end
 
