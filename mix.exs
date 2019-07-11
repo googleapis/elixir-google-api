32848032eee31a5e4a2b57d2e423e155647d1288
Daniel Azuma <dazuma@google.com>
2019-07-11 14:25:11 -0700

    Fix rendering of atoms in parameter documentation

--- mix.exs
+++ mix.exs
@@ -44,7 +44,7 @@ defmodule GoogleApis.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:google_api_discovery, "~> 0.1"},
+      {:google_api_discovery, "~> 0.2.0"},
       {:tesla, "~> 1.0"},
       {:oauth2, "~> 0.9"},
       {:temp, "~> 0.4"},
