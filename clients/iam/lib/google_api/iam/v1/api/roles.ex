a6e67003dc583bc6daf085c2ed0c3580034ab68b
yoshi-automation <yoshi-automation@google.com>
2019-07-13 01:20:58 -0700

    Regenerate IAM client

--- clients/iam/lib/google_api/iam/v1/api/roles.ex
+++ clients/iam/lib/google_api/iam/v1/api/roles.ex
@@ -57,7 +57,7 @@ defmodule GoogleApi.IAM.V1.Api.Roles do
       Note: Wildcard (*) values are invalid; you must specify a complete project
       ID or organization ID.
   *   `optional_params` (*type:* `keyword()`) - Optional parameters
-      *   `:$.xgafv` (*type:* `String.t`) - V1 error format.
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
       *   `:access_token` (*type:* `String.t`) - OAuth access token.
       *   `:alt` (*type:* `String.t`) - Data format for response.
       *   `:callback` (*type:* `String.t`) - JSONP
@@ -112,7 +112,7 @@ defmodule GoogleApi.IAM.V1.Api.Roles do
 
   *   `connection` (*type:* `GoogleApi.IAM.V1.Connection.t`) - Connection to server
   *   `optional_params` (*type:* `keyword()`) - Optional parameters
-      *   `:$.xgafv` (*type:* `String.t`) - V1 error format.
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
       *   `:access_token` (*type:* `String.t`) - OAuth access token.
       *   `:alt` (*type:* `String.t`) - Data format for response.
       *   `:callback` (*type:* `String.t`) - JSONP
@@ -206,7 +206,7 @@ defmodule GoogleApi.IAM.V1.Api.Roles do
 
   *   `connection` (*type:* `GoogleApi.IAM.V1.Connection.t`) - Connection to server
   *   `optional_params` (*type:* `keyword()`) - Optional parameters
-      *   `:$.xgafv` (*type:* `String.t`) - V1 error format.
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
       *   `:access_token` (*type:* `String.t`) - OAuth access token.
       *   `:alt` (*type:* `String.t`) - Data format for response.
       *   `:callback` (*type:* `String.t`) - JSONP
