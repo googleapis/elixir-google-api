0cdbc8bfbe3e644e6a7ac145983b9890950d5b08
yoshi-automation <yoshi-automation@google.com>
2019-07-13 01:27:07 -0700

    Regenerate Poly client

--- clients/poly/lib/google_api/poly/v1/api/assets.ex
+++ clients/poly/lib/google_api/poly/v1/api/assets.ex
@@ -34,7 +34,7 @@ defmodule GoogleApi.Poly.V1.Api.Assets do
   *   `connection` (*type:* `GoogleApi.Poly.V1.Connection.t`) - Connection to server
   *   `name` (*type:* `String.t`) - Required. An asset's name in the form `assets/{ASSET_ID}`.
   *   `optional_params` (*type:* `keyword()`) - Optional parameters
-      *   `:$.xgafv` (*type:* `String.t`) - V1 error format.
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
       *   `:access_token` (*type:* `String.t`) - OAuth access token.
       *   `:alt` (*type:* `String.t`) - Data format for response.
       *   `:callback` (*type:* `String.t`) - JSONP
@@ -91,7 +91,7 @@ defmodule GoogleApi.Poly.V1.Api.Assets do
 
   *   `connection` (*type:* `GoogleApi.Poly.V1.Connection.t`) - Connection to server
   *   `optional_params` (*type:* `keyword()`) - Optional parameters
-      *   `:$.xgafv` (*type:* `String.t`) - V1 error format.
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
       *   `:access_token` (*type:* `String.t`) - OAuth access token.
       *   `:alt` (*type:* `String.t`) - Data format for response.
       *   `:callback` (*type:* `String.t`) - JSONP
--- clients/poly/lib/google_api/poly/v1/api/users.ex
+++ clients/poly/lib/google_api/poly/v1/api/users.ex
@@ -38,7 +38,7 @@ defmodule GoogleApi.Poly.V1.Api.Users do
       currently-authenticated user is supported. To use 'me', you must pass
       an OAuth token with the request.
   *   `optional_params` (*type:* `keyword()`) - Optional parameters
-      *   `:$.xgafv` (*type:* `String.t`) - V1 error format.
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
       *   `:access_token` (*type:* `String.t`) - OAuth access token.
       *   `:alt` (*type:* `String.t`) - Data format for response.
       *   `:callback` (*type:* `String.t`) - JSONP
@@ -118,7 +118,7 @@ defmodule GoogleApi.Poly.V1.Api.Users do
       currently-authenticated user is supported. To use 'me', you must pass
       an OAuth token with the request.
   *   `optional_params` (*type:* `keyword()`) - Optional parameters
-      *   `:$.xgafv` (*type:* `String.t`) - V1 error format.
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
       *   `:access_token` (*type:* `String.t`) - OAuth access token.
       *   `:alt` (*type:* `String.t`) - Data format for response.
       *   `:callback` (*type:* `String.t`) - JSONP
