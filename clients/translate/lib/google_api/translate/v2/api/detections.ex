7cd0aa3f62f4b6400f7b2f667c0223eadded4ef3
yoshi-automation <yoshi-automation@google.com>
2019-07-13 01:37:37 -0700

    Regenerate Translate client

--- clients/translate/lib/google_api/translate/v2/api/detections.ex
+++ clients/translate/lib/google_api/translate/v2/api/detections.ex
@@ -30,7 +30,7 @@ defmodule GoogleApi.Translate.V2.Api.Detections do
 
   *   `connection` (*type:* `GoogleApi.Translate.V2.Connection.t`) - Connection to server
   *   `optional_params` (*type:* `keyword()`) - Optional parameters
-      *   `:$.xgafv` (*type:* `String.t`) - V1 error format.
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
       *   `:access_token` (*type:* `String.t`) - OAuth access token.
       *   `:alt` (*type:* `String.t`) - Data format for response.
       *   `:bearer_token` (*type:* `String.t`) - OAuth bearer token.
@@ -93,7 +93,7 @@ defmodule GoogleApi.Translate.V2.Api.Detections do
   *   `q` (*type:* `list(String.t)`) - The input text upon which to perform language detection. Repeat this
       parameter to perform language detection on multiple text inputs.
   *   `optional_params` (*type:* `keyword()`) - Optional parameters
-      *   `:$.xgafv` (*type:* `String.t`) - V1 error format.
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
       *   `:access_token` (*type:* `String.t`) - OAuth access token.
       *   `:alt` (*type:* `String.t`) - Data format for response.
       *   `:bearer_token` (*type:* `String.t`) - OAuth bearer token.
--- clients/translate/lib/google_api/translate/v2/api/translations.ex
+++ clients/translate/lib/google_api/translate/v2/api/translations.ex
@@ -34,7 +34,7 @@ defmodule GoogleApi.Translate.V2.Api.Translations do
   *   `target` (*type:* `String.t`) - The language to use for translation of the input text, set to one of the
       language codes listed in Language Support.
   *   `optional_params` (*type:* `keyword()`) - Optional parameters
-      *   `:$.xgafv` (*type:* `String.t`) - V1 error format.
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
       *   `:access_token` (*type:* `String.t`) - OAuth access token.
       *   `:alt` (*type:* `String.t`) - Data format for response.
       *   `:bearer_token` (*type:* `String.t`) - OAuth bearer token.
@@ -116,7 +116,7 @@ defmodule GoogleApi.Translate.V2.Api.Translations do
 
   *   `connection` (*type:* `GoogleApi.Translate.V2.Connection.t`) - Connection to server
   *   `optional_params` (*type:* `keyword()`) - Optional parameters
-      *   `:$.xgafv` (*type:* `String.t`) - V1 error format.
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
       *   `:access_token` (*type:* `String.t`) - OAuth access token.
       *   `:alt` (*type:* `String.t`) - Data format for response.
       *   `:bearer_token` (*type:* `String.t`) - OAuth bearer token.
