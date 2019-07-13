2180e84290371cf74109b5bb0506eb58658bd942
yoshi-automation <yoshi-automation@google.com>
2019-07-13 01:21:53 -0700

    Regenerate Indexing client

--- clients/indexing/lib/google_api/indexing/v3/api/url_notifications.ex
+++ clients/indexing/lib/google_api/indexing/v3/api/url_notifications.ex
@@ -32,7 +32,7 @@ defmodule GoogleApi.Indexing.V3.Api.UrlNotifications do
 
   *   `connection` (*type:* `GoogleApi.Indexing.V3.Connection.t`) - Connection to server
   *   `optional_params` (*type:* `keyword()`) - Optional parameters
-      *   `:$.xgafv` (*type:* `String.t`) - V1 error format.
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
       *   `:access_token` (*type:* `String.t`) - OAuth access token.
       *   `:alt` (*type:* `String.t`) - Data format for response.
       *   `:callback` (*type:* `String.t`) - JSONP
@@ -87,7 +87,7 @@ defmodule GoogleApi.Indexing.V3.Api.UrlNotifications do
 
   *   `connection` (*type:* `GoogleApi.Indexing.V3.Connection.t`) - Connection to server
   *   `optional_params` (*type:* `keyword()`) - Optional parameters
-      *   `:$.xgafv` (*type:* `String.t`) - V1 error format.
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
       *   `:access_token` (*type:* `String.t`) - OAuth access token.
       *   `:alt` (*type:* `String.t`) - Data format for response.
       *   `:callback` (*type:* `String.t`) - JSONP
