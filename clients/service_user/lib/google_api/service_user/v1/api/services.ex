f9e95572b3da78560e3be1e309430ae0918a9cb2
yoshi-automation <yoshi-automation@google.com>
2019-07-13 01:32:04 -0700

    Regenerate ServiceUser client

--- clients/service_user/lib/google_api/service_user/v1/api/services.ex
+++ clients/service_user/lib/google_api/service_user/v1/api/services.ex
@@ -34,7 +34,7 @@ defmodule GoogleApi.ServiceUser.V1.Api.Services do
 
   *   `connection` (*type:* `GoogleApi.ServiceUser.V1.Connection.t`) - Connection to server
   *   `optional_params` (*type:* `keyword()`) - Optional parameters
-      *   `:$.xgafv` (*type:* `String.t`) - V1 error format.
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
       *   `:access_token` (*type:* `String.t`) - OAuth access token.
       *   `:alt` (*type:* `String.t`) - Data format for response.
       *   `:callback` (*type:* `String.t`) - JSONP
