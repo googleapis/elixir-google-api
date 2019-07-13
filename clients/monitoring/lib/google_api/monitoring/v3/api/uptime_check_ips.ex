1e470b4751604d77782a41a5868d4753ed078984
yoshi-automation <yoshi-automation@google.com>
2019-07-13 01:24:49 -0700

    Regenerate Monitoring client

--- clients/monitoring/lib/google_api/monitoring/v3/api/uptime_check_ips.ex
+++ clients/monitoring/lib/google_api/monitoring/v3/api/uptime_check_ips.ex
@@ -30,7 +30,7 @@ defmodule GoogleApi.Monitoring.V3.Api.UptimeCheckIps do
 
   *   `connection` (*type:* `GoogleApi.Monitoring.V3.Connection.t`) - Connection to server
   *   `optional_params` (*type:* `keyword()`) - Optional parameters
-      *   `:$.xgafv` (*type:* `String.t`) - V1 error format.
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
       *   `:access_token` (*type:* `String.t`) - OAuth access token.
       *   `:alt` (*type:* `String.t`) - Data format for response.
       *   `:callback` (*type:* `String.t`) - JSONP
