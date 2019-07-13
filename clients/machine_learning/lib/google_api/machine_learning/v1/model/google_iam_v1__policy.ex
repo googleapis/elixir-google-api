9665ef63cf8c74bfad484f5d8ad1c7f22afcc10c
yoshi-automation <yoshi-automation@google.com>
2019-07-13 01:23:52 -0700

    Regenerate MachineLearning client

--- clients/machine_learning/lib/google_api/machine_learning/v1/model/google_iam_v1__policy.ex
+++ clients/machine_learning/lib/google_api/machine_learning/v1/model/google_iam_v1__policy.ex
@@ -77,7 +77,7 @@ defmodule GoogleApi.MachineLearning.V1.Model.GoogleIamV1_Policy do
       ensure that their change will be applied to the same version of the policy.
 
       If no `etag` is provided in the call to `setIamPolicy`, then the existing
-      policy is overwritten blindly.
+      policy is overwritten.
   *   `version` (*type:* `integer()`, *default:* `nil`) - Deprecated.
   """
 
