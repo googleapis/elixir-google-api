5232639447dc5675a33d5cfa97e786bd763a826f
yoshi-automation <yoshi-automation@google.com>
2019-07-16 01:26:59 -0700

    Regenerate Slides client

--- clients/slides/lib/google_api/slides/v1/model/page_properties.ex
+++ clients/slides/lib/google_api/slides/v1/model/page_properties.ex
@@ -31,10 +31,10 @@ defmodule GoogleApi.Slides.V1.Model.PageProperties do
       Slides color scheme, matching the defaults in the Slides editor.
 
       Only the concrete colors of the first 12 ThemeColorTypes are editable. In addition, only
-      the color scheme on Master
-      pages can be updated. To update the field, a color scheme containing
-      mappings from all the first 12 ThemeColorTypes to their concrete colors
-      must be provided. Colors for the remaining ThemeColorTypes will be ignored.
+      the color scheme on `Master` pages can be updated. To update the field, a
+      color scheme containing mappings from all the first 12 ThemeColorTypes to
+      their concrete colors must be provided. Colors for the remaining
+      ThemeColorTypes will be ignored.
   *   `pageBackgroundFill` (*type:* `GoogleApi.Slides.V1.Model.PageBackgroundFill.t`, *default:* `nil`) - The background fill of the page. If unset, the background fill is inherited
       from a parent page if it exists. If the page has no parent, then the
       background fill defaults to the corresponding fill in the Slides editor.
