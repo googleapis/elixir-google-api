d5e87cc52e0a39eaea3b303cf6e31abf44a10b70
yoshi-automation <yoshi-automation@google.com>
2019-07-19 01:22:36 -0700

    Regenerate People client

--- clients/people/lib/google_api/people/v1/api/people.ex
+++ clients/people/lib/google_api/people/v1/api/people.ex
@@ -137,6 +137,98 @@ defmodule GoogleApi.People.V1.Api.People do
   end
 
   @doc """
+  Delete a contact's photo.
+
+  ## Parameters
+
+  *   `connection` (*type:* `GoogleApi.People.V1.Connection.t`) - Connection to server
+  *   `people_id` (*type:* `String.t`) - Part of `resourceName`. The resource name of the contact whose photo will be deleted.
+  *   `optional_params` (*type:* `keyword()`) - Optional parameters
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
+      *   `:access_token` (*type:* `String.t`) - OAuth access token.
+      *   `:alt` (*type:* `String.t`) - Data format for response.
+      *   `:callback` (*type:* `String.t`) - JSONP
+      *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
+      *   `:key` (*type:* `String.t`) - API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+      *   `:oauth_token` (*type:* `String.t`) - OAuth 2.0 token for the current user.
+      *   `:prettyPrint` (*type:* `boolean()`) - Returns response with indentations and line breaks.
+      *   `:quotaUser` (*type:* `String.t`) - Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
+      *   `:uploadType` (*type:* `String.t`) - Legacy upload protocol for media (e.g. "media", "multipart").
+      *   `:upload_protocol` (*type:* `String.t`) - Upload protocol for media (e.g. "raw", "multipart").
+      *   `:personFields` (*type:* `String.t`) - **Optional.** Not specifying any fields will skip the post mutate read.
+          A field mask to restrict which fields on the person are
+          returned. Multiple fields can be specified by separating them with commas.
+          Valid values are:
+
+          * addresses
+          * ageRanges
+          * biographies
+          * birthdays
+          * braggingRights
+          * coverPhotos
+          * emailAddresses
+          * events
+          * genders
+          * imClients
+          * interests
+          * locales
+          * memberships
+          * metadata
+          * names
+          * nicknames
+          * occupations
+          * organizations
+          * phoneNumbers
+          * photos
+          * relations
+          * relationshipInterests
+          * relationshipStatuses
+          * residences
+          * sipAddresses
+          * skills
+          * taglines
+          * urls
+          * userDefined
+  *   `opts` (*type:* `keyword()`) - Call options
+
+  ## Returns
+
+  *   `{:ok, %GoogleApi.People.V1.Model.DeleteContactPhotoResponse{}}` on success
+  *   `{:error, info}` on failure
+  """
+  @spec people_people_delete_contact_photo(Tesla.Env.client(), String.t(), keyword(), keyword()) ::
+          {:ok, GoogleApi.People.V1.Model.DeleteContactPhotoResponse.t()}
+          | {:error, Tesla.Env.t()}
+  def people_people_delete_contact_photo(connection, people_id, optional_params \\ [], opts \\ []) do
+    optional_params_config = %{
+      :"$.xgafv" => :query,
+      :access_token => :query,
+      :alt => :query,
+      :callback => :query,
+      :fields => :query,
+      :key => :query,
+      :oauth_token => :query,
+      :prettyPrint => :query,
+      :quotaUser => :query,
+      :uploadType => :query,
+      :upload_protocol => :query,
+      :personFields => :query
+    }
+
+    request =
+      Request.new()
+      |> Request.method(:delete)
+      |> Request.url("/v1/people/{peopleId}:deleteContactPhoto", %{
+        "peopleId" => URI.encode(people_id, &URI.char_unreserved?/1)
+      })
+      |> Request.add_optional_params(optional_params_config, optional_params)
+
+    connection
+    |> Connection.execute(request)
+    |> Response.decode(opts ++ [struct: %GoogleApi.People.V1.Model.DeleteContactPhotoResponse{}])
+  end
+
+  @doc """
   Provides information about a person by specifying a resource name. Use
   `people/me` to indicate the authenticated user.
   <br>
@@ -440,6 +532,65 @@ defmodule GoogleApi.People.V1.Api.People do
     connection
     |> Connection.execute(request)
     |> Response.decode(opts ++ [struct: %GoogleApi.People.V1.Model.Person{}])
+  end
+
+  @doc """
+  Update a contact's photo.
+
+  ## Parameters
+
+  *   `connection` (*type:* `GoogleApi.People.V1.Connection.t`) - Connection to server
+  *   `people_id` (*type:* `String.t`) - Part of `resourceName`. Person resource name
+  *   `optional_params` (*type:* `keyword()`) - Optional parameters
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
+      *   `:access_token` (*type:* `String.t`) - OAuth access token.
+      *   `:alt` (*type:* `String.t`) - Data format for response.
+      *   `:callback` (*type:* `String.t`) - JSONP
+      *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
+      *   `:key` (*type:* `String.t`) - API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+      *   `:oauth_token` (*type:* `String.t`) - OAuth 2.0 token for the current user.
+      *   `:prettyPrint` (*type:* `boolean()`) - Returns response with indentations and line breaks.
+      *   `:quotaUser` (*type:* `String.t`) - Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
+      *   `:uploadType` (*type:* `String.t`) - Legacy upload protocol for media (e.g. "media", "multipart").
+      *   `:upload_protocol` (*type:* `String.t`) - Upload protocol for media (e.g. "raw", "multipart").
+      *   `:body` (*type:* `GoogleApi.People.V1.Model.UpdateContactPhotoRequest.t`) - 
+  *   `opts` (*type:* `keyword()`) - Call options
+
+  ## Returns
+
+  *   `{:ok, %GoogleApi.People.V1.Model.UpdateContactPhotoResponse{}}` on success
+  *   `{:error, info}` on failure
+  """
+  @spec people_people_update_contact_photo(Tesla.Env.client(), String.t(), keyword(), keyword()) ::
+          {:ok, GoogleApi.People.V1.Model.UpdateContactPhotoResponse.t()}
+          | {:error, Tesla.Env.t()}
+  def people_people_update_contact_photo(connection, people_id, optional_params \\ [], opts \\ []) do
+    optional_params_config = %{
+      :"$.xgafv" => :query,
+      :access_token => :query,
+      :alt => :query,
+      :callback => :query,
+      :fields => :query,
+      :key => :query,
+      :oauth_token => :query,
+      :prettyPrint => :query,
+      :quotaUser => :query,
+      :uploadType => :query,
+      :upload_protocol => :query,
+      :body => :body
+    }
+
+    request =
+      Request.new()
+      |> Request.method(:patch)
+      |> Request.url("/v1/people/{peopleId}:updateContactPhoto", %{
+        "peopleId" => URI.encode(people_id, &URI.char_unreserved?/1)
+      })
+      |> Request.add_optional_params(optional_params_config, optional_params)
+
+    connection
+    |> Connection.execute(request)
+    |> Response.decode(opts ++ [struct: %GoogleApi.People.V1.Model.UpdateContactPhotoResponse{}])
   end
 
   @doc """
