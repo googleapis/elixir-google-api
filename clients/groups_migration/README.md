62d5019b731ece425e50ac6ade647276a007f840
yoshi-automation <yoshi-automation@google.com>
2019-08-14 11:19:33 -0700

    Regenerate GroupsMigration client

--- clients/groups_migration/README.md
+++ clients/groups_migration/README.md
@@ -1,18 +1,23 @@
-# GoogleApi.GroupsMigration.V1
+# GoogleApi.GroupsMigration
+
+Client library for Groups Migration API from Google.
 
 Groups Migration Api.
 
 ## Installation
 
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `google_api_groups_migration` to your list of dependencies in `mix.exs`:
+Install this package from [Hex](https://hex.pm) by adding
+`google_api_groups_migration` to your list of dependencies in `mix.exs`:
 
 ```elixir
 def deps do
-  [{:google_api_groups_migration, "~> 0.0.1"}]
+  [{:google_api_groups_migration, "~> 0.3"}]
 end
 ```
 
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at [https://hexdocs.pm/google_api_groups_migration](https://hexdocs.pm/google_api_groups_migration).
+## For more information
+
+Product documentation is available at [https://developers.google.com/google-apps/groups-migration/](https://developers.google.com/google-apps/groups-migration/).
+
+Library reference documentation is published on Hexdocs at
+[https://hexdocs.pm/google_api_groups_migration](https://hexdocs.pm/google_api_groups_migration).
--- clients/groups_migration/lib/google_api/groups_migration/v1/api/archive.ex
+++ clients/groups_migration/lib/google_api/groups_migration/v1/api/archive.ex
@@ -23,6 +23,8 @@ defmodule GoogleApi.GroupsMigration.V1.Api.Archive do
   alias GoogleApi.GroupsMigration.V1.Connection
   alias GoogleApi.Gax.{Request, Response}
 
+  @library_version Mix.Project.config() |> Keyword.get(:version, "")
+
   @doc """
   Inserts a new mail into the archive of the Google group.
 
@@ -65,6 +67,7 @@ defmodule GoogleApi.GroupsMigration.V1.Api.Archive do
         "groupId" => URI.encode(group_id, &URI.char_unreserved?/1)
       })
       |> Request.add_optional_params(optional_params_config, optional_params)
+      |> Request.library_version(@library_version)
 
     connection
     |> Connection.execute(request)
@@ -126,6 +129,7 @@ defmodule GoogleApi.GroupsMigration.V1.Api.Archive do
       })
       |> Request.add_param(:query, :uploadType, upload_type)
       |> Request.add_optional_params(optional_params_config, optional_params)
+      |> Request.library_version(@library_version)
 
     connection
     |> Connection.execute(request)
@@ -195,6 +199,7 @@ defmodule GoogleApi.GroupsMigration.V1.Api.Archive do
       |> Request.add_param(:body, :metadata, metadata)
       |> Request.add_param(:file, :data, data)
       |> Request.add_optional_params(optional_params_config, optional_params)
+      |> Request.library_version(@library_version)
 
     connection
     |> Connection.execute(request)
