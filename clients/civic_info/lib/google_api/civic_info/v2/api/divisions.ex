5f6603a941bff56b15f075809588b3396572adf7
yoshi-automation <yoshi-automation@google.com>
2019-08-14 11:04:27 -0700

    Regenerate CivicInfo client

--- clients/civic_info/lib/google_api/civic_info/v2/api/divisions.ex
+++ clients/civic_info/lib/google_api/civic_info/v2/api/divisions.ex
@@ -23,6 +23,8 @@ defmodule GoogleApi.CivicInfo.V2.Api.Divisions do
   alias GoogleApi.CivicInfo.V2.Connection
   alias GoogleApi.Gax.{Request, Response}
 
+  @library_version Mix.Project.config() |> Keyword.get(:version, "")
+
   @doc """
   Searches for political divisions by their natural name or OCD ID.
 
@@ -66,6 +68,7 @@ defmodule GoogleApi.CivicInfo.V2.Api.Divisions do
       |> Request.method(:get)
       |> Request.url("/divisions", %{})
       |> Request.add_optional_params(optional_params_config, optional_params)
+      |> Request.library_version(@library_version)
 
     connection
     |> Connection.execute(request)
