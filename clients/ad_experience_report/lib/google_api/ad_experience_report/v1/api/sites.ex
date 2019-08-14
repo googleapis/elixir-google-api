630e943a914c82905024593b3f5dcf56cacb97f8
yoshi-automation <yoshi-automation@google.com>
2019-08-13 17:01:37 -0700

    Regenerate AdExperienceReport client

--- clients/ad_experience_report/lib/google_api/ad_experience_report/v1/api/sites.ex
+++ clients/ad_experience_report/lib/google_api/ad_experience_report/v1/api/sites.ex
@@ -23,6 +23,8 @@ defmodule GoogleApi.AdExperienceReport.V1.Api.Sites do
   alias GoogleApi.AdExperienceReport.V1.Connection
   alias GoogleApi.Gax.{Request, Response}
 
+  @library_version Mix.Project.config() |> Keyword.get(:version, "")
+
   @doc """
   Gets a summary of the ad experience rating of a site.
 
@@ -80,6 +82,7 @@ defmodule GoogleApi.AdExperienceReport.V1.Api.Sites do
         "sitesId" => URI.encode(sites_id, &URI.char_unreserved?/1)
       })
       |> Request.add_optional_params(optional_params_config, optional_params)
+      |> Request.library_version(@library_version)
 
     connection
     |> Connection.execute(request)
