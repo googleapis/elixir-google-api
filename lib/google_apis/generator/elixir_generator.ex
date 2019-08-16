768b04b2c0f2b0cb7699096bb4711b8be36ef9e2
Daniel Azuma <dazuma@google.com>
2019-08-16 12:45:34 -0700

    fix(generator): Limit hex description field to 299 characters (#2439)

--- lib/google_apis/generator/elixir_generator.ex
+++ lib/google_apis/generator/elixir_generator.ex
@@ -138,9 +138,8 @@ defmodule GoogleApis.Generator.ElixirGenerator do
     path = Path.join(token.root_dir, "mix.exs")
     IO.puts("Writing mix.exs")
 
-    api_title = api_title_for(token.rest_description)
+    description = generate_hex_description(token.rest_description)
     docs_link = docs_link_for(token.rest_description)
-    description = description_for(token.rest_description)
     version = version_from_mix_exs(path)
 
     File.write!(
@@ -148,7 +147,6 @@ defmodule GoogleApis.Generator.ElixirGenerator do
       Renderer.mix_exs(
         token.root_namespace,
         token.library_name,
-        api_title,
         docs_link,
         version,
         description
@@ -182,6 +180,18 @@ defmodule GoogleApis.Generator.ElixirGenerator do
     )
 
     token
+  end
+
+  defp generate_hex_description(rest_info) do
+    api_title = api_title_for(rest_info)
+    description_start = "#{api_title} client library."
+    rest_description = description_for(rest_info)
+
+    if String.length(description_start) + String.length(rest_description) > 298 do
+      description_start
+    else
+      "#{description_start} #{rest_description}"
+    end
   end
 
   defp scopes_for(%{auth: nil}), do: []
