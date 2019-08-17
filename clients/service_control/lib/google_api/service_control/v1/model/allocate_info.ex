bbf094e2ebe2c7fbbc9159411a3cfdef9da24802
yoshi-automation <yoshi-automation@google.com>
2019-08-17 05:24:08 -0700

    Regenerate ServiceControl client

--- clients/service_control/lib/google_api/service_control/v1/model/allocate_info.ex
+++ clients/service_control/lib/google_api/service_control/v1/model/allocate_info.ex
@@ -24,7 +24,8 @@ defmodule GoogleApi.ServiceControl.V1.Model.AllocateInfo do
   *   `unusedArguments` (*type:* `list(String.t)`, *default:* `nil`) - A list of label keys that were unused by the server in processing the
       request. Thus, for similar requests repeated in a certain future time
       window, the caller can choose to ignore these labels in the requests
-      to achieve better client-side cache hits and quota aggregation.
+      to achieve better client-side cache hits and quota aggregation for rate
+      quota. This field is not populated for allocation quota checks.
   """
 
   use GoogleApi.Gax.ModelBase
