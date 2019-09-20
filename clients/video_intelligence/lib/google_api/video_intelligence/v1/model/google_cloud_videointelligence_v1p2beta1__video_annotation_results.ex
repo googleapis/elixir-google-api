eb1fb7b96d4a1fc4e43ee90906b7dcdc9ac2f237
yoshi-automation <yoshi-automation@google.com>
2019-09-20 05:29:00 -0700

    Regenerate VideoIntelligence client

--- clients/video_intelligence/lib/google_api/video_intelligence/v1/model/google_cloud_videointelligence_v1p2beta1__video_annotation_results.ex
+++ clients/video_intelligence/lib/google_api/video_intelligence/v1/model/google_cloud_videointelligence_v1p2beta1__video_annotation_results.ex
@@ -33,12 +33,19 @@ defmodule GoogleApi.VideoIntelligence.V1.Model.GoogleCloudVideointelligenceV1p2b
   *   `segmentLabelAnnotations` (*type:* `list(GoogleApi.VideoIntelligence.V1.Model.GoogleCloudVideointelligenceV1p2beta1_LabelAnnotation.t)`, *default:* `nil`) - Topical label annotations on video level or user specified segment level.
       There is exactly one element for each unique label.
   *   `segmentPresenceLabelAnnotations` (*type:* `list(GoogleApi.VideoIntelligence.V1.Model.GoogleCloudVideointelligenceV1p2beta1_LabelAnnotation.t)`, *default:* `nil`) - Presence label annotations on video level or user specified segment level.
-      There is exactly one element for each unique label.
+      There is exactly one element for each unique label. Compared to the
+      existing topical `segment_label_annotations`, this field presents more
+      fine-grained, segment-level labels detected in video content and is made
+      available only when the client sets `LabelDetectionConfig.model` to
+      "builtin/latest" in the request.
   *   `shotAnnotations` (*type:* `list(GoogleApi.VideoIntelligence.V1.Model.GoogleCloudVideointelligenceV1p2beta1_VideoSegment.t)`, *default:* `nil`) - Shot annotations. Each shot is represented as a video segment.
   *   `shotLabelAnnotations` (*type:* `list(GoogleApi.VideoIntelligence.V1.Model.GoogleCloudVideointelligenceV1p2beta1_LabelAnnotation.t)`, *default:* `nil`) - Topical label annotations on shot level.
       There is exactly one element for each unique label.
   *   `shotPresenceLabelAnnotations` (*type:* `list(GoogleApi.VideoIntelligence.V1.Model.GoogleCloudVideointelligenceV1p2beta1_LabelAnnotation.t)`, *default:* `nil`) - Presence label annotations on shot level. There is exactly one element for
-      each unique label.
+      each unique label. Compared to the existing topical
+      `shot_label_annotations`, this field presents more fine-grained, shot-level
+      labels detected in video content and is made available only when the client
+      sets `LabelDetectionConfig.model` to "builtin/latest" in the request.
   *   `speechTranscriptions` (*type:* `list(GoogleApi.VideoIntelligence.V1.Model.GoogleCloudVideointelligenceV1p2beta1_SpeechTranscription.t)`, *default:* `nil`) - Speech transcription.
   *   `textAnnotations` (*type:* `list(GoogleApi.VideoIntelligence.V1.Model.GoogleCloudVideointelligenceV1p2beta1_TextAnnotation.t)`, *default:* `nil`) - OCR text detection and tracking.
       Annotations for list of detected text snippets. Each will have list of
