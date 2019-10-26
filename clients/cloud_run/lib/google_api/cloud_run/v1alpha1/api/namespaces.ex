15962221e71bc0bee5ff44b171d46e30070668a2
yoshi-automation <yoshi-automation@google.com>
2019-10-26 05:07:39 -0700

    Regenerate CloudRun client

--- clients/cloud_run/lib/google_api/cloud_run/v1alpha1/api/namespaces.ex
+++ clients/cloud_run/lib/google_api/cloud_run/v1alpha1/api/namespaces.ex
@@ -741,6 +741,390 @@ defmodule GoogleApi.CloudRun.V1alpha1.Api.Namespaces do
   end
 
   @doc """
+  Creates a new pubsub.
+
+  ## Parameters
+
+  *   `connection` (*type:* `GoogleApi.CloudRun.V1alpha1.Connection.t`) - Connection to server
+  *   `namespaces_id` (*type:* `String.t`) - Part of `parent`. The project ID or project number in which this pubsub should
+      be created.
+  *   `optional_params` (*type:* `keyword()`) - Optional parameters
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
+      *   `:access_token` (*type:* `String.t`) - OAuth access token.
+      *   `:alt` (*type:* `String.t`) - Data format for response.
+      *   `:callback` (*type:* `String.t`) - JSONP
+      *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
+      *   `:key` (*type:* `String.t`) - API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+      *   `:oauth_token` (*type:* `String.t`) - OAuth 2.0 token for the current user.
+      *   `:prettyPrint` (*type:* `boolean()`) - Returns response with indentations and line breaks.
+      *   `:quotaUser` (*type:* `String.t`) - Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
+      *   `:uploadType` (*type:* `String.t`) - Legacy upload protocol for media (e.g. "media", "multipart").
+      *   `:upload_protocol` (*type:* `String.t`) - Upload protocol for media (e.g. "raw", "multipart").
+      *   `:body` (*type:* `GoogleApi.CloudRun.V1alpha1.Model.PubSub.t`) - 
+  *   `opts` (*type:* `keyword()`) - Call options
+
+  ## Returns
+
+  *   `{:ok, %GoogleApi.CloudRun.V1alpha1.Model.PubSub{}}` on success
+  *   `{:error, info}` on failure
+  """
+  @spec run_namespaces_pubsubs_create(Tesla.Env.client(), String.t(), keyword(), keyword()) ::
+          {:ok, GoogleApi.CloudRun.V1alpha1.Model.PubSub.t()} | {:error, Tesla.Env.t()}
+  def run_namespaces_pubsubs_create(connection, namespaces_id, optional_params \\ [], opts \\ []) do
+    optional_params_config = %{
+      :"$.xgafv" => :query,
+      :access_token => :query,
+      :alt => :query,
+      :callback => :query,
+      :fields => :query,
+      :key => :query,
+      :oauth_token => :query,
+      :prettyPrint => :query,
+      :quotaUser => :query,
+      :uploadType => :query,
+      :upload_protocol => :query,
+      :body => :body
+    }
+
+    request =
+      Request.new()
+      |> Request.method(:post)
+      |> Request.url(
+        "/apis/events.cloud.google.com/v1alpha1/namespaces/{namespacesId}/pubsubs",
+        %{
+          "namespacesId" => URI.encode(namespaces_id, &URI.char_unreserved?/1)
+        }
+      )
+      |> Request.add_optional_params(optional_params_config, optional_params)
+      |> Request.library_version(@library_version)
+
+    connection
+    |> Connection.execute(request)
+    |> Response.decode(opts ++ [struct: %GoogleApi.CloudRun.V1alpha1.Model.PubSub{}])
+  end
+
+  @doc """
+  Rpc to delete a pubsub.
+
+  ## Parameters
+
+  *   `connection` (*type:* `GoogleApi.CloudRun.V1alpha1.Connection.t`) - Connection to server
+  *   `namespaces_id` (*type:* `String.t`) - Part of `name`. The name of the pubsub being deleted. If needed, replace
+      {namespace_id} with the project ID.
+  *   `pubsubs_id` (*type:* `String.t`) - Part of `name`. See documentation of `namespacesId`.
+  *   `optional_params` (*type:* `keyword()`) - Optional parameters
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
+      *   `:access_token` (*type:* `String.t`) - OAuth access token.
+      *   `:alt` (*type:* `String.t`) - Data format for response.
+      *   `:callback` (*type:* `String.t`) - JSONP
+      *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
+      *   `:key` (*type:* `String.t`) - API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+      *   `:oauth_token` (*type:* `String.t`) - OAuth 2.0 token for the current user.
+      *   `:prettyPrint` (*type:* `boolean()`) - Returns response with indentations and line breaks.
+      *   `:quotaUser` (*type:* `String.t`) - Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
+      *   `:uploadType` (*type:* `String.t`) - Legacy upload protocol for media (e.g. "media", "multipart").
+      *   `:upload_protocol` (*type:* `String.t`) - Upload protocol for media (e.g. "raw", "multipart").
+      *   `:apiVersion` (*type:* `String.t`) - Cloud Run currently ignores this parameter.
+      *   `:kind` (*type:* `String.t`) - Cloud Run currently ignores this parameter.
+      *   `:propagationPolicy` (*type:* `String.t`) - Specifies the propagation policy of delete. Cloud Run currently ignores
+          this setting, and deletes in the background. Please see
+          kubernetes.io/docs/concepts/workloads/controllers/garbage-collection/ for
+          more information.
+  *   `opts` (*type:* `keyword()`) - Call options
+
+  ## Returns
+
+  *   `{:ok, %GoogleApi.CloudRun.V1alpha1.Model.Empty{}}` on success
+  *   `{:error, info}` on failure
+  """
+  @spec run_namespaces_pubsubs_delete(
+          Tesla.Env.client(),
+          String.t(),
+          String.t(),
+          keyword(),
+          keyword()
+        ) :: {:ok, GoogleApi.CloudRun.V1alpha1.Model.Empty.t()} | {:error, Tesla.Env.t()}
+  def run_namespaces_pubsubs_delete(
+        connection,
+        namespaces_id,
+        pubsubs_id,
+        optional_params \\ [],
+        opts \\ []
+      ) do
+    optional_params_config = %{
+      :"$.xgafv" => :query,
+      :access_token => :query,
+      :alt => :query,
+      :callback => :query,
+      :fields => :query,
+      :key => :query,
+      :oauth_token => :query,
+      :prettyPrint => :query,
+      :quotaUser => :query,
+      :uploadType => :query,
+      :upload_protocol => :query,
+      :apiVersion => :query,
+      :kind => :query,
+      :propagationPolicy => :query
+    }
+
+    request =
+      Request.new()
+      |> Request.method(:delete)
+      |> Request.url(
+        "/apis/events.cloud.google.com/v1alpha1/namespaces/{namespacesId}/pubsubs/{pubsubsId}",
+        %{
+          "namespacesId" => URI.encode(namespaces_id, &URI.char_unreserved?/1),
+          "pubsubsId" => URI.encode(pubsubs_id, &URI.char_unreserved?/1)
+        }
+      )
+      |> Request.add_optional_params(optional_params_config, optional_params)
+      |> Request.library_version(@library_version)
+
+    connection
+    |> Connection.execute(request)
+    |> Response.decode(opts ++ [struct: %GoogleApi.CloudRun.V1alpha1.Model.Empty{}])
+  end
+
+  @doc """
+  Rpc to get information about a pubsub.
+
+  ## Parameters
+
+  *   `connection` (*type:* `GoogleApi.CloudRun.V1alpha1.Connection.t`) - Connection to server
+  *   `namespaces_id` (*type:* `String.t`) - Part of `name`. The name of the pubsub being retrieved. If needed, replace
+      {namespace_id} with the project ID.
+  *   `pubsubs_id` (*type:* `String.t`) - Part of `name`. See documentation of `namespacesId`.
+  *   `optional_params` (*type:* `keyword()`) - Optional parameters
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
+      *   `:access_token` (*type:* `String.t`) - OAuth access token.
+      *   `:alt` (*type:* `String.t`) - Data format for response.
+      *   `:callback` (*type:* `String.t`) - JSONP
+      *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
+      *   `:key` (*type:* `String.t`) - API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+      *   `:oauth_token` (*type:* `String.t`) - OAuth 2.0 token for the current user.
+      *   `:prettyPrint` (*type:* `boolean()`) - Returns response with indentations and line breaks.
+      *   `:quotaUser` (*type:* `String.t`) - Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
+      *   `:uploadType` (*type:* `String.t`) - Legacy upload protocol for media (e.g. "media", "multipart").
+      *   `:upload_protocol` (*type:* `String.t`) - Upload protocol for media (e.g. "raw", "multipart").
+  *   `opts` (*type:* `keyword()`) - Call options
+
+  ## Returns
+
+  *   `{:ok, %GoogleApi.CloudRun.V1alpha1.Model.PubSub{}}` on success
+  *   `{:error, info}` on failure
+  """
+  @spec run_namespaces_pubsubs_get(
+          Tesla.Env.client(),
+          String.t(),
+          String.t(),
+          keyword(),
+          keyword()
+        ) :: {:ok, GoogleApi.CloudRun.V1alpha1.Model.PubSub.t()} | {:error, Tesla.Env.t()}
+  def run_namespaces_pubsubs_get(
+        connection,
+        namespaces_id,
+        pubsubs_id,
+        optional_params \\ [],
+        opts \\ []
+      ) do
+    optional_params_config = %{
+      :"$.xgafv" => :query,
+      :access_token => :query,
+      :alt => :query,
+      :callback => :query,
+      :fields => :query,
+      :key => :query,
+      :oauth_token => :query,
+      :prettyPrint => :query,
+      :quotaUser => :query,
+      :uploadType => :query,
+      :upload_protocol => :query
+    }
+
+    request =
+      Request.new()
+      |> Request.method(:get)
+      |> Request.url(
+        "/apis/events.cloud.google.com/v1alpha1/namespaces/{namespacesId}/pubsubs/{pubsubsId}",
+        %{
+          "namespacesId" => URI.encode(namespaces_id, &URI.char_unreserved?/1),
+          "pubsubsId" => URI.encode(pubsubs_id, &URI.char_unreserved?/1)
+        }
+      )
+      |> Request.add_optional_params(optional_params_config, optional_params)
+      |> Request.library_version(@library_version)
+
+    connection
+    |> Connection.execute(request)
+    |> Response.decode(opts ++ [struct: %GoogleApi.CloudRun.V1alpha1.Model.PubSub{}])
+  end
+
+  @doc """
+  Rpc to list pubsubs.
+
+  ## Parameters
+
+  *   `connection` (*type:* `GoogleApi.CloudRun.V1alpha1.Connection.t`) - Connection to server
+  *   `namespaces_id` (*type:* `String.t`) - Part of `parent`. The project ID or project number from which the pubsubs should
+      be listed.
+  *   `optional_params` (*type:* `keyword()`) - Optional parameters
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
+      *   `:access_token` (*type:* `String.t`) - OAuth access token.
+      *   `:alt` (*type:* `String.t`) - Data format for response.
+      *   `:callback` (*type:* `String.t`) - JSONP
+      *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
+      *   `:key` (*type:* `String.t`) - API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+      *   `:oauth_token` (*type:* `String.t`) - OAuth 2.0 token for the current user.
+      *   `:prettyPrint` (*type:* `boolean()`) - Returns response with indentations and line breaks.
+      *   `:quotaUser` (*type:* `String.t`) - Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
+      *   `:uploadType` (*type:* `String.t`) - Legacy upload protocol for media (e.g. "media", "multipart").
+      *   `:upload_protocol` (*type:* `String.t`) - Upload protocol for media (e.g. "raw", "multipart").
+      *   `:continue` (*type:* `String.t`) - Optional encoded string to continue paging.
+      *   `:fieldSelector` (*type:* `String.t`) - Allows to filter resources based on a specific value for a field name.
+          Send this in a query string format. i.e. 'metadata.name%3Dlorem'.
+          Not currently used by Cloud Run.
+      *   `:includeUninitialized` (*type:* `boolean()`) - Not currently used by Cloud Run.
+      *   `:labelSelector` (*type:* `String.t`) - Allows to filter resources based on a label. Supported operations are
+          =, !=, exists, in, and notIn.
+      *   `:limit` (*type:* `integer()`) - The maximum number of records that should be returned.
+      *   `:resourceVersion` (*type:* `String.t`) - The baseline resource version from which the list or watch operation should
+          start. Not currently used by Cloud Run.
+      *   `:watch` (*type:* `boolean()`) - Flag that indicates that the client expects to watch this resource as well.
+          Not currently used by Cloud Run.
+  *   `opts` (*type:* `keyword()`) - Call options
+
+  ## Returns
+
+  *   `{:ok, %GoogleApi.CloudRun.V1alpha1.Model.ListPubSubsResponse{}}` on success
+  *   `{:error, info}` on failure
+  """
+  @spec run_namespaces_pubsubs_list(Tesla.Env.client(), String.t(), keyword(), keyword()) ::
+          {:ok, GoogleApi.CloudRun.V1alpha1.Model.ListPubSubsResponse.t()}
+          | {:error, Tesla.Env.t()}
+  def run_namespaces_pubsubs_list(connection, namespaces_id, optional_params \\ [], opts \\ []) do
+    optional_params_config = %{
+      :"$.xgafv" => :query,
+      :access_token => :query,
+      :alt => :query,
+      :callback => :query,
+      :fields => :query,
+      :key => :query,
+      :oauth_token => :query,
+      :prettyPrint => :query,
+      :quotaUser => :query,
+      :uploadType => :query,
+      :upload_protocol => :query,
+      :continue => :query,
+      :fieldSelector => :query,
+      :includeUninitialized => :query,
+      :labelSelector => :query,
+      :limit => :query,
+      :resourceVersion => :query,
+      :watch => :query
+    }
+
+    request =
+      Request.new()
+      |> Request.method(:get)
+      |> Request.url(
+        "/apis/events.cloud.google.com/v1alpha1/namespaces/{namespacesId}/pubsubs",
+        %{
+          "namespacesId" => URI.encode(namespaces_id, &URI.char_unreserved?/1)
+        }
+      )
+      |> Request.add_optional_params(optional_params_config, optional_params)
+      |> Request.library_version(@library_version)
+
+    connection
+    |> Connection.execute(request)
+    |> Response.decode(opts ++ [struct: %GoogleApi.CloudRun.V1alpha1.Model.ListPubSubsResponse{}])
+  end
+
+  @doc """
+  Rpc to replace a pubsub.
+
+  Only the spec and metadata labels and annotations are modifiable. After
+  the Update request, Cloud Run will work to make the 'status'
+  match the requested 'spec'.
+
+  May provide metadata.resourceVersion to enforce update from last read for
+  optimistic concurrency control.
+
+  ## Parameters
+
+  *   `connection` (*type:* `GoogleApi.CloudRun.V1alpha1.Connection.t`) - Connection to server
+  *   `namespaces_id` (*type:* `String.t`) - Part of `name`. The name of the pubsub being retrieved. If needed, replace
+      {namespace_id} with the project ID.
+  *   `pubsubs_id` (*type:* `String.t`) - Part of `name`. See documentation of `namespacesId`.
+  *   `optional_params` (*type:* `keyword()`) - Optional parameters
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
+      *   `:access_token` (*type:* `String.t`) - OAuth access token.
+      *   `:alt` (*type:* `String.t`) - Data format for response.
+      *   `:callback` (*type:* `String.t`) - JSONP
+      *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
+      *   `:key` (*type:* `String.t`) - API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+      *   `:oauth_token` (*type:* `String.t`) - OAuth 2.0 token for the current user.
+      *   `:prettyPrint` (*type:* `boolean()`) - Returns response with indentations and line breaks.
+      *   `:quotaUser` (*type:* `String.t`) - Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
+      *   `:uploadType` (*type:* `String.t`) - Legacy upload protocol for media (e.g. "media", "multipart").
+      *   `:upload_protocol` (*type:* `String.t`) - Upload protocol for media (e.g. "raw", "multipart").
+      *   `:body` (*type:* `GoogleApi.CloudRun.V1alpha1.Model.PubSub.t`) - 
+  *   `opts` (*type:* `keyword()`) - Call options
+
+  ## Returns
+
+  *   `{:ok, %GoogleApi.CloudRun.V1alpha1.Model.PubSub{}}` on success
+  *   `{:error, info}` on failure
+  """
+  @spec run_namespaces_pubsubs_replace_pub_sub(
+          Tesla.Env.client(),
+          String.t(),
+          String.t(),
+          keyword(),
+          keyword()
+        ) :: {:ok, GoogleApi.CloudRun.V1alpha1.Model.PubSub.t()} | {:error, Tesla.Env.t()}
+  def run_namespaces_pubsubs_replace_pub_sub(
+        connection,
+        namespaces_id,
+        pubsubs_id,
+        optional_params \\ [],
+        opts \\ []
+      ) do
+    optional_params_config = %{
+      :"$.xgafv" => :query,
+      :access_token => :query,
+      :alt => :query,
+      :callback => :query,
+      :fields => :query,
+      :key => :query,
+      :oauth_token => :query,
+      :prettyPrint => :query,
+      :quotaUser => :query,
+      :uploadType => :query,
+      :upload_protocol => :query,
+      :body => :body
+    }
+
+    request =
+      Request.new()
+      |> Request.method(:put)
+      |> Request.url(
+        "/apis/events.cloud.google.com/v1alpha1/namespaces/{namespacesId}/pubsubs/{pubsubsId}",
+        %{
+          "namespacesId" => URI.encode(namespaces_id, &URI.char_unreserved?/1),
+          "pubsubsId" => URI.encode(pubsubs_id, &URI.char_unreserved?/1)
+        }
+      )
+      |> Request.add_optional_params(optional_params_config, optional_params)
+      |> Request.library_version(@library_version)
+
+    connection
+    |> Connection.execute(request)
+    |> Response.decode(opts ++ [struct: %GoogleApi.CloudRun.V1alpha1.Model.PubSub{}])
+  end
+
+  @doc """
   Rpc to delete a revision.
 
   ## Parameters
