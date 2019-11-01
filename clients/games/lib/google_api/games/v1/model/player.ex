90b9f2b314e8d0761d12dae29350d8da2fca5e26
yoshi-automation <yoshi-automation@google.com>
2019-11-01 05:13:18 -0700

    Regenerate Games client

--- clients/games/lib/google_api/games/v1/model/player.ex
+++ clients/games/lib/google_api/games/v1/model/player.ex
@@ -31,6 +31,7 @@ defmodule GoogleApi.Games.V1.Model.Player do
   *   `name` (*type:* `GoogleApi.Games.V1.Model.PlayerName.t`, *default:* `nil`) - An object representation of the individual components of the player's name. For some players, these fields may not be present.
   *   `originalPlayerId` (*type:* `String.t`, *default:* `nil`) - The player ID that was used for this player the first time they signed into the game in question. This is only populated for calls to player.get for the requesting player, only if the player ID has subsequently changed, and only to clients that support remapping player IDs.
   *   `playerId` (*type:* `String.t`, *default:* `nil`) - The ID of the player.
+  *   `playerStattus` (*type:* `String.t`, *default:* `nil`) - 
   *   `profileSettings` (*type:* `GoogleApi.Games.V1.Model.ProfileSettings.t`, *default:* `nil`) - The player's profile settings. Controls whether or not the player's profile is visible to other players.
   *   `title` (*type:* `String.t`, *default:* `nil`) - The player's title rewarded for their game activities.
   """
@@ -48,6 +49,7 @@ defmodule GoogleApi.Games.V1.Model.Player do
           :name => GoogleApi.Games.V1.Model.PlayerName.t(),
           :originalPlayerId => String.t(),
           :playerId => String.t(),
+          :playerStattus => String.t(),
           :profileSettings => GoogleApi.Games.V1.Model.ProfileSettings.t(),
           :title => String.t()
         }
@@ -62,6 +64,7 @@ defmodule GoogleApi.Games.V1.Model.Player do
   field(:name, as: GoogleApi.Games.V1.Model.PlayerName)
   field(:originalPlayerId)
   field(:playerId)
+  field(:playerStattus)
   field(:profileSettings, as: GoogleApi.Games.V1.Model.ProfileSettings)
   field(:title)
 end
