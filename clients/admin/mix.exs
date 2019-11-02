4e873c2f768f0d13251a2174e07b56ca0d436078
yoshi-automation <yoshi-automation@google.com>
2019-11-02 05:02:01 -0700

    Regenerate Admin client

--- clients/admin/mix.exs
+++ clients/admin/mix.exs
@@ -18,7 +18,7 @@
 defmodule GoogleApi.Admin.Mixfile do
   use Mix.Project
 
-  @version "0.9.0"
+  @version "0.10.0"
 
   def project() do
     [
@@ -59,7 +59,7 @@ defmodule GoogleApi.Admin.Mixfile do
       licenses: ["Apache 2.0"],
       links: %{
         "GitHub" => "https://github.com/googleapis/elixir-google-api/tree/master/clients/admin",
-        "Homepage" => "https://developers.google.com/admin-sdk/reports/"
+        "Homepage" => "/admin-sdk/reports/"
       }
     ]
   end
