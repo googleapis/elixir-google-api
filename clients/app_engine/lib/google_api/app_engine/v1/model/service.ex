8ffd109153f3790ad973a0fbebb8f9ad116d4e5f
yoshi-automation <yoshi-automation@google.com>
2019-11-09 05:03:43 -0800

    Regenerate AppEngine client

--- clients/app_engine/lib/google_api/app_engine/v1/model/service.ex
+++ clients/app_engine/lib/google_api/app_engine/v1/model/service.ex
@@ -17,7 +17,7 @@
 
 defmodule GoogleApi.AppEngine.V1.Model.Service do
   @moduledoc """
-  A Service resource is a logical component of an application that can share state and communicate in a secure fashion with other services. For example, an application that handles customer requests might include separate services to handle tasks such as backend data analysis or API requests from mobile devices. Each service has a collection of versions that define a specific set of code used to implement the functionality of that service.
+  A Service resource is a logical component of an application that can share state and communicate in a secure fashion with other services. For example, an application that handles customer requests might include separate services to handle tasks such as backend data analysis or API requests from mobile devices. Each service has a collection of versions that define a specific set of code used to implement the functionality of that service. Next tag: 6
 
   ## Attributes
 
