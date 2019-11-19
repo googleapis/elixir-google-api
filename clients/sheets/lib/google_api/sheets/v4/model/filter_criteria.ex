f400670d5d029f4a3ada0b9f5e2f1dee7a2ffe56
yoshi-automation <yoshi-automation@google.com>
2019-11-19 05:24:37 -0800

    Regenerate Sheets client

--- clients/sheets/lib/google_api/sheets/v4/model/filter_criteria.ex
+++ clients/sheets/lib/google_api/sheets/v4/model/filter_criteria.ex
@@ -22,7 +22,7 @@ defmodule GoogleApi.Sheets.V4.Model.FilterCriteria do
   ## Attributes
 
   *   `condition` (*type:* `GoogleApi.Sheets.V4.Model.BooleanCondition.t`, *default:* `nil`) - A condition that must be true for values to be shown.
-      (This does not override hiddenValues -- if a value is listed there,
+      (This does not override hidden_values -- if a value is listed there,
        it will still be hidden.)
   *   `hiddenValues` (*type:* `list(String.t)`, *default:* `nil`) - Values that should be hidden.
   *   `visibleBackgroundColor` (*type:* `GoogleApi.Sheets.V4.Model.Color.t`, *default:* `nil`) - The background fill color to filter by; only cells with this fill color are
