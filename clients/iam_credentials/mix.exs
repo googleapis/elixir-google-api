f5115ea5b4220150bdbb042d99dc12166e5681ea
yoshi-automation <yoshi-automation@google.com>
2019-12-07 05:27:38 -0800

    Regenerate IAMCredentials client

--- clients/iam_credentials/mix.exs
+++ clients/iam_credentials/mix.exs
@@ -18,13 +18,13 @@
 defmodule GoogleApi.IAMCredentials.Mixfile do
   use Mix.Project
 
-  @version "0.6.0"
+  @version "0.7.0"
 
   def project() do
     [
       app: :google_api_iam_credentials,
       version: @version,
-      elixir: "~> 1.4",
+      elixir: "~> 1.6",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       description: description(),
