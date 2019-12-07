b30fd63b745f58b15508d895bf8a02b8793dda79
yoshi-automation <yoshi-automation@google.com>
2019-12-07 05:06:02 -0800

    Regenerate AppEngine client

--- clients/app_engine/mix.exs
+++ clients/app_engine/mix.exs
@@ -18,13 +18,13 @@
 defmodule GoogleApi.AppEngine.Mixfile do
   use Mix.Project
 
-  @version "0.18.0"
+  @version "0.19.0"
 
   def project() do
     [
       app: :google_api_app_engine,
       version: @version,
-      elixir: "~> 1.4",
+      elixir: "~> 1.6",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       description: description(),
