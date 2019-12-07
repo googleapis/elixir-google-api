88f1119100c4c9d7c8ad731037bb9cbfca0dc304
yoshi-automation <yoshi-automation@google.com>
2019-12-07 05:16:26 -0800

    Regenerate CustomSearch client

--- clients/custom_search/mix.exs
+++ clients/custom_search/mix.exs
@@ -18,13 +18,13 @@
 defmodule GoogleApi.CustomSearch.Mixfile do
   use Mix.Project
 
-  @version "0.6.0"
+  @version "0.7.0"
 
   def project() do
     [
       app: :google_api_custom_search,
       version: @version,
-      elixir: "~> 1.4",
+      elixir: "~> 1.6",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       description: description(),
