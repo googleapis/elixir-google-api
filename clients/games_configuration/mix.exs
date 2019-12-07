8ebf1af6e4fc90427bbf4a1f504ab458a9b1e3d3
yoshi-automation <yoshi-automation@google.com>
2019-12-07 05:24:47 -0800

    Regenerate GamesConfiguration client

--- clients/games_configuration/mix.exs
+++ clients/games_configuration/mix.exs
@@ -18,13 +18,13 @@
 defmodule GoogleApi.GamesConfiguration.Mixfile do
   use Mix.Project
 
-  @version "0.4.0"
+  @version "0.5.0"
 
   def project() do
     [
       app: :google_api_games_configuration,
       version: @version,
-      elixir: "~> 1.4",
+      elixir: "~> 1.6",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       description: description(),
