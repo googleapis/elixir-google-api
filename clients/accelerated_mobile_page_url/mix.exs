1f26358a666b8ddbc9c2383d9db236b7466f2099
yoshi-automation <yoshi-automation@google.com>
2019-12-07 05:01:26 -0800

    Regenerate AcceleratedMobilePageUrl client

--- clients/accelerated_mobile_page_url/mix.exs
+++ clients/accelerated_mobile_page_url/mix.exs
@@ -18,13 +18,13 @@
 defmodule GoogleApi.AcceleratedMobilePageUrl.Mixfile do
   use Mix.Project
 
-  @version "0.6.0"
+  @version "0.7.0"
 
   def project() do
     [
       app: :google_api_accelerated_mobile_page_url,
       version: @version,
-      elixir: "~> 1.4",
+      elixir: "~> 1.6",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       description: description(),
