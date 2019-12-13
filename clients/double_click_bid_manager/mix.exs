dd825b8629238324fe1f8ff2d1ff181c2c9b60c4
yoshi-automation <yoshi-automation@google.com>
2019-12-13 11:45:46 -0800

    Regenerate DoubleClickBidManager client

--- clients/double_click_bid_manager/mix.exs
+++ clients/double_click_bid_manager/mix.exs
@@ -18,13 +18,13 @@
 defmodule GoogleApi.DoubleClickBidManager.Mixfile do
   use Mix.Project
 
-  @version "0.8.0"
+  @version "0.9.0"
 
   def project() do
     [
       app: :google_api_double_click_bid_manager,
       version: @version,
-      elixir: "~> 1.4",
+      elixir: "~> 1.6",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       description: description(),
