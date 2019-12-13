3917676c90d2b7238c311c1f8d20771f33efaaca
yoshi-automation <yoshi-automation@google.com>
2019-12-13 12:12:40 -0800

    Regenerate Vault client

--- clients/vault/mix.exs
+++ clients/vault/mix.exs
@@ -18,13 +18,13 @@
 defmodule GoogleApi.Vault.Mixfile do
   use Mix.Project
 
-  @version "0.11.0"
+  @version "0.12.0"
 
   def project() do
     [
       app: :google_api_vault,
       version: @version,
-      elixir: "~> 1.4",
+      elixir: "~> 1.6",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       description: description(),
