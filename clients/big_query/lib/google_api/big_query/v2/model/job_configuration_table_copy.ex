1fe402b172e500779bb8d64016ecea2e0ba906d1
yoshi-automation <yoshi-automation@google.com>
2019-12-13 11:28:35 -0800

    Regenerate BigQuery client

--- clients/big_query/lib/google_api/big_query/v2/model/job_configuration_table_copy.ex
+++ clients/big_query/lib/google_api/big_query/v2/model/job_configuration_table_copy.ex
@@ -43,8 +43,7 @@ defmodule GoogleApi.BigQuery.V2.Model.JobConfigurationTableCopy do
 
   field(:createDisposition)
 
-  field(
-    :destinationEncryptionConfiguration,
+  field(:destinationEncryptionConfiguration,
     as: GoogleApi.BigQuery.V2.Model.EncryptionConfiguration
   )
 
