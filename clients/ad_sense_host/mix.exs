491015ae6eadf25fa20073bd3fae7f545f34cab6
yoshi-automation <yoshi-automation@google.com>
2019-12-13 11:25:02 -0800

    Regenerate AdSenseHost client

--- clients/ad_sense_host/mix.exs
+++ clients/ad_sense_host/mix.exs
@@ -18,13 +18,13 @@
 defmodule GoogleApi.AdSenseHost.Mixfile do
   use Mix.Project
 
-  @version "0.6.0"
+  @version "0.7.0"
 
   def project() do
     [
       app: :google_api_ad_sense_host,
       version: @version,
-      elixir: "~> 1.4",
+      elixir: "~> 1.6",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       description: description(),
