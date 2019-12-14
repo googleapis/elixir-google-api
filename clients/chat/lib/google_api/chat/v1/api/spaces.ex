97c9f78a1fdc9697dabeaccd96ea97a2053be42c
yoshi-automation <yoshi-automation@google.com>
2019-12-13 16:37:23 -0800

    Regenerate Chat client

--- clients/chat/lib/google_api/chat/v1/api/spaces.ex
+++ clients/chat/lib/google_api/chat/v1/api/spaces.ex
@@ -481,9 +481,12 @@ defmodule GoogleApi.Chat.V1.Api.Spaces do
       *   `:quotaUser` (*type:* `String.t`) - Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
       *   `:uploadType` (*type:* `String.t`) - Legacy upload protocol for media (e.g. "media", "multipart").
       *   `:upload_protocol` (*type:* `String.t`) - Upload protocol for media (e.g. "raw", "multipart").
-      *   `:updateMask` (*type:* `String.t`) - Required. The field paths to be updated.
+      *   `:updateMask` (*type:* `String.t`) - Required. The field paths to be updated, comma separated if there are
+          multiple.
 
-          Currently supported field paths: "text", "cards".
+          Currently supported field paths:
+          * text
+          * cards
       *   `:body` (*type:* `GoogleApi.Chat.V1.Model.Message.t`) - 
   *   `opts` (*type:* `keyword()`) - Call options
 
