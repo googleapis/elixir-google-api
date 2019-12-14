d9a271e409a28db6bfbc695ecbda74e3a96257ba
yoshi-automation <yoshi-automation@google.com>
2019-12-13 16:58:13 -0800

    Regenerate GroupsMigration client

--- clients/groups_migration/lib/google_api/groups_migration/v1/api/archive.ex
+++ clients/groups_migration/lib/google_api/groups_migration/v1/api/archive.ex
@@ -81,6 +81,76 @@ defmodule GoogleApi.GroupsMigration.V1.Api.Archive do
 
   *   `connection` (*type:* `GoogleApi.GroupsMigration.V1.Connection.t`) - Connection to server
   *   `group_id` (*type:* `String.t`) - The group ID
+  *   `upload_type` (*type:* `String.t`) - Upload type. Must be "multipart".
+  *   `metadata` (*type:* `String.t`) - string metadata
+  *   `data` (*type:* `iodata`) - Content to upload, as a string or iolist
+  *   `optional_params` (*type:* `keyword()`) - Optional parameters
+      *   `:alt` (*type:* `String.t`) - Data format for the response.
+      *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
+      *   `:key` (*type:* `String.t`) - API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+      *   `:oauth_token` (*type:* `String.t`) - OAuth 2.0 token for the current user.
+      *   `:prettyPrint` (*type:* `boolean()`) - Returns response with indentations and line breaks.
+      *   `:quotaUser` (*type:* `String.t`) - An opaque string that represents a user for quota purposes. Must not exceed 40 characters.
+      *   `:userIp` (*type:* `String.t`) - Deprecated. Please use quotaUser instead.
+  *   `opts` (*type:* `keyword()`) - Call options
+
+  ## Returns
+
+  *   `{:ok, %GoogleApi.GroupsMigration.V1.Model.Groups{}}` on success
+  *   `{:error, info}` on failure
+  """
+  @spec groupsmigration_archive_insert_iodata(
+          Tesla.Env.client(),
+          String.t(),
+          String.t(),
+          String.t(),
+          iodata,
+          keyword(),
+          keyword()
+        ) :: {:ok, GoogleApi.GroupsMigration.V1.Model.Groups.t()} | {:error, Tesla.Env.t()}
+  def groupsmigration_archive_insert_iodata(
+        connection,
+        group_id,
+        upload_type,
+        metadata,
+        data,
+        optional_params \\ [],
+        opts \\ []
+      ) do
+    optional_params_config = %{
+      :alt => :query,
+      :fields => :query,
+      :key => :query,
+      :oauth_token => :query,
+      :prettyPrint => :query,
+      :quotaUser => :query,
+      :userIp => :query
+    }
+
+    request =
+      Request.new()
+      |> Request.method(:post)
+      |> Request.url("/upload/groups/v1/groups/{groupId}/archive", %{
+        "groupId" => URI.encode(group_id, &URI.char_unreserved?/1)
+      })
+      |> Request.add_param(:query, :uploadType, upload_type)
+      |> Request.add_param(:body, :metadata, metadata)
+      |> Request.add_param(:body, :data, data)
+      |> Request.add_optional_params(optional_params_config, optional_params)
+      |> Request.library_version(@library_version)
+
+    connection
+    |> Connection.execute(request)
+    |> Response.decode(opts ++ [struct: %GoogleApi.GroupsMigration.V1.Model.Groups{}])
+  end
+
+  @doc """
+  Inserts a new mail into the archive of the Google group.
+
+  ## Parameters
+
+  *   `connection` (*type:* `GoogleApi.GroupsMigration.V1.Connection.t`) - Connection to server
+  *   `group_id` (*type:* `String.t`) - The group ID
   *   `upload_type` (*type:* `String.t`) - Upload type. Must be "resumable".
   *   `optional_params` (*type:* `keyword()`) - Optional parameters
       *   `:alt` (*type:* `String.t`) - Data format for the response.
@@ -145,7 +215,7 @@ defmodule GoogleApi.GroupsMigration.V1.Api.Archive do
   *   `group_id` (*type:* `String.t`) - The group ID
   *   `upload_type` (*type:* `String.t`) - Upload type. Must be "multipart".
   *   `metadata` (*type:* `String.t`) - string metadata
-  *   `data` (*type:* `String.t`) - Path to file
+  *   `data` (*type:* `String.t`) - Path to file containing content to upload
   *   `optional_params` (*type:* `keyword()`) - Optional parameters
       *   `:alt` (*type:* `String.t`) - Data format for the response.
       *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
