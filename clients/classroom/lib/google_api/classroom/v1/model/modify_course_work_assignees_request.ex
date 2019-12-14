bbc24fc16971bfef0aaa9b1ddcd1cb0e1cb94b9d
yoshi-automation <yoshi-automation@google.com>
2019-12-13 16:38:03 -0800

    Regenerate Classroom client

--- clients/classroom/lib/google_api/classroom/v1/model/modify_course_work_assignees_request.ex
+++ clients/classroom/lib/google_api/classroom/v1/model/modify_course_work_assignees_request.ex
@@ -37,8 +37,7 @@ defmodule GoogleApi.Classroom.V1.Model.ModifyCourseWorkAssigneesRequest do
 
   field(:assigneeMode)
 
-  field(
-    :modifyIndividualStudentsOptions,
+  field(:modifyIndividualStudentsOptions,
     as: GoogleApi.Classroom.V1.Model.ModifyIndividualStudentsOptions
   )
 end
