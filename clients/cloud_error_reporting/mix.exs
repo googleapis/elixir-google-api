cadd6c5c864db2df5a59f96688f238bc62455645
yoshi-automation <yoshi-automation@google.com>
2019-12-13 16:39:43 -0800

    Regenerate CloudErrorReporting client

--- clients/cloud_error_reporting/mix.exs
+++ clients/cloud_error_reporting/mix.exs
@@ -18,13 +18,13 @@
 defmodule GoogleApi.CloudErrorReporting.Mixfile do
   use Mix.Project
 
-  @version "0.9.0"
+  @version "0.10.0"
 
   def project() do
     [
       app: :google_api_cloud_error_reporting,
       version: @version,
-      elixir: "~> 1.4",
+      elixir: "~> 1.6",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       description: description(),
