b8ee66a08a304cdc3741abe0f1d5f134017b5507
yoshi-automation <yoshi-automation@google.com>
2019-12-13 17:11:28 -0800

    Regenerate SearchConsole client

--- clients/search_console/lib/google_api/search_console/v1/model/run_mobile_friendly_test_response.ex
+++ clients/search_console/lib/google_api/search_console/v1/model/run_mobile_friendly_test_response.ex
@@ -41,8 +41,7 @@ defmodule GoogleApi.SearchConsole.V1.Model.RunMobileFriendlyTestResponse do
 
   field(:mobileFriendliness)
 
-  field(
-    :mobileFriendlyIssues,
+  field(:mobileFriendlyIssues,
     as: GoogleApi.SearchConsole.V1.Model.MobileFriendlyIssue,
     type: :list
   )
