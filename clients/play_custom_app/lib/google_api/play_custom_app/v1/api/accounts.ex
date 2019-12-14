08fa94346d39ee5e10f32261c97cbe183f6529b8
yoshi-automation <yoshi-automation@google.com>
2019-12-13 17:06:21 -0800

    Regenerate PlayCustomApp client

--- clients/play_custom_app/lib/google_api/play_custom_app/v1/api/accounts.ex
+++ clients/play_custom_app/lib/google_api/play_custom_app/v1/api/accounts.ex
@@ -92,6 +92,76 @@ defmodule GoogleApi.PlayCustomApp.V1.Api.Accounts do
 
   *   `connection` (*type:* `GoogleApi.PlayCustomApp.V1.Connection.t`) - Connection to server
   *   `account` (*type:* `String.t`) - Developer account ID.
+  *   `upload_type` (*type:* `String.t`) - Upload type. Must be "multipart".
+  *   `metadata` (*type:* `GoogleApi.PlayCustomApp.V1.Model.CustomApp.t`) - object metadata
+  *   `data` (*type:* `iodata`) - Content to upload, as a string or iolist
+  *   `optional_params` (*type:* `keyword()`) - Optional parameters
+      *   `:alt` (*type:* `String.t`) - Data format for the response.
+      *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
+      *   `:key` (*type:* `String.t`) - API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+      *   `:oauth_token` (*type:* `String.t`) - OAuth 2.0 token for the current user.
+      *   `:prettyPrint` (*type:* `boolean()`) - Returns response with indentations and line breaks.
+      *   `:quotaUser` (*type:* `String.t`) - An opaque string that represents a user for quota purposes. Must not exceed 40 characters.
+      *   `:userIp` (*type:* `String.t`) - Deprecated. Please use quotaUser instead.
+  *   `opts` (*type:* `keyword()`) - Call options
+
+  ## Returns
+
+  *   `{:ok, %GoogleApi.PlayCustomApp.V1.Model.CustomApp{}}` on success
+  *   `{:error, info}` on failure
+  """
+  @spec playcustomapp_accounts_custom_apps_create_iodata(
+          Tesla.Env.client(),
+          String.t(),
+          String.t(),
+          GoogleApi.PlayCustomApp.V1.Model.CustomApp.t(),
+          iodata,
+          keyword(),
+          keyword()
+        ) :: {:ok, GoogleApi.PlayCustomApp.V1.Model.CustomApp.t()} | {:error, Tesla.Env.t()}
+  def playcustomapp_accounts_custom_apps_create_iodata(
+        connection,
+        account,
+        upload_type,
+        metadata,
+        data,
+        optional_params \\ [],
+        opts \\ []
+      ) do
+    optional_params_config = %{
+      :alt => :query,
+      :fields => :query,
+      :key => :query,
+      :oauth_token => :query,
+      :prettyPrint => :query,
+      :quotaUser => :query,
+      :userIp => :query
+    }
+
+    request =
+      Request.new()
+      |> Request.method(:post)
+      |> Request.url("/upload/playcustomapp/v1/accounts/{account}/customApps", %{
+        "account" => URI.encode(account, &URI.char_unreserved?/1)
+      })
+      |> Request.add_param(:query, :uploadType, upload_type)
+      |> Request.add_param(:body, :metadata, metadata)
+      |> Request.add_param(:body, :data, data)
+      |> Request.add_optional_params(optional_params_config, optional_params)
+      |> Request.library_version(@library_version)
+
+    connection
+    |> Connection.execute(request)
+    |> Response.decode(opts ++ [struct: %GoogleApi.PlayCustomApp.V1.Model.CustomApp{}])
+  end
+
+  @doc """
+  Create and publish a new custom app.
+
+  ## Parameters
+
+  *   `connection` (*type:* `GoogleApi.PlayCustomApp.V1.Connection.t`) - Connection to server
+  *   `account` (*type:* `String.t`) - Developer account ID.
   *   `upload_type` (*type:* `String.t`) - Upload type. Must be "resumable".
   *   `optional_params` (*type:* `keyword()`) - Optional parameters
       *   `:alt` (*type:* `String.t`) - Data format for the response.
@@ -158,7 +228,7 @@ defmodule GoogleApi.PlayCustomApp.V1.Api.Accounts do
   *   `account` (*type:* `String.t`) - Developer account ID.
   *   `upload_type` (*type:* `String.t`) - Upload type. Must be "multipart".
   *   `metadata` (*type:* `GoogleApi.PlayCustomApp.V1.Model.CustomApp.t`) - object metadata
-  *   `data` (*type:* `String.t`) - Path to file
+  *   `data` (*type:* `String.t`) - Path to file containing content to upload
   *   `optional_params` (*type:* `keyword()`) - Optional parameters
       *   `:alt` (*type:* `String.t`) - Data format for the response.
       *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
