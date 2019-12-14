08a875aceb61e84d5922ebb6d3161312ac4a1a06
yoshi-automation <yoshi-automation@google.com>
2019-12-13 16:41:00 -0800

    Regenerate CloudKMS client

--- clients/cloud_kms/README.md
+++ clients/cloud_kms/README.md
@@ -12,7 +12,7 @@ Install this package from [Hex](https://hex.pm) by adding
 
 ```elixir
 def deps do
-  [{:google_api_cloud_kms, "~> 0.20"}]
+  [{:google_api_cloud_kms, "~> 0.21"}]
 end
 ```
 
--- clients/cloud_kms/mix.exs
+++ clients/cloud_kms/mix.exs
@@ -18,13 +18,13 @@
 defmodule GoogleApi.CloudKMS.Mixfile do
   use Mix.Project
 
-  @version "0.20.0"
+  @version "0.21.0"
 
   def project() do
     [
       app: :google_api_cloud_kms,
       version: @version,
-      elixir: "~> 1.4",
+      elixir: "~> 1.6",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       description: description(),
