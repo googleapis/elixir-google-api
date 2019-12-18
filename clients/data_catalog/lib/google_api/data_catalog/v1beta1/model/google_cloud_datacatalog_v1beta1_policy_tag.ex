104fe3cd0960b6f74fc2767dee0059a8d5821395
yoshi-automation <yoshi-automation@google.com>
2019-12-18 05:16:21 -0800

    Regenerate DataCatalog client

--- clients/data_catalog/lib/google_api/data_catalog/v1beta1/model/google_cloud_datacatalog_v1beta1_policy_tag.ex
+++ clients/data_catalog/lib/google_api/data_catalog/v1beta1/model/google_cloud_datacatalog_v1beta1_policy_tag.ex
@@ -19,13 +19,8 @@ defmodule GoogleApi.DataCatalog.V1beta1.Model.GoogleCloudDatacatalogV1beta1Polic
   @moduledoc """
   Denotes one policy tag in a taxonomy (e.g. ssn). Policy Tags can be defined
   in a hierarchy. For example, consider the following hierachy:
-                Geolocation
-                     |
-    ------------------------------------
-    |                |                 |
-  LatLong          City              ZipCode
-  PolicyTag "Geolocation" contains three child policy tags: "LatLong",
-  "City", and "ZipCode".
+  Geolocation -&gt; (LatLong, City, ZipCode). PolicyTag "Geolocation"
+  contains three child policy tags: "LatLong", "City", and "ZipCode".
 
   ## Attributes
 
