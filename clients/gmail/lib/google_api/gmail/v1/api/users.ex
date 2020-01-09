5441e08bc21e1857174815960438dfa94141aea5
yoshi-automation <yoshi-automation@google.com>
2020-01-09 05:23:18 -0800

    Regenerate Gmail client

--- clients/gmail/lib/google_api/gmail/v1/api/users.ex
+++ clients/gmail/lib/google_api/gmail/v1/api/users.ex
@@ -3347,7 +3347,7 @@ defmodule GoogleApi.Gmail.V1.Api.Users do
   @doc """
   Adds a delegate with its verification status set directly to accepted, without sending any verification email. The delegate user must be a member of the same G Suite organization as the delegator user.
 
-  Gmail imposes limtations on the number of delegates and delegators each user in a G Suite organization can have. These limits depend on your organization, but in general each user can have up to 25 delegates and up to 10 delegators.
+  Gmail imposes limitations on the number of delegates and delegators each user in a G Suite organization can have. These limits depend on your organization, but in general each user can have up to 25 delegates and up to 10 delegators.
 
   Note that a delegate user must be referred to by their primary email address, and not an email alias.
 
