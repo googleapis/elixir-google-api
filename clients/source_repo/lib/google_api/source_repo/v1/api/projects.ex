d07fcec3fc10e93ffd0243473d7d6e08f5ca84c3
yoshi-automation <yoshi-automation@google.com>
2020-01-18 05:41:16 -0800

    Regenerate SourceRepo client

--- clients/source_repo/lib/google_api/source_repo/v1/api/projects.ex
+++ clients/source_repo/lib/google_api/source_repo/v1/api/projects.ex
@@ -344,6 +344,14 @@ defmodule GoogleApi.SourceRepo.V1.Api.Projects do
       *   `:quotaUser` (*type:* `String.t`) - Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
       *   `:uploadType` (*type:* `String.t`) - Legacy upload protocol for media (e.g. "media", "multipart").
       *   `:upload_protocol` (*type:* `String.t`) - Upload protocol for media (e.g. "raw", "multipart").
+      *   `:"options.requestedPolicyVersion"` (*type:* `integer()`) - Optional. The policy format version to be returned.
+
+          Valid values are 0, 1, and 3. Requests specifying an invalid value will be
+          rejected.
+
+          Requests for policies with any conditional bindings must specify version 3.
+          Policies without any conditional bindings may specify any valid value or
+          leave the field unset.
   *   `opts` (*type:* `keyword()`) - Call options
 
   ## Returns
@@ -374,7 +382,8 @@ defmodule GoogleApi.SourceRepo.V1.Api.Projects do
       :prettyPrint => :query,
       :quotaUser => :query,
       :uploadType => :query,
-      :upload_protocol => :query
+      :upload_protocol => :query,
+      :"options.requestedPolicyVersion" => :query
     }
 
     request =
