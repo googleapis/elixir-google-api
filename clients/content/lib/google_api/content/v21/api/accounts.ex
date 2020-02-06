df8374a14b7a3f304281cf66134569cb58fc3a48
yoshi-automation <yoshi-automation@google.com>
2020-02-06 09:22:39 -0800

    Regenerate Content client

--- clients/content/lib/google_api/content/v21/api/accounts.ex
+++ clients/content/lib/google_api/content/v21/api/accounts.ex
@@ -465,6 +465,74 @@ defmodule GoogleApi.Content.V21.Api.Accounts do
   end
 
   @doc """
+  Returns the list of accounts linked to your Merchant Center account.
+
+  ## Parameters
+
+  *   `connection` (*type:* `GoogleApi.Content.V21.Connection.t`) - Connection to server
+  *   `merchant_id` (*type:* `String.t`) - The ID of the managing account. If this parameter is not the same as accountId, then this account must be a multi-client account and accountId must be the ID of a sub-account of this account.
+  *   `account_id` (*type:* `String.t`) - The ID of the account for which to list links.
+  *   `optional_params` (*type:* `keyword()`) - Optional parameters
+      *   `:alt` (*type:* `String.t`) - Data format for the response.
+      *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
+      *   `:key` (*type:* `String.t`) - API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+      *   `:oauth_token` (*type:* `String.t`) - OAuth 2.0 token for the current user.
+      *   `:prettyPrint` (*type:* `boolean()`) - Returns response with indentations and line breaks.
+      *   `:quotaUser` (*type:* `String.t`) - An opaque string that represents a user for quota purposes. Must not exceed 40 characters.
+      *   `:userIp` (*type:* `String.t`) - Deprecated. Please use quotaUser instead.
+      *   `:maxResults` (*type:* `integer()`) - The maximum number of links to return in the response, used for pagination.
+      *   `:pageToken` (*type:* `String.t`) - The token returned by the previous request.
+  *   `opts` (*type:* `keyword()`) - Call options
+
+  ## Returns
+
+  *   `{:ok, %GoogleApi.Content.V21.Model.AccountsListLinksResponse{}}` on success
+  *   `{:error, info}` on failure
+  """
+  @spec content_accounts_listlinks(
+          Tesla.Env.client(),
+          String.t(),
+          String.t(),
+          keyword(),
+          keyword()
+        ) ::
+          {:ok, GoogleApi.Content.V21.Model.AccountsListLinksResponse.t()}
+          | {:error, Tesla.Env.t()}
+  def content_accounts_listlinks(
+        connection,
+        merchant_id,
+        account_id,
+        optional_params \\ [],
+        opts \\ []
+      ) do
+    optional_params_config = %{
+      :alt => :query,
+      :fields => :query,
+      :key => :query,
+      :oauth_token => :query,
+      :prettyPrint => :query,
+      :quotaUser => :query,
+      :userIp => :query,
+      :maxResults => :query,
+      :pageToken => :query
+    }
+
+    request =
+      Request.new()
+      |> Request.method(:get)
+      |> Request.url("/{merchantId}/accounts/{accountId}/listlinks", %{
+        "merchantId" => URI.encode(merchant_id, &URI.char_unreserved?/1),
+        "accountId" => URI.encode(account_id, &URI.char_unreserved?/1)
+      })
+      |> Request.add_optional_params(optional_params_config, optional_params)
+      |> Request.library_version(@library_version)
+
+    connection
+    |> Connection.execute(request)
+    |> Response.decode(opts ++ [struct: %GoogleApi.Content.V21.Model.AccountsListLinksResponse{}])
+  end
+
+  @doc """
   Updates a Merchant Center account.
 
   ## Parameters
