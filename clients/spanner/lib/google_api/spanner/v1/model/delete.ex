6a4ffd77309f6544234e3ce38ec38fd6c33b745f
yoshi-automation <yoshi-automation@google.com>
2020-02-11 05:29:14 -0800

    Regenerate Spanner client

--- clients/spanner/lib/google_api/spanner/v1/model/delete.ex
+++ clients/spanner/lib/google_api/spanner/v1/model/delete.ex
@@ -21,7 +21,10 @@ defmodule GoogleApi.Spanner.V1.Model.Delete do
 
   ## Attributes
 
-  *   `keySet` (*type:* `GoogleApi.Spanner.V1.Model.KeySet.t`, *default:* `nil`) - Required. The primary keys of the rows within table to delete.
+  *   `keySet` (*type:* `GoogleApi.Spanner.V1.Model.KeySet.t`, *default:* `nil`) - Required. The primary keys of the rows within table to delete.  The
+      primary keys must be specified in the order in which they appear in the
+      `PRIMARY KEY()` clause of the table's equivalent DDL statement (the DDL
+      statement used to create the table).
       Delete is idempotent. The transaction will succeed even if some or all
       rows do not exist.
   *   `table` (*type:* `String.t`, *default:* `nil`) - Required. The table whose rows will be deleted.
--- clients/spanner/lib/google_api/spanner/v1/model/expr.ex
+++ clients/spanner/lib/google_api/spanner/v1/model/expr.ex
@@ -17,24 +17,47 @@
 
 defmodule GoogleApi.Spanner.V1.Model.Expr do
   @moduledoc """
-  Represents an expression text. Example:
+  Represents a textual expression in the Common Expression Language (CEL)
+  syntax. CEL is a C-like expression language. The syntax and semantics of CEL
+  are documented at https://github.com/google/cel-spec.
 
-      title: "User account presence"
-      description: "Determines whether the request has a user account"
-      expression: "size(request.user) > 0"
+  Example (Comparison):
+
+      title: "Summary size limit"
+      description: "Determines if a summary is less than 100 chars"
+      expression: "document.summary.size() < 100"
+
+  Example (Equality):
+
+      title: "Requestor is owner"
+      description: "Determines if requestor is the document owner"
+      expression: "document.owner == request.auth.claims.email"
+
+  Example (Logic):
+
+      title: "Public documents"
+      description: "Determine whether the document should be publicly visible"
+      expression: "document.type != 'private' && document.type != 'internal'"
+
+  Example (Data Manipulation):
+
+      title: "Notification string"
+      description: "Create a notification string with a timestamp."
+      expression: "'New message received at ' + string(document.create_time)"
+
+  The exact variables and functions that may be referenced within an expression
+  are determined by the service that evaluates it. See the service
+  documentation for additional information.
 
   ## Attributes
 
-  *   `description` (*type:* `String.t`, *default:* `nil`) - An optional description of the expression. This is a longer text which
+  *   `description` (*type:* `String.t`, *default:* `nil`) - Optional. Description of the expression. This is a longer text which
       describes the expression, e.g. when hovered over it in a UI.
-  *   `expression` (*type:* `String.t`, *default:* `nil`) - Textual representation of an expression in
-      Common Expression Language syntax.
-
-      The application context of the containing message determines which
-      well-known feature set of CEL is supported.
-  *   `location` (*type:* `String.t`, *default:* `nil`) - An optional string indicating the location of the expression for error
+  *   `expression` (*type:* `String.t`, *default:* `nil`) - Textual representation of an expression in Common Expression Language
+      syntax.
+  *   `location` (*type:* `String.t`, *default:* `nil`) - Optional. String indicating the location of the expression for error
       reporting, e.g. a file name and a position in the file.
-  *   `title` (*type:* `String.t`, *default:* `nil`) - An optional title for the expression, i.e. a short string describing
+  *   `title` (*type:* `String.t`, *default:* `nil`) - Optional. Title for the expression, i.e. a short string describing
       its purpose. This can be used e.g. in UIs which allow to enter the
       expression.
   """
