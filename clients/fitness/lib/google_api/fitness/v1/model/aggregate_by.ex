798412b89e6b7df581f4c3d80f84537509fddf3d
yoshi-automation <yoshi-automation@google.com>
2020-02-20 05:19:21 -0800

    Regenerate Fitness client

--- clients/fitness/lib/google_api/fitness/v1/model/aggregate_by.ex
+++ clients/fitness/lib/google_api/fitness/v1/model/aggregate_by.ex
@@ -21,8 +21,8 @@ defmodule GoogleApi.Fitness.V1.Model.AggregateBy do
 
   ## Attributes
 
-  *   `dataSourceId` (*type:* `String.t`, *default:* `nil`) - A data source ID to aggregate. Mutually exclusive of dataTypeName. Only data from the specified data source ID will be included in the aggregation. The dataset in the response will have the same data source ID.
-  *   `dataTypeName` (*type:* `String.t`, *default:* `nil`) - The data type to aggregate. All data sources providing this data type will contribute data to the aggregation. The response will contain a single dataset for this data type name. The dataset will have a data source ID of derived:com.google.:com.google.android.gms:aggregated
+  *   `dataSourceId` (*type:* `String.t`, *default:* `nil`) - A data source ID to aggregate. Only data from the specified data source ID will be included in the aggregation. If specified, this data source must exist; the OAuth scopes in the supplied credentials must grant read access to this data type. The dataset in the response will have the same data source ID. Note: Data can be aggregated by either the dataTypeName or the dataSourceId, not both.
+  *   `dataTypeName` (*type:* `String.t`, *default:* `nil`) - The data type to aggregate. All data sources providing this data type will contribute data to the aggregation. The response will contain a single dataset for this data type name. The dataset will have a data source ID of derived::com.google.android.gms:aggregated. If the user has no data for this data type, an empty data set will be returned. Note: Data can be aggregated by either the dataTypeName or the dataSourceId, not both.
   """
 
   use GoogleApi.Gax.ModelBase
