a1eb23794a0ec14c3fa018fdbebb45a3e38571c6
yoshi-automation <yoshi-automation@google.com>
2020-02-22 05:03:27 -0800

    Regenerate AccessContextManager client

--- clients/access_context_manager/lib/google_api/access_context_manager/v1/api/access_policies.ex
+++ clients/access_context_manager/lib/google_api/access_context_manager/v1/api/access_policies.ex
@@ -762,6 +762,172 @@ defmodule GoogleApi.AccessContextManager.V1.Api.AccessPolicies do
   end
 
   @doc """
+  Replace all existing Access Levels in an Access
+  Policy with
+  the Access Levels provided. This
+  is done within one transaction. The longrunning operation from this RPC
+  will have a successful status once all replacements have propagated to
+  long-lasting storage. Replacements containing errors will result in an
+  error response for the first error encountered and the transaction will be
+  cancelled. Operation.response field will contain
+  ReplaceAccessLevelsResponse. Removing Access Levels contained in existing
+  Service Perimeters will result in
+  error.
+
+  ## Parameters
+
+  *   `connection` (*type:* `GoogleApi.AccessContextManager.V1.Connection.t`) - Connection to server
+  *   `parent` (*type:* `String.t`) - Required. Resource name for the access policy which owns these
+      Access Levels.
+
+      Format: `accessPolicies/{policy_id}`
+  *   `optional_params` (*type:* `keyword()`) - Optional parameters
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
+      *   `:access_token` (*type:* `String.t`) - OAuth access token.
+      *   `:alt` (*type:* `String.t`) - Data format for response.
+      *   `:callback` (*type:* `String.t`) - JSONP
+      *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
+      *   `:key` (*type:* `String.t`) - API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+      *   `:oauth_token` (*type:* `String.t`) - OAuth 2.0 token for the current user.
+      *   `:prettyPrint` (*type:* `boolean()`) - Returns response with indentations and line breaks.
+      *   `:quotaUser` (*type:* `String.t`) - Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
+      *   `:uploadType` (*type:* `String.t`) - Legacy upload protocol for media (e.g. "media", "multipart").
+      *   `:upload_protocol` (*type:* `String.t`) - Upload protocol for media (e.g. "raw", "multipart").
+      *   `:body` (*type:* `GoogleApi.AccessContextManager.V1.Model.ReplaceAccessLevelsRequest.t`) - 
+  *   `opts` (*type:* `keyword()`) - Call options
+
+  ## Returns
+
+  *   `{:ok, %GoogleApi.AccessContextManager.V1.Model.Operation{}}` on success
+  *   `{:error, info}` on failure
+  """
+  @spec accesscontextmanager_access_policies_access_levels_replace_all(
+          Tesla.Env.client(),
+          String.t(),
+          keyword(),
+          keyword()
+        ) ::
+          {:ok, GoogleApi.AccessContextManager.V1.Model.Operation.t()} | {:error, Tesla.Env.t()}
+  def accesscontextmanager_access_policies_access_levels_replace_all(
+        connection,
+        parent,
+        optional_params \\ [],
+        opts \\ []
+      ) do
+    optional_params_config = %{
+      :"$.xgafv" => :query,
+      :access_token => :query,
+      :alt => :query,
+      :callback => :query,
+      :fields => :query,
+      :key => :query,
+      :oauth_token => :query,
+      :prettyPrint => :query,
+      :quotaUser => :query,
+      :uploadType => :query,
+      :upload_protocol => :query,
+      :body => :body
+    }
+
+    request =
+      Request.new()
+      |> Request.method(:post)
+      |> Request.url("/v1/{+parent}/accessLevels:replaceAll", %{
+        "parent" => URI.encode(parent, &URI.char_unreserved?/1)
+      })
+      |> Request.add_optional_params(optional_params_config, optional_params)
+      |> Request.library_version(@library_version)
+
+    connection
+    |> Connection.execute(request)
+    |> Response.decode(opts ++ [struct: %GoogleApi.AccessContextManager.V1.Model.Operation{}])
+  end
+
+  @doc """
+  Commit the dry-run spec for all the Service Perimeters in an
+  Access Policy.
+  A commit operation on a Service Perimeter involves copying its `spec` field
+  to that Service Perimeter's `status` field. Only Service Perimeters with
+  `use_explicit_dry_run_spec` field set to true are affected by a commit
+  operation. The longrunning operation from this RPC will have a successful
+  status once the dry-run specs for all the Service Perimeters have been
+  committed. If a commit fails, it will cause the longrunning operation to
+  return an error response and the entire commit operation will be cancelled.
+  When successful, Operation.response field will contain
+  CommitServicePerimetersResponse. The `dry_run` and the `spec` fields will
+  be cleared after a successful commit operation.
+
+  ## Parameters
+
+  *   `connection` (*type:* `GoogleApi.AccessContextManager.V1.Connection.t`) - Connection to server
+  *   `parent` (*type:* `String.t`) - Required. Resource name for the parent Access Policy which owns all
+      Service Perimeters in scope for
+      the commit operation.
+
+      Format: `accessPolicies/{policy_id}`
+  *   `optional_params` (*type:* `keyword()`) - Optional parameters
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
+      *   `:access_token` (*type:* `String.t`) - OAuth access token.
+      *   `:alt` (*type:* `String.t`) - Data format for response.
+      *   `:callback` (*type:* `String.t`) - JSONP
+      *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
+      *   `:key` (*type:* `String.t`) - API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+      *   `:oauth_token` (*type:* `String.t`) - OAuth 2.0 token for the current user.
+      *   `:prettyPrint` (*type:* `boolean()`) - Returns response with indentations and line breaks.
+      *   `:quotaUser` (*type:* `String.t`) - Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
+      *   `:uploadType` (*type:* `String.t`) - Legacy upload protocol for media (e.g. "media", "multipart").
+      *   `:upload_protocol` (*type:* `String.t`) - Upload protocol for media (e.g. "raw", "multipart").
+      *   `:body` (*type:* `GoogleApi.AccessContextManager.V1.Model.CommitServicePerimetersRequest.t`) - 
+  *   `opts` (*type:* `keyword()`) - Call options
+
+  ## Returns
+
+  *   `{:ok, %GoogleApi.AccessContextManager.V1.Model.Operation{}}` on success
+  *   `{:error, info}` on failure
+  """
+  @spec accesscontextmanager_access_policies_service_perimeters_commit(
+          Tesla.Env.client(),
+          String.t(),
+          keyword(),
+          keyword()
+        ) ::
+          {:ok, GoogleApi.AccessContextManager.V1.Model.Operation.t()} | {:error, Tesla.Env.t()}
+  def accesscontextmanager_access_policies_service_perimeters_commit(
+        connection,
+        parent,
+        optional_params \\ [],
+        opts \\ []
+      ) do
+    optional_params_config = %{
+      :"$.xgafv" => :query,
+      :access_token => :query,
+      :alt => :query,
+      :callback => :query,
+      :fields => :query,
+      :key => :query,
+      :oauth_token => :query,
+      :prettyPrint => :query,
+      :quotaUser => :query,
+      :uploadType => :query,
+      :upload_protocol => :query,
+      :body => :body
+    }
+
+    request =
+      Request.new()
+      |> Request.method(:post)
+      |> Request.url("/v1/{+parent}/servicePerimeters:commit", %{
+        "parent" => URI.encode(parent, &URI.char_unreserved?/1)
+      })
+      |> Request.add_optional_params(optional_params_config, optional_params)
+      |> Request.library_version(@library_version)
+
+    connection
+    |> Connection.execute(request)
+    |> Response.decode(opts ++ [struct: %GoogleApi.AccessContextManager.V1.Model.Operation{}])
+  end
+
+  @doc """
   Create an Service Perimeter. The
   longrunning operation from this RPC will have a successful status once the
   Service Perimeter has
@@ -1132,6 +1298,86 @@ defmodule GoogleApi.AccessContextManager.V1.Api.AccessPolicies do
       |> Request.method(:patch)
       |> Request.url("/v1/{+name}", %{
         "name" => URI.encode(name, &URI.char_unreserved?/1)
+      })
+      |> Request.add_optional_params(optional_params_config, optional_params)
+      |> Request.library_version(@library_version)
+
+    connection
+    |> Connection.execute(request)
+    |> Response.decode(opts ++ [struct: %GoogleApi.AccessContextManager.V1.Model.Operation{}])
+  end
+
+  @doc """
+  Replace all existing Service Perimeters in an
+  Access Policy
+  with the Service Perimeters provided.
+  This is done within one transaction. The longrunning operation from this
+  RPC will have a successful status once all replacements have propagated to
+  long-lasting storage. Replacements containing errors will result in an
+  error response for the first error encountered and the transaction will be
+  cancelled. Operation.response field will contain
+  ReplaceServicePerimetersResponse.
+
+  ## Parameters
+
+  *   `connection` (*type:* `GoogleApi.AccessContextManager.V1.Connection.t`) - Connection to server
+  *   `parent` (*type:* `String.t`) - Required. Resource name for the access policy which owns these
+      Service Perimeters.
+
+      Format: `accessPolicies/{policy_id}`
+  *   `optional_params` (*type:* `keyword()`) - Optional parameters
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
+      *   `:access_token` (*type:* `String.t`) - OAuth access token.
+      *   `:alt` (*type:* `String.t`) - Data format for response.
+      *   `:callback` (*type:* `String.t`) - JSONP
+      *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
+      *   `:key` (*type:* `String.t`) - API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+      *   `:oauth_token` (*type:* `String.t`) - OAuth 2.0 token for the current user.
+      *   `:prettyPrint` (*type:* `boolean()`) - Returns response with indentations and line breaks.
+      *   `:quotaUser` (*type:* `String.t`) - Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
+      *   `:uploadType` (*type:* `String.t`) - Legacy upload protocol for media (e.g. "media", "multipart").
+      *   `:upload_protocol` (*type:* `String.t`) - Upload protocol for media (e.g. "raw", "multipart").
+      *   `:body` (*type:* `GoogleApi.AccessContextManager.V1.Model.ReplaceServicePerimetersRequest.t`) - 
+  *   `opts` (*type:* `keyword()`) - Call options
+
+  ## Returns
+
+  *   `{:ok, %GoogleApi.AccessContextManager.V1.Model.Operation{}}` on success
+  *   `{:error, info}` on failure
+  """
+  @spec accesscontextmanager_access_policies_service_perimeters_replace_all(
+          Tesla.Env.client(),
+          String.t(),
+          keyword(),
+          keyword()
+        ) ::
+          {:ok, GoogleApi.AccessContextManager.V1.Model.Operation.t()} | {:error, Tesla.Env.t()}
+  def accesscontextmanager_access_policies_service_perimeters_replace_all(
+        connection,
+        parent,
+        optional_params \\ [],
+        opts \\ []
+      ) do
+    optional_params_config = %{
+      :"$.xgafv" => :query,
+      :access_token => :query,
+      :alt => :query,
+      :callback => :query,
+      :fields => :query,
+      :key => :query,
+      :oauth_token => :query,
+      :prettyPrint => :query,
+      :quotaUser => :query,
+      :uploadType => :query,
+      :upload_protocol => :query,
+      :body => :body
+    }
+
+    request =
+      Request.new()
+      |> Request.method(:post)
+      |> Request.url("/v1/{+parent}/servicePerimeters:replaceAll", %{
+        "parent" => URI.encode(parent, &URI.char_unreserved?/1)
       })
       |> Request.add_optional_params(optional_params_config, optional_params)
       |> Request.library_version(@library_version)
--- clients/access_context_manager/lib/google_api/access_context_manager/v1/model/service_perimeter.ex
+++ clients/access_context_manager/lib/google_api/access_context_manager/v1/model/service_perimeter.ex
@@ -17,15 +17,16 @@
 
 defmodule GoogleApi.AccessContextManager.V1.Model.ServicePerimeter do
   @moduledoc """
-  `ServicePerimeter` describes a set of GCP resources which can freely import
-  and export data amongst themselves, but not export outside of the
+  `ServicePerimeter` describes a set of Google Cloud resources which can freely
+  import and export data amongst themselves, but not export outside of the
   `ServicePerimeter`. If a request with a source within this `ServicePerimeter`
   has a target outside of the `ServicePerimeter`, the request will be blocked.
   Otherwise the request is allowed. There are two types of Service Perimeter -
-  Regular and Bridge. Regular Service Perimeters cannot overlap, a single GCP
-  project can only belong to a single regular Service Perimeter. Service
-  Perimeter Bridges can contain only GCP projects as members, a single GCP
-  project may belong to multiple Service Perimeter Bridges.
+  Regular and Bridge. Regular Service Perimeters cannot overlap, a single
+  Google Cloud project can only belong to a single regular Service Perimeter.
+  Service Perimeter Bridges can contain only Google Cloud projects as members,
+  a single Google Cloud project may belong to multiple Service Perimeter
+  Bridges.
 
   ## Attributes
 
@@ -41,11 +42,25 @@ defmodule GoogleApi.AccessContextManager.V1.Model.ServicePerimeter do
       without being included in regular perimeter. For perimeter bridges,
       the restricted service list as well as access level lists must be
       empty.
+  *   `spec` (*type:* `GoogleApi.AccessContextManager.V1.Model.ServicePerimeterConfig.t`, *default:* `nil`) - Proposed (or dry run) ServicePerimeter configuration. This configuration
+      allows to specify and test ServicePerimeter configuration without enforcing
+      actual access restrictions. Only allowed to be set when the
+      "use_explicit_dry_run_spec" flag is set.
   *   `status` (*type:* `GoogleApi.AccessContextManager.V1.Model.ServicePerimeterConfig.t`, *default:* `nil`) - Current ServicePerimeter configuration. Specifies sets of resources,
       restricted services and access levels that determine perimeter
       content and boundaries.
   *   `title` (*type:* `String.t`, *default:* `nil`) - Human readable title. Must be unique within the Policy.
   *   `updateTime` (*type:* `DateTime.t`, *default:* `nil`) - Output only. Time the `ServicePerimeter` was updated in UTC.
+  *   `useExplicitDryRunSpec` (*type:* `boolean()`, *default:* `nil`) - Use explicit dry run spec flag. Ordinarily, a dry-run spec implicitly
+      exists  for all Service Perimeters, and that spec is identical to the
+      status for those Service Perimeters. When this flag is set, it inhibits the
+      generation of the implicit spec, thereby allowing the user to explicitly
+      provide a configuration ("spec") to use in a dry-run version of the Service
+      Perimeter. This allows the user to test changes to the enforced config
+      ("status") without actually enforcing them. This testing is done through
+      analyzing the differences between currently enforced and suggested
+      restrictions. use_explicit_dry_run_spec must bet set to True if any of the
+      fields in the spec are set to non-default values.
   """
 
   use GoogleApi.Gax.ModelBase
@@ -55,18 +70,22 @@ defmodule GoogleApi.AccessContextManager.V1.Model.ServicePerimeter do
           :description => String.t(),
           :name => String.t(),
           :perimeterType => String.t(),
+          :spec => GoogleApi.AccessContextManager.V1.Model.ServicePerimeterConfig.t(),
           :status => GoogleApi.AccessContextManager.V1.Model.ServicePerimeterConfig.t(),
           :title => String.t(),
-          :updateTime => DateTime.t()
+          :updateTime => DateTime.t(),
+          :useExplicitDryRunSpec => boolean()
         }
 
   field(:createTime, as: DateTime)
   field(:description)
   field(:name)
   field(:perimeterType)
+  field(:spec, as: GoogleApi.AccessContextManager.V1.Model.ServicePerimeterConfig)
   field(:status, as: GoogleApi.AccessContextManager.V1.Model.ServicePerimeterConfig)
   field(:title)
   field(:updateTime, as: DateTime)
+  field(:useExplicitDryRunSpec)
 end
 
 defimpl Poison.Decoder, for: GoogleApi.AccessContextManager.V1.Model.ServicePerimeter do
