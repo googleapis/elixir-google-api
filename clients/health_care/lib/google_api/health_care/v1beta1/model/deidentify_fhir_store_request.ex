a46de005c62824447d36c532ed0087030c66caf2
yoshi-automation <yoshi-automation@google.com>
2020-02-22 05:24:18 -0800

    Regenerate HealthCare client

--- clients/health_care/lib/google_api/health_care/v1beta1/model/deidentify_fhir_store_request.ex
+++ clients/health_care/lib/google_api/health_care/v1beta1/model/deidentify_fhir_store_request.ex
@@ -29,9 +29,9 @@ defmodule GoogleApi.HealthCare.V1beta1.Model.DeidentifyFhirStoreRequest do
        * The destination dataset must exist.
        * The source dataset and destination dataset must both reside in the same
          project. De-identifying data across multiple projects is not supported.
-       * The destination FHIR store must not exist.
-       * The caller must have the necessary permissions to create the destination
-         FHIR store.
+       * The destination FHIR store must exist.
+       * The caller must have the healthcare.fhirResources.update permission to
+         write to the destination FHIR store.
   *   `resourceFilter` (*type:* `GoogleApi.HealthCare.V1beta1.Model.FhirFilter.t`, *default:* `nil`) - A filter specifying the resources to include in the output. If not
       specified, all resources are included in the output.
   """
