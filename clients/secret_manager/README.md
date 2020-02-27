58e47f1e8995f4ad39bcd40d3a13c84cf57b7a84
yoshi-automation <yoshi-automation@google.com>
2020-02-27 05:28:31 -0800

    Regenerate SecretManager client

--- clients/secret_manager/README.md
+++ clients/secret_manager/README.md
@@ -2,7 +2,7 @@
 
 Secret Manager API client library.
 
-Stores and manages access to application secrets. Provides convenience while improving security.
+Stores API keys, passwords, certificates, and other sensitive data. Provides convenience while improving security.
 
 
 ## Installation
@@ -12,7 +12,7 @@ Install this package from [Hex](https://hex.pm) by adding
 
 ```elixir
 def deps do
-  [{:google_api_secret_manager, "~> 0.2"}]
+  [{:google_api_secret_manager, "~> 0.3"}]
 end
 ```
 
--- clients/secret_manager/mix.exs
+++ clients/secret_manager/mix.exs
@@ -18,7 +18,7 @@
 defmodule GoogleApi.SecretManager.Mixfile do
   use Mix.Project
 
-  @version "0.2.0"
+  @version "0.3.0"
 
   def project() do
     [
@@ -48,7 +48,7 @@ defmodule GoogleApi.SecretManager.Mixfile do
 
   defp description() do
     """
-    Secret Manager API client library. Stores and manages access to application secrets. Provides convenience while improving security.
+    Secret Manager API client library. Stores API keys, passwords, certificates, and other sensitive data. Provides convenience while improving security.
 
     """
   end
