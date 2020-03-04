45c1a4da6af1cfc891ff82bd0d353345a95c6186
Daniel Azuma <dazuma@google.com>
2020-03-04 11:58:47 -0800

    Regenerate CloudFunctions client (#4115)

--- clients/cloud_functions/lib/google_api/cloud_functions/v1/api/operations.ex
+++ clients/cloud_functions/lib/google_api/cloud_functions/v1/api/operations.ex
@@ -54,7 +54,9 @@ defmodule GoogleApi.CloudFunctions.V1.Api.Operations do
   *   `{:error, info}` on failure
   """
   @spec cloudfunctions_operations_get(Tesla.Env.client(), String.t(), keyword(), keyword()) ::
-          {:ok, GoogleApi.CloudFunctions.V1.Model.Operation.t()} | {:error, Tesla.Env.t()}
+          {:ok, GoogleApi.CloudFunctions.V1.Model.Operation.t()}
+          | {:ok, Tesla.Env.t()}
+          | {:error, Tesla.Env.t()}
   def cloudfunctions_operations_get(connection, operations_id, optional_params \\ [], opts \\ []) do
     optional_params_config = %{
       :"$.xgafv" => :query,
@@ -124,6 +126,7 @@ defmodule GoogleApi.CloudFunctions.V1.Api.Operations do
   """
   @spec cloudfunctions_operations_list(Tesla.Env.client(), keyword(), keyword()) ::
           {:ok, GoogleApi.CloudFunctions.V1.Model.ListOperationsResponse.t()}
+          | {:ok, Tesla.Env.t()}
           | {:error, Tesla.Env.t()}
   def cloudfunctions_operations_list(connection, optional_params \\ [], opts \\ []) do
     optional_params_config = %{
