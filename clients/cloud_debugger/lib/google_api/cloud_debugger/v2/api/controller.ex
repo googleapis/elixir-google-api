9624c4a9021b5cd621addf03a34348c750c94327
Daniel Azuma <dazuma@google.com>
2020-03-04 11:59:30 -0800

    Regenerate CloudDebugger client (#4113)

--- clients/cloud_debugger/lib/google_api/cloud_debugger/v2/api/controller.ex
+++ clients/cloud_debugger/lib/google_api/cloud_debugger/v2/api/controller.ex
@@ -62,6 +62,7 @@ defmodule GoogleApi.CloudDebugger.V2.Api.Controller do
   """
   @spec clouddebugger_controller_debuggees_register(Tesla.Env.client(), keyword(), keyword()) ::
           {:ok, GoogleApi.CloudDebugger.V2.Model.RegisterDebuggeeResponse.t()}
+          | {:ok, Tesla.Env.t()}
           | {:error, Tesla.Env.t()}
   def clouddebugger_controller_debuggees_register(connection, optional_params \\ [], opts \\ []) do
     optional_params_config = %{
@@ -148,6 +149,7 @@ defmodule GoogleApi.CloudDebugger.V2.Api.Controller do
           keyword()
         ) ::
           {:ok, GoogleApi.CloudDebugger.V2.Model.ListActiveBreakpointsResponse.t()}
+          | {:ok, Tesla.Env.t()}
           | {:error, Tesla.Env.t()}
   def clouddebugger_controller_debuggees_breakpoints_list(
         connection,
@@ -230,6 +232,7 @@ defmodule GoogleApi.CloudDebugger.V2.Api.Controller do
           keyword()
         ) ::
           {:ok, GoogleApi.CloudDebugger.V2.Model.UpdateActiveBreakpointResponse.t()}
+          | {:ok, Tesla.Env.t()}
           | {:error, Tesla.Env.t()}
   def clouddebugger_controller_debuggees_breakpoints_update(
         connection,
