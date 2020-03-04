0b8d6417306dbc95b2d18039274ed2f69cf14c0d
Daniel Azuma <dazuma@google.com>
2020-03-04 12:15:52 -0800

    Regenerate AcceleratedMobilePageUrl client (#4080)

--- clients/accelerated_mobile_page_url/lib/google_api/accelerated_mobile_page_url/v1/api/amp_urls.ex
+++ clients/accelerated_mobile_page_url/lib/google_api/accelerated_mobile_page_url/v1/api/amp_urls.ex
@@ -54,6 +54,7 @@ defmodule GoogleApi.AcceleratedMobilePageUrl.V1.Api.AmpUrls do
   """
   @spec acceleratedmobilepageurl_amp_urls_batch_get(Tesla.Env.client(), keyword(), keyword()) ::
           {:ok, GoogleApi.AcceleratedMobilePageUrl.V1.Model.BatchGetAmpUrlsResponse.t()}
+          | {:ok, Tesla.Env.t()}
           | {:error, Tesla.Env.t()}
   def acceleratedmobilepageurl_amp_urls_batch_get(connection, optional_params \\ [], opts \\ []) do
     optional_params_config = %{
