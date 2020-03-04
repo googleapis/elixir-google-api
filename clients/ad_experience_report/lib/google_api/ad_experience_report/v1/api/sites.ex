ec12745d02cfe6e17c45ec3141fc3ba79e26f884
yoshi-automation <yoshi-automation@google.com>
2020-03-04 05:04:34 -0800

    Regenerate AdExperienceReport client

--- clients/ad_experience_report/lib/google_api/ad_experience_report/v1/api/sites.ex
+++ clients/ad_experience_report/lib/google_api/ad_experience_report/v1/api/sites.ex
@@ -56,6 +56,7 @@ defmodule GoogleApi.AdExperienceReport.V1.Api.Sites do
   """
   @spec adexperiencereport_sites_get(Tesla.Env.client(), String.t(), keyword(), keyword()) ::
           {:ok, GoogleApi.AdExperienceReport.V1.Model.SiteSummaryResponse.t()}
+          | {:ok, Tesla.Env.t()}
           | {:error, Tesla.Env.t()}
   def adexperiencereport_sites_get(connection, sites_id, optional_params \\ [], opts \\ []) do
     optional_params_config = %{
