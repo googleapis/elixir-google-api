50c75d7be066b42f8ee2530987d6e480e184953e
yoshi-automation <yoshi-automation@google.com>
2020-03-04 05:52:55 -0800

    Regenerate Webmaster client

--- clients/webmaster/lib/google_api/webmaster/v3/api/sites.ex
+++ clients/webmaster/lib/google_api/webmaster/v3/api/sites.ex
@@ -48,7 +48,7 @@ defmodule GoogleApi.Webmaster.V3.Api.Sites do
   *   `{:error, info}` on failure
   """
   @spec webmasters_sites_add(Tesla.Env.client(), String.t(), keyword(), keyword()) ::
-          {:ok, nil} | {:error, Tesla.Env.t()}
+          {:ok, nil} | {:ok, Tesla.Env.t()} | {:error, Tesla.Env.t()}
   def webmasters_sites_add(connection, site_url, optional_params \\ [], opts \\ []) do
     optional_params_config = %{
       :alt => :query,
@@ -97,7 +97,7 @@ defmodule GoogleApi.Webmaster.V3.Api.Sites do
   *   `{:error, info}` on failure
   """
   @spec webmasters_sites_delete(Tesla.Env.client(), String.t(), keyword(), keyword()) ::
-          {:ok, nil} | {:error, Tesla.Env.t()}
+          {:ok, nil} | {:ok, Tesla.Env.t()} | {:error, Tesla.Env.t()}
   def webmasters_sites_delete(connection, site_url, optional_params \\ [], opts \\ []) do
     optional_params_config = %{
       :alt => :query,
@@ -146,7 +146,9 @@ defmodule GoogleApi.Webmaster.V3.Api.Sites do
   *   `{:error, info}` on failure
   """
   @spec webmasters_sites_get(Tesla.Env.client(), String.t(), keyword(), keyword()) ::
-          {:ok, GoogleApi.Webmaster.V3.Model.WmxSite.t()} | {:error, Tesla.Env.t()}
+          {:ok, GoogleApi.Webmaster.V3.Model.WmxSite.t()}
+          | {:ok, Tesla.Env.t()}
+          | {:error, Tesla.Env.t()}
   def webmasters_sites_get(connection, site_url, optional_params \\ [], opts \\ []) do
     optional_params_config = %{
       :alt => :query,
@@ -194,7 +196,9 @@ defmodule GoogleApi.Webmaster.V3.Api.Sites do
   *   `{:error, info}` on failure
   """
   @spec webmasters_sites_list(Tesla.Env.client(), keyword(), keyword()) ::
-          {:ok, GoogleApi.Webmaster.V3.Model.SitesListResponse.t()} | {:error, Tesla.Env.t()}
+          {:ok, GoogleApi.Webmaster.V3.Model.SitesListResponse.t()}
+          | {:ok, Tesla.Env.t()}
+          | {:error, Tesla.Env.t()}
   def webmasters_sites_list(connection, optional_params \\ [], opts \\ []) do
     optional_params_config = %{
       :alt => :query,
