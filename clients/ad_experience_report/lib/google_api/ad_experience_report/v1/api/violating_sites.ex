de4a344e049c02a198c9b5fa687c9c858d94889b
Daniel Azuma <dazuma@google.com>
2020-03-04 12:13:51 -0800

    Regenerate AdExperienceReport client (#4084)

--- clients/ad_experience_report/lib/google_api/ad_experience_report/v1/api/violating_sites.ex
+++ clients/ad_experience_report/lib/google_api/ad_experience_report/v1/api/violating_sites.ex
@@ -53,6 +53,7 @@ defmodule GoogleApi.AdExperienceReport.V1.Api.ViolatingSites do
   """
   @spec adexperiencereport_violating_sites_list(Tesla.Env.client(), keyword(), keyword()) ::
           {:ok, GoogleApi.AdExperienceReport.V1.Model.ViolatingSitesResponse.t()}
+          | {:ok, Tesla.Env.t()}
           | {:error, Tesla.Env.t()}
   def adexperiencereport_violating_sites_list(connection, optional_params \\ [], opts \\ []) do
     optional_params_config = %{
