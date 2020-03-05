99c2c8815c24747a8cd0e6fae55770ffc6df25a4
yoshi-automation <yoshi-automation@google.com>
2020-03-05 05:30:27 -0800

    Regenerate Run client

--- clients/run/lib/google_api/run/v1/api/api.ex
+++ clients/run/lib/google_api/run/v1/api/api.ex
@@ -26,6 +26,131 @@ defmodule GoogleApi.Run.V1.Api.Api do
   @library_version Mix.Project.config() |> Keyword.get(:version, "")
 
   @doc """
+  Rpc to get information about a namespace.
+
+  ## Parameters
+
+  *   `connection` (*type:* `GoogleApi.Run.V1.Connection.t`) - Connection to server
+  *   `namespaces_id` (*type:* `String.t`) - Part of `name`. Required. The name of the namespace being retrieved. If needed, replace
+      {namespace_id} with the project ID.
+  *   `optional_params` (*type:* `keyword()`) - Optional parameters
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
+      *   `:access_token` (*type:* `String.t`) - OAuth access token.
+      *   `:alt` (*type:* `String.t`) - Data format for response.
+      *   `:callback` (*type:* `String.t`) - JSONP
+      *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
+      *   `:key` (*type:* `String.t`) - API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+      *   `:oauth_token` (*type:* `String.t`) - OAuth 2.0 token for the current user.
+      *   `:prettyPrint` (*type:* `boolean()`) - Returns response with indentations and line breaks.
+      *   `:quotaUser` (*type:* `String.t`) - Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
+      *   `:uploadType` (*type:* `String.t`) - Legacy upload protocol for media (e.g. "media", "multipart").
+      *   `:upload_protocol` (*type:* `String.t`) - Upload protocol for media (e.g. "raw", "multipart").
+  *   `opts` (*type:* `keyword()`) - Call options
+
+  ## Returns
+
+  *   `{:ok, %GoogleApi.Run.V1.Model.Namespace{}}` on success
+  *   `{:error, info}` on failure
+  """
+  @spec run_api_v1_namespaces_get(Tesla.Env.client(), String.t(), keyword(), keyword()) ::
+          {:ok, GoogleApi.Run.V1.Model.Namespace.t()}
+          | {:ok, Tesla.Env.t()}
+          | {:error, Tesla.Env.t()}
+  def run_api_v1_namespaces_get(connection, namespaces_id, optional_params \\ [], opts \\ []) do
+    optional_params_config = %{
+      :"$.xgafv" => :query,
+      :access_token => :query,
+      :alt => :query,
+      :callback => :query,
+      :fields => :query,
+      :key => :query,
+      :oauth_token => :query,
+      :prettyPrint => :query,
+      :quotaUser => :query,
+      :uploadType => :query,
+      :upload_protocol => :query
+    }
+
+    request =
+      Request.new()
+      |> Request.method(:get)
+      |> Request.url("/api/v1/namespaces/{namespacesId}", %{
+        "namespacesId" => URI.encode(namespaces_id, &URI.char_unreserved?/1)
+      })
+      |> Request.add_optional_params(optional_params_config, optional_params)
+      |> Request.library_version(@library_version)
+
+    connection
+    |> Connection.execute(request)
+    |> Response.decode(opts ++ [struct: %GoogleApi.Run.V1.Model.Namespace{}])
+  end
+
+  @doc """
+  Rpc to update a namespace.
+
+  ## Parameters
+
+  *   `connection` (*type:* `GoogleApi.Run.V1.Connection.t`) - Connection to server
+  *   `namespaces_id` (*type:* `String.t`) - Part of `name`. Required. The name of the namespace being retrieved. If needed, replace
+      {namespace_id} with the project ID.
+  *   `optional_params` (*type:* `keyword()`) - Optional parameters
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
+      *   `:access_token` (*type:* `String.t`) - OAuth access token.
+      *   `:alt` (*type:* `String.t`) - Data format for response.
+      *   `:callback` (*type:* `String.t`) - JSONP
+      *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
+      *   `:key` (*type:* `String.t`) - API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+      *   `:oauth_token` (*type:* `String.t`) - OAuth 2.0 token for the current user.
+      *   `:prettyPrint` (*type:* `boolean()`) - Returns response with indentations and line breaks.
+      *   `:quotaUser` (*type:* `String.t`) - Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
+      *   `:uploadType` (*type:* `String.t`) - Legacy upload protocol for media (e.g. "media", "multipart").
+      *   `:upload_protocol` (*type:* `String.t`) - Upload protocol for media (e.g. "raw", "multipart").
+      *   `:updateMask` (*type:* `String.t`) - Required. Indicates which fields in the provided namespace to update.
+          This field is currently unused.
+      *   `:body` (*type:* `GoogleApi.Run.V1.Model.Namespace.t`) - 
+  *   `opts` (*type:* `keyword()`) - Call options
+
+  ## Returns
+
+  *   `{:ok, %GoogleApi.Run.V1.Model.Namespace{}}` on success
+  *   `{:error, info}` on failure
+  """
+  @spec run_api_v1_namespaces_patch(Tesla.Env.client(), String.t(), keyword(), keyword()) ::
+          {:ok, GoogleApi.Run.V1.Model.Namespace.t()}
+          | {:ok, Tesla.Env.t()}
+          | {:error, Tesla.Env.t()}
+  def run_api_v1_namespaces_patch(connection, namespaces_id, optional_params \\ [], opts \\ []) do
+    optional_params_config = %{
+      :"$.xgafv" => :query,
+      :access_token => :query,
+      :alt => :query,
+      :callback => :query,
+      :fields => :query,
+      :key => :query,
+      :oauth_token => :query,
+      :prettyPrint => :query,
+      :quotaUser => :query,
+      :uploadType => :query,
+      :upload_protocol => :query,
+      :updateMask => :query,
+      :body => :body
+    }
+
+    request =
+      Request.new()
+      |> Request.method(:patch)
+      |> Request.url("/api/v1/namespaces/{namespacesId}", %{
+        "namespacesId" => URI.encode(namespaces_id, &URI.char_unreserved?/1)
+      })
+      |> Request.add_optional_params(optional_params_config, optional_params)
+      |> Request.library_version(@library_version)
+
+    connection
+    |> Connection.execute(request)
+    |> Response.decode(opts ++ [struct: %GoogleApi.Run.V1.Model.Namespace{}])
+  end
+
+  @doc """
   Creates a new secret.
 
   ## Parameters
