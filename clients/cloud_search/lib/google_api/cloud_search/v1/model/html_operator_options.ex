1237e6924522b9e58004c446a0208a6c059eaf8f
yoshi-automation <yoshi-automation@google.com>
2020-03-12 05:12:15 -0700

    Regenerate CloudSearch client

--- clients/cloud_search/lib/google_api/cloud_search/v1/model/html_operator_options.ex
+++ clients/cloud_search/lib/google_api/cloud_search/v1/model/html_operator_options.ex
@@ -26,9 +26,9 @@ defmodule GoogleApi.CloudSearch.V1.Model.HtmlOperatorOptions do
   *   `operatorName` (*type:* `String.t`, *default:* `nil`) - Indicates the operator name required in the query in order to isolate the
       html property. For example, if operatorName is *subject* and the
       property's name is *subjectLine*, then queries like
-      *subject:&lt;value&gt;* will show results only where the value of the
+      *subject:&lt;value&gt;* show results only where the value of the
       property named *subjectLine* matches *&lt;value&gt;*. By contrast, a
-      search that uses the same *&lt;value&gt;* without an operator will return
+      search that uses the same *&lt;value&gt;* without an operator return
       all items where *&lt;value&gt;* matches the value of any
       html properties or text within the content field for the item.
       The operator name can only contain lowercase letters (a-z).
