8bccb770e86af85a2445595140233fa88a2f6799
yoshi-automation <yoshi-automation@google.com>
2020-04-27 05:15:10 -0700

    Regenerate CustomSearch client

--- clients/custom_search/lib/google_api/custom_search/v1/model/search.ex
+++ clients/custom_search/lib/google_api/custom_search/v1/model/search.ex
@@ -17,38 +17,49 @@
 
 defmodule GoogleApi.CustomSearch.V1.Model.Search do
   @moduledoc """
-
+  Response to a custom search request.
 
   ## Attributes
 
-  *   `context` (*type:* `GoogleApi.CustomSearch.V1.Model.Context.t`, *default:* `nil`) - 
-  *   `items` (*type:* `list(GoogleApi.CustomSearch.V1.Model.Result.t)`, *default:* `nil`) - 
-  *   `kind` (*type:* `String.t`, *default:* `customsearch#search`) - 
-  *   `promotions` (*type:* `list(GoogleApi.CustomSearch.V1.Model.Promotion.t)`, *default:* `nil`) - 
-  *   `queries` (*type:* `map()`, *default:* `nil`) - 
-  *   `searchInformation` (*type:* `GoogleApi.CustomSearch.V1.Model.SearchSearchInformation.t`, *default:* `nil`) - 
-  *   `spelling` (*type:* `GoogleApi.CustomSearch.V1.Model.SearchSpelling.t`, *default:* `nil`) - 
-  *   `url` (*type:* `GoogleApi.CustomSearch.V1.Model.SearchUrl.t`, *default:* `nil`) - 
+  *   `context` (*type:* `map()`, *default:* `nil`) - Metadata and refinements associated with the given search engine,
+      including:
+
+      * The name of the search engine that was used for the query.
+
+      *   A set of [facet
+      objects](https://developers.google.com/custom-search/docs/refinements#create)
+      (refinements) you can use for refining a search.
+  *   `items` (*type:* `list(GoogleApi.CustomSearch.V1.Model.Result.t)`, *default:* `nil`) - The current set of custom search results.
+  *   `kind` (*type:* `String.t`, *default:* `nil`) - Unique identifier for the type of current object. For this API, it is
+      customsearch#search.
+  *   `promotions` (*type:* `list(GoogleApi.CustomSearch.V1.Model.Promotion.t)`, *default:* `nil`) - The set of
+      [promotions](https://developers.google.com/custom-search/docs/promotions).
+      Present only if the custom search engine's configuration files define any
+      promotions for the given query.
+  *   `queries` (*type:* `GoogleApi.CustomSearch.V1.Model.SearchQueries.t`, *default:* `nil`) - Query metadata for the previous, current, and next pages of results.
+  *   `searchInformation` (*type:* `GoogleApi.CustomSearch.V1.Model.SearchSearchInformation.t`, *default:* `nil`) - Metadata about a search operation.
+  *   `spelling` (*type:* `GoogleApi.CustomSearch.V1.Model.SearchSpelling.t`, *default:* `nil`) - Spell correction information for a query.
+  *   `url` (*type:* `GoogleApi.CustomSearch.V1.Model.SearchUrl.t`, *default:* `nil`) - OpenSearch template and URL.
   """
 
   use GoogleApi.Gax.ModelBase
 
   @type t :: %__MODULE__{
-          :context => GoogleApi.CustomSearch.V1.Model.Context.t(),
+          :context => map(),
           :items => list(GoogleApi.CustomSearch.V1.Model.Result.t()),
           :kind => String.t(),
           :promotions => list(GoogleApi.CustomSearch.V1.Model.Promotion.t()),
-          :queries => map(),
+          :queries => GoogleApi.CustomSearch.V1.Model.SearchQueries.t(),
           :searchInformation => GoogleApi.CustomSearch.V1.Model.SearchSearchInformation.t(),
           :spelling => GoogleApi.CustomSearch.V1.Model.SearchSpelling.t(),
           :url => GoogleApi.CustomSearch.V1.Model.SearchUrl.t()
         }
 
-  field(:context, as: GoogleApi.CustomSearch.V1.Model.Context)
+  field(:context, type: :map)
   field(:items, as: GoogleApi.CustomSearch.V1.Model.Result, type: :list)
   field(:kind)
   field(:promotions, as: GoogleApi.CustomSearch.V1.Model.Promotion, type: :list)
-  field(:queries, type: :map)
+  field(:queries, as: GoogleApi.CustomSearch.V1.Model.SearchQueries)
   field(:searchInformation, as: GoogleApi.CustomSearch.V1.Model.SearchSearchInformation)
   field(:spelling, as: GoogleApi.CustomSearch.V1.Model.SearchSpelling)
   field(:url, as: GoogleApi.CustomSearch.V1.Model.SearchUrl)
