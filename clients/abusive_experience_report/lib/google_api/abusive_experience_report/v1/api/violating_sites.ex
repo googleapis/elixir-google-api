671ef3c5156135f84aee14e0fde8c678ee984c40
yoshi-automation <yoshi-automation@google.com>
2020-05-07 05:02:38 -0700

    Regenerate AbusiveExperienceReport client

--- clients/abusive_experience_report/lib/google_api/abusive_experience_report/v1/api/violating_sites.ex
+++ clients/abusive_experience_report/lib/google_api/abusive_experience_report/v1/api/violating_sites.ex
@@ -53,7 +53,7 @@ defmodule GoogleApi.AbusiveExperienceReport.V1.Api.ViolatingSites do
   @spec abusiveexperiencereport_violating_sites_list(Tesla.Env.client(), keyword(), keyword()) ::
           {:ok, GoogleApi.AbusiveExperienceReport.V1.Model.ViolatingSitesResponse.t()}
           | {:ok, Tesla.Env.t()}
-          | {:error, Tesla.Env.t()}
+          | {:error, any()}
   def abusiveexperiencereport_violating_sites_list(connection, optional_params \\ [], opts \\ []) do
     optional_params_config = %{
       :"$.xgafv" => :query,
