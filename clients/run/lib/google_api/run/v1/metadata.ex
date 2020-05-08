217126308c4d016f944e1a5fab0b852ce0fa8435
yoshi-automation <yoshi-automation@google.com>
2020-05-08 05:19:15 -0700

    Regenerate Run client

--- clients/run/lib/google_api/run/v1/metadata.ex
+++ clients/run/lib/google_api/run/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.Run.V1 do
   API client metadata for GoogleApi.Run.V1.
   """
 
-  @discovery_revision "20200415"
+  @discovery_revision "20200505"
 
   def discovery_revision(), do: @discovery_revision
 end
