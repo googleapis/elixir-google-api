bca7a9114163a4139f2bd92c4963529ba10c0854
yoshi-automation <yoshi-automation@google.com>
2020-05-08 05:13:54 -0700

    Regenerate LibraryAgent client

--- clients/library_agent/lib/google_api/library_agent/v1/metadata.ex
+++ clients/library_agent/lib/google_api/library_agent/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.LibraryAgent.V1 do
   API client metadata for GoogleApi.LibraryAgent.V1.
   """
 
-  @discovery_revision "20200505"
+  @discovery_revision "20200506"
 
   def discovery_revision(), do: @discovery_revision
 end
