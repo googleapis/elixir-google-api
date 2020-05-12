4f91e24b6bc4ecc54ecd28d3c84bc06983be6cd3
yoshi-automation <yoshi-automation@google.com>
2020-05-12 09:54:05 -0700

    Regenerate FactCheckTools client

--- clients/fact_check_tools/lib/google_api/fact_check_tools/v1alpha1/metadata.ex
+++ clients/fact_check_tools/lib/google_api/fact_check_tools/v1alpha1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.FactCheckTools.V1alpha1 do
   API client metadata for GoogleApi.FactCheckTools.V1alpha1.
   """
 
-  @discovery_revision "20200505"
+  @discovery_revision "20200511"
 
   def discovery_revision(), do: @discovery_revision
 end
