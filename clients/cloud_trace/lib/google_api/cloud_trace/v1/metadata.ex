53e84f027e865bda01ad43f01de14e6c0c808d37
yoshi-automation <yoshi-automation@google.com>
2020-05-12 09:50:33 -0700

    Regenerate CloudTrace client

--- clients/cloud_trace/lib/google_api/cloud_trace/v1/metadata.ex
+++ clients/cloud_trace/lib/google_api/cloud_trace/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.CloudTrace.V1 do
   API client metadata for GoogleApi.CloudTrace.V1.
   """
 
-  @discovery_revision "20200414"
+  @discovery_revision "20200504"
 
   def discovery_revision(), do: @discovery_revision
 end
