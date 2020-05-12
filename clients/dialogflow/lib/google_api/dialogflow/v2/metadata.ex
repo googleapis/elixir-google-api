4d253547ed2762682e61fa6965b92a0892009357
yoshi-automation <yoshi-automation@google.com>
2020-05-12 09:52:48 -0700

    Regenerate Dialogflow client

--- clients/dialogflow/lib/google_api/dialogflow/v2/metadata.ex
+++ clients/dialogflow/lib/google_api/dialogflow/v2/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.Dialogflow.V2 do
   API client metadata for GoogleApi.Dialogflow.V2.
   """
 
-  @discovery_revision "20200503"
+  @discovery_revision "20200509"
 
   def discovery_revision(), do: @discovery_revision
 end
