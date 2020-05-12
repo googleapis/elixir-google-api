c4f69c7113348baecc802f19564ebcbbd95f0bc5
yoshi-automation <yoshi-automation@google.com>
2020-05-12 10:02:06 -0700

    Regenerate Slides client

--- clients/slides/lib/google_api/slides/v1/metadata.ex
+++ clients/slides/lib/google_api/slides/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.Slides.V1 do
   API client metadata for GoogleApi.Slides.V1.
   """
 
-  @discovery_revision "20200429"
+  @discovery_revision "20200508"
 
   def discovery_revision(), do: @discovery_revision
 end
