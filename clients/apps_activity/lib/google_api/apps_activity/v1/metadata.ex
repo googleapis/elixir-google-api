5109b1f82636872d71eb708e3e66c4932b3f3aef
yoshi-automation <yoshi-automation@google.com>
2020-05-13 05:04:34 -0700

    Regenerate AppsActivity client

--- clients/apps_activity/lib/google_api/apps_activity/v1/metadata.ex
+++ clients/apps_activity/lib/google_api/apps_activity/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.AppsActivity.V1 do
   API client metadata for GoogleApi.AppsActivity.V1.
   """
 
-  @discovery_revision "20200428"
+  @discovery_revision "20200503"
 
   def discovery_revision(), do: @discovery_revision
 end
