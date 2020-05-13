73be36083ef8144014d7c997578a1f6bc7f282db
yoshi-automation <yoshi-automation@google.com>
2020-05-13 05:10:37 -0700

    Regenerate DomainsRDAP client

--- clients/domains_rdap/lib/google_api/domains_rdap/v1/metadata.ex
+++ clients/domains_rdap/lib/google_api/domains_rdap/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.DomainsRDAP.V1 do
   API client metadata for GoogleApi.DomainsRDAP.V1.
   """
 
-  @discovery_revision "20200505"
+  @discovery_revision "20200509"
 
   def discovery_revision(), do: @discovery_revision
 end
