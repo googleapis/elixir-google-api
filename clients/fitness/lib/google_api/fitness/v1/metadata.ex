fceab85cbc3d92bee6afd5d950111c3aff973361
yoshi-automation <yoshi-automation@google.com>
2020-05-13 05:12:19 -0700

    Regenerate Fitness client

--- clients/fitness/lib/google_api/fitness/v1/metadata.ex
+++ clients/fitness/lib/google_api/fitness/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.Fitness.V1 do
   API client metadata for GoogleApi.Fitness.V1.
   """
 
-  @discovery_revision "20200429"
+  @discovery_revision "20200505"
 
   def discovery_revision(), do: @discovery_revision
 end
