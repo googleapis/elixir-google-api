5a855810ec69aee84bfa4a1fdcc72577fa6eb1e0
yoshi-automation <yoshi-automation@google.com>
2020-06-10 05:39:26 -0700

    Regenerate DataFusion client

--- clients/data_fusion/lib/google_api/data_fusion/v1beta1/model/audit_log_config.ex
+++ clients/data_fusion/lib/google_api/data_fusion/v1beta1/model/audit_log_config.ex
@@ -29,7 +29,7 @@ defmodule GoogleApi.DataFusion.V1beta1.Model.AuditLogConfig do
             ]
           },
           {
-            "log_type": "DATA_WRITE",
+            "log_type": "DATA_WRITE"
           }
         ]
       }
