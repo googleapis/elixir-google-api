36388256308669f278f639853042aaa40295b39f
yoshi-automation <yoshi-automation@google.com>
2020-06-18 05:26:23 -0700

    Regenerate CloudErrorReporting client

--- clients/cloud_error_reporting/lib/google_api/cloud_error_reporting/v1beta1/metadata.ex
+++ clients/cloud_error_reporting/lib/google_api/cloud_error_reporting/v1beta1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.CloudErrorReporting.V1beta1 do
   API client metadata for GoogleApi.CloudErrorReporting.V1beta1.
   """
 
-  @discovery_revision "20200415"
+  @discovery_revision "20200610"
 
   def discovery_revision(), do: @discovery_revision
 end
