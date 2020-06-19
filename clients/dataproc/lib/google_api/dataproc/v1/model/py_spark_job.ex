4c4703aeab890176f22948d9a1724886cf89f5e0
yoshi-automation <yoshi-automation@google.com>
2020-06-19 05:43:18 -0700

    Regenerate Dataproc client

--- clients/dataproc/lib/google_api/dataproc/v1/model/py_spark_job.ex
+++ clients/dataproc/lib/google_api/dataproc/v1/model/py_spark_job.ex
@@ -21,9 +21,9 @@ defmodule GoogleApi.Dataproc.V1.Model.PySparkJob do
 
   ## Attributes
 
-  *   `archiveUris` (*type:* `list(String.t)`, *default:* `nil`) - Optional. HCFS URIs of archives to be extracted in the working directory of .jar, .tar, .tar.gz, .tgz, and .zip.
+  *   `archiveUris` (*type:* `list(String.t)`, *default:* `nil`) - Optional. HCFS URIs of archives to be extracted into the working directory of each executor. Supported file types: .jar, .tar, .tar.gz, .tgz, and .zip.
   *   `args` (*type:* `list(String.t)`, *default:* `nil`) - Optional. The arguments to pass to the driver. Do not include arguments, such as --conf, that can be set as job properties, since a collision may occur that causes an incorrect job submission.
-  *   `fileUris` (*type:* `list(String.t)`, *default:* `nil`) - Optional. HCFS URIs of files to be copied to the working directory of Python drivers and distributed tasks. Useful for naively parallel tasks.
+  *   `fileUris` (*type:* `list(String.t)`, *default:* `nil`) - Optional. HCFS URIs of files to be placed in the working directory of each executor. Useful for naively parallel tasks.
   *   `jarFileUris` (*type:* `list(String.t)`, *default:* `nil`) - Optional. HCFS URIs of jar files to add to the CLASSPATHs of the Python driver and tasks.
   *   `loggingConfig` (*type:* `GoogleApi.Dataproc.V1.Model.LoggingConfig.t`, *default:* `nil`) - Optional. The runtime log config for job execution.
   *   `mainPythonFileUri` (*type:* `String.t`, *default:* `nil`) - Required. The HCFS URI of the main Python file to use as the driver. Must be a .py file.
