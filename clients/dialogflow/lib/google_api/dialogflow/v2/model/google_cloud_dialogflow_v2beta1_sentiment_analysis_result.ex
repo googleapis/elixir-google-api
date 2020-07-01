fe60522fdeaccab737e9e2654c919c0b05454fdb
yoshi-automation <yoshi-automation@google.com>
2020-07-01 05:44:05 -0700

    Regenerate Dialogflow client

--- clients/dialogflow/lib/google_api/dialogflow/v2/model/google_cloud_dialogflow_v2beta1_sentiment_analysis_result.ex
+++ clients/dialogflow/lib/google_api/dialogflow/v2/model/google_cloud_dialogflow_v2beta1_sentiment_analysis_result.ex
@@ -17,8 +17,16 @@
 
 defmodule GoogleApi.Dialogflow.V2.Model.GoogleCloudDialogflowV2beta1SentimentAnalysisResult do
   @moduledoc """
-  The result of sentiment analysis as configured by
-  `sentiment_analysis_request_config`.
+  The result of sentiment analysis. Sentiment analysis inspects user input
+  and identifies the prevailing subjective opinion, especially to determine a
+  user's attitude as positive, negative, or neutral.
+  For Participants.AnalyzeContent, it needs to be configured in
+  DetectIntentRequest.query_params. For
+  Participants.StreamingAnalyzeContent, it needs to be configured in
+  StreamingDetectIntentRequest.query_params.
+  And for Participants.AnalyzeContent and
+  Participants.StreamingAnalyzeContent, it needs to be configured in
+  ConversationProfile.human_agent_assistant_config
 
   ## Attributes
 
