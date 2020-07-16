824b73c86c9b5b8e31a4383f694b17fdb6aac4b2
yoshi-automation <yoshi-automation@google.com>
2020-07-16 05:59:48 -0700

    Regenerate Firestore client

--- clients/firestore/lib/google_api/firestore/v1/api/projects.ex
+++ clients/firestore/lib/google_api/firestore/v1/api/projects.ex
@@ -832,6 +832,86 @@ defmodule GoogleApi.Firestore.V1.Api.Projects do
   end
 
   @doc """
+  Applies a batch of write operations.
+
+  The BatchWrite method does not apply the write operations atomically
+  and can apply them out of order. Method does not allow more than one write
+  per document. Each write succeeds or fails independently. See the
+  BatchWriteResponse for the success status of each write.
+
+  If you require an atomically applied set of writes, use
+  Commit instead.
+
+  ## Parameters
+
+  *   `connection` (*type:* `GoogleApi.Firestore.V1.Connection.t`) - Connection to server
+  *   `database` (*type:* `String.t`) - Required. The database name. In the format:
+      `projects/{project_id}/databases/{database_id}`.
+  *   `optional_params` (*type:* `keyword()`) - Optional parameters
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
+      *   `:access_token` (*type:* `String.t`) - OAuth access token.
+      *   `:alt` (*type:* `String.t`) - Data format for response.
+      *   `:callback` (*type:* `String.t`) - JSONP
+      *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
+      *   `:key` (*type:* `String.t`) - API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+      *   `:oauth_token` (*type:* `String.t`) - OAuth 2.0 token for the current user.
+      *   `:prettyPrint` (*type:* `boolean()`) - Returns response with indentations and line breaks.
+      *   `:quotaUser` (*type:* `String.t`) - Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
+      *   `:uploadType` (*type:* `String.t`) - Legacy upload protocol for media (e.g. "media", "multipart").
+      *   `:upload_protocol` (*type:* `String.t`) - Upload protocol for media (e.g. "raw", "multipart").
+      *   `:body` (*type:* `GoogleApi.Firestore.V1.Model.BatchWriteRequest.t`) - 
+  *   `opts` (*type:* `keyword()`) - Call options
+
+  ## Returns
+
+  *   `{:ok, %GoogleApi.Firestore.V1.Model.BatchWriteResponse{}}` on success
+  *   `{:error, info}` on failure
+  """
+  @spec firestore_projects_databases_documents_batch_write(
+          Tesla.Env.client(),
+          String.t(),
+          keyword(),
+          keyword()
+        ) ::
+          {:ok, GoogleApi.Firestore.V1.Model.BatchWriteResponse.t()}
+          | {:ok, Tesla.Env.t()}
+          | {:error, any()}
+  def firestore_projects_databases_documents_batch_write(
+        connection,
+        database,
+        optional_params \\ [],
+        opts \\ []
+      ) do
+    optional_params_config = %{
+      :"$.xgafv" => :query,
+      :access_token => :query,
+      :alt => :query,
+      :callback => :query,
+      :fields => :query,
+      :key => :query,
+      :oauth_token => :query,
+      :prettyPrint => :query,
+      :quotaUser => :query,
+      :uploadType => :query,
+      :upload_protocol => :query,
+      :body => :body
+    }
+
+    request =
+      Request.new()
+      |> Request.method(:post)
+      |> Request.url("/v1/{+database}/documents:batchWrite", %{
+        "database" => URI.encode(database, &URI.char_unreserved?/1)
+      })
+      |> Request.add_optional_params(optional_params_config, optional_params)
+      |> Request.library_version(@library_version)
+
+    connection
+    |> Connection.execute(request)
+    |> Response.decode(opts ++ [struct: %GoogleApi.Firestore.V1.Model.BatchWriteResponse{}])
+  end
+
+  @doc """
   Starts a new transaction.
 
   ## Parameters
@@ -1458,6 +1538,82 @@ defmodule GoogleApi.Firestore.V1.Api.Projects do
     connection
     |> Connection.execute(request)
     |> Response.decode(opts ++ [struct: %GoogleApi.Firestore.V1.Model.ListenResponse{}])
+  end
+
+  @doc """
+  Partitions a query by returning partition cursors that can be used to run
+  the query in parallel. The returned partition cursors are split points that
+  can be used by RunQuery as starting/end points for the query results.
+
+  ## Parameters
+
+  *   `connection` (*type:* `GoogleApi.Firestore.V1.Connection.t`) - Connection to server
+  *   `parent` (*type:* `String.t`) - Required. The parent resource name. In the format:
+      `projects/{project_id}/databases/{database_id}/documents`.
+      Document resource names are not supported; only database resource names
+      can be specified.
+  *   `optional_params` (*type:* `keyword()`) - Optional parameters
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
+      *   `:access_token` (*type:* `String.t`) - OAuth access token.
+      *   `:alt` (*type:* `String.t`) - Data format for response.
+      *   `:callback` (*type:* `String.t`) - JSONP
+      *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
+      *   `:key` (*type:* `String.t`) - API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+      *   `:oauth_token` (*type:* `String.t`) - OAuth 2.0 token for the current user.
+      *   `:prettyPrint` (*type:* `boolean()`) - Returns response with indentations and line breaks.
+      *   `:quotaUser` (*type:* `String.t`) - Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
+      *   `:uploadType` (*type:* `String.t`) - Legacy upload protocol for media (e.g. "media", "multipart").
+      *   `:upload_protocol` (*type:* `String.t`) - Upload protocol for media (e.g. "raw", "multipart").
+      *   `:body` (*type:* `GoogleApi.Firestore.V1.Model.PartitionQueryRequest.t`) - 
+  *   `opts` (*type:* `keyword()`) - Call options
+
+  ## Returns
+
+  *   `{:ok, %GoogleApi.Firestore.V1.Model.PartitionQueryResponse{}}` on success
+  *   `{:error, info}` on failure
+  """
+  @spec firestore_projects_databases_documents_partition_query(
+          Tesla.Env.client(),
+          String.t(),
+          keyword(),
+          keyword()
+        ) ::
+          {:ok, GoogleApi.Firestore.V1.Model.PartitionQueryResponse.t()}
+          | {:ok, Tesla.Env.t()}
+          | {:error, any()}
+  def firestore_projects_databases_documents_partition_query(
+        connection,
+        parent,
+        optional_params \\ [],
+        opts \\ []
+      ) do
+    optional_params_config = %{
+      :"$.xgafv" => :query,
+      :access_token => :query,
+      :alt => :query,
+      :callback => :query,
+      :fields => :query,
+      :key => :query,
+      :oauth_token => :query,
+      :prettyPrint => :query,
+      :quotaUser => :query,
+      :uploadType => :query,
+      :upload_protocol => :query,
+      :body => :body
+    }
+
+    request =
+      Request.new()
+      |> Request.method(:post)
+      |> Request.url("/v1/{+parent}:partitionQuery", %{
+        "parent" => URI.encode(parent, &URI.char_unreserved?/1)
+      })
+      |> Request.add_optional_params(optional_params_config, optional_params)
+      |> Request.library_version(@library_version)
+
+    connection
+    |> Connection.execute(request)
+    |> Response.decode(opts ++ [struct: %GoogleApi.Firestore.V1.Model.PartitionQueryResponse{}])
   end
 
   @doc """
