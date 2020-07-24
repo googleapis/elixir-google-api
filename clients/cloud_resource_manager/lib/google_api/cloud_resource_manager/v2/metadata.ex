e77d0754af592c052da245cf48a9cc0b4f5d5d97
yoshi-automation <yoshi-automation@google.com>
2020-07-24 05:28:41 -0700

    Regenerate CloudResourceManager client

--- clients/cloud_resource_manager/lib/google_api/cloud_resource_manager/v2/metadata.ex
+++ clients/cloud_resource_manager/lib/google_api/cloud_resource_manager/v2/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.CloudResourceManager.V2 do
   API client metadata for GoogleApi.CloudResourceManager.V2.
   """
 
-  @discovery_revision "20200617"
+  @discovery_revision "20200720"
 
   def discovery_revision(), do: @discovery_revision
 end
--- clients/cloud_resource_manager/lib/google_api/cloud_resource_manager/v2/model/search_folders_request.ex
+++ clients/cloud_resource_manager/lib/google_api/cloud_resource_manager/v2/model/search_folders_request.ex
@@ -22,31 +22,8 @@ defmodule GoogleApi.CloudResourceManager.V2.Model.SearchFoldersRequest do
   ## Attributes
 
   *   `pageSize` (*type:* `integer()`, *default:* `nil`) - Optional. The maximum number of folders to return in the response.
-  *   `pageToken` (*type:* `String.t`, *default:* `nil`) - Optional. A pagination token returned from a previous call to `SearchFolders`
-      that indicates from where search should continue.
-  *   `query` (*type:* `String.t`, *default:* `nil`) - Search criteria used to select the Folders to return.
-      If no search criteria is specified then all accessible folders will be
-      returned.
-
-      Query expressions can be used to restrict results based upon displayName,
-      lifecycleState and parent, where the operators `=`, `NOT`, `AND` and `OR`
-      can be used along with the suffix wildcard symbol `*`.
-
-      The displayName field in a query expression should use escaped quotes
-      for values that include whitespace to prevent unexpected behavior.
-
-      Some example queries are:
-
-      * Query `displayName=Test*` returns Folder resources whose display name
-      starts with "Test".
-      * Query `lifecycleState=ACTIVE` returns Folder resources with
-      `lifecycleState` set to `ACTIVE`.
-      * Query `parent=folders/123` returns Folder resources that have
-      `folders/123` as a parent resource.
-      * Query `parent=folders/123 AND lifecycleState=ACTIVE` returns active
-      Folder resources that have `folders/123` as a parent resource.
-      * Query `displayName=\\\\"Test String\\\\"` returns Folder resources with
-      display names that include both "Test" and "String".
+  *   `pageToken` (*type:* `String.t`, *default:* `nil`) - Optional. A pagination token returned from a previous call to `SearchFolders` that indicates from where search should continue.
+  *   `query` (*type:* `String.t`, *default:* `nil`) - Search criteria used to select the Folders to return. If no search criteria is specified then all accessible folders will be returned. Query expressions can be used to restrict results based upon displayName, lifecycleState and parent, where the operators `=`, `NOT`, `AND` and `OR` can be used along with the suffix wildcard symbol `*`. The displayName field in a query expression should use escaped quotes for values that include whitespace to prevent unexpected behavior. Some example queries are: * Query `displayName=Test*` returns Folder resources whose display name starts with "Test". * Query `lifecycleState=ACTIVE` returns Folder resources with `lifecycleState` set to `ACTIVE`. * Query `parent=folders/123` returns Folder resources that have `folders/123` as a parent resource. * Query `parent=folders/123 AND lifecycleState=ACTIVE` returns active Folder resources that have `folders/123` as a parent resource. * Query `displayName=\\\\"Test String\\\\"` returns Folder resources with display names that include both "Test" and "String".
   """
 
   use GoogleApi.Gax.ModelBase
