1fa96c6168b4590b31f6ab489eada819c0b59208
yoshi-automation <yoshi-automation@google.com>
2020-07-29 11:48:10 -0700

    Regenerate AcceleratedMobilePageUrl client

--- clients/accelerated_mobile_page_url/mix.exs
+++ clients/accelerated_mobile_page_url/mix.exs
@@ -18,7 +18,7 @@
 defmodule GoogleApi.AcceleratedMobilePageUrl.Mixfile do
   use Mix.Project
 
-  @version "0.9.2"
+  @version "0.10.0"
 
   def project() do
     [
@@ -40,7 +40,7 @@ defmodule GoogleApi.AcceleratedMobilePageUrl.Mixfile do
 
   defp deps() do
     [
-      {:google_gax, "~> 0.2"},
+      {:google_gax, "~> 0.4"},
 
       {:ex_doc, "~> 0.16", only: :dev}
     ]
