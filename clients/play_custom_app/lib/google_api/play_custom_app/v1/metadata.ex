e0d220571e005cd9f3b864002eb8861c0e37f8f8
yoshi-automation <yoshi-automation@google.com>
2020-07-29 13:14:09 -0700

    Regenerate PlayCustomApp client

--- clients/play_custom_app/lib/google_api/play_custom_app/v1/metadata.ex
+++ clients/play_custom_app/lib/google_api/play_custom_app/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.PlayCustomApp.V1 do
   API client metadata for GoogleApi.PlayCustomApp.V1.
   """
 
-  @discovery_revision "20200618"
+  @discovery_revision "20200728"
 
   def discovery_revision(), do: @discovery_revision
 end
