ffb3b587c69361a9f419543ca121e3b632d2582c
yoshi-automation <yoshi-automation@google.com>
2020-07-29 12:55:21 -0700

    Regenerate GroupsSettings client

--- clients/groups_settings/lib/google_api/groups_settings/v1/metadata.ex
+++ clients/groups_settings/lib/google_api/groups_settings/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.GroupsSettings.V1 do
   API client metadata for GoogleApi.GroupsSettings.V1.
   """
 
-  @discovery_revision "20190725"
+  @discovery_revision "20200723"
 
   def discovery_revision(), do: @discovery_revision
 end
