7e7299903cc47205dfcb8ed8f94339d3e17a9210
yoshi-automation <yoshi-automation@google.com>
2020-07-29 13:23:32 -0700

    Regenerate SearchConsole client

--- clients/search_console/mix.exs
+++ clients/search_console/mix.exs
@@ -18,7 +18,7 @@
 defmodule GoogleApi.SearchConsole.Mixfile do
   use Mix.Project
 
-  @version "0.11.0"
+  @version "0.12.0"
 
   def project() do
     [
@@ -40,7 +40,7 @@ defmodule GoogleApi.SearchConsole.Mixfile do
 
   defp deps() do
     [
-      {:google_gax, "~> 0.2"},
+      {:google_gax, "~> 0.4"},
 
       {:ex_doc, "~> 0.16", only: :dev}
     ]
