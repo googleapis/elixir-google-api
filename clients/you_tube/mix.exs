0695e91a467807b8e8b69ff0d9daea178335c4fa
yoshi-automation <yoshi-automation@google.com>
2020-07-29 13:46:53 -0700

    Regenerate YouTube client

--- clients/you_tube/mix.exs
+++ clients/you_tube/mix.exs
@@ -18,7 +18,7 @@
 defmodule GoogleApi.YouTube.Mixfile do
   use Mix.Project
 
-  @version "0.21.0"
+  @version "0.22.0"
 
   def project() do
     [
@@ -40,7 +40,7 @@ defmodule GoogleApi.YouTube.Mixfile do
 
   defp deps() do
     [
-      {:google_gax, "~> 0.2"},
+      {:google_gax, "~> 0.4"},
 
       {:ex_doc, "~> 0.16", only: :dev}
     ]
