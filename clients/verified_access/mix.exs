9c69c258890eb2f6a2ab6747038cec0623dcafef
yoshi-automation <yoshi-automation@google.com>
2020-07-29 13:43:03 -0700

    Regenerate VerifiedAccess client

--- clients/verified_access/mix.exs
+++ clients/verified_access/mix.exs
@@ -18,7 +18,7 @@
 defmodule GoogleApi.VerifiedAccess.Mixfile do
   use Mix.Project
 
-  @version "0.5.0"
+  @version "0.6.0"
 
   def project() do
     [
@@ -40,7 +40,7 @@ defmodule GoogleApi.VerifiedAccess.Mixfile do
 
   defp deps() do
     [
-      {:google_gax, "~> 0.2"},
+      {:google_gax, "~> 0.4"},
 
       {:ex_doc, "~> 0.16", only: :dev}
     ]
