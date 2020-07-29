d147cd087bb7ef845e8bbc6d7a1cd2a71798469f
yoshi-automation <yoshi-automation@google.com>
2020-07-29 12:33:37 -0700

    Regenerate Dataproc client

--- clients/dataproc/mix.exs
+++ clients/dataproc/mix.exs
@@ -18,7 +18,7 @@
 defmodule GoogleApi.Dataproc.Mixfile do
   use Mix.Project
 
-  @version "0.34.1"
+  @version "0.35.0"
 
   def project() do
     [
@@ -40,7 +40,7 @@ defmodule GoogleApi.Dataproc.Mixfile do
 
   defp deps() do
     [
-      {:google_gax, "~> 0.2"},
+      {:google_gax, "~> 0.4"},
 
       {:ex_doc, "~> 0.16", only: :dev}
     ]
