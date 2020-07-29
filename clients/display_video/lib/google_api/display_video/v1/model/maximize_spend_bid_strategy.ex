e18e0383707b85f322b63332a588055f95f0a6a1
yoshi-automation <yoshi-automation@google.com>
2020-07-29 05:43:48 -0700

    Regenerate DisplayVideo client

--- clients/display_video/lib/google_api/display_video/v1/model/maximize_spend_bid_strategy.ex
+++ clients/display_video/lib/google_api/display_video/v1/model/maximize_spend_bid_strategy.ex
@@ -21,6 +21,7 @@ defmodule GoogleApi.DisplayVideo.V1.Model.MaximizeSpendBidStrategy do
 
   ## Attributes
 
+  *   `customBiddingAlgorithmId` (*type:* `String.t`, *default:* `nil`) - The ID of the Custom Bidding Algorithm used by this strategy. Only applicable when performance_goal_type is set to `BIDDING_STRATEGY_PERFORMANCE_GOAL_TYPE_CUSTOM_ALGO`.
   *   `maxAverageCpmBidAmountMicros` (*type:* `String.t`, *default:* `nil`) - The maximum average CPM that may be bid, in micros of the advertiser's currency. Must be greater than or equal to a billable unit of the given currency. For example, 1500000 represents 1.5 standard units of the currency.
   *   `performanceGoalType` (*type:* `String.t`, *default:* `nil`) - Required. The type of the performance goal that the bidding strategy tries to minimize while spending the full budget. `BIDDING_STRATEGY_PERFORMANCE_GOAL_TYPE_VIEWABLE_CPM` is not supported for this strategy.
   """
@@ -28,10 +29,12 @@ defmodule GoogleApi.DisplayVideo.V1.Model.MaximizeSpendBidStrategy do
   use GoogleApi.Gax.ModelBase
 
   @type t :: %__MODULE__{
+          :customBiddingAlgorithmId => String.t(),
           :maxAverageCpmBidAmountMicros => String.t(),
           :performanceGoalType => String.t()
         }
 
+  field(:customBiddingAlgorithmId)
   field(:maxAverageCpmBidAmountMicros)
   field(:performanceGoalType)
 end
