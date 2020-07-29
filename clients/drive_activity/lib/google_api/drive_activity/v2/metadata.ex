d120e9079297b67b602634083efaa2006e0ac23f
yoshi-automation <yoshi-automation@google.com>
2020-07-29 12:42:20 -0700

    Regenerate DriveActivity client

--- clients/drive_activity/lib/google_api/drive_activity/v2/metadata.ex
+++ clients/drive_activity/lib/google_api/drive_activity/v2/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.DriveActivity.V2 do
   API client metadata for GoogleApi.DriveActivity.V2.
   """
 
-  @discovery_revision "20200509"
+  @discovery_revision "20200726"
 
   def discovery_revision(), do: @discovery_revision
 end
