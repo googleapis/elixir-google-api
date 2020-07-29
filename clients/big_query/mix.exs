4ce7f346448ed7c584e395df809ccd55aa71d2ff
yoshi-automation <yoshi-automation@google.com>
2020-07-29 12:02:09 -0700

    Regenerate BigQuery client

--- clients/big_query/mix.exs
+++ clients/big_query/mix.exs
@@ -18,7 +18,7 @@
 defmodule GoogleApi.BigQuery.Mixfile do
   use Mix.Project
 
-  @version "0.42.0"
+  @version "0.43.0"
 
   def project() do
     [
@@ -40,7 +40,7 @@ defmodule GoogleApi.BigQuery.Mixfile do
 
   defp deps() do
     [
-      {:google_gax, "~> 0.2"},
+      {:google_gax, "~> 0.4"},
 
       {:ex_doc, "~> 0.16", only: :dev}
     ]
