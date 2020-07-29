ba0aaf1a5f6e1af5c06e5aea7c3c2c6e71460589
yoshi-automation <yoshi-automation@google.com>
2020-07-29 12:44:27 -0700

    Regenerate File client

--- clients/file/lib/google_api/file/v1/metadata.ex
+++ clients/file/lib/google_api/file/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.File.V1 do
   API client metadata for GoogleApi.File.V1.
   """
 
-  @discovery_revision "20200707"
+  @discovery_revision "20200714"
 
   def discovery_revision(), do: @discovery_revision
 end
