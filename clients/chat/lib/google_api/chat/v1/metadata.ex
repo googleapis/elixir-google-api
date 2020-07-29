38c5e1f44a936e01cf4e22e39fb20574eabc2f49
yoshi-automation <yoshi-automation@google.com>
2020-07-29 12:09:30 -0700

    Regenerate Chat client

--- clients/chat/lib/google_api/chat/v1/metadata.ex
+++ clients/chat/lib/google_api/chat/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.Chat.V1 do
   API client metadata for GoogleApi.Chat.V1.
   """
 
-  @discovery_revision "20200722"
+  @discovery_revision "20200723"
 
   def discovery_revision(), do: @discovery_revision
 end
