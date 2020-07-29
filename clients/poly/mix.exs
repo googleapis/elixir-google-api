0cf5d50ff67b0d8d6f89fc9813cc47cd35f7cb41
yoshi-automation <yoshi-automation@google.com>
2020-07-29 13:15:39 -0700

    Regenerate Poly client

--- clients/poly/mix.exs
+++ clients/poly/mix.exs
@@ -18,7 +18,7 @@
 defmodule GoogleApi.Poly.Mixfile do
   use Mix.Project
 
-  @version "0.8.1"
+  @version "0.9.0"
 
   def project() do
     [
@@ -40,7 +40,7 @@ defmodule GoogleApi.Poly.Mixfile do
 
   defp deps() do
     [
-      {:google_gax, "~> 0.2"},
+      {:google_gax, "~> 0.4"},
 
       {:ex_doc, "~> 0.16", only: :dev}
     ]
