ce24468705c6c4e2451529b2a4895e4e79cdb4ce
yoshi-automation <yoshi-automation@google.com>
2020-07-29 13:10:33 -0700

    Regenerate OAuth2 client

--- clients/o_auth2/mix.exs
+++ clients/o_auth2/mix.exs
@@ -18,7 +18,7 @@
 defmodule GoogleApi.OAuth2.Mixfile do
   use Mix.Project
 
-  @version "0.12.0"
+  @version "0.13.0"
 
   def project() do
     [
@@ -40,7 +40,7 @@ defmodule GoogleApi.OAuth2.Mixfile do
 
   defp deps() do
     [
-      {:google_gax, "~> 0.2"},
+      {:google_gax, "~> 0.4"},
 
       {:ex_doc, "~> 0.16", only: :dev}
     ]
