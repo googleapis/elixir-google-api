386094ccdd7bcb84d1dbb0a2bb501dbd456b7786
yoshi-automation <yoshi-automation@google.com>
2020-07-29 13:20:37 -0700

    Regenerate RuntimeConfig client

--- clients/runtime_config/lib/google_api/runtime_config/v1/metadata.ex
+++ clients/runtime_config/lib/google_api/runtime_config/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.RuntimeConfig.V1 do
   API client metadata for GoogleApi.RuntimeConfig.V1.
   """
 
-  @discovery_revision "20200512"
+  @discovery_revision "20200727"
 
   def discovery_revision(), do: @discovery_revision
 end
