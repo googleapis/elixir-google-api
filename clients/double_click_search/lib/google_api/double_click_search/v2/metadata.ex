ce03a1d388db597361ea367f3e017594890f57c3
yoshi-automation <yoshi-automation@google.com>
2020-08-03 10:41:46 -0700

    Regenerate DoubleClickSearch client

--- clients/double_click_search/lib/google_api/double_click_search/v2/metadata.ex
+++ clients/double_click_search/lib/google_api/double_click_search/v2/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.DoubleClickSearch.V2 do
   API client metadata for GoogleApi.DoubleClickSearch.V2.
   """
 
-  @discovery_revision "20200723"
+  @discovery_revision "20200728"
 
   def discovery_revision(), do: @discovery_revision
 end
