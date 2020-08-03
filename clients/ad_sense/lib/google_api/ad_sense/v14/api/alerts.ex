9550213c49675424f115b6aa3de8cff7d0feea27
yoshi-automation <yoshi-automation@google.com>
2020-08-03 09:54:59 -0700

    Regenerate AdSense client

--- clients/ad_sense/lib/google_api/ad_sense/v14/api/alerts.ex
+++ clients/ad_sense/lib/google_api/ad_sense/v14/api/alerts.ex
@@ -64,7 +64,7 @@ defmodule GoogleApi.AdSense.V14.Api.Alerts do
       Request.new()
       |> Request.method(:delete)
       |> Request.url("/alerts/{alertId}", %{
-        "alertId" => URI.encode(alert_id, &URI.char_unreserved?/1)
+        "alertId" => URI.encode(alert_id, &(URI.char_unreserved?(&1) || &1 == ?/))
       })
       |> Request.add_optional_params(optional_params_config, optional_params)
       |> Request.library_version(@library_version)
