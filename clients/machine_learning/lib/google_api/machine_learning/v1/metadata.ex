7dc651c36478882b5c5d61c227c23f9a670b4cb1
yoshi-automation <yoshi-automation@google.com>
2020-08-05 06:10:13 -0700

    Regenerate MachineLearning client

--- clients/machine_learning/lib/google_api/machine_learning/v1/metadata.ex
+++ clients/machine_learning/lib/google_api/machine_learning/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.MachineLearning.V1 do
   API client metadata for GoogleApi.MachineLearning.V1.
   """
 
-  @discovery_revision "20200703"
+  @discovery_revision "20200714"
 
   def discovery_revision(), do: @discovery_revision
 end
