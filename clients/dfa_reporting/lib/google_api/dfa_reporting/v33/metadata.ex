ae0cb24732d8c47ddc306bcc7903304be309130a
yoshi-automation <yoshi-automation@google.com>
2020-08-05 05:38:22 -0700

    Regenerate DFAReporting client

--- clients/dfa_reporting/lib/google_api/dfa_reporting/v33/metadata.ex
+++ clients/dfa_reporting/lib/google_api/dfa_reporting/v33/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.DFAReporting.V33 do
   API client metadata for GoogleApi.DFAReporting.V33.
   """
 
-  @discovery_revision "20200722"
+  @discovery_revision "20200730"
 
   def discovery_revision(), do: @discovery_revision
 end
