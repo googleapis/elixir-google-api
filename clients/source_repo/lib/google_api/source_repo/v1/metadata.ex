cda37c882b31132a290f9c337100eba8d05fe187
yoshi-automation <yoshi-automation@google.com>
2020-08-07 06:19:56 -0700

    Regenerate SourceRepo client

--- clients/source_repo/lib/google_api/source_repo/v1/metadata.ex
+++ clients/source_repo/lib/google_api/source_repo/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.SourceRepo.V1 do
   API client metadata for GoogleApi.SourceRepo.V1.
   """
 
-  @discovery_revision "20200520"
+  @discovery_revision "20200729"
 
   def discovery_revision(), do: @discovery_revision
 end
