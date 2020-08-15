1906ec4aaaea5a8b380b89ccf460e35eaea87016
Daniel Azuma <dazuma@google.com>
2020-08-14 18:54:02 -0700

    chore: Update apis.json and discovery script (#6171)

--- lib/google_apis/discovery.ex
+++ lib/google_apis/discovery.ex
@@ -40,9 +40,10 @@ defmodule GoogleApis.Discovery do
   end
 
   def discover_merge() do
+    exceptions = get_exceptions()
     ApiConfig.load_all()
-    |> remove_entries(discover(preferred: false))
-    |> add_entries(discover(preferred: true))
+    |> remove_entries(discover(preferred: false, exceptions: exceptions), exceptions: exceptions)
+    |> add_entries(discover(preferred: true, exceptions: exceptions))
     |> Enum.sort_by(fn config ->
       {config.publish == false, String.downcase(config.name), config.version, config.url}
     end)
@@ -55,37 +56,98 @@ defmodule GoogleApis.Discovery do
   def discover(opts \\ []) do
     conn = Connection.new()
     preferred = Keyword.get(opts, :preferred, true)
+    exceptions = Keyword.get_lazy(opts, :exceptions, &get_exceptions/0)
     {:ok, %{items: items}} = Apis.discovery_apis_list(conn, preferred: preferred)
 
-    Enum.map(items, fn item ->
-      %ApiConfig{
-        name: item.name,
-        version: item.version,
-        url: item.discoveryRestUrl
-      }
+    Enum.flat_map(items, fn %{name: name, version: version, discoveryRestUrl: url} ->
+      if Enum.any?(exceptions, fn
+        %{type: "omit", name: ^name, version: ^version} -> true
+        _ -> false
+      end) do
+        []
+      else
+        [%ApiConfig{name: name, version: version, url: url}]
+      end
     end)
   end
 
-  defp remove_entries(current, discovered) do
-    Enum.map(current, fn %ApiConfig{url: url, publish: publish} = config ->
-      if publish == false || Enum.any?(discovered, fn c -> c.url == url end) do
-        config
-      else
-        %ApiConfig{config | publish: "FIXME: No longer in discovery. Consider removal."}
-      end
+  defmodule Exception do
+    defstruct type: nil,
+              name: nil,
+              version: nil
+  end
+
+  defp get_exceptions() do
+    "./config/exceptions.json"
+    |> Path.expand()
+    |> File.read!()
+    |> Poison.decode!(as: [%Exception{}])
+  end
+
+  defp remove_entries(current, discovered, opts) do
+    exceptions = Keyword.get_lazy(opts, :exceptions, &get_exceptions/0)
+    Enum.map(current, fn
+      (%ApiConfig{publish: false} = cur_config) ->
+        cur_config
+      (%ApiConfig{url: cur_url, version: cur_version, name: cur_name} = cur_config) ->
+        discovered
+        |> Enum.find_value(fn
+          %ApiConfig{url: ^cur_url} ->
+            cur_config
+          %ApiConfig{version: ^cur_version, name: new_name, url: new_url} ->
+            if String.downcase(new_name) == String.downcase(cur_name) do
+              %ApiConfig{cur_config | url: new_url, publish: "CHECKME: Url changed from #{cur_url}"}
+            else
+              false
+            end
+          _ ->
+            false
+        end)
+        |> case do
+          nil ->
+            if Enum.any?(exceptions, fn
+              %{type: "keep", name: ^cur_name, version: ^cur_version} -> true
+              _ -> false
+            end) do
+              cur_config
+            else
+              %ApiConfig{cur_config | publish: "CHECKME: No longer in discovery. Consider removal."}
+            end
+          altered_config ->
+            altered_config
+        end
     end)
   end
 
   defp add_entries(current, discovered) do
-    added = Enum.flat_map(discovered, fn config ->
-      url = config.url
-      if Enum.any?(current, fn c -> c.url == url end) do
-        []
-      else
-        [%ApiConfig{config | publish: "FIXME: Newly added. Adjust the name capitalization."}]
+    Enum.reduce(discovered, current, fn(%ApiConfig{name: new_name, url: new_url, version: new_version} = new_config, configs) ->
+      configs
+      |> Enum.map_reduce(new_name, fn
+        (cur_config, nil) ->
+          {cur_config, nil}
+        (%ApiConfig{url: ^new_url} = cur_config, _) ->
+          {cur_config, nil}
+        (%ApiConfig{url: cur_url, name: cur_name, version: cur_version} = cur_config, working_name) ->
+          if String.downcase(cur_name) == String.downcase(working_name) do
+            if cur_version == new_version do
+              changed_config = %ApiConfig{cur_config | url: new_url, publish: "CHECKME: Url changed from #{cur_url}"}
+              {changed_config, nil}
+            else
+              {cur_config, cur_name}
+            end
+          else
+            {cur_config, working_name}
+          end
+      end)
+      |> case do
+        {modified_configs, nil} ->
+          modified_configs
+        {modified_configs, new_name} ->
+          new_name = if String.downcase(new_name) == new_name, do: String.capitalize(new_name), else: new_name
+          nc = %ApiConfig{new_config | name: new_name, publish: "CHECKME: Newly added. Adjust the name capitalization."}
+          [nc | modified_configs]
       end
     end)
-    current ++ added
   end
 
   defp format_entries(entries) do
