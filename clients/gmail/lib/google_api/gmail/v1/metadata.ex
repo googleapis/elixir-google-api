e64dab4bda98bb79c6f7b17f3ac93e649862b4ba
yoshi-automation <yoshi-automation@google.com>
2020-08-20 06:03:31 -0700

    Regenerate Gmail client

--- clients/gmail/lib/google_api/gmail/v1/metadata.ex
+++ clients/gmail/lib/google_api/gmail/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.Gmail.V1 do
   API client metadata for GoogleApi.Gmail.V1.
   """
 
-  @discovery_revision "20200810"
+  @discovery_revision "20200817"
 
   def discovery_revision(), do: @discovery_revision
 end
