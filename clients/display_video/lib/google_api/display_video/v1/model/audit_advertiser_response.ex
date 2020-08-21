37af18c95960648540b2346e1a22339c51d92099
yoshi-automation <yoshi-automation@google.com>
2020-08-21 05:47:56 -0700

    Regenerate DisplayVideo client

--- clients/display_video/lib/google_api/display_video/v1/model/audit_advertiser_response.ex
+++ clients/display_video/lib/google_api/display_video/v1/model/audit_advertiser_response.ex
@@ -21,6 +21,11 @@ defmodule GoogleApi.DisplayVideo.V1.Model.AuditAdvertiserResponse do
 
   ## Attributes
 
+  *   `adGroupCriteriaCount` (*type:* `String.t`, *default:* `nil`) - The number of individual targeting options from the following targeting types that are assigned to a line item under this advertiser. These individual targeting options count towards the limit of 4500000 ad group targeting options per advertiser. Qualifying Targeting types: * Channels, URLs, apps, and collections * Demographic * Google Audiences, including Affinity, Custom Affinity, and In-market audiences * Inventory source * Keyword * Mobile app category * User lists * Video targeting * Viewability
+  *   `campaignCriteriaCount` (*type:* `String.t`, *default:* `nil`) - The number of individual targeting options from the following targeting types that are assigned to a line item under this advertiser. These individual targeting options count towards the limit of 900000 campaign targeting options per advertiser. Qualifying Targeting types: * Position * Browser * Connection speed * Day and time * Device and operating system * Digital content label * Sensitive categories * Environment * Geography, including business chains and proximity * ISP * Language * Third-party verification
+  *   `channelsCount` (*type:* `String.t`, *default:* `nil`) - The number of channels created under this advertiser. These channels count towards the limit of 1000 channels per advertiser.
+  *   `negativeKeywordListsCount` (*type:* `String.t`, *default:* `nil`) - The number of negative keyword lists created under this advertiser. These negative keyword lists count towards the limit of 20 negative keyword lists per advertiser.
+  *   `negativelyTargetedChannelsCount` (*type:* `String.t`, *default:* `nil`) - The number of negatively targeted channels created under this advertiser. These negatively targeted channels count towards the limit of 5 negatively targeted channels per advertiser.
   *   `usedCampaignsCount` (*type:* `String.t`, *default:* `nil`) - The number of ACTIVE and PAUSED campaigns under this advertiser. These campaigns count towards the limit of 9999 campaigns per advertiser.
   *   `usedInsertionOrdersCount` (*type:* `String.t`, *default:* `nil`) - The number of ACTIVE, PAUSED and DRAFT insertion orders under this advertiser. These insertion orders count towards the limit of 9999 insertion orders per advertiser.
   *   `usedLineItemsCount` (*type:* `String.t`, *default:* `nil`) - The number of ACTIVE, PAUSED, and DRAFT line items under this advertiser. These line items count towards the limit of 9999 line items per advertiser.
@@ -29,11 +34,21 @@ defmodule GoogleApi.DisplayVideo.V1.Model.AuditAdvertiserResponse do
   use GoogleApi.Gax.ModelBase
 
   @type t :: %__MODULE__{
+          :adGroupCriteriaCount => String.t(),
+          :campaignCriteriaCount => String.t(),
+          :channelsCount => String.t(),
+          :negativeKeywordListsCount => String.t(),
+          :negativelyTargetedChannelsCount => String.t(),
           :usedCampaignsCount => String.t(),
           :usedInsertionOrdersCount => String.t(),
           :usedLineItemsCount => String.t()
         }
 
+  field(:adGroupCriteriaCount)
+  field(:campaignCriteriaCount)
+  field(:channelsCount)
+  field(:negativeKeywordListsCount)
+  field(:negativelyTargetedChannelsCount)
   field(:usedCampaignsCount)
   field(:usedInsertionOrdersCount)
   field(:usedLineItemsCount)
