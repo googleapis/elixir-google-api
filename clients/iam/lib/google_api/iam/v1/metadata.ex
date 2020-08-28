2024f2d6b0725e8f95e337dd93f2685b8d7592c7
yoshi-automation <yoshi-automation@google.com>
2020-08-28 06:08:21 -0700

    Regenerate IAM client

--- clients/iam/lib/google_api/iam/v1/metadata.ex
+++ clients/iam/lib/google_api/iam/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.IAM.V1 do
   API client metadata for GoogleApi.IAM.V1.
   """
 
-  @discovery_revision "20200806"
+  @discovery_revision "20200824"
 
   def discovery_revision(), do: @discovery_revision
 end
