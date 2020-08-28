c9cd2b8abdca02587fafaf1c1accad1a7e6dc886
yoshi-automation <yoshi-automation@google.com>
2020-08-28 06:16:31 -0700

    Regenerate MachineLearning client

--- clients/machine_learning/lib/google_api/machine_learning/v1/model/google_cloud_ml_v1__version.ex
+++ clients/machine_learning/lib/google_api/machine_learning/v1/model/google_cloud_ml_v1__version.ex
@@ -23,10 +23,10 @@ defmodule GoogleApi.MachineLearning.V1.Model.GoogleCloudMlV1_Version do
 
   *   `acceleratorConfig` (*type:* `GoogleApi.MachineLearning.V1.Model.GoogleCloudMlV1_AcceleratorConfig.t`, *default:* `nil`) - Optional. Accelerator config for using GPUs for online prediction (beta). Only specify this field if you have specified a Compute Engine (N1) machine type in the `machineType` field. Learn more about [using GPUs for online prediction](/ml-engine/docs/machine-types-online-prediction#gpus).
   *   `autoScaling` (*type:* `GoogleApi.MachineLearning.V1.Model.GoogleCloudMlV1_AutoScaling.t`, *default:* `nil`) - Automatically scale the number of nodes used to serve the model in response to increases and decreases in traffic. Care should be taken to ramp up traffic according to the model's ability to scale or you will start seeing increases in latency and 429 response codes. Note that you cannot use AutoScaling if your version uses [GPUs](#Version.FIELDS.accelerator_config). Instead, you must use specify `manual_scaling`.
+  *   `container` (*type:* `GoogleApi.MachineLearning.V1.Model.GoogleCloudMlV1_ContainerSpec.t`, *default:* `nil`) - 
   *   `createTime` (*type:* `DateTime.t`, *default:* `nil`) - Output only. The time the version was created.
   *   `deploymentUri` (*type:* `String.t`, *default:* `nil`) - Required. The Cloud Storage location of the trained model used to create the version. See the [guide to model deployment](/ml-engine/docs/tensorflow/deploying-models) for more information. When passing Version to projects.models.versions.create the model service uses the specified location as the source of the model. Once deployed, the model version is hosted by the prediction service, so this location is useful only as a historical record. The total number of model files can't exceed 1000.
   *   `description` (*type:* `String.t`, *default:* `nil`) - Optional. The description specified for the version when it was created.
-  *   `endpoints` (*type:* `GoogleApi.MachineLearning.V1.Model.GoogleCloudMlV1_EndpointMap.t`, *default:* `nil`) - Optional. Output only. If set by server, the http(s) endpoints returned to user after the public/private deployment is successful. https://cloud.google.com/apis/design/design_patterns#output_fields.
   *   `errorMessage` (*type:* `String.t`, *default:* `nil`) - Output only. The details of a failure or a cancellation.
   *   `etag` (*type:* `String.t`, *default:* `nil`) - `etag` is used for optimistic concurrency control as a way to help prevent simultaneous updates of a model from overwriting each other. It is strongly suggested that systems make use of the `etag` in the read-modify-write cycle to perform model updates in order to avoid race conditions: An `etag` is returned in the response to `GetVersion`, and systems are expected to put that etag in the request to `UpdateVersion` to ensure that their change will be applied to the model as intended.
   *   `explanationConfig` (*type:* `GoogleApi.MachineLearning.V1.Model.GoogleCloudMlV1_ExplanationConfig.t`, *default:* `nil`) - Optional. Configures explainability features on the model's version. Some explanation features require additional metadata to be loaded as part of the model payload.
@@ -41,6 +41,7 @@ defmodule GoogleApi.MachineLearning.V1.Model.GoogleCloudMlV1_Version do
   *   `predictionClass` (*type:* `String.t`, *default:* `nil`) - Optional. The fully qualified name (module_name.class_name) of a class that implements the Predictor interface described in this reference field. The module containing this class should be included in a package provided to the [`packageUris` field](#Version.FIELDS.package_uris). Specify this field if and only if you are deploying a [custom prediction routine (beta)](/ml-engine/docs/tensorflow/custom-prediction-routines). If you specify this field, you must set [`runtimeVersion`](#Version.FIELDS.runtime_version) to 1.4 or greater and you must set `machineType` to a [legacy (MLS1) machine type](/ml-engine/docs/machine-types-online-prediction). The following code sample provides the Predictor interface: class Predictor(object): \\"\\"\\"Interface for constructing custom predictors.\\"\\"\\" def predict(self, instances, **kwargs): \\"\\"\\"Performs custom prediction. Instances are the decoded values from the request. They have already been deserialized from JSON. Args: instances: A list of prediction input instances. **kwargs: A dictionary of keyword args provided as additional fields on the predict request body. Returns: A list of outputs containing the prediction results. This list must be JSON serializable. \\"\\"\\" raise NotImplementedError() @classmethod def from_path(cls, model_dir): \\"\\"\\"Creates an instance of Predictor using the given path. Loading of the predictor should be done in this method. Args: model_dir: The local directory that contains the exported model file along with any additional files uploaded when creating the version resource. Returns: An instance implementing this Predictor class. \\"\\"\\" raise NotImplementedError() Learn more about [the Predictor interface and custom prediction routines](/ml-engine/docs/tensorflow/custom-prediction-routines).
   *   `pythonVersion` (*type:* `String.t`, *default:* `nil`) - Required. The version of Python used in prediction. The following Python versions are available: * Python '3.7' is available when `runtime_version` is set to '1.15' or later. * Python '3.5' is available when `runtime_version` is set to a version from '1.4' to '1.14'. * Python '2.7' is available when `runtime_version` is set to '1.15' or earlier. Read more about the Python versions available for [each runtime version](/ml-engine/docs/runtime-version-list).
   *   `requestLoggingConfig` (*type:* `GoogleApi.MachineLearning.V1.Model.GoogleCloudMlV1_RequestLoggingConfig.t`, *default:* `nil`) - Optional. *Only* specify this field in a projects.models.versions.patch request. Specifying it in a projects.models.versions.create request has no effect. Configures the request-response pair logging on predictions from this Version.
+  *   `routes` (*type:* `GoogleApi.MachineLearning.V1.Model.GoogleCloudMlV1_RouteMap.t`, *default:* `nil`) - 
   *   `runtimeVersion` (*type:* `String.t`, *default:* `nil`) - Required. The AI Platform runtime version to use for this deployment. For more information, see the [runtime version list](/ml-engine/docs/runtime-version-list) and [how to manage runtime versions](/ml-engine/docs/versioning).
   *   `serviceAccount` (*type:* `String.t`, *default:* `nil`) - Optional. Specifies the service account for resource access control.
   *   `state` (*type:* `String.t`, *default:* `nil`) - Output only. The state of a version.
@@ -52,10 +53,10 @@ defmodule GoogleApi.MachineLearning.V1.Model.GoogleCloudMlV1_Version do
           :acceleratorConfig =>
             GoogleApi.MachineLearning.V1.Model.GoogleCloudMlV1_AcceleratorConfig.t(),
           :autoScaling => GoogleApi.MachineLearning.V1.Model.GoogleCloudMlV1_AutoScaling.t(),
+          :container => GoogleApi.MachineLearning.V1.Model.GoogleCloudMlV1_ContainerSpec.t(),
           :createTime => DateTime.t(),
           :deploymentUri => String.t(),
           :description => String.t(),
-          :endpoints => GoogleApi.MachineLearning.V1.Model.GoogleCloudMlV1_EndpointMap.t(),
           :errorMessage => String.t(),
           :etag => String.t(),
           :explanationConfig =>
@@ -72,6 +73,7 @@ defmodule GoogleApi.MachineLearning.V1.Model.GoogleCloudMlV1_Version do
           :pythonVersion => String.t(),
           :requestLoggingConfig =>
             GoogleApi.MachineLearning.V1.Model.GoogleCloudMlV1_RequestLoggingConfig.t(),
+          :routes => GoogleApi.MachineLearning.V1.Model.GoogleCloudMlV1_RouteMap.t(),
           :runtimeVersion => String.t(),
           :serviceAccount => String.t(),
           :state => String.t()
@@ -82,10 +84,10 @@ defmodule GoogleApi.MachineLearning.V1.Model.GoogleCloudMlV1_Version do
   )
 
   field(:autoScaling, as: GoogleApi.MachineLearning.V1.Model.GoogleCloudMlV1_AutoScaling)
+  field(:container, as: GoogleApi.MachineLearning.V1.Model.GoogleCloudMlV1_ContainerSpec)
   field(:createTime, as: DateTime)
   field(:deploymentUri)
   field(:description)
-  field(:endpoints, as: GoogleApi.MachineLearning.V1.Model.GoogleCloudMlV1_EndpointMap)
   field(:errorMessage)
   field(:etag)
 
@@ -108,6 +110,7 @@ defmodule GoogleApi.MachineLearning.V1.Model.GoogleCloudMlV1_Version do
     as: GoogleApi.MachineLearning.V1.Model.GoogleCloudMlV1_RequestLoggingConfig
   )
 
+  field(:routes, as: GoogleApi.MachineLearning.V1.Model.GoogleCloudMlV1_RouteMap)
   field(:runtimeVersion)
   field(:serviceAccount)
   field(:state)
