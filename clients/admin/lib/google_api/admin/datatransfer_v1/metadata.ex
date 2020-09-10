87638970ff0f063452b4690426c7b08581cef9de
yoshi-automation <yoshi-automation@google.com>
2020-09-10 05:09:06 -0700

    Regenerate Admin client

--- clients/admin/lib/google_api/admin/datatransfer_v1/metadata.ex
+++ clients/admin/lib/google_api/admin/datatransfer_v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.Admin.Datatransfer_v1 do
   API client metadata for GoogleApi.Admin.Datatransfer_v1.
   """
 
-  @discovery_revision "20200819"
+  @discovery_revision "20200831"
 
   def discovery_revision(), do: @discovery_revision
 end
