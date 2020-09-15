df3a46631c51a5250e0e2c24dc6c0e4020feb9e5
yoshi-automation <yoshi-automation@google.com>
2020-09-15 06:34:22 -0700

    Regenerate Sheets client

--- clients/sheets/lib/google_api/sheets/v4/metadata.ex
+++ clients/sheets/lib/google_api/sheets/v4/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.Sheets.V4 do
   API client metadata for GoogleApi.Sheets.V4.
   """
 
-  @discovery_revision "20200825"
+  @discovery_revision "20200908"
 
   def discovery_revision(), do: @discovery_revision
 end
