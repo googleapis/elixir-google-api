c16d1a52195f595b86cdeac2c55bbc94b061738f
yoshi-automation <yoshi-automation@google.com>
2020-09-16 05:13:14 -0700

    Regenerate AndroidEnterprise client

--- clients/android_enterprise/lib/google_api/android_enterprise/v1/connection.ex
+++ clients/android_enterprise/lib/google_api/android_enterprise/v1/connection.ex
@@ -28,5 +28,5 @@ defmodule GoogleApi.AndroidEnterprise.V1.Connection do
       "https://www.googleapis.com/auth/androidenterprise"
     ],
     otp_app: :google_api_android_enterprise,
-    base_url: "https://www.googleapis.com/"
+    base_url: "https://androidenterprise.googleapis.com/"
 end
