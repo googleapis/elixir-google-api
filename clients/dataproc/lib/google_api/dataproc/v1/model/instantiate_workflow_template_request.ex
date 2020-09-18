6f57841e3011e965461a0a783f7b9b4b130e6de8
yoshi-automation <yoshi-automation@google.com>
2020-09-18 05:47:42 -0700

    Regenerate Dataproc client

--- clients/dataproc/lib/google_api/dataproc/v1/model/instantiate_workflow_template_request.ex
+++ clients/dataproc/lib/google_api/dataproc/v1/model/instantiate_workflow_template_request.ex
@@ -21,7 +21,7 @@ defmodule GoogleApi.Dataproc.V1.Model.InstantiateWorkflowTemplateRequest do
 
   ## Attributes
 
-  *   `parameters` (*type:* `map()`, *default:* `nil`) - Optional. Map from parameter names to values that should be used for those parameters. Values may not exceed 100 characters.
+  *   `parameters` (*type:* `map()`, *default:* `nil`) - Optional. Map from parameter names to values that should be used for those parameters. Values may not exceed 1000 characters.
   *   `requestId` (*type:* `String.t`, *default:* `nil`) - Optional. A tag that prevents multiple concurrent workflow instances with the same tag from running. This mitigates risk of concurrent instances started due to retries.It is recommended to always set this value to a UUID (https://en.wikipedia.org/wiki/Universally_unique_identifier).The tag must contain only letters (a-z, A-Z), numbers (0-9), underscores (_), and hyphens (-). The maximum length is 40 characters.
   *   `version` (*type:* `integer()`, *default:* `nil`) - Optional. The version of workflow template to instantiate. If specified, the workflow will be instantiated only if the current version of the workflow template has the supplied version.This option cannot be used to instantiate a previous version of workflow template.
   """
