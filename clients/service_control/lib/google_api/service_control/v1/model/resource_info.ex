32c9d0536852ad28bcf7652051a0a81ae73339ac
yoshi-automation <yoshi-automation@google.com>
2020-09-19 06:35:39 -0700

    Regenerate ServiceControl client

--- clients/service_control/lib/google_api/service_control/v1/model/resource_info.ex
+++ clients/service_control/lib/google_api/service_control/v1/model/resource_info.ex
@@ -21,7 +21,7 @@ defmodule GoogleApi.ServiceControl.V1.Model.ResourceInfo do
 
   ## Attributes
 
-  *   `resourceContainer` (*type:* `String.t`, *default:* `nil`) - The identifier of the parent of this resource instance. Must be in one of the following formats: - “projects/” - “folders/” - “organizations/”
+  *   `resourceContainer` (*type:* `String.t`, *default:* `nil`) - The identifier of the parent of this resource instance. Must be in one of the following formats: - `projects/` - `folders/` - `organizations/`
   *   `resourceLocation` (*type:* `String.t`, *default:* `nil`) - The location of the resource. If not empty, the resource will be checked against location policy. The value must be a valid zone, region or multiregion. For example: "europe-west4" or "northamerica-northeast1-a"
   *   `resourceName` (*type:* `String.t`, *default:* `nil`) - Name of the resource. This is used for auditing purposes.
   """
