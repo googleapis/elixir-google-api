71b6782143c308c880dc8ff8d9781a1801ecd7cd
yoshi-automation <yoshi-automation@google.com>
2020-09-19 05:29:18 -0700

    Regenerate CloudIdentity client

--- clients/cloud_identity/lib/google_api/cloud_identity/v1/api/groups.ex
+++ clients/cloud_identity/lib/google_api/cloud_identity/v1/api/groups.ex
@@ -796,4 +796,77 @@ defmodule GoogleApi.CloudIdentity.V1.Api.Groups do
       opts ++ [struct: %GoogleApi.CloudIdentity.V1.Model.LookupMembershipNameResponse{}]
     )
   end
+
+  @doc """
+  Modifies the `MembershipRole`s of a `Membership`.
+
+  ## Parameters
+
+  *   `connection` (*type:* `GoogleApi.CloudIdentity.V1.Connection.t`) - Connection to server
+  *   `name` (*type:* `String.t`) - Required. The [resource name](https://cloud.google.com/apis/design/resource_names) of the `Membership` whose roles are to be modified. Must be of the form `groups/{group_id}/memberships/{membership_id}`.
+  *   `optional_params` (*type:* `keyword()`) - Optional parameters
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
+      *   `:access_token` (*type:* `String.t`) - OAuth access token.
+      *   `:alt` (*type:* `String.t`) - Data format for response.
+      *   `:callback` (*type:* `String.t`) - JSONP
+      *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
+      *   `:key` (*type:* `String.t`) - API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+      *   `:oauth_token` (*type:* `String.t`) - OAuth 2.0 token for the current user.
+      *   `:prettyPrint` (*type:* `boolean()`) - Returns response with indentations and line breaks.
+      *   `:quotaUser` (*type:* `String.t`) - Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
+      *   `:uploadType` (*type:* `String.t`) - Legacy upload protocol for media (e.g. "media", "multipart").
+      *   `:upload_protocol` (*type:* `String.t`) - Upload protocol for media (e.g. "raw", "multipart").
+      *   `:body` (*type:* `GoogleApi.CloudIdentity.V1.Model.ModifyMembershipRolesRequest.t`) - 
+  *   `opts` (*type:* `keyword()`) - Call options
+
+  ## Returns
+
+  *   `{:ok, %GoogleApi.CloudIdentity.V1.Model.ModifyMembershipRolesResponse{}}` on success
+  *   `{:error, info}` on failure
+  """
+  @spec cloudidentity_groups_memberships_modify_membership_roles(
+          Tesla.Env.client(),
+          String.t(),
+          keyword(),
+          keyword()
+        ) ::
+          {:ok, GoogleApi.CloudIdentity.V1.Model.ModifyMembershipRolesResponse.t()}
+          | {:ok, Tesla.Env.t()}
+          | {:error, any()}
+  def cloudidentity_groups_memberships_modify_membership_roles(
+        connection,
+        name,
+        optional_params \\ [],
+        opts \\ []
+      ) do
+    optional_params_config = %{
+      :"$.xgafv" => :query,
+      :access_token => :query,
+      :alt => :query,
+      :callback => :query,
+      :fields => :query,
+      :key => :query,
+      :oauth_token => :query,
+      :prettyPrint => :query,
+      :quotaUser => :query,
+      :uploadType => :query,
+      :upload_protocol => :query,
+      :body => :body
+    }
+
+    request =
+      Request.new()
+      |> Request.method(:post)
+      |> Request.url("/v1/{+name}:modifyMembershipRoles", %{
+        "name" => URI.encode(name, &URI.char_unreserved?/1)
+      })
+      |> Request.add_optional_params(optional_params_config, optional_params)
+      |> Request.library_version(@library_version)
+
+    connection
+    |> Connection.execute(request)
+    |> Response.decode(
+      opts ++ [struct: %GoogleApi.CloudIdentity.V1.Model.ModifyMembershipRolesResponse{}]
+    )
+  end
 end
