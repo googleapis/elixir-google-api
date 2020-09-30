994a2c4b9c2581475492ed40dc0b2030748473d3
yoshi-automation <yoshi-automation@google.com>
2020-09-30 05:18:01 -0700

    Regenerate AssuredWorkloads client

--- clients/assured_workloads/lib/google_api/assured_workloads/v1beta1/metadata.ex
+++ clients/assured_workloads/lib/google_api/assured_workloads/v1beta1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.AssuredWorkloads.V1beta1 do
   API client metadata for GoogleApi.AssuredWorkloads.V1beta1.
   """
 
-  @discovery_revision "20200917"
+  @discovery_revision "20200924"
 
   def discovery_revision(), do: @discovery_revision
 end
