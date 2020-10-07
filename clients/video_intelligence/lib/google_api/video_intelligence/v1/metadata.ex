c993ae848e0fd1a3b33440fc935854cbafdbb7c1
yoshi-automation <yoshi-automation@google.com>
2020-10-07 06:54:39 -0700

    Regenerate VideoIntelligence client

--- clients/video_intelligence/lib/google_api/video_intelligence/v1/metadata.ex
+++ clients/video_intelligence/lib/google_api/video_intelligence/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.VideoIntelligence.V1 do
   API client metadata for GoogleApi.VideoIntelligence.V1.
   """
 
-  @discovery_revision "20200921"
+  @discovery_revision "20201002"
 
   def discovery_revision(), do: @discovery_revision
 end
