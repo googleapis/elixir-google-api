8e11695798fa1f3f8ef0f94349b3ec0950e20577
yoshi-automation <yoshi-automation@google.com>
2020-10-13 06:15:03 -0700

    Regenerate Licensing client

--- clients/licensing/lib/google_api/licensing/v1/metadata.ex
+++ clients/licensing/lib/google_api/licensing/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.Licensing.V1 do
   API client metadata for GoogleApi.Licensing.V1.
   """
 
-  @discovery_revision "20201005"
+  @discovery_revision "20201010"
 
   def discovery_revision(), do: @discovery_revision
 end
