69bab8d0b1ea1ebf6e149b25a65cb2ab164e4a7e
yoshi-automation <yoshi-automation@google.com>
2020-10-13 05:40:54 -0700

    Regenerate Content client

--- clients/content/lib/google_api/content/v21/metadata.ex
+++ clients/content/lib/google_api/content/v21/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.Content.V21 do
   API client metadata for GoogleApi.Content.V21.
   """
 
-  @discovery_revision "20201008"
+  @discovery_revision "20201012"
 
   def discovery_revision(), do: @discovery_revision
 end
