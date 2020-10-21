1369576c1712bc11a41a03bc794116dca906c9c2
yoshi-automation <yoshi-automation@google.com>
2020-10-21 06:18:22 -0700

    Regenerate MachineLearning client

--- clients/machine_learning/lib/google_api/machine_learning/v1/model/google_cloud_ml_v1__auto_scaling.ex
+++ clients/machine_learning/lib/google_api/machine_learning/v1/model/google_cloud_ml_v1__auto_scaling.ex
@@ -21,15 +21,21 @@ defmodule GoogleApi.MachineLearning.V1.Model.GoogleCloudMlV1_AutoScaling do
 
   ## Attributes
 
+  *   `maxNodes` (*type:* `integer()`, *default:* `nil`) - The maximum number of nodes to scale this model under load. The actual value will depend on resource quota and availability.
+  *   `metrics` (*type:* `list(GoogleApi.MachineLearning.V1.Model.GoogleCloudMlV1_MetricSpec.t)`, *default:* `nil`) - MetricSpec contains the specifications to use to calculate the desired nodes count.
   *   `minNodes` (*type:* `integer()`, *default:* `nil`) - Optional. The minimum number of nodes to allocate for this model. These nodes are always up, starting from the time the model is deployed. Therefore, the cost of operating this model will be at least `rate` * `min_nodes` * number of hours since last billing cycle, where `rate` is the cost per node-hour as documented in the [pricing guide](/ml-engine/docs/pricing), even if no predictions are performed. There is additional cost for each prediction performed. Unlike manual scaling, if the load gets too heavy for the nodes that are up, the service will automatically add nodes to handle the increased load as well as scale back as traffic drops, always maintaining at least `min_nodes`. You will be charged for the time in which additional nodes are used. If `min_nodes` is not specified and AutoScaling is used with a [legacy (MLS1) machine type](/ml-engine/docs/machine-types-online-prediction), `min_nodes` defaults to 0, in which case, when traffic to a model stops (and after a cool-down period), nodes will be shut down and no charges will be incurred until traffic to the model resumes. If `min_nodes` is not specified and AutoScaling is used with a [Compute Engine (N1) machine type](/ml-engine/docs/machine-types-online-prediction), `min_nodes` defaults to 1. `min_nodes` must be at least 1 for use with a Compute Engine machine type. Note that you cannot use AutoScaling if your version uses [GPUs](#Version.FIELDS.accelerator_config). Instead, you must use ManualScaling. You can set `min_nodes` when creating the model version, and you can also update `min_nodes` for an existing version: update_body.json: { 'autoScaling': { 'minNodes': 5 } } HTTP request: PATCH https://ml.googleapis.com/v1/{name=projects/*/models/*/versions/*}?update_mask=autoScaling.minNodes -d @./update_body.json 
   """
 
   use GoogleApi.Gax.ModelBase
 
   @type t :: %__MODULE__{
+          :maxNodes => integer(),
+          :metrics => list(GoogleApi.MachineLearning.V1.Model.GoogleCloudMlV1_MetricSpec.t()),
           :minNodes => integer()
         }
 
+  field(:maxNodes)
+  field(:metrics, as: GoogleApi.MachineLearning.V1.Model.GoogleCloudMlV1_MetricSpec, type: :list)
   field(:minNodes)
 end
 
--- clients/machine_learning/lib/google_api/machine_learning/v1/model/google_cloud_ml_v1__env_var.ex
+++ clients/machine_learning/lib/google_api/machine_learning/v1/model/google_cloud_ml_v1__env_var.ex
@@ -17,12 +17,12 @@
 
 defmodule GoogleApi.MachineLearning.V1.Model.GoogleCloudMlV1_EnvVar do
   @moduledoc """
-  EnvVar represents an environment variable present in a Container.
+  Represents an environment variable to be made available in a container. This message is a subset of the [Kubernetes EnvVar v1 core specification](https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.18/#envvar-v1-core).
 
   ## Attributes
 
-  *   `name` (*type:* `String.t`, *default:* `nil`) - Name of the environment variable. Must be a C_IDENTIFIER.
-  *   `value` (*type:* `String.t`, *default:* `nil`) - Variable references $(VAR_NAME) are expanded using the previous defined environment variables in the container and any service environment variables. If a variable cannot be resolved, the reference in the input string will be unchanged. The $(VAR_NAME) syntax can be escaped with a double $$, ie: $$(VAR_NAME). Escaped references will never be expanded, regardless of whether the variable exists or not. Defaults to "".
+  *   `name` (*type:* `String.t`, *default:* `nil`) - Name of the environment variable. Must be a [valid C identifier](https://github.com/kubernetes/kubernetes/blob/v1.18.8/staging/src/k8s.io/apimachinery/pkg/util/validation/validation.go#L258) and must not begin with the prefix `AIP_`.
+  *   `value` (*type:* `String.t`, *default:* `nil`) - Value of the environment variable. Defaults to an empty string. In this field, you can reference [environment variables set by AI Platform Prediction](/ai-platform/prediction/docs/custom-container-requirements#aip-variables) and environment variables set earlier in the same env field as where this message occurs. You cannot reference environment variables set in the Docker image. In order for environment variables to be expanded, reference them by using the following syntax: $(VARIABLE_NAME) Note that this differs from Bash variable expansion, which does not use parentheses. If a variable cannot be resolved, the reference in the input string is used unchanged. To avoid variable expansion, you can escape this syntax with `$$`; for example: $$(VARIABLE_NAME)
   """
 
   use GoogleApi.Gax.ModelBase
