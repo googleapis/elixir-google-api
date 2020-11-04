40b3c1df235634e82456ed982218376c2eb5f159
yoshi-automation <yoshi-automation@google.com>
2020-11-04 06:08:01 -0800

    Regenerate GameServices client

--- clients/game_services/lib/google_api/game_services/v1/metadata.ex
+++ clients/game_services/lib/google_api/game_services/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.GameServices.V1 do
   API client metadata for GoogleApi.GameServices.V1.
   """
 
-  @discovery_revision "20201008"
+  @discovery_revision "20201029"
 
   def discovery_revision(), do: @discovery_revision
 end
