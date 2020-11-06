9c5a90729c79b62c57551626129de95bb605e72e
yoshi-automation <yoshi-automation@google.com>
2020-11-06 05:34:13 -0800

    Regenerate CloudResourceManager client

--- clients/cloud_resource_manager/lib/google_api/cloud_resource_manager/v2/model/folder.ex
+++ clients/cloud_resource_manager/lib/google_api/cloud_resource_manager/v2/model/folder.ex
@@ -22,10 +22,10 @@ defmodule GoogleApi.CloudResourceManager.V2.Model.Folder do
   ## Attributes
 
   *   `createTime` (*type:* `DateTime.t`, *default:* `nil`) - Output only. Timestamp when the Folder was created. Assigned by the server.
-  *   `displayName` (*type:* `String.t`, *default:* `nil`) - The folder’s display name. A folder’s display name must be unique amongst its siblings, e.g. no two folders with the same parent can share the same display name. The display name must start and end with a letter or digit, may contain letters, digits, spaces, hyphens and underscores and can be no longer than 30 characters. This is captured by the regular expression: [\\p{L}\\p{N}]([\\p{L}\\p{N}_- ]{0,28}[\\p{L}\\p{N}])?.
+  *   `displayName` (*type:* `String.t`, *default:* `nil`) - The folder's display name. A folder's display name must be unique amongst its siblings, e.g. no two folders with the same parent can share the same display name. The display name must start and end with a letter or digit, may contain letters, digits, spaces, hyphens and underscores and can be no longer than 30 characters. This is captured by the regular expression: `[\\p{L}\\p{N}]([\\p{L}\\p{N}_- ]{0,28}[\\p{L}\\p{N}])?`.
   *   `lifecycleState` (*type:* `String.t`, *default:* `nil`) - Output only. The lifecycle state of the folder. Updates to the lifecycle_state must be performed via DeleteFolder and UndeleteFolder.
   *   `name` (*type:* `String.t`, *default:* `nil`) - Output only. The resource name of the Folder. Its format is `folders/{folder_id}`, for example: "folders/1234".
-  *   `parent` (*type:* `String.t`, *default:* `nil`) - Required. The Folder’s parent's resource name. Updates to the folder's parent must be performed via MoveFolder.
+  *   `parent` (*type:* `String.t`, *default:* `nil`) - Required. The Folder's parent's resource name. Updates to the folder's parent must be performed via MoveFolder.
   """
 
   use GoogleApi.Gax.ModelBase
