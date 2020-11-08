2045bce8ca0e530bb3b2f59401d7082945ce80e5
yoshi-automation <yoshi-automation@google.com>
2020-11-08 05:36:03 -0800

    Regenerate CloudTasks client

--- clients/cloud_tasks/lib/google_api/cloud_tasks/v2beta2/metadata.ex
+++ clients/cloud_tasks/lib/google_api/cloud_tasks/v2beta2/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.CloudTasks.V2beta2 do
   API client metadata for GoogleApi.CloudTasks.V2beta2.
   """
 
-  @discovery_revision "20200909"
+  @discovery_revision "20201030"
 
   def discovery_revision(), do: @discovery_revision
 end
