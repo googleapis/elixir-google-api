3a8672b32be95ac8e28f4b572c3692d9a6b9a206
yoshi-automation <yoshi-automation@google.com>
2020-11-08 05:43:15 -0800

    Regenerate DataCatalog client

--- clients/data_catalog/lib/google_api/data_catalog/v1beta1/metadata.ex
+++ clients/data_catalog/lib/google_api/data_catalog/v1beta1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.DataCatalog.V1beta1 do
   API client metadata for GoogleApi.DataCatalog.V1beta1.
   """
 
-  @discovery_revision "20201021"
+  @discovery_revision "20201102"
 
   def discovery_revision(), do: @discovery_revision
 end
