b569baf42717791e1b8d126aa844f48c21fd24fb
yoshi-automation <yoshi-automation@google.com>
2020-11-18 05:31:32 -0800

    Regenerate CloudIot client

--- clients/cloud_iot/lib/google_api/cloud_iot/v1/metadata.ex
+++ clients/cloud_iot/lib/google_api/cloud_iot/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.CloudIot.V1 do
   API client metadata for GoogleApi.CloudIot.V1.
   """
 
-  @discovery_revision "20201027"
+  @discovery_revision "20201110"
 
   def discovery_revision(), do: @discovery_revision
 end
