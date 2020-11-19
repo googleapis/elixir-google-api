12ec655fa730470cdc205c23f72b72dce0eb425c
yoshi-automation <yoshi-automation@google.com>
2020-11-19 05:10:25 -0800

    Regenerate Admin client

--- clients/admin/lib/google_api/admin/datatransfer_v1/model/application.ex
+++ clients/admin/lib/google_api/admin/datatransfer_v1/model/application.ex
@@ -17,7 +17,7 @@
 
 defmodule GoogleApi.Admin.Datatransfer_v1.Model.Application do
   @moduledoc """
-  The JSON template for an Application resource. STEPLADDER: Generated unstable field number for field 'kind'. (See http://go/stepladder-help#fieldNumber) STEPLADDER: Generated unstable field number for field 'etag'. (See http://go/stepladder-help#fieldNumber)
+  Applications resources represent applications installed on the domain that support transferring ownership of user data.
 
   ## Attributes
 
