e9550e2e840a9a85f660ae4c2bb64ce0c2facafd
yoshi-automation <yoshi-automation@google.com>
2020-12-17 05:42:34 -0800

    Regenerate DLP client

--- clients/dlp/lib/google_api/dlp/v2/api/projects.ex
+++ clients/dlp/lib/google_api/dlp/v2/api/projects.ex
@@ -2430,7 +2430,7 @@ defmodule GoogleApi.DLP.V2.Api.Projects do
   end
 
   @doc """
-  Finish a running hybrid DlpJob. Triggers the finalization steps and running of any enabled actions that have not yet run. Early access feature is in a pre-release state and might change or have limited support. For more information, see https://cloud.google.com/products#product-launch-stages.
+  Finish a running hybrid DlpJob. Triggers the finalization steps and running of any enabled actions that have not yet run.
 
   ## Parameters
 
@@ -2555,7 +2555,7 @@ defmodule GoogleApi.DLP.V2.Api.Projects do
   end
 
   @doc """
-  Inspect hybrid content and store findings to a job. To review the findings, inspect the job. Inspection will occur asynchronously. Early access feature is in a pre-release state and might change or have limited support. For more information, see https://cloud.google.com/products#product-launch-stages.
+  Inspect hybrid content and store findings to a job. To review the findings, inspect the job. Inspection will occur asynchronously.
 
   ## Parameters
 
@@ -3404,7 +3404,7 @@ defmodule GoogleApi.DLP.V2.Api.Projects do
   end
 
   @doc """
-  Inspect hybrid content and store findings to a trigger. The inspection will be processed asynchronously. To review the findings monitor the jobs within the trigger. Early access feature is in a pre-release state and might change or have limited support. For more information, see https://cloud.google.com/products#product-launch-stages.
+  Inspect hybrid content and store findings to a trigger. The inspection will be processed asynchronously. To review the findings monitor the jobs within the trigger.
 
   ## Parameters
 
