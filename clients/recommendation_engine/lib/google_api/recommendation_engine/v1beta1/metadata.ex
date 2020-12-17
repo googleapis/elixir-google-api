a57d5b3ffa0b0eaa3b0abb250ff49a4defa35642
yoshi-automation <yoshi-automation@google.com>
2020-12-17 06:31:26 -0800

    Regenerate RecommendationEngine client

--- clients/recommendation_engine/lib/google_api/recommendation_engine/v1beta1/metadata.ex
+++ clients/recommendation_engine/lib/google_api/recommendation_engine/v1beta1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.RecommendationEngine.V1beta1 do
   API client metadata for GoogleApi.RecommendationEngine.V1beta1.
   """
 
-  @discovery_revision "20201027"
+  @discovery_revision "20201212"
 
   def discovery_revision(), do: @discovery_revision
 end
