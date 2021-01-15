ee0b14d6b46d842999a434a96e816c14511d1a57
yoshi-automation <yoshi-automation@google.com>
2021-01-15 06:25:46 -0800

    Regenerate PrivateCA client

--- clients/private_ca/lib/google_api/private_ca/v1beta1/model/schedule_delete_certificate_authority_request.ex
+++ clients/private_ca/lib/google_api/private_ca/v1beta1/model/schedule_delete_certificate_authority_request.ex
@@ -21,15 +21,18 @@ defmodule GoogleApi.PrivateCA.V1beta1.Model.ScheduleDeleteCertificateAuthorityRe
 
   ## Attributes
 
+  *   `ignoreActiveCertificates` (*type:* `boolean()`, *default:* `nil`) - Optional. This field allows the CA to be scheduled for deletion even if the CA has active certs. Active certs include both unrevoked and unexpired certs.
   *   `requestId` (*type:* `String.t`, *default:* `nil`) - Optional. An ID to identify requests. Specify a unique request ID so that if you must retry your request, the server will know to ignore the request if it has already been completed. The server will guarantee that for at least 60 minutes since the first request. For example, consider a situation where you make an initial request and t he request times out. If you make the request again with the same request ID, the server can check if original operation with the same request ID was received, and if so, will ignore the second request. This prevents clients from accidentally creating duplicate commitments. The request ID must be a valid UUID with the exception that zero UUID is not supported (00000000-0000-0000-0000-000000000000).
   """
 
   use GoogleApi.Gax.ModelBase
 
   @type t :: %__MODULE__{
+          :ignoreActiveCertificates => boolean(),
           :requestId => String.t()
         }
 
+  field(:ignoreActiveCertificates)
   field(:requestId)
 end
 
