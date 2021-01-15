9caa039f8ee9cb3157793feb336845db22e01657
yoshi-automation <yoshi-automation@google.com>
2021-01-15 06:57:07 -0800

    Regenerate WorkflowExecutions client

--- clients/workflow_executions/lib/google_api/workflow_executions/v1beta/metadata.ex
+++ clients/workflow_executions/lib/google_api/workflow_executions/v1beta/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.WorkflowExecutions.V1beta do
   API client metadata for GoogleApi.WorkflowExecutions.V1beta.
   """
 
-  @discovery_revision "20201208"
+  @discovery_revision "20210105"
 
   def discovery_revision(), do: @discovery_revision
 end
