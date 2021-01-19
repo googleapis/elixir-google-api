582373a29a89ed0c346f2fff669db7c5945868f6
yoshi-automation <yoshi-automation@google.com>
2021-01-19 10:49:35 -0800

    Regenerate CloudShell client

--- clients/cloud_shell/lib/google_api/cloud_shell/v1/metadata.ex
+++ clients/cloud_shell/lib/google_api/cloud_shell/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.CloudShell.V1 do
   API client metadata for GoogleApi.CloudShell.V1.
   """
 
-  @discovery_revision "20201003"
+  @discovery_revision "20210119"
 
   def discovery_revision(), do: @discovery_revision
 end
