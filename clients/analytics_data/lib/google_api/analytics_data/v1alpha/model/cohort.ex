a276d731e7fc70c6bd1a775f0c82b206e1d3669c
yoshi-automation <yoshi-automation@google.com>
2021-01-22 05:11:19 -0800

    Regenerate AnalyticsData client

--- clients/analytics_data/lib/google_api/analytics_data/v1alpha/model/cohort.ex
+++ clients/analytics_data/lib/google_api/analytics_data/v1alpha/model/cohort.ex
@@ -17,12 +17,12 @@
 
 defmodule GoogleApi.AnalyticsData.V1alpha.Model.Cohort do
   @moduledoc """
-  Defines a cohort selection criteria. A cohort is a group of users who share a common characteristic. For example, users with the same `firstTouchDate` belong to the same cohort.
+  Defines a cohort selection criteria. A cohort is a group of users who share a common characteristic. For example, users with the same `firstSessionDate` belong to the same cohort.
 
   ## Attributes
 
   *   `dateRange` (*type:* `GoogleApi.AnalyticsData.V1alpha.Model.DateRange.t`, *default:* `nil`) - The cohort selects users whose first touch date is between start date and end date defined in the `dateRange`. This `dateRange` does not specify the full date range of event data that is present in a cohort report. In a cohort report, this `dateRange` is extended by the granularity and offset present in the `cohortsRange`; event data for the extended reporting date range is present in a cohort report. In a cohort request, this `dateRange` is required and the `dateRanges` in the `RunReportRequest` or `RunPivotReportRequest` must be unspecified. This `dateRange` should generally be aligned with the cohort's granularity. If `CohortsRange` uses daily granularity, this `dateRange` can be a single day. If `CohortsRange` uses weekly granularity, this `dateRange` can be aligned to a week boundary, starting at Sunday and ending Saturday. If `CohortsRange` uses monthly granularity, this `dateRange` can be aligned to a month, starting at the first and ending on the last day of the month.
-  *   `dimension` (*type:* `String.t`, *default:* `nil`) - Dimension used by the cohort. Required and only supports `firstTouchDate`.
+  *   `dimension` (*type:* `String.t`, *default:* `nil`) - Dimension used by the cohort. Required and only supports `firstSessionDate`.
   *   `name` (*type:* `String.t`, *default:* `nil`) - Assigns a name to this cohort. The dimension `cohort` is valued to this name in a report response. If set, cannot begin with `cohort_` or `RESERVED_`. If not set, cohorts are named by their zero based index `cohort_0`, `cohort_1`, etc.
   """
 
