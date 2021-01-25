1767d069a10b35ef755a6331e51b3c39a75eebb1
yoshi-automation <yoshi-automation@google.com>
2021-01-25 05:42:42 -0800

    Regenerate DataFusion client

--- clients/data_fusion/lib/google_api/data_fusion/v1beta1/metadata.ex
+++ clients/data_fusion/lib/google_api/data_fusion/v1beta1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.DataFusion.V1beta1 do
   API client metadata for GoogleApi.DataFusion.V1beta1.
   """
 
-  @discovery_revision "20210107"
+  @discovery_revision "20210121"
 
   def discovery_revision(), do: @discovery_revision
 end
