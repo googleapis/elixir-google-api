ad4cb5b2300f43e6049d1beef0fa7fcf6a6b87f0
yoshi-automation <yoshi-automation@google.com>
2021-01-26 05:31:41 -0800

    Regenerate CloudBuild client

--- clients/cloud_build/lib/google_api/cloud_build/v1/api/projects.ex
+++ clients/cloud_build/lib/google_api/cloud_build/v1/api/projects.ex
@@ -1359,4 +1359,83 @@ defmodule GoogleApi.CloudBuild.V1.Api.Projects do
     |> Connection.execute(request)
     |> Response.decode(opts ++ [struct: %GoogleApi.CloudBuild.V1.Model.Operation{}])
   end
+
+  @doc """
+  ReceiveTriggerWebhook [Experimental] is called when the API receives a webhook request targeted at a specific trigger.
+
+  ## Parameters
+
+  *   `connection` (*type:* `GoogleApi.CloudBuild.V1.Connection.t`) - Connection to server
+  *   `project_id` (*type:* `String.t`) - Project in which the specified trigger lives
+  *   `trigger` (*type:* `String.t`) - Name of the trigger to run the payload against
+  *   `optional_params` (*type:* `keyword()`) - Optional parameters
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
+      *   `:access_token` (*type:* `String.t`) - OAuth access token.
+      *   `:alt` (*type:* `String.t`) - Data format for response.
+      *   `:callback` (*type:* `String.t`) - JSONP
+      *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
+      *   `:key` (*type:* `String.t`) - API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+      *   `:oauth_token` (*type:* `String.t`) - OAuth 2.0 token for the current user.
+      *   `:prettyPrint` (*type:* `boolean()`) - Returns response with indentations and line breaks.
+      *   `:quotaUser` (*type:* `String.t`) - Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
+      *   `:uploadType` (*type:* `String.t`) - Legacy upload protocol for media (e.g. "media", "multipart").
+      *   `:upload_protocol` (*type:* `String.t`) - Upload protocol for media (e.g. "raw", "multipart").
+      *   `:secret` (*type:* `String.t`) - Secret token used for authorization if an OAuth token isn't provided.
+      *   `:body` (*type:* `GoogleApi.CloudBuild.V1.Model.HttpBody.t`) - 
+  *   `opts` (*type:* `keyword()`) - Call options
+
+  ## Returns
+
+  *   `{:ok, %GoogleApi.CloudBuild.V1.Model.ReceiveTriggerWebhookResponse{}}` on success
+  *   `{:error, info}` on failure
+  """
+  @spec cloudbuild_projects_triggers_webhook(
+          Tesla.Env.client(),
+          String.t(),
+          String.t(),
+          keyword(),
+          keyword()
+        ) ::
+          {:ok, GoogleApi.CloudBuild.V1.Model.ReceiveTriggerWebhookResponse.t()}
+          | {:ok, Tesla.Env.t()}
+          | {:error, any()}
+  def cloudbuild_projects_triggers_webhook(
+        connection,
+        project_id,
+        trigger,
+        optional_params \\ [],
+        opts \\ []
+      ) do
+    optional_params_config = %{
+      :"$.xgafv" => :query,
+      :access_token => :query,
+      :alt => :query,
+      :callback => :query,
+      :fields => :query,
+      :key => :query,
+      :oauth_token => :query,
+      :prettyPrint => :query,
+      :quotaUser => :query,
+      :uploadType => :query,
+      :upload_protocol => :query,
+      :secret => :query,
+      :body => :body
+    }
+
+    request =
+      Request.new()
+      |> Request.method(:post)
+      |> Request.url("/v1/projects/{projectId}/triggers/{trigger}:webhook", %{
+        "projectId" => URI.encode(project_id, &URI.char_unreserved?/1),
+        "trigger" => URI.encode(trigger, &URI.char_unreserved?/1)
+      })
+      |> Request.add_optional_params(optional_params_config, optional_params)
+      |> Request.library_version(@library_version)
+
+    connection
+    |> Connection.execute(request)
+    |> Response.decode(
+      opts ++ [struct: %GoogleApi.CloudBuild.V1.Model.ReceiveTriggerWebhookResponse{}]
+    )
+  end
 end
