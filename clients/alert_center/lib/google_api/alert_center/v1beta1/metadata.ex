7ddda409913470042e22e0d6a34382dd497655a5
yoshi-automation <yoshi-automation@google.com>
2021-01-26 05:10:37 -0800

    Regenerate AlertCenter client

--- clients/alert_center/lib/google_api/alert_center/v1beta1/metadata.ex
+++ clients/alert_center/lib/google_api/alert_center/v1beta1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.AlertCenter.V1beta1 do
   API client metadata for GoogleApi.AlertCenter.V1beta1.
   """
 
-  @discovery_revision "20201231"
+  @discovery_revision "20210123"
 
   def discovery_revision(), do: @discovery_revision
 end
