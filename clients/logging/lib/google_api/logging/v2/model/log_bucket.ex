1624ffbd001f09a84e9d04b47f695f4edc460ca1
yoshi-automation <yoshi-automation@google.com>
2021-01-28 06:17:46 -0800

    Regenerate Logging client

--- clients/logging/lib/google_api/logging/v2/model/log_bucket.ex
+++ clients/logging/lib/google_api/logging/v2/model/log_bucket.ex
@@ -25,7 +25,7 @@ defmodule GoogleApi.Logging.V2.Model.LogBucket do
   *   `description` (*type:* `String.t`, *default:* `nil`) - Describes this bucket.
   *   `lifecycleState` (*type:* `String.t`, *default:* `nil`) - Output only. The bucket lifecycle state.
   *   `locked` (*type:* `boolean()`, *default:* `nil`) - Whether the bucket has been locked. The retention period on a locked bucket may not be changed. Locked buckets may only be deleted if they are empty.
-  *   `name` (*type:* `String.t`, *default:* `nil`) - Output only. The resource name of the bucket. For example: "projects/my-project-id/locations/my-location/buckets/my-bucket-id The supported locations are: "global"For the location of global it is unspecified where logs are actually stored. Once a bucket has been created, the location can not be changed.
+  *   `name` (*type:* `String.t`, *default:* `nil`) - Output only. The resource name of the bucket. For example: "projects/my-project-id/locations/my-location/buckets/my-bucket-id" The supported locations are: global, us-central1, us-east1, us-west1, asia-east1, europe-west1.For the location of global it is unspecified where logs are actually stored. Once a bucket has been created, the location can not be changed.
   *   `retentionDays` (*type:* `integer()`, *default:* `nil`) - Logs will be retained by default for this amount of time, after which they will automatically be deleted. The minimum retention period is 1 day. If this value is set to zero at bucket creation time, the default time of 30 days will be used.
   *   `updateTime` (*type:* `DateTime.t`, *default:* `nil`) - Output only. The last update timestamp of the bucket.
   """
