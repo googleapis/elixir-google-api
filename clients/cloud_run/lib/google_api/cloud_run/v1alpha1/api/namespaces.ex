43f845664970e328c4de3ad9913d04bc467d0602
yoshi-automation <yoshi-automation@google.com>
2021-01-29 05:32:38 -0800

    Regenerate CloudRun client

--- clients/cloud_run/lib/google_api/cloud_run/v1alpha1/api/namespaces.ex
+++ clients/cloud_run/lib/google_api/cloud_run/v1alpha1/api/namespaces.ex
@@ -2034,6 +2034,292 @@ defmodule GoogleApi.CloudRun.V1alpha1.Api.Namespaces do
   end
 
   @doc """
+  Create a job.
+
+  ## Parameters
+
+  *   `connection` (*type:* `GoogleApi.CloudRun.V1alpha1.Connection.t`) - Connection to server
+  *   `namespaces_id` (*type:* `String.t`) - Part of `parent`. Required. The namespace in which the job should be created. Replace {namespace_id} with the project ID or number.
+  *   `optional_params` (*type:* `keyword()`) - Optional parameters
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
+      *   `:access_token` (*type:* `String.t`) - OAuth access token.
+      *   `:alt` (*type:* `String.t`) - Data format for response.
+      *   `:callback` (*type:* `String.t`) - JSONP
+      *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
+      *   `:key` (*type:* `String.t`) - API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+      *   `:oauth_token` (*type:* `String.t`) - OAuth 2.0 token for the current user.
+      *   `:prettyPrint` (*type:* `boolean()`) - Returns response with indentations and line breaks.
+      *   `:quotaUser` (*type:* `String.t`) - Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
+      *   `:uploadType` (*type:* `String.t`) - Legacy upload protocol for media (e.g. "media", "multipart").
+      *   `:upload_protocol` (*type:* `String.t`) - Upload protocol for media (e.g. "raw", "multipart").
+      *   `:body` (*type:* `GoogleApi.CloudRun.V1alpha1.Model.Job.t`) - 
+  *   `opts` (*type:* `keyword()`) - Call options
+
+  ## Returns
+
+  *   `{:ok, %GoogleApi.CloudRun.V1alpha1.Model.Job{}}` on success
+  *   `{:error, info}` on failure
+  """
+  @spec run_namespaces_jobs_create(Tesla.Env.client(), String.t(), keyword(), keyword()) ::
+          {:ok, GoogleApi.CloudRun.V1alpha1.Model.Job.t()}
+          | {:ok, Tesla.Env.t()}
+          | {:error, any()}
+  def run_namespaces_jobs_create(connection, namespaces_id, optional_params \\ [], opts \\ []) do
+    optional_params_config = %{
+      :"$.xgafv" => :query,
+      :access_token => :query,
+      :alt => :query,
+      :callback => :query,
+      :fields => :query,
+      :key => :query,
+      :oauth_token => :query,
+      :prettyPrint => :query,
+      :quotaUser => :query,
+      :uploadType => :query,
+      :upload_protocol => :query,
+      :body => :body
+    }
+
+    request =
+      Request.new()
+      |> Request.method(:post)
+      |> Request.url("/apis/run.googleapis.com/v1alpha1/namespaces/{namespacesId}/jobs", %{
+        "namespacesId" => URI.encode(namespaces_id, &URI.char_unreserved?/1)
+      })
+      |> Request.add_optional_params(optional_params_config, optional_params)
+      |> Request.library_version(@library_version)
+
+    connection
+    |> Connection.execute(request)
+    |> Response.decode(opts ++ [struct: %GoogleApi.CloudRun.V1alpha1.Model.Job{}])
+  end
+
+  @doc """
+  Delete a job.
+
+  ## Parameters
+
+  *   `connection` (*type:* `GoogleApi.CloudRun.V1alpha1.Connection.t`) - Connection to server
+  *   `namespaces_id` (*type:* `String.t`) - Part of `name`. Required. The name of the job to delete. For Cloud Run (fully managed), replace {namespace_id} with the project ID or number.
+  *   `jobs_id` (*type:* `String.t`) - Part of `name`. See documentation of `namespacesId`.
+  *   `optional_params` (*type:* `keyword()`) - Optional parameters
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
+      *   `:access_token` (*type:* `String.t`) - OAuth access token.
+      *   `:alt` (*type:* `String.t`) - Data format for response.
+      *   `:callback` (*type:* `String.t`) - JSONP
+      *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
+      *   `:key` (*type:* `String.t`) - API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+      *   `:oauth_token` (*type:* `String.t`) - OAuth 2.0 token for the current user.
+      *   `:prettyPrint` (*type:* `boolean()`) - Returns response with indentations and line breaks.
+      *   `:quotaUser` (*type:* `String.t`) - Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
+      *   `:uploadType` (*type:* `String.t`) - Legacy upload protocol for media (e.g. "media", "multipart").
+      *   `:upload_protocol` (*type:* `String.t`) - Upload protocol for media (e.g. "raw", "multipart").
+      *   `:apiVersion` (*type:* `String.t`) - Optional. Cloud Run currently ignores this parameter.
+      *   `:kind` (*type:* `String.t`) - Optional. Cloud Run currently ignores this parameter.
+      *   `:propagationPolicy` (*type:* `String.t`) - Optional. Specifies the propagation policy of delete. Cloud Run currently ignores this setting, and deletes in the background. Please see kubernetes.io/docs/concepts/workloads/controllers/garbage-collection/ for more information.
+  *   `opts` (*type:* `keyword()`) - Call options
+
+  ## Returns
+
+  *   `{:ok, %GoogleApi.CloudRun.V1alpha1.Model.Empty{}}` on success
+  *   `{:error, info}` on failure
+  """
+  @spec run_namespaces_jobs_delete(
+          Tesla.Env.client(),
+          String.t(),
+          String.t(),
+          keyword(),
+          keyword()
+        ) ::
+          {:ok, GoogleApi.CloudRun.V1alpha1.Model.Empty.t()}
+          | {:ok, Tesla.Env.t()}
+          | {:error, any()}
+  def run_namespaces_jobs_delete(
+        connection,
+        namespaces_id,
+        jobs_id,
+        optional_params \\ [],
+        opts \\ []
+      ) do
+    optional_params_config = %{
+      :"$.xgafv" => :query,
+      :access_token => :query,
+      :alt => :query,
+      :callback => :query,
+      :fields => :query,
+      :key => :query,
+      :oauth_token => :query,
+      :prettyPrint => :query,
+      :quotaUser => :query,
+      :uploadType => :query,
+      :upload_protocol => :query,
+      :apiVersion => :query,
+      :kind => :query,
+      :propagationPolicy => :query
+    }
+
+    request =
+      Request.new()
+      |> Request.method(:delete)
+      |> Request.url(
+        "/apis/run.googleapis.com/v1alpha1/namespaces/{namespacesId}/jobs/{jobsId}",
+        %{
+          "namespacesId" => URI.encode(namespaces_id, &URI.char_unreserved?/1),
+          "jobsId" => URI.encode(jobs_id, &(URI.char_unreserved?(&1) || &1 == ?/))
+        }
+      )
+      |> Request.add_optional_params(optional_params_config, optional_params)
+      |> Request.library_version(@library_version)
+
+    connection
+    |> Connection.execute(request)
+    |> Response.decode(opts ++ [struct: %GoogleApi.CloudRun.V1alpha1.Model.Empty{}])
+  end
+
+  @doc """
+  Get information about a job.
+
+  ## Parameters
+
+  *   `connection` (*type:* `GoogleApi.CloudRun.V1alpha1.Connection.t`) - Connection to server
+  *   `namespaces_id` (*type:* `String.t`) - Part of `name`. Required. The name of the job to retrieve. For Cloud Run (fully managed), replace {namespace_id} with the project ID or number.
+  *   `jobs_id` (*type:* `String.t`) - Part of `name`. See documentation of `namespacesId`.
+  *   `optional_params` (*type:* `keyword()`) - Optional parameters
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
+      *   `:access_token` (*type:* `String.t`) - OAuth access token.
+      *   `:alt` (*type:* `String.t`) - Data format for response.
+      *   `:callback` (*type:* `String.t`) - JSONP
+      *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
+      *   `:key` (*type:* `String.t`) - API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+      *   `:oauth_token` (*type:* `String.t`) - OAuth 2.0 token for the current user.
+      *   `:prettyPrint` (*type:* `boolean()`) - Returns response with indentations and line breaks.
+      *   `:quotaUser` (*type:* `String.t`) - Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
+      *   `:uploadType` (*type:* `String.t`) - Legacy upload protocol for media (e.g. "media", "multipart").
+      *   `:upload_protocol` (*type:* `String.t`) - Upload protocol for media (e.g. "raw", "multipart").
+  *   `opts` (*type:* `keyword()`) - Call options
+
+  ## Returns
+
+  *   `{:ok, %GoogleApi.CloudRun.V1alpha1.Model.Job{}}` on success
+  *   `{:error, info}` on failure
+  """
+  @spec run_namespaces_jobs_get(Tesla.Env.client(), String.t(), String.t(), keyword(), keyword()) ::
+          {:ok, GoogleApi.CloudRun.V1alpha1.Model.Job.t()}
+          | {:ok, Tesla.Env.t()}
+          | {:error, any()}
+  def run_namespaces_jobs_get(
+        connection,
+        namespaces_id,
+        jobs_id,
+        optional_params \\ [],
+        opts \\ []
+      ) do
+    optional_params_config = %{
+      :"$.xgafv" => :query,
+      :access_token => :query,
+      :alt => :query,
+      :callback => :query,
+      :fields => :query,
+      :key => :query,
+      :oauth_token => :query,
+      :prettyPrint => :query,
+      :quotaUser => :query,
+      :uploadType => :query,
+      :upload_protocol => :query
+    }
+
+    request =
+      Request.new()
+      |> Request.method(:get)
+      |> Request.url(
+        "/apis/run.googleapis.com/v1alpha1/namespaces/{namespacesId}/jobs/{jobsId}",
+        %{
+          "namespacesId" => URI.encode(namespaces_id, &URI.char_unreserved?/1),
+          "jobsId" => URI.encode(jobs_id, &(URI.char_unreserved?(&1) || &1 == ?/))
+        }
+      )
+      |> Request.add_optional_params(optional_params_config, optional_params)
+      |> Request.library_version(@library_version)
+
+    connection
+    |> Connection.execute(request)
+    |> Response.decode(opts ++ [struct: %GoogleApi.CloudRun.V1alpha1.Model.Job{}])
+  end
+
+  @doc """
+  List jobs.
+
+  ## Parameters
+
+  *   `connection` (*type:* `GoogleApi.CloudRun.V1alpha1.Connection.t`) - Connection to server
+  *   `namespaces_id` (*type:* `String.t`) - Part of `parent`. Required. The namespace from which the jobs should be listed. Replace {namespace_id} with the project ID or number.
+  *   `optional_params` (*type:* `keyword()`) - Optional parameters
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
+      *   `:access_token` (*type:* `String.t`) - OAuth access token.
+      *   `:alt` (*type:* `String.t`) - Data format for response.
+      *   `:callback` (*type:* `String.t`) - JSONP
+      *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
+      *   `:key` (*type:* `String.t`) - API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+      *   `:oauth_token` (*type:* `String.t`) - OAuth 2.0 token for the current user.
+      *   `:prettyPrint` (*type:* `boolean()`) - Returns response with indentations and line breaks.
+      *   `:quotaUser` (*type:* `String.t`) - Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
+      *   `:uploadType` (*type:* `String.t`) - Legacy upload protocol for media (e.g. "media", "multipart").
+      *   `:upload_protocol` (*type:* `String.t`) - Upload protocol for media (e.g. "raw", "multipart").
+      *   `:continue` (*type:* `String.t`) - Optional. Optional encoded string to continue paging.
+      *   `:fieldSelector` (*type:* `String.t`) - Optional. Allows to filter resources based on a specific value for a field name. Send this in a query string format. i.e. 'metadata.name%3Dlorem'. Not currently used by Cloud Run.
+      *   `:includeUninitialized` (*type:* `boolean()`) - Optional. Not currently used by Cloud Run.
+      *   `:labelSelector` (*type:* `String.t`) - Optional. Allows to filter resources based on a label. Supported operations are =, !=, exists, in, and notIn.
+      *   `:limit` (*type:* `integer()`) - Optional. The maximum number of records that should be returned.
+      *   `:resourceVersion` (*type:* `String.t`) - Optional. The baseline resource version from which the list or watch operation should start. Not currently used by Cloud Run.
+      *   `:watch` (*type:* `boolean()`) - Optional. Flag that indicates that the client expects to watch this resource as well. Not currently used by Cloud Run.
+  *   `opts` (*type:* `keyword()`) - Call options
+
+  ## Returns
+
+  *   `{:ok, %GoogleApi.CloudRun.V1alpha1.Model.ListJobsResponse{}}` on success
+  *   `{:error, info}` on failure
+  """
+  @spec run_namespaces_jobs_list(Tesla.Env.client(), String.t(), keyword(), keyword()) ::
+          {:ok, GoogleApi.CloudRun.V1alpha1.Model.ListJobsResponse.t()}
+          | {:ok, Tesla.Env.t()}
+          | {:error, any()}
+  def run_namespaces_jobs_list(connection, namespaces_id, optional_params \\ [], opts \\ []) do
+    optional_params_config = %{
+      :"$.xgafv" => :query,
+      :access_token => :query,
+      :alt => :query,
+      :callback => :query,
+      :fields => :query,
+      :key => :query,
+      :oauth_token => :query,
+      :prettyPrint => :query,
+      :quotaUser => :query,
+      :uploadType => :query,
+      :upload_protocol => :query,
+      :continue => :query,
+      :fieldSelector => :query,
+      :includeUninitialized => :query,
+      :labelSelector => :query,
+      :limit => :query,
+      :resourceVersion => :query,
+      :watch => :query
+    }
+
+    request =
+      Request.new()
+      |> Request.method(:get)
+      |> Request.url("/apis/run.googleapis.com/v1alpha1/namespaces/{namespacesId}/jobs", %{
+        "namespacesId" => URI.encode(namespaces_id, &URI.char_unreserved?/1)
+      })
+      |> Request.add_optional_params(optional_params_config, optional_params)
+      |> Request.library_version(@library_version)
+
+    connection
+    |> Connection.execute(request)
+    |> Response.decode(opts ++ [struct: %GoogleApi.CloudRun.V1alpha1.Model.ListJobsResponse{}])
+  end
+
+  @doc """
   Rpc to delete a revision.
 
   ## Parameters
