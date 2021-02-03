18b396d35b03facbfcb971be3e702c6ad140807c
yoshi-automation <yoshi-automation@google.com>
2021-02-03 06:56:21 -0800

    Regenerate Slides client

--- clients/slides/lib/google_api/slides/v1/model/shape_properties.ex
+++ clients/slides/lib/google_api/slides/v1/model/shape_properties.ex
@@ -21,6 +21,7 @@ defmodule GoogleApi.Slides.V1.Model.ShapeProperties do
 
   ## Attributes
 
+  *   `autofit` (*type:* `GoogleApi.Slides.V1.Model.Autofit.t`, *default:* `nil`) - The autofit properties of the shape. This property is only set for shapes that allow text.
   *   `contentAlignment` (*type:* `String.t`, *default:* `nil`) - The alignment of the content in the shape. If unspecified, the alignment is inherited from a parent placeholder if it exists. If the shape has no parent, the default alignment matches the alignment for new shapes created in the Slides editor.
   *   `link` (*type:* `GoogleApi.Slides.V1.Model.Link.t`, *default:* `nil`) - The hyperlink destination of the shape. If unset, there is no link. Links are not inherited from parent placeholders.
   *   `outline` (*type:* `GoogleApi.Slides.V1.Model.Outline.t`, *default:* `nil`) - The outline of the shape. If unset, the outline is inherited from a parent placeholder if it exists. If the shape has no parent, then the default outline depends on the shape type, matching the defaults for new shapes created in the Slides editor.
@@ -31,6 +32,7 @@ defmodule GoogleApi.Slides.V1.Model.ShapeProperties do
   use GoogleApi.Gax.ModelBase
 
   @type t :: %__MODULE__{
+          :autofit => GoogleApi.Slides.V1.Model.Autofit.t(),
           :contentAlignment => String.t(),
           :link => GoogleApi.Slides.V1.Model.Link.t(),
           :outline => GoogleApi.Slides.V1.Model.Outline.t(),
@@ -38,6 +40,7 @@ defmodule GoogleApi.Slides.V1.Model.ShapeProperties do
           :shapeBackgroundFill => GoogleApi.Slides.V1.Model.ShapeBackgroundFill.t()
         }
 
+  field(:autofit, as: GoogleApi.Slides.V1.Model.Autofit)
   field(:contentAlignment)
   field(:link, as: GoogleApi.Slides.V1.Model.Link)
   field(:outline, as: GoogleApi.Slides.V1.Model.Outline)
