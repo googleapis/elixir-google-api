6c01faf4188a8342d24b4012cb02a8b74391b8d2
yoshi-automation <yoshi-automation@google.com>
2021-02-09 06:35:03 -0800

    Regenerate PubSubLite client

--- clients/pub_sub_lite/lib/google_api/pub_sub_lite/v1/api/topic_stats.ex
+++ clients/pub_sub_lite/lib/google_api/pub_sub_lite/v1/api/topic_stats.ex
@@ -26,6 +26,79 @@ defmodule GoogleApi.PubSubLite.V1.Api.TopicStats do
   @library_version Mix.Project.config() |> Keyword.get(:version, "")
 
   @doc """
+  Compute the head cursor for the partition. The head cursor’s offset is guaranteed to be before or equal to all messages which have not yet been acknowledged to be published, and greater than the offset of any message whose publish has already been acknowledged. It is 0 if there have never been messages on the partition.
+
+  ## Parameters
+
+  *   `connection` (*type:* `GoogleApi.PubSubLite.V1.Connection.t`) - Connection to server
+  *   `topic` (*type:* `String.t`) - Required. The topic for which we should compute the head cursor.
+  *   `optional_params` (*type:* `keyword()`) - Optional parameters
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
+      *   `:access_token` (*type:* `String.t`) - OAuth access token.
+      *   `:alt` (*type:* `String.t`) - Data format for response.
+      *   `:callback` (*type:* `String.t`) - JSONP
+      *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
+      *   `:key` (*type:* `String.t`) - API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+      *   `:oauth_token` (*type:* `String.t`) - OAuth 2.0 token for the current user.
+      *   `:prettyPrint` (*type:* `boolean()`) - Returns response with indentations and line breaks.
+      *   `:quotaUser` (*type:* `String.t`) - Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
+      *   `:uploadType` (*type:* `String.t`) - Legacy upload protocol for media (e.g. "media", "multipart").
+      *   `:upload_protocol` (*type:* `String.t`) - Upload protocol for media (e.g. "raw", "multipart").
+      *   `:body` (*type:* `GoogleApi.PubSubLite.V1.Model.ComputeHeadCursorRequest.t`) - 
+  *   `opts` (*type:* `keyword()`) - Call options
+
+  ## Returns
+
+  *   `{:ok, %GoogleApi.PubSubLite.V1.Model.ComputeHeadCursorResponse{}}` on success
+  *   `{:error, info}` on failure
+  """
+  @spec pubsublite_topic_stats_projects_locations_topics_compute_head_cursor(
+          Tesla.Env.client(),
+          String.t(),
+          keyword(),
+          keyword()
+        ) ::
+          {:ok, GoogleApi.PubSubLite.V1.Model.ComputeHeadCursorResponse.t()}
+          | {:ok, Tesla.Env.t()}
+          | {:error, any()}
+  def pubsublite_topic_stats_projects_locations_topics_compute_head_cursor(
+        connection,
+        topic,
+        optional_params \\ [],
+        opts \\ []
+      ) do
+    optional_params_config = %{
+      :"$.xgafv" => :query,
+      :access_token => :query,
+      :alt => :query,
+      :callback => :query,
+      :fields => :query,
+      :key => :query,
+      :oauth_token => :query,
+      :prettyPrint => :query,
+      :quotaUser => :query,
+      :uploadType => :query,
+      :upload_protocol => :query,
+      :body => :body
+    }
+
+    request =
+      Request.new()
+      |> Request.method(:post)
+      |> Request.url("/v1/topicStats/{+topic}:computeHeadCursor", %{
+        "topic" => URI.encode(topic, &URI.char_unreserved?/1)
+      })
+      |> Request.add_optional_params(optional_params_config, optional_params)
+      |> Request.library_version(@library_version)
+
+    connection
+    |> Connection.execute(request)
+    |> Response.decode(
+      opts ++ [struct: %GoogleApi.PubSubLite.V1.Model.ComputeHeadCursorResponse{}]
+    )
+  end
+
+  @doc """
   Compute statistics about a range of messages in a given topic and partition.
 
   ## Parameters
