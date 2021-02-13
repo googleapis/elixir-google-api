8422dc5f6f3830efbe44d9149e34c4dee8930440
yoshi-automation <yoshi-automation@google.com>
2021-02-13 06:25:49 -0800

    Regenerate Memcache client

--- clients/memcache/lib/google_api/memcache/v1beta2/metadata.ex
+++ clients/memcache/lib/google_api/memcache/v1beta2/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.Memcache.V1beta2 do
   API client metadata for GoogleApi.Memcache.V1beta2.
   """
 
-  @discovery_revision "20210121"
+  @discovery_revision "20210209"
 
   def discovery_revision(), do: @discovery_revision
 end
