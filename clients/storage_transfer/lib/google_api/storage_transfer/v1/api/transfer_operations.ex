af16da73252eed48db65eab9e799a71c2efecf87
yoshi-automation <yoshi-automation@google.com>
2021-02-19 07:15:37 -0800

    Regenerate StorageTransfer client

--- clients/storage_transfer/lib/google_api/storage_transfer/v1/api/transfer_operations.ex
+++ clients/storage_transfer/lib/google_api/storage_transfer/v1/api/transfer_operations.ex
@@ -161,12 +161,12 @@ defmodule GoogleApi.StorageTransfer.V1.Api.TransferOperations do
   end
 
   @doc """
-  Lists transfer operations.
+  Lists transfer operations. Operations are ordered by their creation time in reverse chronological order.
 
   ## Parameters
 
   *   `connection` (*type:* `GoogleApi.StorageTransfer.V1.Connection.t`) - Connection to server
-  *   `name` (*type:* `String.t`) - Required. The value `transferOperations`.
+  *   `name` (*type:* `String.t`) - Not used.
   *   `filter` (*type:* `String.t`) - Required. A list of query parameters specified as JSON text in the form of: `{"projectId":"my_project_id", "jobNames":["jobid1","jobid2",...], "operationNames":["opid1","opid2",...], "transferStatuses":["status1","status2",...]}` Since `jobNames`, `operationNames`, and `transferStatuses` support multiple values, they must be specified with array notation. `projectId` is required. `jobNames`, `operationNames`, and `transferStatuses` are optional. The valid values for `transferStatuses` are case-insensitive: IN_PROGRESS, PAUSED, SUCCESS, FAILED, and ABORTED.
   *   `optional_params` (*type:* `keyword()`) - Optional parameters
       *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
