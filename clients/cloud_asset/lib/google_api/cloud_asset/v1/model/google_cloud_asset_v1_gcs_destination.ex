c5c60291b376497440fb5cde3f10bf19a11df7cc
yoshi-automation <yoshi-automation@google.com>
2021-03-05 05:23:04 -0800

    Regenerate CloudAsset client

--- clients/cloud_asset/lib/google_api/cloud_asset/v1/model/google_cloud_asset_v1_gcs_destination.ex
+++ clients/cloud_asset/lib/google_api/cloud_asset/v1/model/google_cloud_asset_v1_gcs_destination.ex
@@ -21,7 +21,7 @@ defmodule GoogleApi.CloudAsset.V1.Model.GoogleCloudAssetV1GcsDestination do
 
   ## Attributes
 
-  *   `uri` (*type:* `String.t`, *default:* `nil`) - Required. The uri of the Cloud Storage object. It's the same uri that is used by gsutil. For example: "gs://bucket_name/object_name". See [Quickstart: Using the gsutil tool] (https://cloud.google.com/storage/docs/quickstart-gsutil) for examples.
+  *   `uri` (*type:* `String.t`, *default:* `nil`) - Required. The uri of the Cloud Storage object. It's the same uri that is used by gsutil. Example: "gs://bucket_name/object_name". See [Viewing and Editing Object Metadata](https://cloud.google.com/storage/docs/viewing-editing-metadata) for more information. If the specified Cloud Storage object already exists and there is no [hold](https://cloud.google.com/storage/docs/object-holds), it will be overwritten with the analysis result.
   """
 
   use GoogleApi.Gax.ModelBase
