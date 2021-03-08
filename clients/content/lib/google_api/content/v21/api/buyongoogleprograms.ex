6b681b800860a54be8b5416cd2fbde4be79b6994
yoshi-automation <yoshi-automation@google.com>
2021-03-08 05:36:17 -0800

    Regenerate Content client

--- clients/content/lib/google_api/content/v21/api/buyongoogleprograms.ex
+++ clients/content/lib/google_api/content/v21/api/buyongoogleprograms.ex
@@ -99,7 +99,7 @@ defmodule GoogleApi.Content.V21.Api.Buyongoogleprograms do
   end
 
   @doc """
-  Onboards BoG in your Merchant Center account. By using this method, you agree to the [Terms of Service](https://merchants.google.com/mc/termsofservice/transactions/US/latest).
+  Onboards BoG in your Merchant Center account. By using this method, you agree to the [Terms of Service](https://merchants.google.com/mc/termsofservice/transactions/US/latest). Calling this method is only possible if the authenticated account is the same as the merchant id in the request. Calling this method multiple times will only accept Terms of Service if the latest version is not currently signed.
 
   ## Parameters
 
--- clients/content/mix.exs
+++ clients/content/mix.exs
@@ -18,7 +18,7 @@
 defmodule GoogleApi.Content.Mixfile do
   use Mix.Project
 
-  @version "0.52.0"
+  @version "0.52.1"
 
   def project() do
     [
