6c87fb31d996f08fb42ce6066317e9d652a87acc
Daniel Azuma <dazuma@gmail.com>
2021-03-08 18:20:33 -0800

    Revert changes to test client

--- clients/test_client/test/api_test.exs
+++ clients/test_client/test/api_test.exs
@@ -146,7 +146,7 @@ defmodule Gax.ApiTest do
   end
 
   test "url path encoding" do
-    mock(fn %{method: :delete, url: "http://localhost:8080/test/v1/b/bucket-1/o/some%2Fpath%2FFile%20with%20sp%C3%A4ces.zip"} ->
+    mock(fn %{method: :delete, url: "http://localhost:8080/test/v1/b/bucket-1/o/some/path/File%20with%20sp%C3%A4ces.zip"} ->
       %Tesla.Env{status: 200, body: @container_json}
     end)
 
