a055966149c4d07c37132dc537b3aeae9f15d504
yoshi-automation <yoshi-automation@google.com>
2021-03-18 06:52:26 -0700

    Regenerate SASPortal client

--- clients/sas_portal/lib/google_api/sas_portal/v1alpha1/metadata.ex
+++ clients/sas_portal/lib/google_api/sas_portal/v1alpha1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.SASPortal.V1alpha1 do
   API client metadata for GoogleApi.SASPortal.V1alpha1.
   """
 
-  @discovery_revision "20210225"
+  @discovery_revision "20210315"
 
   def discovery_revision(), do: @discovery_revision
 end
