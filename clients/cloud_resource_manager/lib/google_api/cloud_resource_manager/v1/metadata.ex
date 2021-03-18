58a4475beebaf308a1a37a953ba9a28599538563
yoshi-automation <yoshi-automation@google.com>
2021-03-18 05:39:17 -0700

    Regenerate CloudResourceManager client

--- clients/cloud_resource_manager/lib/google_api/cloud_resource_manager/v1/metadata.ex
+++ clients/cloud_resource_manager/lib/google_api/cloud_resource_manager/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.CloudResourceManager.V1 do
   API client metadata for GoogleApi.CloudResourceManager.V1.
   """
 
-  @discovery_revision "20210302"
+  @discovery_revision "20210314"
 
   def discovery_revision(), do: @discovery_revision
 end
--- clients/cloud_resource_manager/lib/google_api/cloud_resource_manager/v2/metadata.ex
+++ clients/cloud_resource_manager/lib/google_api/cloud_resource_manager/v2/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.CloudResourceManager.V2 do
   API client metadata for GoogleApi.CloudResourceManager.V2.
   """
 
-  @discovery_revision "20210302"
+  @discovery_revision "20210314"
 
   def discovery_revision(), do: @discovery_revision
 end
