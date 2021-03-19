22ee6dd29fc6d785f7165a0849734be3bad8c290
yoshi-automation <yoshi-automation@google.com>
2021-03-19 05:16:24 -0700

    Regenerate Apigee client

--- clients/apigee/lib/google_api/apigee/v1/model/google_cloud_apigee_v1_api_product.ex
+++ clients/apigee/lib/google_api/apigee/v1/model/google_cloud_apigee_v1_api_product.ex
@@ -28,6 +28,7 @@ defmodule GoogleApi.Apigee.V1.Model.GoogleCloudApigeeV1ApiProduct do
   *   `description` (*type:* `String.t`, *default:* `nil`) - Description of the API product. Include key information about the API product that is not captured by other fields. Comma-separated list of API resources to be bundled in the API product. By default, the resource paths are mapped from the `proxy.pathsuffix` variable. The proxy path suffix is defined as the URI fragment following the ProxyEndpoint base path. For example, if the `apiResources` element is defined to be `/forecastrss` and the base path defined for the API proxy is `/weather`, then only requests to `/weather/forecastrss` are permitted by the API product. You can select a specific path, or you can select all subpaths with the following wildcard: - `/**`: Indicates that all sub-URIs are included. - `/*` : Indicates that only URIs one level down are included. By default, / supports the same resources as /** as well as the base path defined by the API proxy. For example, if the base path of the API proxy is `/v1/weatherapikey`, then the API product supports requests to `/v1/weatherapikey` and to any sub-URIs, such as `/v1/weatherapikey/forecastrss`, `/v1/weatherapikey/region/CA`, and so on. For more information, see Managing API products.
   *   `displayName` (*type:* `String.t`, *default:* `nil`) - Name displayed in the UI or developer portal to developers registering for API access.
   *   `environments` (*type:* `list(String.t)`, *default:* `nil`) - Comma-separated list of environment names to which the API product is bound. Requests to environments that are not listed are rejected. By specifying one or more environments, you can bind the resources listed in the API product to a specific environment, preventing developers from accessing those resources through API proxies deployed in another environment. This setting is used, for example, to prevent resources associated with API proxies in `prod` from being accessed by API proxies deployed in `test`.
+  *   `graphqlOperationGroup` (*type:* `GoogleApi.Apigee.V1.Model.GoogleCloudApigeeV1GraphQLOperationGroup.t`, *default:* `nil`) - Configuration used to group Apigee proxies or remote services with graphQL operation name, graphQL operation type and quotas. This grouping allows us to precisely set quota for a particular combination of graphQL name and operation type for a particular proxy request. If graphQL name is not set, this would imply quota will be applied on all graphQL requests matching the operation type.
   *   `lastModifiedAt` (*type:* `String.t`, *default:* `nil`) - Response only. Modified time of this environment as milliseconds since epoch.
   *   `name` (*type:* `String.t`, *default:* `nil`) - Internal name of the API product. Characters you can use in the name are restricted to: `A-Z0-9._\\-$ %`. **Note:** The internal name cannot be edited when updating the API product.
   *   `operationGroup` (*type:* `GoogleApi.Apigee.V1.Model.GoogleCloudApigeeV1OperationGroup.t`, *default:* `nil`) - Configuration used to group Apigee proxies or remote services with resources, method types, and quotas. The resource refers to the resource URI (excluding the base path). With this grouping, the API product creator is able to fine-tune and give precise control over which REST methods have access to specific resources and how many calls can be made (using the `quota` setting). **Note:** The `api_resources` setting cannot be specified for both the API product and operation group; otherwise the call will fail.
@@ -48,6 +49,8 @@ defmodule GoogleApi.Apigee.V1.Model.GoogleCloudApigeeV1ApiProduct do
           :description => String.t() | nil,
           :displayName => String.t() | nil,
           :environments => list(String.t()) | nil,
+          :graphqlOperationGroup =>
+            GoogleApi.Apigee.V1.Model.GoogleCloudApigeeV1GraphQLOperationGroup.t() | nil,
           :lastModifiedAt => String.t() | nil,
           :name => String.t() | nil,
           :operationGroup =>
@@ -66,6 +69,11 @@ defmodule GoogleApi.Apigee.V1.Model.GoogleCloudApigeeV1ApiProduct do
   field(:description)
   field(:displayName)
   field(:environments, type: :list)
+
+  field(:graphqlOperationGroup,
+    as: GoogleApi.Apigee.V1.Model.GoogleCloudApigeeV1GraphQLOperationGroup
+  )
+
   field(:lastModifiedAt)
   field(:name)
   field(:operationGroup, as: GoogleApi.Apigee.V1.Model.GoogleCloudApigeeV1OperationGroup)
