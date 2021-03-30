3fe0f609b8422e014275b93324d41f37a6de7f65
yoshi-automation <yoshi-automation@google.com>
2021-03-30 06:49:29 -0700

    Regenerate ServiceUsage client

--- clients/service_usage/lib/google_api/service_usage/v1/model/google_api_service.ex
+++ clients/service_usage/lib/google_api/service_usage/v1/model/google_api_service.ex
@@ -25,7 +25,7 @@ defmodule GoogleApi.ServiceUsage.V1.Model.GoogleApiService do
   *   `authentication` (*type:* `GoogleApi.ServiceUsage.V1.Model.Authentication.t`, *default:* `nil`) - Auth configuration.
   *   `backend` (*type:* `GoogleApi.ServiceUsage.V1.Model.Backend.t`, *default:* `nil`) - API backend configuration.
   *   `billing` (*type:* `GoogleApi.ServiceUsage.V1.Model.Billing.t`, *default:* `nil`) - Billing configuration.
-  *   `configVersion` (*type:* `integer()`, *default:* `nil`) - Deprecated. The service config compiler always sets this field to `3`.
+  *   `configVersion` (*type:* `integer()`, *default:* `nil`) - Obsolete. Do not use. This field has no semantic meaning. The service config compiler always sets this field to `3`.
   *   `context` (*type:* `GoogleApi.ServiceUsage.V1.Model.Context.t`, *default:* `nil`) - Context configuration.
   *   `control` (*type:* `GoogleApi.ServiceUsage.V1.Model.Control.t`, *default:* `nil`) - Configuration for the service control plane.
   *   `customError` (*type:* `GoogleApi.ServiceUsage.V1.Model.CustomError.t`, *default:* `nil`) - Custom error configuration.
