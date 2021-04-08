4d6459e8a24c2fcc03967df8b7c332174badbd99
yoshi-automation <yoshi-automation@google.com>
2021-04-08 05:21:20 -0700

    Regenerate BillingBudgets client

--- clients/billing_budgets/lib/google_api/billing_budgets/v1beta1/model/google_cloud_billing_budgets_v1beta1_budget_amount.ex
+++ clients/billing_budgets/lib/google_api/billing_budgets/v1beta1/model/google_cloud_billing_budgets_v1beta1_budget_amount.ex
@@ -21,7 +21,7 @@ defmodule GoogleApi.BillingBudgets.V1beta1.Model.GoogleCloudBillingBudgetsV1beta
 
   ## Attributes
 
-  *   `lastPeriodAmount` (*type:* `GoogleApi.BillingBudgets.V1beta1.Model.GoogleCloudBillingBudgetsV1beta1LastPeriodAmount.t`, *default:* `nil`) - Use the last period's actual spend as the budget for the present period. Cannot be set in combination with Filter.custom_period.
+  *   `lastPeriodAmount` (*type:* `GoogleApi.BillingBudgets.V1beta1.Model.GoogleCloudBillingBudgetsV1beta1LastPeriodAmount.t`, *default:* `nil`) - Use the last period's actual spend as the budget for the present period. LastPeriodAmount can only be set when the budget's time period is a Filter.calendar_period. It cannot be set in combination with Filter.custom_period.
   *   `specifiedAmount` (*type:* `GoogleApi.BillingBudgets.V1beta1.Model.GoogleTypeMoney.t`, *default:* `nil`) - A specified amount to use as the budget. `currency_code` is optional. If specified when creating a budget, it must match the currency of the billing account. If specified when updating a budget, it must match the currency_code of the existing budget. The `currency_code` is provided on output.
   """
 
