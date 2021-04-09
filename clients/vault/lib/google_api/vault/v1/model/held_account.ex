8564a25a2950e2b302f1ef96d50f84861692d7fb
yoshi-automation <yoshi-automation@google.com>
2021-04-09 06:52:37 -0700

    Regenerate Vault client

--- clients/vault/lib/google_api/vault/v1/model/held_account.ex
+++ clients/vault/lib/google_api/vault/v1/model/held_account.ex
@@ -17,7 +17,7 @@
 
 defmodule GoogleApi.Vault.V1.Model.HeldAccount do
   @moduledoc """
-  An account being held in a particular hold. This structure is immutable. This can be either a single user or a google group, depending on the corpus.
+  An account being held in a particular hold. This structure is immutable. This can be either a single user or a google group, depending on the corpus. To work with Vault resources, the account must have the [required Vault privileges] (https://support.google.com/vault/answer/2799699) and access to the matter. To access a matter, the account must have created the matter, have the matter shared with them, or have the **View All Matters** privilege.
 
   ## Attributes
 
--- clients/vault/lib/google_api/vault/v1/model/saved_query.ex
+++ clients/vault/lib/google_api/vault/v1/model/saved_query.ex
@@ -17,7 +17,7 @@
 
 defmodule GoogleApi.Vault.V1.Model.SavedQuery do
   @moduledoc """
-  Definition of the saved query.
+  Definition of the saved query. To work with Vault resources, the account must have the [required Vault privileges] (https://support.google.com/vault/answer/2799699) and access to the matter. To access a matter, the account must have created the matter, have the matter shared with them, or have the **View All Matters** privilege.
 
   ## Attributes
 
