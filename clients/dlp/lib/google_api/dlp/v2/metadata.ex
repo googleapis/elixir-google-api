0ba262476fe7c5f44e78d621e68aaad991c70532
yoshi-automation <yoshi-automation@google.com>
2021-04-15 05:35:07 -0700

    Regenerate DLP client

--- clients/dlp/lib/google_api/dlp/v2/metadata.ex
+++ clients/dlp/lib/google_api/dlp/v2/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.DLP.V2 do
   API client metadata for GoogleApi.DLP.V2.
   """
 
-  @discovery_revision "20210326"
+  @discovery_revision "20210410"
 
   def discovery_revision(), do: @discovery_revision
 end
