baeb7d60b44688b0de838575955a91f60aaa1587
yoshi-automation <yoshi-automation@google.com>
2021-04-17 05:54:09 -0700

    Regenerate Jobs client

--- clients/jobs/lib/google_api/jobs/v3/metadata.ex
+++ clients/jobs/lib/google_api/jobs/v3/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.Jobs.V3 do
   API client metadata for GoogleApi.Jobs.V3.
   """
 
-  @discovery_revision "20210226"
+  @discovery_revision "20210413"
 
   def discovery_revision(), do: @discovery_revision
 end
