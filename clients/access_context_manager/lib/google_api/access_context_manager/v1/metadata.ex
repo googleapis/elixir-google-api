618318c99e24d2ecf7756b8e52fb09e80d8184d9
yoshi-automation <yoshi-automation@google.com>
2021-04-22 05:05:12 -0700

    Regenerate AccessContextManager client

--- clients/access_context_manager/lib/google_api/access_context_manager/v1/metadata.ex
+++ clients/access_context_manager/lib/google_api/access_context_manager/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.AccessContextManager.V1 do
   API client metadata for GoogleApi.AccessContextManager.V1.
   """
 
-  @discovery_revision "20210220"
+  @discovery_revision "20210412"
 
   def discovery_revision(), do: @discovery_revision
 end
--- clients/access_context_manager/lib/google_api/access_context_manager/v1/model/egress_to.ex
+++ clients/access_context_manager/lib/google_api/access_context_manager/v1/model/egress_to.ex
@@ -17,12 +17,12 @@
 
 defmodule GoogleApi.AccessContextManager.V1.Model.EgressTo do
   @moduledoc """
-  Defines the conditions under which an EgressPolicy matches a request. Conditions are based on information about the ApiOperation intended to be performed on the `resources` specified. Note that if the destination of the request is protected by a ServicePerimeter, then that ServicePerimeter must have an IngressPolicy which allows access in order for this request to succeed.
+  Defines the conditions under which an EgressPolicy matches a request. Conditions are based on information about the ApiOperation intended to be performed on the `resources` specified. Note that if the destination of the request is also protected by a ServicePerimeter, then that ServicePerimeter must have an IngressPolicy which allows access in order for this request to succeed. The request must match `operations` AND `resources` fields in order to be allowed egress out of the perimeter.
 
   ## Attributes
 
-  *   `operations` (*type:* `list(GoogleApi.AccessContextManager.V1.Model.ApiOperation.t)`, *default:* `nil`) - A list of ApiOperations that this egress rule applies to. A request matches if it contains an operation/service in this list.
-  *   `resources` (*type:* `list(String.t)`, *default:* `nil`) - A list of resources, currently only projects in the form `projects/`, that match this to stanza. A request matches if it contains a resource in this list. If `*` is specified for resources, then this EgressTo rule will authorize access to all resources outside the perimeter.
+  *   `operations` (*type:* `list(GoogleApi.AccessContextManager.V1.Model.ApiOperation.t)`, *default:* `nil`) - A list of ApiOperations allowed to be performed by the sources specified in the corresponding EgressFrom. A request matches if it uses an operation/service in this list.
+  *   `resources` (*type:* `list(String.t)`, *default:* `nil`) - A list of resources, currently only projects in the form `projects/`, that are allowed to be accessed by sources defined in the corresponding EgressFrom. A request matches if it contains a resource in this list. If `*` is specified for `resources`, then this EgressTo rule will authorize access to all resources outside the perimeter.
   """
 
   use GoogleApi.Gax.ModelBase
