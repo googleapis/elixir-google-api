33aaa26c62aebe4abd267f36a88c329db373733f
yoshi-automation <yoshi-automation@google.com>
2021-04-22 06:19:17 -0700

    Regenerate IAP client

--- clients/iap/lib/google_api/iap/v1/model/policy_name.ex
+++ clients/iap/lib/google_api/iap/v1/model/policy_name.ex
@@ -17,13 +17,13 @@
 
 defmodule GoogleApi.IAP.V1.Model.PolicyName do
   @moduledoc """
-
+  An internal name for an IAM policy, based on the resource to which the policy applies. Not to be confused with a resource's external full resource name. For more information on this distinction, see go/iam-full-resource-names.
 
   ## Attributes
 
-  *   `id` (*type:* `String.t`, *default:* `nil`) - 
+  *   `id` (*type:* `String.t`, *default:* `nil`) - Identifies an instance of the type. ID format varies by type. The ID format is defined in the IAM .service file that defines the type, either in path_mapping or in a comment.
   *   `region` (*type:* `String.t`, *default:* `nil`) - For Cloud IAM: The location of the Policy. Must be empty or "global" for Policies owned by global IAM. Must name a region from prodspec/cloud-iam-cloudspec for Regional IAM Policies, see go/iam-faq#where-is-iam-currently-deployed. For Local IAM: This field should be set to "local".
-  *   `type` (*type:* `String.t`, *default:* `nil`) - Valid values for type might be 'gce', 'gcs', 'project', 'account' etc.
+  *   `type` (*type:* `String.t`, *default:* `nil`) - Resource type. Types are defined in IAM's .service files. Valid values for type might be 'gce', 'gcs', 'project', 'account' etc.
   """
 
   use GoogleApi.Gax.ModelBase
