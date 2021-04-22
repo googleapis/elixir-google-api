5ae45c8cc88d5cfc0c1a79d3064d6383b2bf8f0b
yoshi-automation <yoshi-automation@google.com>
2021-04-22 06:35:14 -0700

    Regenerate PlayableLocations client

--- clients/playable_locations/lib/google_api/playable_locations/v3/metadata.ex
+++ clients/playable_locations/lib/google_api/playable_locations/v3/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.PlayableLocations.V3 do
   API client metadata for GoogleApi.PlayableLocations.V3.
   """
 
-  @discovery_revision "20210304"
+  @discovery_revision "20210421"
 
   def discovery_revision(), do: @discovery_revision
 end
