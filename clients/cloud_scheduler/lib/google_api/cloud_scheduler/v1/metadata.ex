85120bc661396104853d004f95e4641fd597f4e9
yoshi-automation <yoshi-automation@google.com>
2021-04-30 05:37:33 -0700

    Regenerate CloudScheduler client

--- clients/cloud_scheduler/lib/google_api/cloud_scheduler/v1/metadata.ex
+++ clients/cloud_scheduler/lib/google_api/cloud_scheduler/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.CloudScheduler.V1 do
   API client metadata for GoogleApi.CloudScheduler.V1.
   """
 
-  @discovery_revision "20210315"
+  @discovery_revision "20210420"
 
   def discovery_revision(), do: @discovery_revision
 end
