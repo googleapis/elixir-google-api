5eaa26eae7b2b04c38f30c2115d9587de6fc275e
yoshi-automation <yoshi-automation@google.com>
2021-05-01 06:54:05 -0700

    Regenerate ServiceDirectory client

--- clients/service_directory/lib/google_api/service_directory/v1beta1/model/endpoint.ex
+++ clients/service_directory/lib/google_api/service_directory/v1beta1/model/endpoint.ex
@@ -24,6 +24,7 @@ defmodule GoogleApi.ServiceDirectory.V1beta1.Model.Endpoint do
   *   `address` (*type:* `String.t`, *default:* `nil`) - Optional. An IPv4 or IPv6 address. Service Directory rejects bad addresses like: * `8.8.8` * `8.8.8.8:53` * `test:bad:address` * `[::1]` * `[::1]:8080` Limited to 45 characters.
   *   `metadata` (*type:* `map()`, *default:* `nil`) - Optional. Metadata for the endpoint. This data can be consumed by service clients. Restrictions: * The entire metadata dictionary may contain up to 512 characters, spread accoss all key-value pairs. Metadata that goes beyond this limit are rejected * Valid metadata keys have two segments: an optional prefix and name, separated by a slash (/). The name segment is required and must be 63 characters or less, beginning and ending with an alphanumeric character ([a-z0-9A-Z]) with dashes (-), underscores (_), dots (.), and alphanumerics between. The prefix is optional. If specified, the prefix must be a DNS subdomain: a series of DNS labels separated by dots (.), not longer than 253 characters in total, followed by a slash (/). Metadata that fails to meet these requirements are rejected * The `(*.)google.com/` and `(*.)googleapis.com/` prefixes are reserved for system metadata managed by Service Directory. If the user tries to write to these keyspaces, those entries are silently ignored by the system Note: This field is equivalent to the `annotations` field in the v1 API. They have the same syntax and read/write to the same location in Service Directory.
   *   `name` (*type:* `String.t`, *default:* `nil`) - Immutable. The resource name for the endpoint in the format `projects/*/locations/*/namespaces/*/services/*/endpoints/*`.
+  *   `network` (*type:* `String.t`, *default:* `nil`) - Immutable. The Google Compute Engine network (VPC) of the endpoint in the format `projects//locations/global/networks/*`. The project must be specified by project number (project id is rejected). Incorrectly formatted networks are rejected, but no other validation is performed on this field (ex. network or project existence, reachability, or permissions).
   *   `port` (*type:* `integer()`, *default:* `nil`) - Optional. Service Directory rejects values outside of `[0, 65535]`.
   """
 
@@ -33,12 +34,14 @@ defmodule GoogleApi.ServiceDirectory.V1beta1.Model.Endpoint do
           :address => String.t() | nil,
           :metadata => map() | nil,
           :name => String.t() | nil,
+          :network => String.t() | nil,
           :port => integer() | nil
         }
 
   field(:address)
   field(:metadata, type: :map)
   field(:name)
+  field(:network)
   field(:port)
 end
 
