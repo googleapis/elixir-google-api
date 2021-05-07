63485f43307e1f5c55502a54388cb62803983599
yoshi-automation <yoshi-automation@google.com>
2021-05-07 06:50:55 -0700

    Regenerate ServiceNetworking client

--- clients/service_networking/lib/google_api/service_networking/v1/model/endpoint.ex
+++ clients/service_networking/lib/google_api/service_networking/v1/model/endpoint.ex
@@ -17,11 +17,10 @@
 
 defmodule GoogleApi.ServiceNetworking.V1.Model.Endpoint do
   @moduledoc """
-  `Endpoint` describes a network endpoint of a service that serves a set of APIs. It is commonly known as a service endpoint. A service may expose any number of service endpoints, and all service endpoints share the same service definition, such as quota limits and monitoring metrics. Example service configuration: name: library-example.googleapis.com endpoints: # Below entry makes 'google.example.library.v1.Library' # API be served from endpoint address library-example.googleapis.com. # It also allows HTTP OPTIONS calls to be passed to the backend, for # it to decide whether the subsequent cross-origin request is # allowed to proceed. - name: library-example.googleapis.com allow_cors: true
+  `Endpoint` describes a network address of a service that serves a set of APIs. It is commonly known as a service endpoint. A service may expose any number of service endpoints, and all service endpoints share the same service definition, such as quota limits and monitoring metrics. Example: type: google.api.Service name: library-example.googleapis.com endpoints: # Declares network address `https://library-example.googleapis.com` # for service `library-example.googleapis.com`. The `https` scheme # is implicit for all service endpoints. Other schemes may be # supported in the future. - name: library-example.googleapis.com allow_cors: false - name: content-staging-library-example.googleapis.com # Allows HTTP OPTIONS calls to be passed to the API frontend, for it # to decide whether the subsequent cross-origin request is allowed # to proceed. allow_cors: true
 
   ## Attributes
 
-  *   `aliases` (*type:* `list(String.t)`, *default:* `nil`) - Unimplemented. Dot not use. DEPRECATED: This field is no longer supported. Instead of using aliases, please specify multiple google.api.Endpoint for each of the intended aliases. Additional names that this endpoint will be hosted on.
   *   `allowCors` (*type:* `boolean()`, *default:* `nil`) - Allowing [CORS](https://en.wikipedia.org/wiki/Cross-origin_resource_sharing), aka cross-domain traffic, would allow the backends served from this endpoint to receive and respond to HTTP OPTIONS requests. The response will be used by the browser to determine whether the subsequent cross-origin request is allowed to proceed.
   *   `name` (*type:* `String.t`, *default:* `nil`) - The canonical name of this endpoint.
   *   `target` (*type:* `String.t`, *default:* `nil`) - The specification of an Internet routable address of API frontend that will handle requests to this [API Endpoint](https://cloud.google.com/apis/design/glossary). It should be either a valid IPv4 address or a fully-qualified domain name. For example, "8.8.8.8" or "myservice.appspot.com".
@@ -30,13 +29,11 @@ defmodule GoogleApi.ServiceNetworking.V1.Model.Endpoint do
   use GoogleApi.Gax.ModelBase
 
   @type t :: %__MODULE__{
-          :aliases => list(String.t()) | nil,
           :allowCors => boolean() | nil,
           :name => String.t() | nil,
           :target => String.t() | nil
         }
 
-  field(:aliases, type: :list)
   field(:allowCors)
   field(:name)
   field(:target)
