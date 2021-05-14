4d1e5c3c7277ee346c76c001693622a4e1291fe3
yoshi-automation <yoshi-automation@google.com>
2021-05-14 06:34:11 -0700

    Regenerate STS client

--- clients/sts/lib/google_api/sts/v1/metadata.ex
+++ clients/sts/lib/google_api/sts/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.STS.V1 do
   API client metadata for GoogleApi.STS.V1.
   """
 
-  @discovery_revision "20210227"
+  @discovery_revision "20210508"
 
   def discovery_revision(), do: @discovery_revision
 end
