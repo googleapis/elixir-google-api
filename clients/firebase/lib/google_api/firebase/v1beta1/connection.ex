26b9e47870c1ffae1fa552287142c78b08d9cbb8
yoshi-automation <yoshi-automation@google.com>
2021-05-27 05:52:50 -0700

    Regenerate Firebase client

--- clients/firebase/lib/google_api/firebase/v1beta1/connection.ex
+++ clients/firebase/lib/google_api/firebase/v1beta1/connection.ex
@@ -24,7 +24,7 @@ defmodule GoogleApi.Firebase.V1beta1.Connection do
 
   use GoogleApi.Gax.Connection,
     scopes: [
-      # View and manage your data across Google Cloud Platform services
+      # See, edit, configure, and delete your Google Cloud Platform data
       "https://www.googleapis.com/auth/cloud-platform",
 
       # View your data across Google Cloud Platform services
