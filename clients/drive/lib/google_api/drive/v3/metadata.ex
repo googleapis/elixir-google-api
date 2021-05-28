cdba32645a2215af956e11d4ecee4fc0c00366f8
yoshi-automation <yoshi-automation@google.com>
2021-05-28 05:53:47 -0700

    Regenerate Drive client

--- clients/drive/lib/google_api/drive/v3/metadata.ex
+++ clients/drive/lib/google_api/drive/v3/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.Drive.V3 do
   API client metadata for GoogleApi.Drive.V3.
   """
 
-  @discovery_revision "20210509"
+  @discovery_revision "20210524"
 
   def discovery_revision(), do: @discovery_revision
 end
