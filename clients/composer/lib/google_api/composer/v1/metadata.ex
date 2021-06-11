ba94cd285294b9cfa251ac0df10af2dd4e305687
yoshi-automation <yoshi-automation@google.com>
2021-06-11 05:33:01 -0700

    Regenerate Composer client

--- clients/composer/lib/google_api/composer/v1/metadata.ex
+++ clients/composer/lib/google_api/composer/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.Composer.V1 do
   API client metadata for GoogleApi.Composer.V1.
   """
 
-  @discovery_revision "20210518"
+  @discovery_revision "20210601"
 
   def discovery_revision(), do: @discovery_revision
 end
