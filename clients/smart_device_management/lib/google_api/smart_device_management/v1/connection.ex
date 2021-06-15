d8f83291513344dfaacecf793dd2bcf769b9ed87
yoshi-automation <yoshi-automation@google.com>
2021-06-15 06:48:52 -0700

    Regenerate SmartDeviceManagement client

--- clients/smart_device_management/lib/google_api/smart_device_management/v1/connection.ex
+++ clients/smart_device_management/lib/google_api/smart_device_management/v1/connection.ex
@@ -25,7 +25,10 @@ defmodule GoogleApi.SmartDeviceManagement.V1.Connection do
   use GoogleApi.Gax.Connection,
     scopes: [
       # See and/or control the devices that you selected
-      "https://www.googleapis.com/auth/sdm.service"
+      "https://www.googleapis.com/auth/sdm.service",
+
+      # See and control the Nest thermostats that you select
+      "https://www.googleapis.com/auth/sdm.thermostat.service"
     ],
     otp_app: :google_api_smart_device_management,
     base_url: "https://smartdevicemanagement.googleapis.com/"
