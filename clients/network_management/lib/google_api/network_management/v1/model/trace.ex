0d5df0bddd25aaadcf6b9121befebdd797b3fb9e
yoshi-automation <yoshi-automation@google.com>
2021-06-17 06:23:04 -0700

    Regenerate NetworkManagement client

--- clients/network_management/lib/google_api/network_management/v1/model/trace.ex
+++ clients/network_management/lib/google_api/network_management/v1/model/trace.ex
@@ -21,7 +21,7 @@ defmodule GoogleApi.NetworkManagement.V1.Model.Trace do
 
   ## Attributes
 
-  *   `endpointInfo` (*type:* `GoogleApi.NetworkManagement.V1.Model.EndpointInfo.t`, *default:* `nil`) - Derived from the source and destination endpoints definition, and validated by the data plane model. If there are multiple traces starting from different source locations, then the endpoint_info may be different between traces.
+  *   `endpointInfo` (*type:* `GoogleApi.NetworkManagement.V1.Model.EndpointInfo.t`, *default:* `nil`) - Derived from the source and destination endpoints definition specified by user request, and validated by the data plane model. If there are multiple traces starting from different source locations, then the endpoint_info may be different between traces.
   *   `steps` (*type:* `list(GoogleApi.NetworkManagement.V1.Model.Step.t)`, *default:* `nil`) - A trace of a test contains multiple steps from the initial state to the final state (delivered, dropped, forwarded, or aborted). The steps are ordered by the processing sequence within the simulated network state machine. It is critical to preserve the order of the steps and avoid reordering or sorting them.
   """
 
