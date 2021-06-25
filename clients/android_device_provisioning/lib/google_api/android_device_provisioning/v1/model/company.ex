46978bcd7b22e4a9ba7cb4533b9b44a82c86c4b7
yoshi-automation <yoshi-automation@google.com>
2021-06-25 05:10:58 -0700

    Regenerate AndroidDeviceProvisioning client

--- clients/android_device_provisioning/lib/google_api/android_device_provisioning/v1/model/company.ex
+++ clients/android_device_provisioning/lib/google_api/android_device_provisioning/v1/model/company.ex
@@ -24,8 +24,10 @@ defmodule GoogleApi.AndroidDeviceProvisioning.V1.Model.Company do
   *   `adminEmails` (*type:* `list(String.t)`, *default:* `nil`) - Optional. Email address of customer's users in the admin role. Each email address must be associated with a Google Account.
   *   `companyId` (*type:* `String.t`, *default:* `nil`) - Output only. The ID of the company. Assigned by the server.
   *   `companyName` (*type:* `String.t`, *default:* `nil`) - Required. The name of the company. For example _XYZ Corp_. Displayed to the company's employees in the zero-touch enrollment portal.
+  *   `languageCode` (*type:* `String.t`, *default:* `nil`) - Input only. The preferred locale of the customer represented as a BCP47 language code. This field is validated on input and requests containing unsupported language codes will be rejected. Supported language codes: Arabic (ar) Chinese (Hong Kong) (zh-HK) Chinese (Simplified) (zh-CN) Chinese (Traditional) (zh-TW) Czech (cs) Danish (da) Dutch (nl) English (UK) (en-GB) English (US) (en-US) Filipino (fil) Finnish (fi) French (fr) German (de) Hebrew (iw) Hindi (hi) Hungarian (hu) Indonesian (id) Italian (it) Japanese (ja) Korean (ko) Norwegian (Bokmal) (no) Polish (pl) Portuguese (Brazil) (pt-BR) Portuguese (Portugal) (pt-PT) Russian (ru) Spanish (es) Spanish (Latin America) (es-419) Swedish (sv) Thai (th) Turkish (tr) Ukrainian (uk) Vietnamese (vi)
   *   `name` (*type:* `String.t`, *default:* `nil`) - Output only. The API resource name of the company. The resource name is one of the following formats: * `partners/[PARTNER_ID]/customers/[CUSTOMER_ID]` * `partners/[PARTNER_ID]/vendors/[VENDOR_ID]` * `partners/[PARTNER_ID]/vendors/[VENDOR_ID]/customers/[CUSTOMER_ID]` Assigned by the server.
-  *   `ownerEmails` (*type:* `list(String.t)`, *default:* `nil`) - Required. Input only. Email address of customer's users in the owner role. At least one `owner_email` is required. Each email address must be associated with a Google Account. Owners share the same access as admins but can also add, delete, and edit your organization's portal users.
+  *   `ownerEmails` (*type:* `list(String.t)`, *default:* `nil`) - Required. Input only. Email address of customer's users in the owner role. At least one `owner_email` is required. Owners share the same access as admins but can also add, delete, and edit your organization's portal users.
+  *   `skipWelcomeEmail` (*type:* `boolean()`, *default:* `nil`) - Input only. If set to true, welcome email will not be sent to the customer. It is recommended to skip the welcome email if devices will be claimed with additional DEVICE_PROTECTION service, as the customer will receive separate emails at device claim time. This field is ignored if this is not a Zero-touch customer.
   *   `termsStatus` (*type:* `String.t`, *default:* `nil`) - Output only. Whether any user from the company has accepted the latest Terms of Service (ToS). See TermsStatus.
   """
 
@@ -35,16 +37,20 @@ defmodule GoogleApi.AndroidDeviceProvisioning.V1.Model.Company do
           :adminEmails => list(String.t()) | nil,
           :companyId => String.t() | nil,
           :companyName => String.t() | nil,
+          :languageCode => String.t() | nil,
           :name => String.t() | nil,
           :ownerEmails => list(String.t()) | nil,
+          :skipWelcomeEmail => boolean() | nil,
           :termsStatus => String.t() | nil
         }
 
   field(:adminEmails, type: :list)
   field(:companyId)
   field(:companyName)
+  field(:languageCode)
   field(:name)
   field(:ownerEmails, type: :list)
+  field(:skipWelcomeEmail)
   field(:termsStatus)
 end
 
