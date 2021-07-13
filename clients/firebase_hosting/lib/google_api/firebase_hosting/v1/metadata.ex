bb2e9f5a7e86300c8515506af68345fb2614fae1
yoshi-automation <yoshi-automation@google.com>
2021-07-13 06:04:24 -0700

    Regenerate FirebaseHosting client

--- clients/firebase_hosting/lib/google_api/firebase_hosting/v1/metadata.ex
+++ clients/firebase_hosting/lib/google_api/firebase_hosting/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.FirebaseHosting.V1 do
   API client metadata for GoogleApi.FirebaseHosting.V1.
   """
 
-  @discovery_revision "20210315"
+  @discovery_revision "20210628"
 
   def discovery_revision(), do: @discovery_revision
 end
