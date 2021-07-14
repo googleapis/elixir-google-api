521a4ac37f082ded78dc5a627a07688c2bfca224
yoshi-automation <yoshi-automation@google.com>
2021-07-14 05:37:34 -0700

    Regenerate Composer client

--- clients/composer/lib/google_api/composer/v1beta1/model/software_config.ex
+++ clients/composer/lib/google_api/composer/v1beta1/model/software_config.ex
@@ -26,6 +26,7 @@ defmodule GoogleApi.Composer.V1beta1.Model.SoftwareConfig do
   *   `imageVersion` (*type:* `String.t`, *default:* `nil`) - The version of the software running in the environment. This encapsulates both the version of Cloud Composer functionality and the version of Apache Airflow. It must match the regular expression `composer-([0-9]+\\.[0-9]+\\.[0-9]+|latest)-airflow-[0-9]+\\.[0-9]+(\\.[0-9]+.*)?`. When used as input, the server also checks if the provided version is supported and denies the request for an unsupported version. The Cloud Composer portion of the version is a [semantic version](https://semver.org) or `latest`. When the patch version is omitted, the current Cloud Composer patch version is selected. When `latest` is provided instead of an explicit version number, the server replaces `latest` with the current Cloud Composer version and stores that version number in the same field. The portion of the image version that follows *airflow-* is an official Apache Airflow repository [release name](https://github.com/apache/incubator-airflow/releases). See also [Version List](/composer/docs/concepts/versioning/composer-versions).
   *   `pypiPackages` (*type:* `map()`, *default:* `nil`) - Optional. Custom Python Package Index (PyPI) packages to be installed in the environment. Keys refer to the lowercase package name such as "numpy" and values are the lowercase extras and version specifier such as "==1.12.0", "[devel,gcp_api]", or "[devel]>=1.8.2, <1.9.2". To specify a package without pinning it to a version specifier, use the empty string as the value.
   *   `pythonVersion` (*type:* `String.t`, *default:* `nil`) - Optional. The major version of Python used to run the Apache Airflow scheduler, worker, and webserver processes. Can be set to '2' or '3'. If not specified, the default is '3'. Cannot be updated.
+  *   `schedulerCount` (*type:* `integer()`, *default:* `nil`) - Optional. The number of schedulers for Airflow.
   """
 
   use GoogleApi.Gax.ModelBase
@@ -35,7 +36,8 @@ defmodule GoogleApi.Composer.V1beta1.Model.SoftwareConfig do
           :envVariables => map() | nil,
           :imageVersion => String.t() | nil,
           :pypiPackages => map() | nil,
-          :pythonVersion => String.t() | nil
+          :pythonVersion => String.t() | nil,
+          :schedulerCount => integer() | nil
         }
 
   field(:airflowConfigOverrides, type: :map)
@@ -43,6 +45,7 @@ defmodule GoogleApi.Composer.V1beta1.Model.SoftwareConfig do
   field(:imageVersion)
   field(:pypiPackages, type: :map)
   field(:pythonVersion)
+  field(:schedulerCount)
 end
 
 defimpl Poison.Decoder, for: GoogleApi.Composer.V1beta1.Model.SoftwareConfig do
