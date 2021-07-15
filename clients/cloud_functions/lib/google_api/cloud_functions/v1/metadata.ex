99ff4bae2acb001696bf18873272138ad940824d
yoshi-automation <yoshi-automation@google.com>
2021-07-15 05:29:47 -0700

    Regenerate CloudFunctions client

--- clients/cloud_functions/lib/google_api/cloud_functions/v1/metadata.ex
+++ clients/cloud_functions/lib/google_api/cloud_functions/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.CloudFunctions.V1 do
   API client metadata for GoogleApi.CloudFunctions.V1.
   """
 
-  @discovery_revision "20210409"
+  @discovery_revision "20210708"
 
   def discovery_revision(), do: @discovery_revision
 end
