f163b21c537229a1ce7e7b372f50872cc40a642a
yoshi-automation <yoshi-automation@google.com>
2021-07-17 06:44:24 -0700

    Regenerate StorageTransfer client

--- clients/storage_transfer/lib/google_api/storage_transfer/v1/model/object_conditions.ex
+++ clients/storage_transfer/lib/google_api/storage_transfer/v1/model/object_conditions.ex
@@ -17,7 +17,7 @@
 
 defmodule GoogleApi.StorageTransfer.V1.Model.ObjectConditions do
   @moduledoc """
-  Conditions that determine which objects are transferred. Applies only to Cloud Data Sources such as S3, Azure, and Cloud Storage. The "last modification time" refers to the time of the last change to the object's content or metadata — specifically, this is the `updated` property of Cloud Storage objects, the `LastModified` field of S3 objects, and the `Last-Modified` header of Azure blobs.
+  Conditions that determine which objects are transferred. Applies only to Cloud Data Sources such as S3, Azure, and Cloud Storage. The "last modification time" refers to the time of the last change to the object's content or metadata — specifically, this is the `updated` property of Cloud Storage objects, the `LastModified` field of S3 objects, and the `Last-Modified` header of Azure blobs. This is not supported for transfers involving PosixFilesystem.
 
   ## Attributes
 
