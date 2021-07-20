e158daa4b4b84acef9a2515839f61bc0253e3e6a
yoshi-automation <yoshi-automation@google.com>
2021-07-20 05:06:47 -0700

    Regenerate AdMob client

--- clients/ad_mob/lib/google_api/ad_mob/v1/metadata.ex
+++ clients/ad_mob/lib/google_api/ad_mob/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.AdMob.V1 do
   API client metadata for GoogleApi.AdMob.V1.
   """
 
-  @discovery_revision "20210421"
+  @discovery_revision "20210718"
 
   def discovery_revision(), do: @discovery_revision
 end
