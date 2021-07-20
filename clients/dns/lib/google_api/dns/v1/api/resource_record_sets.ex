e45a8f28625ffe8eaf18a5f279157a468cffdb3b
yoshi-automation <yoshi-automation@google.com>
2021-07-20 05:44:23 -0700

    Regenerate DNS client

--- clients/dns/lib/google_api/dns/v1/api/resource_record_sets.ex
+++ clients/dns/lib/google_api/dns/v1/api/resource_record_sets.ex
@@ -26,6 +26,260 @@ defmodule GoogleApi.DNS.V1.Api.ResourceRecordSets do
   @library_version Mix.Project.config() |> Keyword.get(:version, "")
 
   @doc """
+  Creates a new ResourceRecordSet.
+
+  ## Parameters
+
+  *   `connection` (*type:* `GoogleApi.DNS.V1.Connection.t`) - Connection to server
+  *   `project` (*type:* `String.t`) - Identifies the project addressed by this request.
+  *   `managed_zone` (*type:* `String.t`) - Identifies the managed zone addressed by this request. Can be the managed zone name or ID.
+  *   `optional_params` (*type:* `keyword()`) - Optional parameters
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
+      *   `:access_token` (*type:* `String.t`) - OAuth access token.
+      *   `:alt` (*type:* `String.t`) - Data format for response.
+      *   `:callback` (*type:* `String.t`) - JSONP
+      *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
+      *   `:key` (*type:* `String.t`) - API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+      *   `:oauth_token` (*type:* `String.t`) - OAuth 2.0 token for the current user.
+      *   `:prettyPrint` (*type:* `boolean()`) - Returns response with indentations and line breaks.
+      *   `:quotaUser` (*type:* `String.t`) - Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
+      *   `:uploadType` (*type:* `String.t`) - Legacy upload protocol for media (e.g. "media", "multipart").
+      *   `:upload_protocol` (*type:* `String.t`) - Upload protocol for media (e.g. "raw", "multipart").
+      *   `:clientOperationId` (*type:* `String.t`) - For mutating operation requests only. An optional identifier specified by the client. Must be unique for operation resources in the Operations collection.
+      *   `:body` (*type:* `GoogleApi.DNS.V1.Model.ResourceRecordSet.t`) - 
+  *   `opts` (*type:* `keyword()`) - Call options
+
+  ## Returns
+
+  *   `{:ok, %GoogleApi.DNS.V1.Model.ResourceRecordSet{}}` on success
+  *   `{:error, info}` on failure
+  """
+  @spec dns_resource_record_sets_create(
+          Tesla.Env.client(),
+          String.t(),
+          String.t(),
+          keyword(),
+          keyword()
+        ) ::
+          {:ok, GoogleApi.DNS.V1.Model.ResourceRecordSet.t()}
+          | {:ok, Tesla.Env.t()}
+          | {:ok, list()}
+          | {:error, any()}
+  def dns_resource_record_sets_create(
+        connection,
+        project,
+        managed_zone,
+        optional_params \\ [],
+        opts \\ []
+      ) do
+    optional_params_config = %{
+      :"$.xgafv" => :query,
+      :access_token => :query,
+      :alt => :query,
+      :callback => :query,
+      :fields => :query,
+      :key => :query,
+      :oauth_token => :query,
+      :prettyPrint => :query,
+      :quotaUser => :query,
+      :uploadType => :query,
+      :upload_protocol => :query,
+      :clientOperationId => :query,
+      :body => :body
+    }
+
+    request =
+      Request.new()
+      |> Request.method(:post)
+      |> Request.url("/dns/v1/projects/{project}/managedZones/{managedZone}/rrsets", %{
+        "project" => URI.encode(project, &URI.char_unreserved?/1),
+        "managedZone" => URI.encode(managed_zone, &URI.char_unreserved?/1)
+      })
+      |> Request.add_optional_params(optional_params_config, optional_params)
+      |> Request.library_version(@library_version)
+
+    connection
+    |> Connection.execute(request)
+    |> Response.decode(opts ++ [struct: %GoogleApi.DNS.V1.Model.ResourceRecordSet{}])
+  end
+
+  @doc """
+  Deletes a previously created ResourceRecordSet.
+
+  ## Parameters
+
+  *   `connection` (*type:* `GoogleApi.DNS.V1.Connection.t`) - Connection to server
+  *   `project` (*type:* `String.t`) - Identifies the project addressed by this request.
+  *   `managed_zone` (*type:* `String.t`) - Identifies the managed zone addressed by this request. Can be the managed zone name or ID.
+  *   `name` (*type:* `String.t`) - Fully qualified domain name.
+  *   `type` (*type:* `String.t`) - RRSet type.
+  *   `optional_params` (*type:* `keyword()`) - Optional parameters
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
+      *   `:access_token` (*type:* `String.t`) - OAuth access token.
+      *   `:alt` (*type:* `String.t`) - Data format for response.
+      *   `:callback` (*type:* `String.t`) - JSONP
+      *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
+      *   `:key` (*type:* `String.t`) - API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+      *   `:oauth_token` (*type:* `String.t`) - OAuth 2.0 token for the current user.
+      *   `:prettyPrint` (*type:* `boolean()`) - Returns response with indentations and line breaks.
+      *   `:quotaUser` (*type:* `String.t`) - Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
+      *   `:uploadType` (*type:* `String.t`) - Legacy upload protocol for media (e.g. "media", "multipart").
+      *   `:upload_protocol` (*type:* `String.t`) - Upload protocol for media (e.g. "raw", "multipart").
+      *   `:clientOperationId` (*type:* `String.t`) - For mutating operation requests only. An optional identifier specified by the client. Must be unique for operation resources in the Operations collection.
+  *   `opts` (*type:* `keyword()`) - Call options
+
+  ## Returns
+
+  *   `{:ok, %GoogleApi.DNS.V1.Model.ResourceRecordSetsDeleteResponse{}}` on success
+  *   `{:error, info}` on failure
+  """
+  @spec dns_resource_record_sets_delete(
+          Tesla.Env.client(),
+          String.t(),
+          String.t(),
+          String.t(),
+          String.t(),
+          keyword(),
+          keyword()
+        ) ::
+          {:ok, GoogleApi.DNS.V1.Model.ResourceRecordSetsDeleteResponse.t()}
+          | {:ok, Tesla.Env.t()}
+          | {:ok, list()}
+          | {:error, any()}
+  def dns_resource_record_sets_delete(
+        connection,
+        project,
+        managed_zone,
+        name,
+        type,
+        optional_params \\ [],
+        opts \\ []
+      ) do
+    optional_params_config = %{
+      :"$.xgafv" => :query,
+      :access_token => :query,
+      :alt => :query,
+      :callback => :query,
+      :fields => :query,
+      :key => :query,
+      :oauth_token => :query,
+      :prettyPrint => :query,
+      :quotaUser => :query,
+      :uploadType => :query,
+      :upload_protocol => :query,
+      :clientOperationId => :query
+    }
+
+    request =
+      Request.new()
+      |> Request.method(:delete)
+      |> Request.url(
+        "/dns/v1/projects/{project}/managedZones/{managedZone}/rrsets/{name}/{type}",
+        %{
+          "project" => URI.encode(project, &URI.char_unreserved?/1),
+          "managedZone" => URI.encode(managed_zone, &URI.char_unreserved?/1),
+          "name" => URI.encode(name, &URI.char_unreserved?/1),
+          "type" => URI.encode(type, &(URI.char_unreserved?(&1) || &1 == ?/))
+        }
+      )
+      |> Request.add_optional_params(optional_params_config, optional_params)
+      |> Request.library_version(@library_version)
+
+    connection
+    |> Connection.execute(request)
+    |> Response.decode(
+      opts ++ [struct: %GoogleApi.DNS.V1.Model.ResourceRecordSetsDeleteResponse{}]
+    )
+  end
+
+  @doc """
+  Fetches the representation of an existing ResourceRecordSet.
+
+  ## Parameters
+
+  *   `connection` (*type:* `GoogleApi.DNS.V1.Connection.t`) - Connection to server
+  *   `project` (*type:* `String.t`) - Identifies the project addressed by this request.
+  *   `managed_zone` (*type:* `String.t`) - Identifies the managed zone addressed by this request. Can be the managed zone name or ID.
+  *   `name` (*type:* `String.t`) - Fully qualified domain name.
+  *   `type` (*type:* `String.t`) - RRSet type.
+  *   `optional_params` (*type:* `keyword()`) - Optional parameters
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
+      *   `:access_token` (*type:* `String.t`) - OAuth access token.
+      *   `:alt` (*type:* `String.t`) - Data format for response.
+      *   `:callback` (*type:* `String.t`) - JSONP
+      *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
+      *   `:key` (*type:* `String.t`) - API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+      *   `:oauth_token` (*type:* `String.t`) - OAuth 2.0 token for the current user.
+      *   `:prettyPrint` (*type:* `boolean()`) - Returns response with indentations and line breaks.
+      *   `:quotaUser` (*type:* `String.t`) - Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
+      *   `:uploadType` (*type:* `String.t`) - Legacy upload protocol for media (e.g. "media", "multipart").
+      *   `:upload_protocol` (*type:* `String.t`) - Upload protocol for media (e.g. "raw", "multipart").
+      *   `:clientOperationId` (*type:* `String.t`) - For mutating operation requests only. An optional identifier specified by the client. Must be unique for operation resources in the Operations collection.
+  *   `opts` (*type:* `keyword()`) - Call options
+
+  ## Returns
+
+  *   `{:ok, %GoogleApi.DNS.V1.Model.ResourceRecordSet{}}` on success
+  *   `{:error, info}` on failure
+  """
+  @spec dns_resource_record_sets_get(
+          Tesla.Env.client(),
+          String.t(),
+          String.t(),
+          String.t(),
+          String.t(),
+          keyword(),
+          keyword()
+        ) ::
+          {:ok, GoogleApi.DNS.V1.Model.ResourceRecordSet.t()}
+          | {:ok, Tesla.Env.t()}
+          | {:ok, list()}
+          | {:error, any()}
+  def dns_resource_record_sets_get(
+        connection,
+        project,
+        managed_zone,
+        name,
+        type,
+        optional_params \\ [],
+        opts \\ []
+      ) do
+    optional_params_config = %{
+      :"$.xgafv" => :query,
+      :access_token => :query,
+      :alt => :query,
+      :callback => :query,
+      :fields => :query,
+      :key => :query,
+      :oauth_token => :query,
+      :prettyPrint => :query,
+      :quotaUser => :query,
+      :uploadType => :query,
+      :upload_protocol => :query,
+      :clientOperationId => :query
+    }
+
+    request =
+      Request.new()
+      |> Request.method(:get)
+      |> Request.url(
+        "/dns/v1/projects/{project}/managedZones/{managedZone}/rrsets/{name}/{type}",
+        %{
+          "project" => URI.encode(project, &URI.char_unreserved?/1),
+          "managedZone" => URI.encode(managed_zone, &URI.char_unreserved?/1),
+          "name" => URI.encode(name, &URI.char_unreserved?/1),
+          "type" => URI.encode(type, &(URI.char_unreserved?(&1) || &1 == ?/))
+        }
+      )
+      |> Request.add_optional_params(optional_params_config, optional_params)
+      |> Request.library_version(@library_version)
+
+    connection
+    |> Connection.execute(request)
+    |> Response.decode(opts ++ [struct: %GoogleApi.DNS.V1.Model.ResourceRecordSet{}])
+  end
+
+  @doc """
   Enumerates ResourceRecordSets that you have created but not yet deleted.
 
   ## Parameters
@@ -105,5 +359,94 @@ defmodule GoogleApi.DNS.V1.Api.ResourceRecordSets do
     connection
     |> Connection.execute(request)
     |> Response.decode(opts ++ [struct: %GoogleApi.DNS.V1.Model.ResourceRecordSetsListResponse{}])
+  end
+
+  @doc """
+  Applies a partial update to an existing ResourceRecordSet.
+
+  ## Parameters
+
+  *   `connection` (*type:* `GoogleApi.DNS.V1.Connection.t`) - Connection to server
+  *   `project` (*type:* `String.t`) - Identifies the project addressed by this request.
+  *   `managed_zone` (*type:* `String.t`) - Identifies the managed zone addressed by this request. Can be the managed zone name or ID.
+  *   `name` (*type:* `String.t`) - Fully qualified domain name.
+  *   `type` (*type:* `String.t`) - RRSet type.
+  *   `optional_params` (*type:* `keyword()`) - Optional parameters
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
+      *   `:access_token` (*type:* `String.t`) - OAuth access token.
+      *   `:alt` (*type:* `String.t`) - Data format for response.
+      *   `:callback` (*type:* `String.t`) - JSONP
+      *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
+      *   `:key` (*type:* `String.t`) - API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+      *   `:oauth_token` (*type:* `String.t`) - OAuth 2.0 token for the current user.
+      *   `:prettyPrint` (*type:* `boolean()`) - Returns response with indentations and line breaks.
+      *   `:quotaUser` (*type:* `String.t`) - Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
+      *   `:uploadType` (*type:* `String.t`) - Legacy upload protocol for media (e.g. "media", "multipart").
+      *   `:upload_protocol` (*type:* `String.t`) - Upload protocol for media (e.g. "raw", "multipart").
+      *   `:clientOperationId` (*type:* `String.t`) - For mutating operation requests only. An optional identifier specified by the client. Must be unique for operation resources in the Operations collection.
+      *   `:body` (*type:* `GoogleApi.DNS.V1.Model.ResourceRecordSet.t`) - 
+  *   `opts` (*type:* `keyword()`) - Call options
+
+  ## Returns
+
+  *   `{:ok, %GoogleApi.DNS.V1.Model.ResourceRecordSet{}}` on success
+  *   `{:error, info}` on failure
+  """
+  @spec dns_resource_record_sets_patch(
+          Tesla.Env.client(),
+          String.t(),
+          String.t(),
+          String.t(),
+          String.t(),
+          keyword(),
+          keyword()
+        ) ::
+          {:ok, GoogleApi.DNS.V1.Model.ResourceRecordSet.t()}
+          | {:ok, Tesla.Env.t()}
+          | {:ok, list()}
+          | {:error, any()}
+  def dns_resource_record_sets_patch(
+        connection,
+        project,
+        managed_zone,
+        name,
+        type,
+        optional_params \\ [],
+        opts \\ []
+      ) do
+    optional_params_config = %{
+      :"$.xgafv" => :query,
+      :access_token => :query,
+      :alt => :query,
+      :callback => :query,
+      :fields => :query,
+      :key => :query,
+      :oauth_token => :query,
+      :prettyPrint => :query,
+      :quotaUser => :query,
+      :uploadType => :query,
+      :upload_protocol => :query,
+      :clientOperationId => :query,
+      :body => :body
+    }
+
+    request =
+      Request.new()
+      |> Request.method(:patch)
+      |> Request.url(
+        "/dns/v1/projects/{project}/managedZones/{managedZone}/rrsets/{name}/{type}",
+        %{
+          "project" => URI.encode(project, &URI.char_unreserved?/1),
+          "managedZone" => URI.encode(managed_zone, &URI.char_unreserved?/1),
+          "name" => URI.encode(name, &URI.char_unreserved?/1),
+          "type" => URI.encode(type, &(URI.char_unreserved?(&1) || &1 == ?/))
+        }
+      )
+      |> Request.add_optional_params(optional_params_config, optional_params)
+      |> Request.library_version(@library_version)
+
+    connection
+    |> Connection.execute(request)
+    |> Response.decode(opts ++ [struct: %GoogleApi.DNS.V1.Model.ResourceRecordSet{}])
   end
 end
