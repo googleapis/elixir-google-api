f786f35e3c181596a3095f6beac36809c7b7da6b
yoshi-automation <yoshi-automation@google.com>
2021-08-06 05:30:41 -0700

    Regenerate CloudKMS client

--- clients/cloud_kms/lib/google_api/cloud_kms/v1/api/projects.ex
+++ clients/cloud_kms/lib/google_api/cloud_kms/v1/api/projects.ex
@@ -26,6 +26,84 @@ defmodule GoogleApi.CloudKMS.V1.Api.Projects do
   @library_version Mix.Project.config() |> Keyword.get(:version, "")
 
   @doc """
+  Generate random bytes using the Cloud KMS randomness source in the provided location.
+
+  ## Parameters
+
+  *   `connection` (*type:* `GoogleApi.CloudKMS.V1.Connection.t`) - Connection to server
+  *   `projects_id` (*type:* `String.t`) - Part of `location`. The project-specific location in which to generate random bytes. For example, "projects/my-project/locations/us-central1".
+  *   `locations_id` (*type:* `String.t`) - Part of `location`. See documentation of `projectsId`.
+  *   `optional_params` (*type:* `keyword()`) - Optional parameters
+      *   `:"$.xgafv"` (*type:* `String.t`) - V1 error format.
+      *   `:access_token` (*type:* `String.t`) - OAuth access token.
+      *   `:alt` (*type:* `String.t`) - Data format for response.
+      *   `:callback` (*type:* `String.t`) - JSONP
+      *   `:fields` (*type:* `String.t`) - Selector specifying which fields to include in a partial response.
+      *   `:key` (*type:* `String.t`) - API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token.
+      *   `:oauth_token` (*type:* `String.t`) - OAuth 2.0 token for the current user.
+      *   `:prettyPrint` (*type:* `boolean()`) - Returns response with indentations and line breaks.
+      *   `:quotaUser` (*type:* `String.t`) - Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
+      *   `:uploadType` (*type:* `String.t`) - Legacy upload protocol for media (e.g. "media", "multipart").
+      *   `:upload_protocol` (*type:* `String.t`) - Upload protocol for media (e.g. "raw", "multipart").
+      *   `:body` (*type:* `GoogleApi.CloudKMS.V1.Model.GenerateRandomBytesRequest.t`) - 
+  *   `opts` (*type:* `keyword()`) - Call options
+
+  ## Returns
+
+  *   `{:ok, %GoogleApi.CloudKMS.V1.Model.GenerateRandomBytesResponse{}}` on success
+  *   `{:error, info}` on failure
+  """
+  @spec cloudkms_projects_locations_generate_random_bytes(
+          Tesla.Env.client(),
+          String.t(),
+          String.t(),
+          keyword(),
+          keyword()
+        ) ::
+          {:ok, GoogleApi.CloudKMS.V1.Model.GenerateRandomBytesResponse.t()}
+          | {:ok, Tesla.Env.t()}
+          | {:ok, list()}
+          | {:error, any()}
+  def cloudkms_projects_locations_generate_random_bytes(
+        connection,
+        projects_id,
+        locations_id,
+        optional_params \\ [],
+        opts \\ []
+      ) do
+    optional_params_config = %{
+      :"$.xgafv" => :query,
+      :access_token => :query,
+      :alt => :query,
+      :callback => :query,
+      :fields => :query,
+      :key => :query,
+      :oauth_token => :query,
+      :prettyPrint => :query,
+      :quotaUser => :query,
+      :uploadType => :query,
+      :upload_protocol => :query,
+      :body => :body
+    }
+
+    request =
+      Request.new()
+      |> Request.method(:post)
+      |> Request.url("/v1/projects/{projectsId}/locations/{locationsId}:generateRandomBytes", %{
+        "projectsId" => URI.encode(projects_id, &URI.char_unreserved?/1),
+        "locationsId" => URI.encode(locations_id, &URI.char_unreserved?/1)
+      })
+      |> Request.add_optional_params(optional_params_config, optional_params)
+      |> Request.library_version(@library_version)
+
+    connection
+    |> Connection.execute(request)
+    |> Response.decode(
+      opts ++ [struct: %GoogleApi.CloudKMS.V1.Model.GenerateRandomBytesResponse{}]
+    )
+  end
+
+  @doc """
   Gets information about a location.
 
   ## Parameters
--- clients/cloud_kms/lib/google_api/cloud_kms/v1/model/crypto_key.ex
+++ clients/cloud_kms/lib/google_api/cloud_kms/v1/model/crypto_key.ex
@@ -22,6 +22,8 @@ defmodule GoogleApi.CloudKMS.V1.Model.CryptoKey do
   ## Attributes
 
   *   `createTime` (*type:* `DateTime.t`, *default:* `nil`) - Output only. The time at which this CryptoKey was created.
+  *   `destroyScheduledDuration` (*type:* `String.t`, *default:* `nil`) - Immutable. The period of time that versions of this key spend in the DESTROY_SCHEDULED state before transitioning to DESTROYED. If not specified at creation time, the default duration is 24 hours.
+  *   `importOnly` (*type:* `boolean()`, *default:* `nil`) - Immutable. Whether this key may contain imported versions only.
   *   `labels` (*type:* `map()`, *default:* `nil`) - Labels with user-defined metadata. For more information, see [Labeling Keys](https://cloud.google.com/kms/docs/labeling-keys).
   *   `name` (*type:* `String.t`, *default:* `nil`) - Output only. The resource name for this CryptoKey in the format `projects/*/locations/*/keyRings/*/cryptoKeys/*`.
   *   `nextRotationTime` (*type:* `DateTime.t`, *default:* `nil`) - At next_rotation_time, the Key Management Service will automatically: 1. Create a new version of this CryptoKey. 2. Mark the new version as primary. Key rotations performed manually via CreateCryptoKeyVersion and UpdateCryptoKeyPrimaryVersion do not affect next_rotation_time. Keys with purpose ENCRYPT_DECRYPT support automatic rotation. For other keys, this field must be omitted.
@@ -35,6 +37,8 @@ defmodule GoogleApi.CloudKMS.V1.Model.CryptoKey do
 
   @type t :: %__MODULE__{
           :createTime => DateTime.t() | nil,
+          :destroyScheduledDuration => String.t() | nil,
+          :importOnly => boolean() | nil,
           :labels => map() | nil,
           :name => String.t() | nil,
           :nextRotationTime => DateTime.t() | nil,
@@ -45,6 +49,8 @@ defmodule GoogleApi.CloudKMS.V1.Model.CryptoKey do
         }
 
   field(:createTime, as: DateTime)
+  field(:destroyScheduledDuration)
+  field(:importOnly)
   field(:labels, type: :map)
   field(:name)
   field(:nextRotationTime, as: DateTime)
