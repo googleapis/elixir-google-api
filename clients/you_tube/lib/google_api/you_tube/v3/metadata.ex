9b7de98a4b573eede6ea331ad81ecb19352fa071
yoshi-automation <yoshi-automation@google.com>
2021-08-13 06:48:13 -0700

    Regenerate YouTube client

--- clients/you_tube/lib/google_api/you_tube/v3/metadata.ex
+++ clients/you_tube/lib/google_api/you_tube/v3/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.YouTube.V3 do
   API client metadata for GoogleApi.YouTube.V3.
   """
 
-  @discovery_revision "20210624"
+  @discovery_revision "20210811"
 
   def discovery_revision(), do: @discovery_revision
 end
