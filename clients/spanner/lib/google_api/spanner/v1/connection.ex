661355cdd3d7ec593bb498fad16096ae5957c201
yoshi-automation <yoshi-automation@google.com>
2021-08-24 06:42:56 -0700

    Regenerate Spanner client

--- clients/spanner/lib/google_api/spanner/v1/connection.ex
+++ clients/spanner/lib/google_api/spanner/v1/connection.ex
@@ -24,7 +24,7 @@ defmodule GoogleApi.Spanner.V1.Connection do
 
   use GoogleApi.Gax.Connection,
     scopes: [
-      # See, edit, configure, and delete your Google Cloud Platform data
+      # See, edit, configure, and delete your Google Cloud data and see the email address for your Google Account.
       "https://www.googleapis.com/auth/cloud-platform",
 
       # Administer your Spanner databases
