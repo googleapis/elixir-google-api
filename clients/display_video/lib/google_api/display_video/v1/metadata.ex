ab144af02d9e2119419b9e24cf06fe9747b207a1
yoshi-automation <yoshi-automation@google.com>
2021-08-24 05:48:28 -0700

    Regenerate DisplayVideo client

--- clients/display_video/lib/google_api/display_video/v1/metadata.ex
+++ clients/display_video/lib/google_api/display_video/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.DisplayVideo.V1 do
   API client metadata for GoogleApi.DisplayVideo.V1.
   """
 
-  @discovery_revision "20210809"
+  @discovery_revision "20210819"
 
   def discovery_revision(), do: @discovery_revision
 end
