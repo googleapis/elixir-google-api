8f42e12562536886f2170c387b469a1f6d40a170
yoshi-automation <yoshi-automation@google.com>
2021-08-25 06:15:41 -0700

    Regenerate MachineLearning client

--- clients/machine_learning/lib/google_api/machine_learning/v1/connection.ex
+++ clients/machine_learning/lib/google_api/machine_learning/v1/connection.ex
@@ -24,10 +24,10 @@ defmodule GoogleApi.MachineLearning.V1.Connection do
 
   use GoogleApi.Gax.Connection,
     scopes: [
-      # See, edit, configure, and delete your Google Cloud Platform data
+      # See, edit, configure, and delete your Google Cloud data and see the email address for your Google Account.
       "https://www.googleapis.com/auth/cloud-platform",
 
-      # View your data across Google Cloud Platform services
+      # View your data across Google Cloud services and see the email address of your Google Account
       "https://www.googleapis.com/auth/cloud-platform.read-only"
     ],
     otp_app: :google_api_machine_learning,
