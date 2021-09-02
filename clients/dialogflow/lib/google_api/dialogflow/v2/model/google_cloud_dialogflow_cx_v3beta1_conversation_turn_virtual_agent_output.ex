eb42aafc40b0188bd1ff1a50e704cef2cde668d4
yoshi-automation <yoshi-automation@google.com>
2021-09-02 05:43:04 -0700

    Regenerate Dialogflow client

--- clients/dialogflow/lib/google_api/dialogflow/v2/model/google_cloud_dialogflow_cx_v3beta1_conversation_turn_virtual_agent_output.ex
+++ clients/dialogflow/lib/google_api/dialogflow/v2/model/google_cloud_dialogflow_cx_v3beta1_conversation_turn_virtual_agent_output.ex
@@ -22,7 +22,7 @@ defmodule GoogleApi.Dialogflow.V2.Model.GoogleCloudDialogflowCxV3beta1Conversati
   ## Attributes
 
   *   `currentPage` (*type:* `GoogleApi.Dialogflow.V2.Model.GoogleCloudDialogflowCxV3beta1Page.t`, *default:* `nil`) - The Page on which the utterance was spoken. Only name and displayName will be set.
-  *   `diagnosticInfo` (*type:* `map()`, *default:* `nil`) - Required. Input only. The diagnostic info output for the turn.
+  *   `diagnosticInfo` (*type:* `map()`, *default:* `nil`) - Required. Input only. The diagnostic info output for the turn. Required to calculate the testing coverage.
   *   `differences` (*type:* `list(GoogleApi.Dialogflow.V2.Model.GoogleCloudDialogflowCxV3beta1TestRunDifference.t)`, *default:* `nil`) - Output only. If this is part of a result conversation turn, the list of differences between the original run and the replay for this output, if any.
   *   `sessionParameters` (*type:* `map()`, *default:* `nil`) - The session parameters available to the bot at this point.
   *   `status` (*type:* `GoogleApi.Dialogflow.V2.Model.GoogleRpcStatus.t`, *default:* `nil`) - Response error from the agent in the test result. If set, other output is empty.
--- clients/dialogflow/mix.exs
+++ clients/dialogflow/mix.exs
@@ -18,7 +18,7 @@
 defmodule GoogleApi.Dialogflow.Mixfile do
   use Mix.Project
 
-  @version "0.66.0"
+  @version "0.66.1"
 
   def project() do
     [
