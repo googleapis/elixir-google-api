171fbcdf49c8059a8e175c4ced5627e0ba60c209
yoshi-automation <yoshi-automation@google.com>
2021-09-21 05:47:20 -0700

    Regenerate DisplayVideo client

--- clients/display_video/lib/google_api/display_video/v1/model/business_chain_assigned_targeting_option_details.ex
+++ clients/display_video/lib/google_api/display_video/v1/model/business_chain_assigned_targeting_option_details.ex
@@ -24,7 +24,7 @@ defmodule GoogleApi.DisplayVideo.V1.Model.BusinessChainAssignedTargetingOptionDe
   *   `displayName` (*type:* `String.t`, *default:* `nil`) - Output only. The display name of a business chain, e.g. "KFC", "Chase Bank".
   *   `proximityRadiusAmount` (*type:* `float()`, *default:* `nil`) - Required. The radius of the area around the business chain that will be targeted. The units of the radius are specified by proximity_radius_unit. Must be 1 to 800 if unit is `DISTANCE_UNIT_KILOMETERS` and 1 to 500 if unit is `DISTANCE_UNIT_MILES`. The minimum increment for both cases is 0.1. Inputs will be rounded to the nearest acceptable value if it is too granular, e.g. 15.57 will become 15.6.
   *   `proximityRadiusUnit` (*type:* `String.t`, *default:* `nil`) - Required. The unit of distance by which the targeting radius is measured.
-  *   `targetingOptionId` (*type:* `String.t`, *default:* `nil`) - Required. The targeting_option_id of a TargetingOption of type `TARGETING_TYPE_BUSINESS_CHAIN`.
+  *   `targetingOptionId` (*type:* `String.t`, *default:* `nil`) - Required. The targeting_option_id of a TargetingOption of type `TARGETING_TYPE_BUSINESS_CHAIN`. Accepted business chain targeting option IDs can be retrieved using SearchTargetingOptions.
   """
 
   use GoogleApi.Gax.ModelBase
