0be4c2553d2b400ba8a9bd48e42b31c57f910eae
yoshi-automation <yoshi-automation@google.com>
2021-10-05 06:26:52 -0700

    Regenerate PolicyTroubleshooter client

--- clients/policy_troubleshooter/lib/google_api/policy_troubleshooter/v1/model/google_cloud_policytroubleshooter_v1_access_tuple.ex
+++ clients/policy_troubleshooter/lib/google_api/policy_troubleshooter/v1/model/google_cloud_policytroubleshooter_v1_access_tuple.ex
@@ -17,13 +17,13 @@
 
 defmodule GoogleApi.PolicyTroubleshooter.V1.Model.GoogleCloudPolicytroubleshooterV1AccessTuple do
   @moduledoc """
-  Information about the member, resource, and permission to check.
+  Information about the principal, resource, and permission to check.
 
   ## Attributes
 
   *   `fullResourceName` (*type:* `String.t`, *default:* `nil`) - Required. The full resource name that identifies the resource. For example, `//compute.googleapis.com/projects/my-project/zones/us-central1-a/instances/my-instance`. For examples of full resource names for Google Cloud services, see https://cloud.google.com/iam/help/troubleshooter/full-resource-names.
-  *   `permission` (*type:* `String.t`, *default:* `nil`) - Required. The IAM permission to check for the specified member and resource. For a complete list of IAM permissions, see https://cloud.google.com/iam/help/permissions/reference. For a complete list of predefined IAM roles and the permissions in each role, see https://cloud.google.com/iam/help/roles/reference.
-  *   `principal` (*type:* `String.t`, *default:* `nil`) - Required. The member, or principal, whose access you want to check, in the form of the email address that represents that member. For example, `alice@example.com` or `my-service-account@my-project.iam.gserviceaccount.com`. The member must be a Google Account or a service account. Other types of members are not supported.
+  *   `permission` (*type:* `String.t`, *default:* `nil`) - Required. The IAM permission to check for the specified principal and resource. For a complete list of IAM permissions, see https://cloud.google.com/iam/help/permissions/reference. For a complete list of predefined IAM roles and the permissions in each role, see https://cloud.google.com/iam/help/roles/reference.
+  *   `principal` (*type:* `String.t`, *default:* `nil`) - Required. The principal whose access you want to check, in the form of the email address that represents that principal. For example, `alice@example.com` or `my-service-account@my-project.iam.gserviceaccount.com`. The principal must be a Google Account or a service account. Other types of principals are not supported.
   """
 
   use GoogleApi.Gax.ModelBase
