7942e0d2d77e845fb763095c49364545b46afa33
yoshi-automation <yoshi-automation@google.com>
2021-10-05 06:29:36 -0700

    Regenerate RealTimeBidding client

--- clients/real_time_bidding/lib/google_api/real_time_bidding/v1/api/bidders.ex
+++ clients/real_time_bidding/lib/google_api/real_time_bidding/v1/api/bidders.ex
@@ -169,7 +169,7 @@ defmodule GoogleApi.RealTimeBidding.V1.Api.Bidders do
       *   `:upload_protocol` (*type:* `String.t`) - Upload protocol for media (e.g. "raw", "multipart").
       *   `:filter` (*type:* `String.t`) - Query string to filter creatives. If no filter is specified, all active creatives will be returned. Example: 'accountId=12345 AND (dealsStatus:DISAPPROVED AND disapprovalReason:UNACCEPTABLE_CONTENT) OR declaredAttributes:IS_COOKIE_TARGETED'
       *   `:pageSize` (*type:* `integer()`) - Requested page size. The server may return fewer creatives than requested (due to timeout constraint) even if more are available via another call. If unspecified, server will pick an appropriate default. Acceptable values are 1 to 1000, inclusive.
-      *   `:pageToken` (*type:* `String.t`) - A token identifying a page of results the server should return. Typically, this is the value of ListCreativesResponse.nextPageToken returned from the previous call to the 'ListCreatives' method.
+      *   `:pageToken` (*type:* `String.t`) - A token identifying a page of results the server should return. Typically, this is the value of ListCreativesResponse.nextPageToken returned from the previous call to the 'ListCreatives' method. Page tokens for continued pages are valid for up to five hours, counting from the call to 'ListCreatives' for the first page.
       *   `:view` (*type:* `String.t`) - Controls the amount of information included in the response. By default only creativeServingDecision is included. To retrieve the entire creative resource (including the declared fields and the creative content) specify the view as "FULL".
   *   `opts` (*type:* `keyword()`) - Call options
 
--- clients/real_time_bidding/lib/google_api/real_time_bidding/v1/metadata.ex
+++ clients/real_time_bidding/lib/google_api/real_time_bidding/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.RealTimeBidding.V1 do
   API client metadata for GoogleApi.RealTimeBidding.V1.
   """
 
-  @discovery_revision "20210529"
+  @discovery_revision "20211002"
 
   def discovery_revision(), do: @discovery_revision
 end
