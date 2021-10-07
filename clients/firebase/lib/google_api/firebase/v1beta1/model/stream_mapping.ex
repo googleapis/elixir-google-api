ec43346a10bd185324bfd5d4e72b7aecd42950a0
yoshi-automation <yoshi-automation@google.com>
2021-10-07 06:00:56 -0700

    Regenerate Firebase client

--- clients/firebase/lib/google_api/firebase/v1beta1/model/stream_mapping.ex
+++ clients/firebase/lib/google_api/firebase/v1beta1/model/stream_mapping.ex
@@ -22,7 +22,7 @@ defmodule GoogleApi.Firebase.V1beta1.Model.StreamMapping do
   ## Attributes
 
   *   `app` (*type:* `String.t`, *default:* `nil`) - The resource name of the Firebase App associated with the Google Analytics data stream, in the format: projects/PROJECT_IDENTIFIER/androidApps/APP_ID or projects/PROJECT_IDENTIFIER/iosApps/APP_ID or projects/PROJECT_IDENTIFIER /webApps/APP_ID Refer to the `FirebaseProject` [`name`](../projects#FirebaseProject.FIELDS.name) field for details about PROJECT_IDENTIFIER values.
-  *   `measurementId` (*type:* `String.t`, *default:* `nil`) - Applicable for Firebase Web Apps only. The unique Google-assigned identifier of the Google Analytics web stream associated with the Firebase Web App. Firebase SDKs use this ID to interact with Google Analytics APIs. Learn more about this ID and Google Analytics web streams in the [Analytics documentation](https://support.google.com/analytics/topic/9303475).
+  *   `measurementId` (*type:* `String.t`, *default:* `nil`) - Applicable for Firebase Web Apps only. The unique Google-assigned identifier of the Google Analytics web stream associated with the Firebase Web App. Firebase SDKs use this ID to interact with Google Analytics APIs. Learn more about this ID and Google Analytics web streams in the [Analytics documentation](https://support.google.com/analytics/answer/9304153).
   *   `streamId` (*type:* `String.t`, *default:* `nil`) - The unique Google-assigned identifier of the Google Analytics data stream associated with the Firebase App. Learn more about Google Analytics data streams in the [Analytics documentation](https://support.google.com/analytics/answer/9303323).
   """
 
