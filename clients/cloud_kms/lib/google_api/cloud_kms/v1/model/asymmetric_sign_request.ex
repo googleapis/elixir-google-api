680aef5112d6eff096c50bd7affdc3464e8f424b
yoshi-automation <yoshi-automation@google.com>
2021-10-21 05:34:32 -0700

    Regenerate CloudKMS client

--- clients/cloud_kms/lib/google_api/cloud_kms/v1/model/asymmetric_sign_request.ex
+++ clients/cloud_kms/lib/google_api/cloud_kms/v1/model/asymmetric_sign_request.ex
@@ -21,6 +21,8 @@ defmodule GoogleApi.CloudKMS.V1.Model.AsymmetricSignRequest do
 
   ## Attributes
 
+  *   `data` (*type:* `String.t`, *default:* `nil`) - Optional. This field will only be honored for RAW_PKCS1 keys. The data to sign. A digest is computed over the data that will be signed, PKCS #1 padding is applied to the digest directly and then encrypted.
+  *   `dataCrc32c` (*type:* `String.t`, *default:* `nil`) - Optional. An optional CRC32C checksum of the AsymmetricSignRequest.data. If specified, KeyManagementService will verify the integrity of the received AsymmetricSignRequest.data using this checksum. KeyManagementService will report an error if the checksum verification fails. If you receive a checksum error, your client should verify that CRC32C(AsymmetricSignRequest.data) is equal to AsymmetricSignRequest.data_crc32c, and if so, perform a limited number of retries. A persistent mismatch may indicate an issue in your computation of the CRC32C checksum. Note: This field is defined as int64 for reasons of compatibility across different languages. However, it is a non-negative integer, which will never exceed 2^32-1, and can be safely downconverted to uint32 in languages that support this type.
   *   `digest` (*type:* `GoogleApi.CloudKMS.V1.Model.Digest.t`, *default:* `nil`) - Optional. The digest of the data to sign. The digest must be produced with the same digest algorithm as specified by the key version's algorithm.
   *   `digestCrc32c` (*type:* `String.t`, *default:* `nil`) - Optional. An optional CRC32C checksum of the AsymmetricSignRequest.digest. If specified, KeyManagementService will verify the integrity of the received AsymmetricSignRequest.digest using this checksum. KeyManagementService will report an error if the checksum verification fails. If you receive a checksum error, your client should verify that CRC32C(AsymmetricSignRequest.digest) is equal to AsymmetricSignRequest.digest_crc32c, and if so, perform a limited number of retries. A persistent mismatch may indicate an issue in your computation of the CRC32C checksum. Note: This field is defined as int64 for reasons of compatibility across different languages. However, it is a non-negative integer, which will never exceed 2^32-1, and can be safely downconverted to uint32 in languages that support this type.
   """
@@ -28,10 +30,14 @@ defmodule GoogleApi.CloudKMS.V1.Model.AsymmetricSignRequest do
   use GoogleApi.Gax.ModelBase
 
   @type t :: %__MODULE__{
+          :data => String.t() | nil,
+          :dataCrc32c => String.t() | nil,
           :digest => GoogleApi.CloudKMS.V1.Model.Digest.t() | nil,
           :digestCrc32c => String.t() | nil
         }
 
+  field(:data)
+  field(:dataCrc32c)
   field(:digest, as: GoogleApi.CloudKMS.V1.Model.Digest)
   field(:digestCrc32c)
 end
--- clients/cloud_kms/lib/google_api/cloud_kms/v1/model/asymmetric_sign_response.ex
+++ clients/cloud_kms/lib/google_api/cloud_kms/v1/model/asymmetric_sign_response.ex
@@ -25,6 +25,7 @@ defmodule GoogleApi.CloudKMS.V1.Model.AsymmetricSignResponse do
   *   `protectionLevel` (*type:* `String.t`, *default:* `nil`) - The ProtectionLevel of the CryptoKeyVersion used for signing.
   *   `signature` (*type:* `String.t`, *default:* `nil`) - The created signature.
   *   `signatureCrc32c` (*type:* `String.t`, *default:* `nil`) - Integrity verification field. A CRC32C checksum of the returned AsymmetricSignResponse.signature. An integrity check of AsymmetricSignResponse.signature can be performed by computing the CRC32C checksum of AsymmetricSignResponse.signature and comparing your results to this field. Discard the response in case of non-matching checksum values, and perform a limited number of retries. A persistent mismatch may indicate an issue in your computation of the CRC32C checksum. Note: This field is defined as int64 for reasons of compatibility across different languages. However, it is a non-negative integer, which will never exceed 2^32-1, and can be safely downconverted to uint32 in languages that support this type.
+  *   `verifiedDataCrc32c` (*type:* `boolean()`, *default:* `nil`) - Integrity verification field. A flag indicating whether AsymmetricSignRequest.data_crc32c was received by KeyManagementService and used for the integrity verification of the data. A false value of this field indicates either that AsymmetricSignRequest.data_crc32c was left unset or that it was not delivered to KeyManagementService. If you've set AsymmetricSignRequest.data_crc32c but this field is still false, discard the response and perform a limited number of retries.
   *   `verifiedDigestCrc32c` (*type:* `boolean()`, *default:* `nil`) - Integrity verification field. A flag indicating whether AsymmetricSignRequest.digest_crc32c was received by KeyManagementService and used for the integrity verification of the digest. A false value of this field indicates either that AsymmetricSignRequest.digest_crc32c was left unset or that it was not delivered to KeyManagementService. If you've set AsymmetricSignRequest.digest_crc32c but this field is still false, discard the response and perform a limited number of retries.
   """
 
@@ -35,6 +36,7 @@ defmodule GoogleApi.CloudKMS.V1.Model.AsymmetricSignResponse do
           :protectionLevel => String.t() | nil,
           :signature => String.t() | nil,
           :signatureCrc32c => String.t() | nil,
+          :verifiedDataCrc32c => boolean() | nil,
           :verifiedDigestCrc32c => boolean() | nil
         }
 
@@ -42,6 +44,7 @@ defmodule GoogleApi.CloudKMS.V1.Model.AsymmetricSignResponse do
   field(:protectionLevel)
   field(:signature)
   field(:signatureCrc32c)
+  field(:verifiedDataCrc32c)
   field(:verifiedDigestCrc32c)
 end
 
