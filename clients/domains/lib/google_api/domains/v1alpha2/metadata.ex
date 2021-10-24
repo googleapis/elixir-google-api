e05098823af0ffe2e9b8c45743e50680a423138f
yoshi-automation <yoshi-automation@google.com>
2021-10-24 05:48:44 -0700

    Regenerate Domains client

--- clients/domains/lib/google_api/domains/v1alpha2/metadata.ex
+++ clients/domains/lib/google_api/domains/v1alpha2/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.Domains.V1alpha2 do
   API client metadata for GoogleApi.Domains.V1alpha2.
   """
 
-  @discovery_revision "20211004"
+  @discovery_revision "20211019"
 
   def discovery_revision(), do: @discovery_revision
 end
