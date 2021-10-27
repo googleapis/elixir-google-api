d3bb14b5bd968f8efbe0f646415746061affb445
yoshi-automation <yoshi-automation@google.com>
2021-10-27 06:54:35 -0700

    Regenerate ServiceUsage client

--- clients/service_usage/lib/google_api/service_usage/v1/metadata.ex
+++ clients/service_usage/lib/google_api/service_usage/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.ServiceUsage.V1 do
   API client metadata for GoogleApi.ServiceUsage.V1.
   """
 
-  @discovery_revision "20210731"
+  @discovery_revision "20211025"
 
   def discovery_revision(), do: @discovery_revision
 end
