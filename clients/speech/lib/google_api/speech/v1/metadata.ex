bda6cbd59f7b482e89504519d8cda238deca03ff
yoshi-automation <yoshi-automation@google.com>
2021-10-28 07:00:26 -0700

    Regenerate Speech client

--- clients/speech/lib/google_api/speech/v1/metadata.ex
+++ clients/speech/lib/google_api/speech/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.Speech.V1 do
   API client metadata for GoogleApi.Speech.V1.
   """
 
-  @discovery_revision "20211008"
+  @discovery_revision "20211021"
 
   def discovery_revision(), do: @discovery_revision
 end
