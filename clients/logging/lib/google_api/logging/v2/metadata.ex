192b2762bde7862f44b02e4b4a9adf8e99423535
yoshi-automation <yoshi-automation@google.com>
2021-10-29 06:24:31 -0700

    Regenerate Logging client

--- clients/logging/lib/google_api/logging/v2/metadata.ex
+++ clients/logging/lib/google_api/logging/v2/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.Logging.V2 do
   API client metadata for GoogleApi.Logging.V2.
   """
 
-  @discovery_revision "20211015"
+  @discovery_revision "20211019"
 
   def discovery_revision(), do: @discovery_revision
 end
