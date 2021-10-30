09ba710004e28894a135930edd5ce153fb3b7e18
yoshi-automation <yoshi-automation@google.com>
2021-10-30 07:25:18 -0700

    Regenerate ServiceControl client

--- clients/service_control/lib/google_api/service_control/v1/metadata.ex
+++ clients/service_control/lib/google_api/service_control/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.ServiceControl.V1 do
   API client metadata for GoogleApi.ServiceControl.V1.
   """
 
-  @discovery_revision "20210924"
+  @discovery_revision "20211022"
 
   def discovery_revision(), do: @discovery_revision
 end
