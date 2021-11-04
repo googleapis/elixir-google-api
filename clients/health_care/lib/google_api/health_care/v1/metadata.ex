22559a09089501cee7e06bdbd7e30356a6ec50c0
yoshi-automation <yoshi-automation@google.com>
2021-11-04 06:11:45 -0700

    Regenerate HealthCare client

--- clients/health_care/lib/google_api/health_care/v1/metadata.ex
+++ clients/health_care/lib/google_api/health_care/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.HealthCare.V1 do
   API client metadata for GoogleApi.HealthCare.V1.
   """
 
-  @discovery_revision "20211016"
+  @discovery_revision "20211025"
 
   def discovery_revision(), do: @discovery_revision
 end
