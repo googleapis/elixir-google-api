3aed26bddd444d073cb1315e1cf403aee32257ae
yoshi-automation <yoshi-automation@google.com>
2021-11-10 05:38:24 -0800

    Regenerate ContainerAnalysis client

--- clients/container_analysis/lib/google_api/container_analysis/v1alpha1/metadata.ex
+++ clients/container_analysis/lib/google_api/container_analysis/v1alpha1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.ContainerAnalysis.V1alpha1 do
   API client metadata for GoogleApi.ContainerAnalysis.V1alpha1.
   """
 
-  @discovery_revision "20211015"
+  @discovery_revision "20211105"
 
   def discovery_revision(), do: @discovery_revision
 end
