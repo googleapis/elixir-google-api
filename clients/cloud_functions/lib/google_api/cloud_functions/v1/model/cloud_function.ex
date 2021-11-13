fe3c3ef02b3222f68509a1cb5d887e89ffcd1e2a
yoshi-automation <yoshi-automation@google.com>
2021-11-13 05:31:52 -0800

    Regenerate CloudFunctions client

--- clients/cloud_functions/lib/google_api/cloud_functions/v1/model/cloud_function.ex
+++ clients/cloud_functions/lib/google_api/cloud_functions/v1/model/cloud_function.ex
@@ -27,11 +27,13 @@ defmodule GoogleApi.CloudFunctions.V1.Model.CloudFunction do
   *   `buildName` (*type:* `String.t`, *default:* `nil`) - Output only. The Cloud Build Name of the function deployment. `projects//locations//builds/`.
   *   `buildWorkerPool` (*type:* `String.t`, *default:* `nil`) - Name of the Cloud Build Custom Worker Pool that should be used to build the function. The format of this field is `projects/{project}/locations/{region}/workerPools/{workerPool}` where `{project}` and `{region}` are the project id and region respectively where the worker pool is defined and `{workerPool}` is the short name of the worker pool. If the project id is not the same as the function, then the Cloud Functions Service Agent (`service-@gcf-admin-robot.iam.gserviceaccount.com`) must be granted the role Cloud Build Custom Workers Builder (`roles/cloudbuild.customworkers.builder`) in the project.
   *   `description` (*type:* `String.t`, *default:* `nil`) - User-provided description of a function.
+  *   `dockerRepository` (*type:* `String.t`, *default:* `nil`) - User managed repository created in Artifact Registry optionally with a customer managed encryption key. If specified, deployments will use Artifact Registry. If unspecified and the deployment is eligible to use Artifact Registry, GCF will create and use a repository named 'gcf-artifacts' for every deployed region. This is the repository to which the function docker image will be pushed after it is built by Cloud Build. It must match the pattern `projects/{project}/locations/{location}/repositories/{repository}`. Cross-project repositories are not supported. Cross-location repositories are not supported. Repository format must be 'DOCKER'.
   *   `entryPoint` (*type:* `String.t`, *default:* `nil`) - The name of the function (as defined in source code) that will be executed. Defaults to the resource name suffix, if not specified. For backward compatibility, if function with given name is not found, then the system will try to use function named "function". For Node.js this is name of a function exported by the module specified in `source_location`.
   *   `environmentVariables` (*type:* `map()`, *default:* `nil`) - Environment variables that shall be available during function execution.
   *   `eventTrigger` (*type:* `GoogleApi.CloudFunctions.V1.Model.EventTrigger.t`, *default:* `nil`) - A source that fires events in response to a condition in another service.
   *   `httpsTrigger` (*type:* `GoogleApi.CloudFunctions.V1.Model.HttpsTrigger.t`, *default:* `nil`) - An HTTPS endpoint type of source that can be triggered via URL.
   *   `ingressSettings` (*type:* `String.t`, *default:* `nil`) - The ingress settings for the function, controlling what traffic can reach it.
+  *   `kmsKeyName` (*type:* `String.t`, *default:* `nil`) - Resource name of a KMS crypto key (managed by the user) used to encrypt/decrypt function resources. It must match the pattern `projects/{project}/locations/{location}/keyRings/{key_ring}/cryptoKeys/{crypto_key}`. If specified, you must also provide an artifact registry repository using the `docker_repository` field that was created with the same KMS crypto key. The following service accounts need to be granted the role 'Cloud KMS CryptoKey Encrypter/Decrypter (roles/cloudkms.cryptoKeyEncrypterDecrypter)' on the Key/KeyRing/Project/Organization (least access preferred). 1. Google Cloud Functions service account (service-{project_number}@gcf-admin-robot.iam.gserviceaccount.com) - Required to protect the function's image. 2. Google Storage service account (service-{project_number}@gs-project-accounts.iam.gserviceaccount.com) - Required to protect the function's source code. If this service account does not exist, deploying a function without a KMS key or retrieving the service agent name provisions it. For more information, see https://cloud.google.com/storage/docs/projects#service-agents and https://cloud.google.com/storage/docs/getting-service-agent#gsutil. Google Cloud Functions delegates access to service agents to protect function resources in internal projects that are not accessible by the end user.
   *   `labels` (*type:* `map()`, *default:* `nil`) - Labels associated with this Cloud Function.
   *   `maxInstances` (*type:* `integer()`, *default:* `nil`) - The limit on the maximum number of function instances that may coexist at a given time. In some cases, such as rapid traffic surges, Cloud Functions may, for a short period of time, create more instances than the specified max instances limit. If your function cannot tolerate this temporary behavior, you may want to factor in a safety margin and set a lower max instances value than your function can tolerate. See the [Max Instances](https://cloud.google.com/functions/docs/max-instances) Guide for more details.
   *   `minInstances` (*type:* `integer()`, *default:* `nil`) - A lower bound for the number function instances that may coexist at a given time.
@@ -62,11 +64,13 @@ defmodule GoogleApi.CloudFunctions.V1.Model.CloudFunction do
           :buildName => String.t() | nil,
           :buildWorkerPool => String.t() | nil,
           :description => String.t() | nil,
+          :dockerRepository => String.t() | nil,
           :entryPoint => String.t() | nil,
           :environmentVariables => map() | nil,
           :eventTrigger => GoogleApi.CloudFunctions.V1.Model.EventTrigger.t() | nil,
           :httpsTrigger => GoogleApi.CloudFunctions.V1.Model.HttpsTrigger.t() | nil,
           :ingressSettings => String.t() | nil,
+          :kmsKeyName => String.t() | nil,
           :labels => map() | nil,
           :maxInstances => integer() | nil,
           :minInstances => integer() | nil,
@@ -95,11 +99,13 @@ defmodule GoogleApi.CloudFunctions.V1.Model.CloudFunction do
   field(:buildName)
   field(:buildWorkerPool)
   field(:description)
+  field(:dockerRepository)
   field(:entryPoint)
   field(:environmentVariables, type: :map)
   field(:eventTrigger, as: GoogleApi.CloudFunctions.V1.Model.EventTrigger)
   field(:httpsTrigger, as: GoogleApi.CloudFunctions.V1.Model.HttpsTrigger)
   field(:ingressSettings)
+  field(:kmsKeyName)
   field(:labels, type: :map)
   field(:maxInstances)
   field(:minInstances)
