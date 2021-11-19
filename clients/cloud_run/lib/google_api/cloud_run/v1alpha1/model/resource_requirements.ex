2c1b3adc28c853843dcf1338f209d08a719a53c9
yoshi-automation <yoshi-automation@google.com>
2021-11-19 05:34:51 -0800

    Regenerate CloudRun client

--- clients/cloud_run/lib/google_api/cloud_run/v1alpha1/model/resource_requirements.ex
+++ clients/cloud_run/lib/google_api/cloud_run/v1alpha1/model/resource_requirements.ex
@@ -22,7 +22,7 @@ defmodule GoogleApi.CloudRun.V1alpha1.Model.ResourceRequirements do
   ## Attributes
 
   *   `limits` (*type:* `map()`, *default:* `nil`) - (Optional) Only memory and CPU are supported. Note: The only supported values for CPU are '1', '2', and '4'. Setting 4 CPU requires at least 2Gi of memory. Limits describes the maximum amount of compute resources allowed. The values of the map is string form of the 'quantity' k8s type: https://github.com/kubernetes/kubernetes/blob/master/staging/src/k8s.io/apimachinery/pkg/api/resource/quantity.go
-  *   `requests` (*type:* `map()`, *default:* `nil`) - (Optional) Only memory and CPU are supported. Note: The only supported values for CPU are '1' and '2'. Requests describes the minimum amount of compute resources required. If Requests is omitted for a container, it defaults to Limits if that is explicitly specified, otherwise to an implementation-defined value. The values of the map is string form of the 'quantity' k8s type: https://github.com/kubernetes/kubernetes/blob/master/staging/src/k8s.io/apimachinery/pkg/api/resource/quantity.go
+  *   `requests` (*type:* `map()`, *default:* `nil`) - (Optional) Only memory and CPU are supported. Note: The only supported values for CPU are '1', '2', and '4'. Requests describes the minimum amount of compute resources required. If Requests is omitted for a container, it defaults to Limits if that is explicitly specified, otherwise to an implementation-defined value. The values of the map is string form of the 'quantity' k8s type: https://github.com/kubernetes/kubernetes/blob/master/staging/src/k8s.io/apimachinery/pkg/api/resource/quantity.go
   """
 
   use GoogleApi.Gax.ModelBase
