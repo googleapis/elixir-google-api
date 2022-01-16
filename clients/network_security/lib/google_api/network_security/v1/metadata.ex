e80dc48ba56f26de6258ee4f4c3118aaf5df0aa1
yoshi-automation <yoshi-automation@google.com>
2022-01-16 06:48:21 -0800

    Regenerate NetworkSecurity client

--- clients/network_security/lib/google_api/network_security/v1/metadata.ex
+++ clients/network_security/lib/google_api/network_security/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.NetworkSecurity.V1 do
   API client metadata for GoogleApi.NetworkSecurity.V1.
   """
 
-  @discovery_revision "20210908"
+  @discovery_revision "20220105"
 
   def discovery_revision(), do: @discovery_revision
 end
