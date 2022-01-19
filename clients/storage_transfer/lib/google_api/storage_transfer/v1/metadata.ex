959b7a4d10cbbb5b00b1d425c94b25bbd461a54d
yoshi-automation <yoshi-automation@google.com>
2022-01-19 07:29:08 -0800

    Regenerate StorageTransfer client

--- clients/storage_transfer/lib/google_api/storage_transfer/v1/metadata.ex
+++ clients/storage_transfer/lib/google_api/storage_transfer/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.StorageTransfer.V1 do
   API client metadata for GoogleApi.StorageTransfer.V1.
   """
 
-  @discovery_revision "20220105"
+  @discovery_revision "20220113"
 
   def discovery_revision(), do: @discovery_revision
 end
