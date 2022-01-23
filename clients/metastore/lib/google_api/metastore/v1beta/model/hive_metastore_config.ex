cc1728239db5a7439882dff9ef41a4c5c59486f4
yoshi-automation <yoshi-automation@google.com>
2022-01-23 06:41:44 -0800

    Regenerate Metastore client

--- clients/metastore/lib/google_api/metastore/v1beta/model/hive_metastore_config.ex
+++ clients/metastore/lib/google_api/metastore/v1beta/model/hive_metastore_config.ex
@@ -21,6 +21,7 @@ defmodule GoogleApi.Metastore.V1beta.Model.HiveMetastoreConfig do
 
   ## Attributes
 
+  *   `auxiliaryVersions` (*type:* `%{optional(String.t) => GoogleApi.Metastore.V1beta.Model.AuxiliaryVersionConfig.t}`, *default:* `nil`) - A mapping of Hive metastore version to the auxiliary version configuration. When specified, a secondary Hive metastore service is created along with the primary service. All auxiliary versions must be less than the service's primary version. The key is the auxiliary service name and it must match the regular expression a-z?. This means that the first character must be a lowercase letter, and all the following characters must be hyphens, lowercase letters, or digits, except the last character, which cannot be a hyphen.
   *   `configOverrides` (*type:* `map()`, *default:* `nil`) - A mapping of Hive metastore configuration key-value pairs to apply to the Hive metastore (configured in hive-site.xml). The mappings override system defaults (some keys cannot be overridden). These overrides are also applied to auxiliary versions and can be further customized in the auxiliary version's AuxiliaryVersionConfig.
   *   `endpointProtocol` (*type:* `String.t`, *default:* `nil`) - The protocol to use for the metastore service endpoint. If unspecified, defaults to THRIFT.
   *   `kerberosConfig` (*type:* `GoogleApi.Metastore.V1beta.Model.KerberosConfig.t`, *default:* `nil`) - Information used to configure the Hive metastore service as a service principal in a Kerberos realm. To disable Kerberos, use the UpdateService method and specify this field's path (hive_metastore_config.kerberos_config) in the request's update_mask while omitting this field from the request's service.
@@ -30,11 +31,19 @@ defmodule GoogleApi.Metastore.V1beta.Model.HiveMetastoreConfig do
   use GoogleApi.Gax.ModelBase
 
   @type t :: %__MODULE__{
+          :auxiliaryVersions =>
+            %{optional(String.t()) => GoogleApi.Metastore.V1beta.Model.AuxiliaryVersionConfig.t()}
+            | nil,
           :configOverrides => map() | nil,
           :endpointProtocol => String.t() | nil,
           :kerberosConfig => GoogleApi.Metastore.V1beta.Model.KerberosConfig.t() | nil,
           :version => String.t() | nil
         }
+
+  field(:auxiliaryVersions,
+    as: GoogleApi.Metastore.V1beta.Model.AuxiliaryVersionConfig,
+    type: :map
+  )
 
   field(:configOverrides, type: :map)
   field(:endpointProtocol)
