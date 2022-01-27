27f23a15459997d1a295ebdf505df60c0056ba7e
yoshi-automation <yoshi-automation@google.com>
2022-01-27 06:30:24 -0800

    Regenerate HealthCare client

--- clients/health_care/lib/google_api/health_care/v1beta1/model/deidentify_dicom_store_request.ex
+++ clients/health_care/lib/google_api/health_care/v1beta1/model/deidentify_dicom_store_request.ex
@@ -21,7 +21,7 @@ defmodule GoogleApi.HealthCare.V1beta1.Model.DeidentifyDicomStoreRequest do
 
   ## Attributes
 
-  *   `config` (*type:* `GoogleApi.HealthCare.V1beta1.Model.DeidentifyConfig.t`, *default:* `nil`) - De-identify configuration.
+  *   `config` (*type:* `GoogleApi.HealthCare.V1beta1.Model.DeidentifyConfig.t`, *default:* `nil`) - Deidentify configuration.
   *   `destinationStore` (*type:* `String.t`, *default:* `nil`) - The name of the DICOM store to create and write the redacted data to. For example, `projects/{project_id}/locations/{location_id}/datasets/{dataset_id}/dicomStores/{dicom_store_id}`. * The destination dataset must exist. * The source dataset and destination dataset must both reside in the same location. De-identifying data across multiple locations is not supported. * The destination DICOM store must not exist. * The caller must have the necessary permissions to create the destination DICOM store.
   *   `filterConfig` (*type:* `GoogleApi.HealthCare.V1beta1.Model.DicomFilterConfig.t`, *default:* `nil`) - Filter configuration.
   """
