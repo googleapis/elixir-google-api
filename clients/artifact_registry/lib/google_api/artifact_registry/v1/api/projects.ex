aeab50503a82b20fecff317814ba0c27f6d3b8e4
yoshi-automation <yoshi-automation@google.com>
2022-01-27 05:19:22 -0800

    Regenerate ArtifactRegistry client

--- clients/artifact_registry/lib/google_api/artifact_registry/v1/api/projects.ex
+++ clients/artifact_registry/lib/google_api/artifact_registry/v1/api/projects.ex
@@ -2801,7 +2801,7 @@ defmodule GoogleApi.ArtifactRegistry.V1.Api.Projects do
       *   `:quotaUser` (*type:* `String.t`) - Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters.
       *   `:uploadType` (*type:* `String.t`) - Legacy upload protocol for media (e.g. "media", "multipart").
       *   `:upload_protocol` (*type:* `String.t`) - Upload protocol for media (e.g. "raw", "multipart").
-      *   `:orderBy` (*type:* `String.t`) - Optional. Sorting field and order
+      *   `:orderBy` (*type:* `String.t`) - Optional. The field to order the results by.
       *   `:pageSize` (*type:* `integer()`) - The maximum number of versions to return. Maximum page size is 1,000.
       *   `:pageToken` (*type:* `String.t`) - The next_page_token value returned from a previous list request, if any.
       *   `:view` (*type:* `String.t`) - The view that should be returned in the response.
