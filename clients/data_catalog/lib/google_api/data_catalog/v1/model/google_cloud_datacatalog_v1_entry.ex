6107a9766d47a2cea54a3976350b80c50da63b5d
yoshi-automation <yoshi-automation@google.com>
2022-02-04 05:56:01 -0800

    Regenerate DataCatalog client

--- clients/data_catalog/lib/google_api/data_catalog/v1/model/google_cloud_datacatalog_v1_entry.ex
+++ clients/data_catalog/lib/google_api/data_catalog/v1/model/google_cloud_datacatalog_v1_entry.ex
@@ -23,7 +23,7 @@ defmodule GoogleApi.DataCatalog.V1.Model.GoogleCloudDatacatalogV1Entry do
 
   *   `bigqueryDateShardedSpec` (*type:* `GoogleApi.DataCatalog.V1.Model.GoogleCloudDatacatalogV1BigQueryDateShardedSpec.t`, *default:* `nil`) - Specification for a group of BigQuery tables with the `[prefix]YYYYMMDD` name pattern. For more information, see [Introduction to partitioned tables] (https://cloud.google.com/bigquery/docs/partitioned-tables#partitioning_versus_sharding).
   *   `bigqueryTableSpec` (*type:* `GoogleApi.DataCatalog.V1.Model.GoogleCloudDatacatalogV1BigQueryTableSpec.t`, *default:* `nil`) - Specification that applies to a BigQuery table. Valid only for entries with the `TABLE` type.
-  *   `businessContext` (*type:* `GoogleApi.DataCatalog.V1.Model.GoogleCloudDatacatalogV1BusinessContext.t`, *default:* `nil`) - Business Context of the entry.
+  *   `businessContext` (*type:* `GoogleApi.DataCatalog.V1.Model.GoogleCloudDatacatalogV1BusinessContext.t`, *default:* `nil`) - Business Context of the entry. Not supported for BigQuery datasets
   *   `dataSource` (*type:* `GoogleApi.DataCatalog.V1.Model.GoogleCloudDatacatalogV1DataSource.t`, *default:* `nil`) - Output only. Physical location of the entry.
   *   `dataSourceConnectionSpec` (*type:* `GoogleApi.DataCatalog.V1.Model.GoogleCloudDatacatalogV1DataSourceConnectionSpec.t`, *default:* `nil`) - Specification that applies to a data source connection. Valid only for entries with the `DATA_SOURCE_CONNECTION` type.
   *   `databaseTableSpec` (*type:* `GoogleApi.DataCatalog.V1.Model.GoogleCloudDatacatalogV1DatabaseTableSpec.t`, *default:* `nil`) - Specification that applies to a table resource. Valid only for entries with the `TABLE` type.
