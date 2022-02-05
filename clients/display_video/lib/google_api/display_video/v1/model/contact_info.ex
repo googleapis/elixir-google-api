6549780253ffb095c62acbb82d73521c5f855e66
yoshi-automation <yoshi-automation@google.com>
2022-02-05 06:05:31 -0800

    Regenerate DisplayVideo client

--- clients/display_video/lib/google_api/display_video/v1/model/contact_info.ex
+++ clients/display_video/lib/google_api/display_video/v1/model/contact_info.ex
@@ -21,12 +21,12 @@ defmodule GoogleApi.DisplayVideo.V1.Model.ContactInfo do
 
   ## Attributes
 
-  *   `countryCode` (*type:* `String.t`, *default:* `nil`) - Country code of the member.
-  *   `hashedEmails` (*type:* `list(String.t)`, *default:* `nil`) - A list of SHA256 hashed email of the member.
-  *   `hashedFirstName` (*type:* `String.t`, *default:* `nil`) - SHA256 hashed first name of the member.
-  *   `hashedLastName` (*type:* `String.t`, *default:* `nil`) - SHA256 hashed last name of the member.
-  *   `hashedPhoneNumbers` (*type:* `list(String.t)`, *default:* `nil`) - A list of SHA256 hashed phone numbers of the member.
-  *   `zipCodes` (*type:* `list(String.t)`, *default:* `nil`) - A list of zip codes of the member.
+  *   `countryCode` (*type:* `String.t`, *default:* `nil`) - Country code of the member. Must also be set with the following fields: * hashed_first_name * hashed_last_name * zip_codes
+  *   `hashedEmails` (*type:* `list(String.t)`, *default:* `nil`) - A list of SHA256 hashed email of the member. Before hashing, remove all whitespace and make sure the string is all lowercase.
+  *   `hashedFirstName` (*type:* `String.t`, *default:* `nil`) - SHA256 hashed first name of the member. Before hashing, remove all whitespace and make sure the string is all lowercase. Must also be set with the following fields: * country_code * hashed_last_name * zip_codes
+  *   `hashedLastName` (*type:* `String.t`, *default:* `nil`) - SHA256 hashed last name of the member. Before hashing, remove all whitespace and make sure the string is all lowercase. Must also be set with the following fields: * country_code * hashed_first_name * zip_codes
+  *   `hashedPhoneNumbers` (*type:* `list(String.t)`, *default:* `nil`) - A list of SHA256 hashed phone numbers of the member. Before hashing, all phone numbers must be formatted using the [E.164 format](//en.wikipedia.org/wiki/E.164) and include the country calling code.
+  *   `zipCodes` (*type:* `list(String.t)`, *default:* `nil`) - A list of zip codes of the member. Must also be set with the following fields: * country_code * hashed_first_name * hashed_last_name
   """
 
   use GoogleApi.Gax.ModelBase
