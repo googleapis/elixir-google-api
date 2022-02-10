20895ef2a770fae7590a384ba7302927e76a8948
yoshi-automation <yoshi-automation@google.com>
2022-02-10 07:01:58 -0800

    Regenerate PolicySimulator client

--- clients/policy_simulator/lib/google_api/policy_simulator/v1/metadata.ex
+++ clients/policy_simulator/lib/google_api/policy_simulator/v1/metadata.ex
@@ -20,7 +20,7 @@ defmodule GoogleApi.PolicySimulator.V1 do
   API client metadata for GoogleApi.PolicySimulator.V1.
   """
 
-  @discovery_revision "20220128"
+  @discovery_revision "20220205"
 
   def discovery_revision(), do: @discovery_revision
 end
