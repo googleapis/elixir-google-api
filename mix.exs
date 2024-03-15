3d64007c55feec0fd32522cbb731eec274621ca0
Daniel Azuma <dazuma@google.com>
2024-03-15 17:46:43 +0000

    chore: Update CI and release jobs to use the latest elixir

--- mix.exs
+++ mix.exs
@@ -45,7 +45,7 @@ defmodule GoogleApis.Mixfile do
   defp deps do
     [
       {:google_api_discovery, "~> 0.12.0"},
-      {:tesla, "~> 1.0"},
+      {:tesla, "~> 1.8"},
       {:oauth2, "~> 0.9"},
       {:temp, "~> 0.4"},
       {:jason, "~> 1.1"},
